f16a268f2e378fd7f4ad5c7c4039228931310c1d
Unknown <mcpkmart@hotmail.nl>
2018-10-02 10:05:12 +0200

    Pestle

--- src/main/java/epicsquid/roots/Roots.java
+++ src/main/java/epicsquid/roots/Roots.java
@@ -1,6 +1,9 @@
 package epicsquid.roots;
 
+import epicsquid.roots.init.ModItems;
 import epicsquid.roots.proxy.CommonProxy;
+import net.minecraft.creativetab.CreativeTabs;
+import net.minecraft.item.ItemStack;
 import net.minecraftforge.common.MinecraftForge;
 import net.minecraftforge.fml.common.Loader;
 import net.minecraftforge.fml.common.Mod;
@@ -11,10 +14,12 @@ import net.minecraftforge.fml.common.SidedProxy;
 import net.minecraftforge.fml.common.event.FMLInitializationEvent;
 import net.minecraftforge.fml.common.event.FMLPostInitializationEvent;
 import net.minecraftforge.fml.common.event.FMLPreInitializationEvent;
+import net.minecraftforge.fml.relauncher.Side;
+import net.minecraftforge.fml.relauncher.SideOnly;
 
 @Mod(modid = Roots.MODID, version = Roots.VERSION, name = Roots.NAME)
 public class Roots {
-  public static final String MODID = "roots-3";
+  public static final String MODID = "roots";
   public static final String DOMAIN = "roots";
   public static final String NAME = "Roots";
   public static final String VERSION = "@VERSION@";
@@ -26,18 +31,18 @@ public class Roots {
 
   @Instance(MODID) public static Roots instance;
 
-//  public static CreativeTabs tab = new CreativeTabs("roots") {
-//    @Override
-//    public String getTabLabel() {
-//      return "roots";
-//    }
-//
-//    @Override
-//    @SideOnly(Side.CLIENT)
-//    public ItemStack getTabIconItem() {
-//      return new ItemStack(ModItems.carapace, 1);
-//    }
-//  };
+  public static CreativeTabs tab = new CreativeTabs("roots") {
+    @Override
+    public String getTabLabel() {
+      return "roots";
+    }
+
+    @Override
+    @SideOnly(Side.CLIENT)
+    public ItemStack getTabIconItem() {
+      return new ItemStack(ModItems.pestle, 1);
+    }
+  };
 
   @EventHandler
   public void preInit(FMLPreInitializationEvent event) {
--- src/main/java/epicsquid/roots/init/ModItems.java
+++ src/main/java/epicsquid/roots/init/ModItems.java
@@ -3,16 +3,20 @@ package epicsquid.roots.init;
 import javax.annotation.Nonnull;
 
 import epicsquid.mysticallib.event.RegisterContentEvent;
+import epicsquid.mysticallib.item.ItemBase;
+import epicsquid.roots.Roots;
+import net.minecraft.item.Item;
 
 public class ModItems {
 
   // All mod items
+  public static Item pestle;
 
   /**
    * Register all items
    */
   public static void registerItems(@Nonnull RegisterContentEvent event) {
-
+    event.addItem(pestle = new ItemBase("pestle").setModelCustom(true).setCreativeTab(Roots.tab).setMaxStackSize(1));
   }
 
   /**
