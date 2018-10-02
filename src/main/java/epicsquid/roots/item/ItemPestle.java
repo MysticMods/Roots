527bad57a775d248bdc5a161f250cedd607195b5
Unknown <mcpkmart@hotmail.nl>
2018-10-02 10:05:12 +0200

    Pestle

---
+++ src/main/java/epicsquid/roots/item/ItemPestle.java
@@ -0,0 +1,16 @@
+package epicsquid.roots.item;
+
+import javax.annotation.Nonnull;
+
+import epicsquid.mysticallib.item.ItemBase;
+import epicsquid.roots.Roots;
+
+public class ItemPestle extends ItemBase {
+
+  public ItemPestle(@Nonnull String name) {
+    super(name);
+    setMaxStackSize(1);
+    setCreativeTab(Roots.tab);
+  }
+  
+}
