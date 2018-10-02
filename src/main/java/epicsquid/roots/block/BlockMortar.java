7af49aa365e23034ccf9f5a14ed3b8579a7f7b66
Unknown <mcpkmart@hotmail.nl>
2018-10-02 11:13:19 +0200

    Mortar

---
+++ src/main/java/epicsquid/roots/block/BlockMortar.java
@@ -0,0 +1,37 @@
+package epicsquid.roots.block;
+
+import javax.annotation.Nonnull;
+
+import epicsquid.mysticallib.block.BlockTEBase;
+import net.minecraft.block.Block;
+import net.minecraft.block.SoundType;
+import net.minecraft.block.material.Material;
+import net.minecraft.block.state.IBlockState;
+import net.minecraft.tileentity.TileEntity;
+import net.minecraft.util.math.AxisAlignedBB;
+import net.minecraft.util.math.BlockPos;
+import net.minecraft.world.IBlockAccess;
+
+public class BlockMortar extends BlockTEBase {
+
+  public BlockMortar(@Nonnull Material mat, @Nonnull SoundType type, float hardness, @Nonnull String name, @Nonnull Class<? extends TileEntity> teClass) {
+    super(mat, type, hardness, name, teClass);
+  }
+
+  @Override
+  public boolean isFullCube(@Nonnull IBlockState state) {
+    return false;
+  }
+
+  @Override
+  public boolean isOpaqueCube(@Nonnull IBlockState state) {
+    return false;
+  }
+
+  @Nonnull
+  @Override
+  public AxisAlignedBB getBoundingBox(@Nonnull IBlockState state, @Nonnull IBlockAccess world, @Nonnull BlockPos pos) {
+    return new AxisAlignedBB(0.3125,0,0.3125,0.6875,0.4375,0.6875);
+  }
+
+}
--- src/main/java/epicsquid/roots/init/ModBlocks.java
+++ src/main/java/epicsquid/roots/init/ModBlocks.java
@@ -3,15 +3,20 @@ package epicsquid.roots.init;
 import javax.annotation.Nonnull;
 
 import epicsquid.mysticallib.event.RegisterContentEvent;
+import epicsquid.roots.Roots;
+import epicsquid.roots.block.BlockMortar;
+import epicsquid.roots.tileentity.TileEntityMortar;
+import net.minecraft.block.SoundType;
+import net.minecraft.block.material.Material;
 
 public class ModBlocks {
 
-  // All blocks
+  // All block
 
   /**
-   * Register all blocks
+   * Register all block
    */
   public static void registerBlocks(@Nonnull RegisterContentEvent event) {
-
+    event.addBlock(new BlockMortar(Material.ROCK, SoundType.STONE, 1.4f, "mortar", TileEntityMortar.class)).setCreativeTab(Roots.tab).setLightOpacity(0);
   }
 }
