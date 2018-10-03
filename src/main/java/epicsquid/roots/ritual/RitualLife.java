eabcf4d5c4644230f58eeea8109a0177bbf39c19
Unknown <mcpkmart@hotmail.nl>
2018-10-03 09:13:10 +0200

    Ritual of life

---
+++ src/main/java/epicsquid/roots/ritual/RitualLife.java
@@ -0,0 +1,57 @@
+package epicsquid.roots.ritual;
+
+import java.util.List;
+
+import epicsquid.mysticalworld.init.ModItems;
+import epicsquid.roots.entity.EntityRitualLife;
+import net.minecraft.block.BlockOldLog;
+import net.minecraft.block.BlockPlanks.EnumType;
+import net.minecraft.block.state.IBlockState;
+import net.minecraft.init.Blocks;
+import net.minecraft.item.ItemStack;
+import net.minecraft.util.math.AxisAlignedBB;
+import net.minecraft.util.math.BlockPos;
+import net.minecraft.world.World;
+
+public class RitualLife extends RitualBase {
+  public RitualLife(String name, int duration, boolean doUpdateValidity){
+    super(name,duration,doUpdateValidity);
+    addIngredient(new ItemStack(ModItems.terra_moss,1));
+    addIngredient(new ItemStack(ModItems.bark_oak,1));
+    addIngredient(new ItemStack(ModItems.bark_birch,1));
+    addIngredient(new ItemStack(ModItems.bark_birch,1));
+    addIngredient(new ItemStack(Blocks.SAPLING,1,2));
+  }
+
+  @Override
+  public boolean isValidForPos(World world, BlockPos pos){
+    int birchTreeCount = 0;
+    for (int i = -9; i < 10; i ++){
+      for (int j = -9; j < 10; j ++){
+        IBlockState state = world.getBlockState(pos.add(i, 1, j));
+        if (state.getBlock() instanceof BlockOldLog){
+          if (state.getValue(BlockOldLog.VARIANT) == EnumType.BIRCH){
+            birchTreeCount ++;
+          }
+        }
+      }
+    }
+    return birchTreeCount >= 4;
+  }
+
+  @Override
+  public void doEffect(World world, BlockPos pos){
+    List<EntityRitualLife> pastRituals = world.getEntitiesWithinAABB(EntityRitualLife.class, new AxisAlignedBB(pos.getX(),pos.getY(),pos.getZ(),pos.getX()+1,pos.getY()+100,pos.getZ()+1));
+    if (pastRituals.size() == 0 && !world.isRemote){
+      EntityRitualLife ritual = new EntityRitualLife(world);
+      ritual.setPosition(pos.getX()+0.5, pos.getY()+6.5, pos.getZ()+0.5);
+      world.spawnEntity(ritual);
+    }
+    else if (pastRituals.size() > 0){
+      for (EntityRitualLife ritual : pastRituals){
+        ritual.getDataManager().set(EntityRitualLife.lifetime, duration+20);
+        ritual.getDataManager().setDirty(EntityRitualLife.lifetime);
+      }
+    }
+  }
+}
