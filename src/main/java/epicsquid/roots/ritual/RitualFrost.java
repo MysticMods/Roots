db987c1f3903edd66ac2f484f0fad29cb28773ab
Unknown <mcpkmart@hotmail.nl>
2018-10-03 14:19:35 +0200

    Frost Ritual

---
+++ src/main/java/epicsquid/roots/ritual/RitualFrost.java
@@ -0,0 +1,44 @@
+package epicsquid.roots.ritual;
+
+import epicsquid.roots.init.ModBlocks;
+import net.minecraft.init.Blocks;
+import net.minecraft.init.Items;
+import net.minecraft.item.ItemStack;
+import net.minecraft.util.math.BlockPos;
+import net.minecraft.world.World;
+
+public class RitualFrost extends RitualBase {
+
+  public RitualFrost(String name, int duration, boolean doUpdateValidity) {
+    super(name, duration, doUpdateValidity);
+    addIngredient(new ItemStack(Items.SNOWBALL, 1));
+    addIngredient(new ItemStack(Items.SNOWBALL, 1));
+    addIngredient(new ItemStack(Items.SNOWBALL, 1));
+    addIngredient(new ItemStack(Items.SNOWBALL, 1));
+    addIngredient(new ItemStack(Items.SNOWBALL, 1));
+  }
+
+  @Override
+  public void doEffect(World world, BlockPos pos) {
+    super.doEffect(world, pos);
+
+    for (int i = -19; i < 20; i++) {
+      for (int j = -19; j < 20; j++) {
+        BlockPos topBlockPos = world.getTopSolidOrLiquidBlock(pos.add(i, 0, j));
+        if(world.getBlockState(topBlockPos).getBlock() == Blocks.WATER){
+          world.setBlockState(topBlockPos, Blocks.ICE.getDefaultState());
+        }
+        else{
+          if(!world.getBlockState(topBlockPos).getBlock().isLeaves(world.getBlockState(topBlockPos), world, topBlockPos)
+              && world.getBlockState(topBlockPos.add(0, -1, 0)).getBlock() != ModBlocks.bonfire
+              && world.getBlockState(topBlockPos.add(0, -1, 0)).getBlock() != ModBlocks.mortar){
+            world.setBlockState(topBlockPos, Blocks.SNOW_LAYER.getDefaultState());
+          }
+        }
+      }
+    }
+
+    //todo: add nice particle effects in the area
+  }
+
+}
