366547ec508f81777f91e4041c60fccf30367fe7
Unknown <mcpkmart@hotmail.nl>
2018-10-06 15:10:29 +0200

    Added offertory plate

---
+++ src/main/java/epicsquid/roots/tileentity/TileEntityOffertoryPlate.java
@@ -0,0 +1,124 @@
+package epicsquid.roots.tileentity;
+
+import java.util.UUID;
+
+import javax.annotation.Nonnull;
+
+import epicsquid.mysticallib.network.MessageTEUpdate;
+import epicsquid.mysticallib.network.PacketHandler;
+import epicsquid.mysticallib.tile.TileBase;
+import epicsquid.mysticallib.util.Util;
+import epicsquid.roots.util.OfferingUtil;
+import net.minecraft.block.state.IBlockState;
+import net.minecraft.entity.item.EntityItem;
+import net.minecraft.entity.player.EntityPlayer;
+import net.minecraft.item.ItemStack;
+import net.minecraft.nbt.NBTTagCompound;
+import net.minecraft.nbt.NBTUtil;
+import net.minecraft.network.NetworkManager;
+import net.minecraft.network.play.server.SPacketUpdateTileEntity;
+import net.minecraft.util.EnumFacing;
+import net.minecraft.util.EnumHand;
+import net.minecraft.util.math.BlockPos;
+import net.minecraft.world.World;
+import net.minecraftforge.items.ItemStackHandler;
+
+public class TileEntityOffertoryPlate extends TileBase {
+  public ItemStackHandler inventory = new ItemStackHandler(1){
+    @Override
+    protected void onContentsChanged(int slot) {
+      TileEntityOffertoryPlate.this.markDirty();
+      if (!world.isRemote){
+        PacketHandler.INSTANCE.sendToAll(new MessageTEUpdate(TileEntityOffertoryPlate.this.getUpdateTag()));
+      }
+    }
+  };
+  public UUID lastPlayer = null;
+  int progress = 0;
+  public float angle = 0;
+
+  public TileEntityOffertoryPlate(){
+    super();
+  }
+
+  @Override
+  public NBTTagCompound writeToNBT(NBTTagCompound tag){
+    super.writeToNBT(tag);
+    tag.setTag("inventory", inventory.serializeNBT());
+    tag.setInteger("progress", progress);
+    if (lastPlayer != null){
+      tag.setTag("lastPlayer", NBTUtil.createUUIDTag(lastPlayer));
+    }
+    return tag;
+  }
+
+  @Override
+  public void readFromNBT(NBTTagCompound tag){
+    super.readFromNBT(tag);
+    if (tag.hasKey("lastPlayer")){
+      lastPlayer = NBTUtil.getUUIDFromTag(tag.getCompoundTag("lastPlayer"));
+    }
+    inventory.deserializeNBT(tag.getCompoundTag("inventory"));
+    progress = tag.getInteger("progress");
+  }
+
+  @Override
+  public NBTTagCompound getUpdateTag() {
+    return writeToNBT(new NBTTagCompound());
+  }
+
+  @Override
+  public SPacketUpdateTileEntity getUpdatePacket() {
+    return new SPacketUpdateTileEntity(getPos(), 0, getUpdateTag());
+  }
+
+  @Override
+  public void onDataPacket(NetworkManager net, SPacketUpdateTileEntity pkt) {
+    readFromNBT(pkt.getNbtCompound());
+  }
+
+  @Override
+  public boolean activate(@Nonnull World world, @Nonnull BlockPos pos, @Nonnull IBlockState state, @Nonnull EntityPlayer player, @Nonnull EnumHand hand,
+      @Nonnull EnumFacing side, float hitX, float hitY, float hitZ) {
+    ItemStack heldItem = player.getHeldItem(hand);
+    if (!heldItem.isEmpty() && OfferingUtil.getValue(heldItem) != 0){
+      if (inventory.getStackInSlot(0).isEmpty()){
+        ItemStack toInsert = heldItem.copy();
+        ItemStack attemptedInsert = inventory.insertItem(0, toInsert, true);
+        if (attemptedInsert.isEmpty()){
+          inventory.insertItem(0, toInsert, false);
+          player.getHeldItem(hand).shrink(toInsert.getCount());
+          if (player.getHeldItem(hand).getCount() == 0){
+            player.setHeldItem(hand, ItemStack.EMPTY);
+          }
+          markDirty();
+          return true;
+        }
+      }
+    }
+    if (heldItem.isEmpty() && !world.isRemote && hand == EnumHand.MAIN_HAND){
+      if (!inventory.getStackInSlot(0).isEmpty()){
+        ItemStack extracted = inventory.extractItem(0, inventory.getStackInSlot(0).getCount(), false);
+        if (!world.isRemote){
+          world.spawnEntity(new EntityItem(world,getPos().getX()+0.5,getPos().getY()+0.5,getPos().getZ()+0.5,extracted));
+        }
+        markDirty();
+        PacketHandler.INSTANCE.sendToAll(new MessageTEUpdate(this.getUpdateTag()));
+        return true;
+      }
+    }
+    if (!world.isRemote){
+      lastPlayer = player.getUniqueID();
+      markDirty();
+      PacketHandler.INSTANCE.sendToAll(new MessageTEUpdate(this.getUpdateTag()));
+    }
+    return false;
+  }
+
+  @Override
+  public void breakBlock(World world, BlockPos pos, IBlockState state, EntityPlayer player) {
+    if (!world.isRemote){
+      Util.spawnInventoryInWorld(world, getPos().getX()+0.5,getPos().getY()+0.5,getPos().getZ()+0.5, inventory);
+    }
+  }
+}
---
+++ src/main/java/epicsquid/roots/tileentity/TileEntityOffertoryPlateRenderer.java
@@ -0,0 +1,57 @@
+package epicsquid.roots.tileentity;
+
+import java.util.Random;
+
+import epicsquid.roots.block.BlockOffertoryPlate;
+import net.minecraft.client.Minecraft;
+import net.minecraft.client.renderer.GlStateManager;
+import net.minecraft.client.renderer.RenderItem;
+import net.minecraft.client.renderer.block.model.ItemCameraTransforms.TransformType;
+import net.minecraft.client.renderer.tileentity.TileEntitySpecialRenderer;
+import net.minecraft.item.ItemBlock;
+import net.minecraft.item.ItemStack;
+import net.minecraft.util.EnumFacing;
+
+public class TileEntityOffertoryPlateRenderer extends TileEntitySpecialRenderer<TileEntityOffertoryPlate> {
+
+  @Override
+  public void render(TileEntityOffertoryPlate tei, double x, double y, double z, float partialTicks, int destroyStage, float alpha) {
+    if (tei.inventory.getStackInSlot(0) != ItemStack.EMPTY) {
+      int count = getCount(tei.inventory.getStackInSlot(0));
+      RenderItem r = Minecraft.getMinecraft().getRenderItem();
+      EnumFacing f = tei.getWorld().getBlockState(tei.getPos()).getValue(BlockOffertoryPlate.FACING);
+      for (int i = 0; i < count; i++) {
+        GlStateManager.pushMatrix();
+        GlStateManager.translate(x + 0.5, y + 0.8125 + 0.0625 * (double) i + 0.0625 * (tei.inventory.getStackInSlot(0).getItem() instanceof ItemBlock ? 1.0 : 0),
+            z + 0.5);
+        GlStateManager.rotate(180 - f.getHorizontalAngle(), 0, 1, 0);
+        GlStateManager.rotate(67.5f, 1.0f, 0, 0);
+        Random random = new Random();
+        random.setSeed(tei.inventory.getStackInSlot(0).hashCode() + 256 * i);
+        GlStateManager.translate(0.125f * (random.nextFloat() - 0.5f), -0.1875 + 0.125f * (random.nextFloat() - 0.5f), 0);
+        r.renderItem(tei.inventory.getStackInSlot(0), TransformType.GROUND);
+        GlStateManager.popMatrix();
+      }
+    }
+  }
+
+  public int getCount(ItemStack s){
+    if (s.getCount() == 64){
+      return 5;
+    }
+    if (s.getCount() > 33){
+      return 4;
+    }
+    if (s.getCount() > 16){
+      return 3;
+    }
+    if (s.getCount() >= 2){
+      return 2;
+    }
+    if (s.getCount() == 1){
+      return 1;
+    }
+    return 0;
+  }
+
+}
