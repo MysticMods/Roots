d6f75288773bc763e23f4af06cb2a1724b6c1cd1
Unknown <mcpkmart@hotmail.nl>
2018-11-19 19:36:36 +0100

    Fixed the opacity of the water

--- src/main/java/epicsquid/roots/block/BlockUnendingBowl.java
+++ src/main/java/epicsquid/roots/block/BlockUnendingBowl.java
@@ -7,6 +7,7 @@ import net.minecraft.block.SoundType;
 import net.minecraft.block.material.Material;
 import net.minecraft.block.state.IBlockState;
 import net.minecraft.tileentity.TileEntity;
+import net.minecraft.util.BlockRenderLayer;
 import net.minecraft.util.math.AxisAlignedBB;
 import net.minecraft.util.math.BlockPos;
 import net.minecraft.world.IBlockAccess;
@@ -34,5 +35,11 @@ public class BlockUnendingBowl extends BlockTEBase {
   @Override
   public AxisAlignedBB getBoundingBox(@Nonnull IBlockState state, @Nonnull IBlockAccess world, @Nonnull BlockPos pos) {
     return new AxisAlignedBB(0.125, 0, 0.125, 0.875, 0.3125, 0.875);
+  }
+
+  @Nonnull
+  @Override
+  public BlockRenderLayer getBlockLayer() {
+    return BlockRenderLayer.TRANSLUCENT;
   }
 }
