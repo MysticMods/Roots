252b4d4266108d517df10a6b34e911b6d0423014
Unknown <mcpkmart@hotmail.nl>
2018-11-19 12:32:27 +0100

    Unending Bowl

---
+++ src/main/java/epicsquid/roots/block/BlockUnendingBowl.java
@@ -0,0 +1,38 @@
+package epicsquid.roots.block;
+
+import javax.annotation.Nonnull;
+
+import epicsquid.mysticallib.block.BlockTEBase;
+import net.minecraft.block.SoundType;
+import net.minecraft.block.material.Material;
+import net.minecraft.block.state.IBlockState;
+import net.minecraft.tileentity.TileEntity;
+import net.minecraft.util.math.AxisAlignedBB;
+import net.minecraft.util.math.BlockPos;
+import net.minecraft.world.IBlockAccess;
+
+public class BlockUnendingBowl extends BlockTEBase {
+
+  /*
+  This block is a TE since I wanna do some render magic eventually
+   */
+  public BlockUnendingBowl(@Nonnull Material mat, @Nonnull SoundType type, float hardness, @Nonnull String name, @Nonnull Class<? extends TileEntity> teClass) {
+    super(mat, type, hardness, name, teClass);
+  }
+
+  @Override
+  public boolean isFullCube(@Nonnull IBlockState state) {
+    return false;
+  }
+
+  @Override
+  public boolean isOpaqueCube(@Nonnull IBlockState state) {
+    return false;
+  }
+
+  @Nonnull
+  @Override
+  public AxisAlignedBB getBoundingBox(@Nonnull IBlockState state, @Nonnull IBlockAccess world, @Nonnull BlockPos pos) {
+    return new AxisAlignedBB(0.125, 0, 0.125, 0.875, 0.375, 0.875);
+  }
+}
