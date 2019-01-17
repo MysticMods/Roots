76274f47277aa5a3606b6db3c76cd27fabb8d3f0
Mart <mcpkmart@hotmail.nl>
2019-01-17 22:55:22 +0100

    More rune

---
+++ src/main/java/epicsquid/roots/util/RgbColor.java
@@ -0,0 +1,24 @@
+package epicsquid.roots.util;
+
+public class RgbColor {
+
+  private float red, green, blue;
+
+  public RgbColor(float red, float green, float blue) {
+    this.red = red;
+    this.green = green;
+    this.blue = blue;
+  }
+
+  public float getRed() {
+    return red;
+  }
+
+  public float getGreen() {
+    return green;
+  }
+
+  public float getBlue() {
+    return blue;
+  }
+}
---
+++ src/main/java/epicsquid/roots/util/RgbColorUtil.java
@@ -0,0 +1,15 @@
+package epicsquid.roots.util;
+
+public class RgbColorUtil {
+
+  public static RgbColor FIRE = new RgbColor(255, 103, 16);
+  public static RgbColor WATER = new RgbColor(32, 188, 255);
+  public static RgbColor EARTH = new RgbColor(52, 255, 36);
+  public static RgbColor WIND = new RgbColor(255, 255, 255);
+  public static RgbColor TIME = new RgbColor(108, 52, 181);
+  public static RgbColor LIFE = new RgbColor(255, 33, 33);
+  public static RgbColor AER = new RgbColor(255, 240, 32);
+  public static RgbColor MOON = new RgbColor(255, 30, 217);
+
+
+}
