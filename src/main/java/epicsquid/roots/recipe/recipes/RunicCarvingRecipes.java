9fd506649bd101096d51387d5c1a15662c330b37
EpicSquid <epicsquid315@gmail.com>
2019-02-08 19:49:43 +1100

    Added runic soil crafting recipes

--- src/main/java/epicsquid/roots/recipe/recipes/RunicCarvingRecipes.java
+++ src/main/java/epicsquid/roots/recipe/recipes/RunicCarvingRecipes.java
@@ -11,6 +11,9 @@ import net.minecraft.util.ResourceLocation;
 public class RunicCarvingRecipes {
 
   public static void initRecipes() {
-    ModRecipes.addRunicCarvingRecipe(new RunicCarvingRecipe(Blocks.DIRT.getDefaultState(), ModBlocks.runic_soil_fire.getDefaultState(), HerbRegistry.getHerbByName("infernal_bulb")));
+    ModRecipes.addRunicCarvingRecipe(new RunicCarvingRecipe(ModBlocks.runic_soil.getDefaultState(), ModBlocks.runic_soil_fire.getDefaultState(), HerbRegistry.getHerbByName("infernal_bulb")));
+    ModRecipes.addRunicCarvingRecipe(new RunicCarvingRecipe(ModBlocks.runic_soil.getDefaultState(), ModBlocks.runic_soil_water.getDefaultState(), HerbRegistry.getHerbByName("dewgonia")));
+    ModRecipes.addRunicCarvingRecipe(new RunicCarvingRecipe(ModBlocks.runic_soil.getDefaultState(), ModBlocks.runic_soil_air.getDefaultState(), HerbRegistry.getHerbByName("cloud_berry")));
+    ModRecipes.addRunicCarvingRecipe(new RunicCarvingRecipe(ModBlocks.runic_soil.getDefaultState(), ModBlocks.runic_soil_earth.getDefaultState(), HerbRegistry.getHerbByName("stalicripe")));
   }
 }
