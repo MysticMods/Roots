a8df0e212abacff50d62f9b43b5992651610a9c0
EpicSquid <epicsquid315@gmail.com>
2019-02-22 20:24:07 +1100

    Culled some seeds from the herb registry

--- src/main/java/epicsquid/roots/init/HerbRegistry.java
+++ src/main/java/epicsquid/roots/init/HerbRegistry.java
@@ -38,14 +38,9 @@ public class HerbRegistry {
   @SubscribeEvent
   public static void registerHerbs(@Nonnull RegisterHerbEvent event) {
     event.register(ModItems.spirit_herb, "spirit_herb");
-    event.register(ModItems.spirit_herb_seed, "spirit_herb_seed");
-    event.register(ModItems.aubergine, "aubergine");
-    event.register(ModItems.aubergine_seed, "aubergine_seed");
     event.register(Item.getItemFromBlock(ModBlocks.baffle_cap_mushroom), "baffle_cap");
     event.register(ModItems.moonglow_leaf, "moonglow_leaf");
-    event.register(ModItems.moonglow_seed, "moonglow_seed");
     event.register(ModItems.pereskia, "pereskia");
-    event.register(ModItems.pereskia_bulb, "pereskia_bulb");
     event.register(ModItems.terra_moss, "terra_moss");
     event.register(ModItems.wildroot, "wildroot");
     event.register(ModItems.wildewheet, "wildewheet");
