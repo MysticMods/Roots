4d84d0cfa3baed5d80fb80edccc7aac6bd39adc6
Davoleo <davoleo2001@gmail.com>
2019-03-05 20:54:17 +0100

    Flower Growth Ritual (p1)

---
+++ src/main/java/epicsquid/roots/ritual/natural/RitualFlowerGrowth.java
@@ -0,0 +1,30 @@
+package epicsquid.roots.ritual.natural;
+
+import epicsquid.roots.entity.ritual.EntityRitualFlowerGrowth;
+import epicsquid.roots.init.ModItems;
+import epicsquid.roots.recipe.conditions.ConditionItems;
+import epicsquid.roots.ritual.RitualBase;
+import net.minecraft.item.ItemStack;
+import net.minecraft.util.math.BlockPos;
+import net.minecraft.world.World;
+
+public class RitualFlowerGrowth extends RitualBase {
+
+    public RitualFlowerGrowth(String name, int duration)
+    {
+        super(name, duration);
+        addCondition(new ConditionItems(
+                new ItemStack(ModItems.pereskia),
+                new ItemStack(ModItems.pereskia),
+                new ItemStack(ModItems.cloud_berry),
+                new ItemStack(ModItems.wildroot),
+                new ItemStack(ModItems.spirit_herb)
+        ));
+    }
+
+    @Override
+    public void doEffect(World world, BlockPos pos)
+    {
+        this.spawnEntity(world, pos, EntityRitualFlowerGrowth.class);
+    }
+}
