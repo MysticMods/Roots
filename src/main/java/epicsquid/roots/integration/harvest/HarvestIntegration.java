d8dcc187b2e197d57381c81efd0dab1ff60b7aea
Jon McManus <due@wxwhatever.com>
2019-03-05 19:27:21 +1000

    In-built integration with TehNut's harvest.

---
+++ src/main/java/epicsquid/roots/integration/harvest/HarvestIntegration.java
@@ -0,0 +1,37 @@
+package epicsquid.roots.integration.harvest;
+
+import epicsquid.mysticallib.block.BlockCropBase;
+import epicsquid.roots.init.ModBlocks;
+import net.minecraftforge.fml.common.Loader;
+import tehnut.harvest.BlockStack;
+import tehnut.harvest.Crop;
+import tehnut.harvest.Harvest;
+
+import java.util.Arrays;
+import java.util.List;
+import java.util.Map;
+
+public class HarvestIntegration {
+    public static void init() {
+        List<BlockCropBase> crops = Arrays.asList(ModBlocks.moonglow,
+                ModBlocks.aubergine,
+                ModBlocks.pereskia,
+                ModBlocks.wildroot,
+                ModBlocks.spirit_herb,
+                ModBlocks.wildewheet,
+                ModBlocks.cloud_berry,
+                ModBlocks.infernal_bulb,
+                ModBlocks.dewgonia,
+                ModBlocks.stalicripe);
+
+        if (Loader.isModLoaded("harvest")) {
+            Map<BlockStack, Crop> map = Harvest.config.getCropMap();
+            for (BlockCropBase block : crops) {
+                Harvest.CUSTOM_HANDLERS.put(block, block);
+                BlockStack blockStack = new BlockStack(block, 7);
+                Crop crop = new Crop(block, 7);
+                map.put(blockStack, crop);
+            }
+        }
+    }
+}
