682af1a1f6c9beb81bdb7c601e041f443f931f29
EpicSquid <epicsquid315@gmail.com>
2019-03-07 10:27:02 +1100

    Added JER plants integration

---
+++ src/main/java/epicsquid/roots/integration/jer/JERIntegration.java
@@ -0,0 +1,45 @@
+package epicsquid.roots.integration.jer;
+
+import epicsquid.roots.init.ModItems;
+import jeresources.api.IJERAPI;
+import jeresources.api.IPlantRegistry;
+import jeresources.api.JERPlugin;
+import jeresources.api.drop.PlantDrop;
+import net.minecraft.item.ItemStack;
+
+public class JERIntegration {
+
+  @JERPlugin
+  public static IJERAPI JERApi;
+
+  public static void init() {
+    // Plant Drops
+    IPlantRegistry registry = JERApi.getPlantRegistry();
+
+    registry.register(ModItems.moonglow_seed,
+        new PlantDrop(new ItemStack(ModItems.moonglow_leaf), 1, 1),
+        new PlantDrop(new ItemStack(ModItems.moonglow_seed), 1, 4));
+    registry.register(ModItems.wildroot,
+        new PlantDrop(new ItemStack(ModItems.wildroot), 1, 4));
+    registry.register(ModItems.pereskia_bulb,
+        new PlantDrop(new ItemStack(ModItems.pereskia), 1, 1),
+        new PlantDrop(new ItemStack(ModItems.pereskia_bulb), 1, 4));
+    registry.register(ModItems.wildewheet_seed,
+        new PlantDrop(new ItemStack(ModItems.wildewheet), 1, 1),
+        new PlantDrop(new ItemStack(ModItems.wildewheet_seed), 1, 4));
+    registry.register(ModItems.spirit_herb_seed,
+        new PlantDrop(new ItemStack(ModItems.spirit_herb), 1, 1),
+        new PlantDrop(new ItemStack(ModItems.spirit_herb_seed), 1, 4));
+    registry.register(ModItems.aubergine_seed,
+        new PlantDrop(new ItemStack(ModItems.aubergine), 1, 1),
+        new PlantDrop(new ItemStack(ModItems.aubergine_seed), 1, 4));
+    registry.register(ModItems.infernal_bulb,
+        new PlantDrop(new ItemStack(ModItems.infernal_bulb), 1, 4));
+    registry.register(ModItems.dewgonia,
+        new PlantDrop(new ItemStack(ModItems.dewgonia), 1, 4));
+    registry.register(ModItems.stalicripe,
+        new PlantDrop(new ItemStack(ModItems.stalicripe), 1, 4));
+    registry.register(ModItems.cloud_berry,
+        new PlantDrop(new ItemStack(ModItems.cloud_berry), 1, 4));
+  }
+}
