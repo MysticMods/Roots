bb4901570cc886aafd03abd26ff9a0828ff1c9a1
Jon McManus <due@wxwhatever.com>
2019-03-31 21:33:17 +1000

    Mortar (spell and recipe) HUD.
    Does what it says on the tin: either shows the name of the spell about
    to be created, or shows the output of the recipe.
    
    Whether or not the ingredients remain shown is up for debate, as it
    could just display the output.

---
+++ src/main/java/epicsquid/roots/client/render/hud/RenderHUD.java
@@ -0,0 +1,39 @@
+package epicsquid.roots.client.render.hud;
+
+import epicsquid.roots.Roots;
+import epicsquid.roots.init.ModBlocks;
+import net.minecraft.block.Block;
+import net.minecraft.block.state.IBlockState;
+import net.minecraft.client.Minecraft;
+import net.minecraft.util.math.RayTraceResult;
+import net.minecraftforge.client.event.RenderGameOverlayEvent;
+import net.minecraftforge.fml.common.Mod;
+import net.minecraftforge.fml.common.eventhandler.EventPriority;
+import net.minecraftforge.fml.common.eventhandler.SubscribeEvent;
+import net.minecraftforge.fml.relauncher.Side;
+import net.minecraftforge.fml.relauncher.SideOnly;
+
+@Mod.EventBusSubscriber(value= Side.CLIENT, modid= Roots.MODID)
+public class RenderHUD {
+  private RenderHUD () {
+  }
+
+  @SideOnly(Side.CLIENT)
+  @SubscribeEvent(priority= EventPriority.HIGHEST)
+  public static void onDrawScreenPost (RenderGameOverlayEvent.Post event) {
+    if (event.getType() != RenderGameOverlayEvent.ElementType.ALL) return;
+
+    Minecraft mc = Minecraft.getMinecraft();
+    RayTraceResult trace = mc.objectMouseOver;
+    if (trace == null || trace.typeOfHit != RayTraceResult.Type.BLOCK) return;
+
+    IBlockState state = mc.world.getBlockState(trace.getBlockPos());
+    Block block = state.getBlock();
+
+    if (block == ModBlocks.mortar) {
+      RenderMortar.render(mc, trace.getBlockPos(), state, event);
+    } else if (block == ModBlocks.bonfire) {
+
+    }
+  }
+}
