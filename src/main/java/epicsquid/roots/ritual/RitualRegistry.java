e44859e358c16102a22f12ce26d601bfe1b624fc
Jon McManus <due@wxwhatever.com>
2019-04-01 21:42:50 +1000

    Update patchouli; make Animal Harvest ritual a gradual thing.
    Instead of being instantly repeatable and it-all-happens-at-once, it
    picks an entity to harvest at random every 80 or so ticks for the duration.

--- src/main/java/epicsquid/roots/ritual/RitualRegistry.java
+++ src/main/java/epicsquid/roots/ritual/RitualRegistry.java
@@ -5,7 +5,6 @@ import java.util.Map;
 
 import epicsquid.roots.ritual.natural.RitualFlowerGrowth;
 import epicsquid.roots.ritual.natural.RitualWildGrowth;
-import epicsquid.roots.ritual.wild.RitualAnimalHarvest;
 import epicsquid.roots.ritual.wild.RitualSummoning;
 import epicsquid.roots.tileentity.TileEntityBonfire;
 import net.minecraft.entity.player.EntityPlayer;
@@ -52,7 +51,7 @@ public class RitualRegistry {
     addRitual(ritual_natural_aura = new RitualNaturalAura("ritual_natural_aura", 1200));
     addRitual(ritual_purity = new RitualPurity("ritual_purity", 1200));
     addRitual(ritual_frost = new RitualFrost("ritual_frost", 6400));
-    addRitual(ritual_animal_harvest = new RitualAnimalHarvest("ritual_animal_harvest", 0));
+    addRitual(ritual_animal_harvest = new RitualAnimalHarvest("ritual_animal_harvest", 3200));
     addRitual(ritual_summoning = new RitualSummoning("ritual_summoning", 0));
     addRitual(ritual_wild_growth = new RitualWildGrowth("ritual_wild_growth", 0));
     addRitual(ritual_overgrowth = new RitualOvergrowth("ritual_overgrowth", 6400));
