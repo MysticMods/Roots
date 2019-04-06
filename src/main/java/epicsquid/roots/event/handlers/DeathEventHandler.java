bdd31c8d660c3875d001dede3d289e6efd696987
Jon McManus <due@wxwhatever.com>
2019-04-06 19:15:26 +1000

    Convert pacifist advancement to an actual registry.

--- src/main/java/epicsquid/roots/event/handlers/DeathEventHandler.java
+++ src/main/java/epicsquid/roots/event/handlers/DeathEventHandler.java
@@ -1,9 +1,12 @@
 package epicsquid.roots.event.handlers;
 
 import epicsquid.roots.Roots;
+import epicsquid.roots.init.ModRecipes;
+import epicsquid.roots.recipe.PacifistEntry;
 import net.minecraft.entity.Entity;
 import net.minecraft.entity.EntityLivingBase;
 import net.minecraft.entity.monster.IMob;
+import net.minecraft.entity.player.EntityPlayer;
 import net.minecraft.entity.player.EntityPlayerMP;
 import net.minecraft.util.DamageSource;
 import net.minecraft.util.EntityDamageSource;
@@ -17,12 +20,15 @@ public class DeathEventHandler {
   @SubscribeEvent
   public static void onDeath(LivingDeathEvent event) {
     EntityLivingBase entity = event.getEntityLiving();
-    if (!entity.isServerWorld() || entity instanceof IMob) return;
+    PacifistEntry entry = ModRecipes.getPacifistEntry(entity);
+    if (entry == null || !entity.isServerWorld()) return;
 
     DamageSource source = event.getSource();
     if (!(source instanceof EntityDamageSource)) return;
     Entity trueSource = source.getTrueSource();
     if (!(trueSource instanceof EntityPlayerMP)) return;
+
+    if (!entry.matches(entity, (EntityPlayer) trueSource)) return;
 
     Roots.PACIFIST_TRIGGER.trigger((EntityPlayerMP) trueSource, event);
   }
