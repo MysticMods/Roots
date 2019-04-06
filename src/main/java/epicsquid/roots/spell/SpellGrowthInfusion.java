86348debd24ce1343b97569b4622a08feb9c156d
Jon McManus <due@wxwhatever.com>
2019-04-06 19:55:26 +1000

    Improve logic check so you don't keep spamming on ungrowables.

--- src/main/java/epicsquid/roots/spell/SpellGrowthInfusion.java
+++ src/main/java/epicsquid/roots/spell/SpellGrowthInfusion.java
@@ -1,22 +1,23 @@
 package epicsquid.roots.spell;
 
-import java.util.List;
-import java.util.Random;
-
 import epicsquid.mysticallib.network.PacketHandler;
 import epicsquid.roots.init.HerbRegistry;
 import epicsquid.roots.init.ModItems;
 import epicsquid.roots.network.fx.MessageLifeInfusionFX;
 import epicsquid.roots.spell.modules.SpellModule;
+import net.minecraft.block.Block;
+import net.minecraft.block.IGrowable;
 import net.minecraft.block.state.IBlockState;
 import net.minecraft.entity.player.EntityPlayer;
-import net.minecraft.init.Blocks;
 import net.minecraft.init.Items;
 import net.minecraft.item.ItemStack;
 import net.minecraft.util.math.BlockPos;
 import net.minecraft.util.math.RayTraceResult;
 import net.minecraft.util.text.TextFormatting;
 import net.minecraftforge.oredict.OreIngredient;
+
+import java.util.List;
+import java.util.Random;
 
 public class SpellGrowthInfusion extends SpellBase {
   public static String spellName = "spell_growth_infusion";
@@ -39,20 +40,21 @@ public class SpellGrowthInfusion extends SpellBase {
 
   @Override
   public boolean cast(EntityPlayer player, List<SpellModule> modules) {
-    if (!player.world.isRemote) {
-      RayTraceResult result = player.world.rayTraceBlocks(player.getPositionVector().addVector(0, player.getEyeHeight(), 0),
-          player.getLookVec().scale(8.0f).add(player.getPositionVector().addVector(0, player.getEyeHeight(), 0)));
+    RayTraceResult result = player.world.rayTraceBlocks(player.getPositionVector().addVector(0, player.getEyeHeight(), 0), player.getLookVec().scale(8.0f).add(player.getPositionVector().addVector(0, player.getEyeHeight(), 0)));
       if (result != null) {
         if (result.typeOfHit == RayTraceResult.Type.BLOCK) {
           BlockPos pos = result.getBlockPos();
           IBlockState state = player.world.getBlockState(pos);
-          for (int i = 0; i < 1; i++) {
-            state.getBlock().randomTick(player.world, pos, state, new Random());
+          Block block = state.getBlock();
+          if (block instanceof IGrowable && !((IGrowable) block).canGrow(player.world, pos, state, player.world.isRemote)) return false;
+          if (!player.world.isRemote) {
+            for (int i = 0; i < 1; i++) {
+              state.getBlock().randomTick(player.world, pos, state, new Random());
+            }
+            PacketHandler.sendToAllTracking(new MessageLifeInfusionFX(pos.getX(), pos.getY(), pos.getZ()), player);
           }
-          PacketHandler.sendToAllTracking(new MessageLifeInfusionFX(pos.getX(), pos.getY(), pos.getZ()), player);
         }
       }
-    }
     return true;
   }
 }
