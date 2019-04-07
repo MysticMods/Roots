db494b81d465aaa0b5302153a76701791b1f24c9
Jon McManus <due@wxwhatever.com>
2019-04-07 12:44:38 +1000

    Recipe for sugar cane->sugar.
    Makes sense that this is something that can be done in a mortar!

--- src/main/java/epicsquid/roots/init/ModRecipes.java
+++ src/main/java/epicsquid/roots/init/ModRecipes.java
@@ -498,6 +498,7 @@ public class ModRecipes {
     addMortarRecipe(new ItemStack(ModItems.flour), Ingredient.fromItem(Items.WHEAT), 1f, 1f, 0f, 1f, 1f, 0f);
     addMortarRecipe(new ItemStack(ModItems.flour), Ingredient.fromItem(Items.POTATO), 1f, 1f, 0, 1f, 1f, 0f);
     addMortarRecipe(new ItemStack(Items.DYE, 4, 15), Ingredient.fromItem(Items.BONE), 0f, 0f, 0f, 0f, 0f, 0f);
+    addMortarRecipe(new ItemStack(Items.SUGAR, 2), new OreIngredient("sugarcane"), 0f, 0f, 0f, 1f, 1f, 1f);
 
     for (Metal metal : Metal.values()) {
       if (!metal.isEnabled()) continue;
