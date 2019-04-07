25aa0f3d904605c779f3c8b967602276ed80b5d2
Jon McManus <due@wxwhatever.com>
2019-04-08 09:34:44 +1000

    Automatically remap thatch -> thatch.

---
+++ src/main/java/epicsquid/roots/event/handlers/MappingsEvent.java
@@ -0,0 +1,33 @@
+package epicsquid.roots.event.handlers;
+
+import epicsquid.mysticalworld.init.ModBlocks;
+import epicsquid.roots.Roots;
+import net.minecraft.block.Block;
+import net.minecraft.item.Item;
+import net.minecraft.util.ResourceLocation;
+import net.minecraftforge.event.RegistryEvent;
+import net.minecraftforge.fml.common.Mod;
+import net.minecraftforge.fml.common.eventhandler.SubscribeEvent;
+
+@Mod.EventBusSubscriber(modid= Roots.MODID)
+public class MappingsEvent {
+  @SubscribeEvent
+  public static void onMissingBlockMappings(RegistryEvent.MissingMappings<Block> event) {
+    for (RegistryEvent.MissingMappings.Mapping<Block> mapping : event.getMappings()) {
+      ResourceLocation missing = mapping.key;
+      if (missing.getResourceDomain().equals(Roots.MODID) && missing.getResourcePath().equals("thatch")) {
+        mapping.remap(ModBlocks.thatch);
+      }
+    }
+  }
+
+  @SubscribeEvent
+  public static void onMissingItemMappings (RegistryEvent.MissingMappings<Item> event) {
+    for (RegistryEvent.MissingMappings.Mapping<Item> mapping : event.getMappings()) {
+      ResourceLocation missing = mapping.key;
+      if (missing.getResourceDomain().equals(Roots.MODID) && missing.getResourcePath().equals("thatch")) {
+        mapping.remap(Item.getItemFromBlock(ModBlocks.thatch));
+      }
+    }
+  }
+}
