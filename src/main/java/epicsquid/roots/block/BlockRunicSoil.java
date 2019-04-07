1ec398da91f47442d88f591d6d9211cf2cafbe02
Jon McManus <due@wxwhatever.com>
2019-04-07 23:05:40 +1000

    Tentatively give crops grown on runic soil additional growth ticks.
    They have a 1-in-5 chance of gaining an additional tick every time runic
    soil ticks. I'm worried this might actually be way too much, but I can't
    think of the math for it.

--- src/main/java/epicsquid/roots/block/BlockRunicSoil.java
+++ src/main/java/epicsquid/roots/block/BlockRunicSoil.java
@@ -5,14 +5,19 @@ import javax.annotation.Nonnull;
 import epicsquid.mysticallib.block.BlockBase;
 import epicsquid.roots.api.CustomPlantType;
 import epicsquid.mysticalworld.util.EnumRunicSoilType;
+import net.minecraft.block.Block;
+import net.minecraft.block.IGrowable;
 import net.minecraft.block.SoundType;
 import net.minecraft.block.material.Material;
 import net.minecraft.block.state.IBlockState;
 import net.minecraft.util.EnumFacing;
 import net.minecraft.util.math.BlockPos;
 import net.minecraft.world.IBlockAccess;
+import net.minecraft.world.World;
 import net.minecraftforge.common.EnumPlantType;
 import net.minecraftforge.common.IPlantable;
+
+import java.util.Random;
 
 public class BlockRunicSoil extends BlockBase {
 
@@ -38,6 +43,21 @@ public class BlockRunicSoil extends BlockBase {
         || plant == CustomPlantType.ELEMENT_AIR && soilType == EnumRunicSoilType.AIR
         || plant == CustomPlantType.ELEMENT_EARTH && soilType == EnumRunicSoilType.EARTH
         || plant == CustomPlantType.ELEMENT_WATER && soilType == EnumRunicSoilType.WATER;
+  }
+
+  @Override
+  public void updateTick(World world, BlockPos pos, IBlockState state, Random rand) {
+    super.updateTick(world, pos, state, rand);
+
+    if (world.isAirBlock(pos.up())) return;
+
+    Block upBlock = world.getBlockState(pos.up()).getBlock();
+    if (!(upBlock instanceof IGrowable)) return;
+
+    // TODO: Who knows if this value is any good
+    if (rand.nextInt(5) == 0) {
+      upBlock.randomTick(world, pos.up(), state, rand);
+    }
   }
 }
 
