d629ab19a280d50d3bd6bdba10bb3eeeb352be96
Jon McManus <due@wxwhatever.com>
2019-04-15 22:33:24 +1000

    Fix a whoospie with the Apothecary Pouch.

--- src/main/java/epicsquid/roots/gui/container/ContainerPouch.java
+++ src/main/java/epicsquid/roots/gui/container/ContainerPouch.java
@@ -121,13 +121,13 @@ public class ContainerPouch extends Container {
     for (int i = 0; i < herbsHandler.getSlots(); i++) {
       // Add Herb Slots
       if (q >= 18 && q < 21) {
-        addSlotToContainer(new SlotItemHandler(herbsHandler, i++, xOffset + 149 + (16 * (q % 3)), yOffset + 16 + (4 * (q % 2))));
+        addSlotToContainer(new SlotItemHandler(herbsHandler, q++, xOffset + 149 + (16 * (q % 3)), yOffset + 16 + (4 * (q % 2))));
       }
       if (q >= 21 && q < 24) {
-        addSlotToContainer(new SlotItemHandler(herbsHandler, i++, xOffset + 149 + (16 * (q % 3)), yOffset + 39 + (4 * ((q + 1) % 2))));
+        addSlotToContainer(new SlotItemHandler(herbsHandler, q++, xOffset + 149 + (16 * (q % 3)), yOffset + 39 + (4 * ((q + 1) % 2))));
       }
       if (q >= 24 && q < 27) {
-        addSlotToContainer(new SlotItemHandler(herbsHandler, i++, xOffset + 149 + (16 * (q % 3)), yOffset + 64 + (4 * (q % 2))));
+        addSlotToContainer(new SlotItemHandler(herbsHandler, q++, xOffset + 149 + (16 * (q % 3)), yOffset + 64 + (4 * (q % 2))));
       }
       q++;
     }
