41f828d08f3a93e7394e374f82d06cdffb0d0e91
Jon McManus <due@wxwhatever.com>
2019-04-15 10:09:16 +1000

    Finish Pouch conversion to NBT.
    Currently herb consumption isn't actually happening.
    
    Apothecary Recipe is untested.

--- src/main/java/epicsquid/roots/inventory/PouchHandler.java
+++ src/main/java/epicsquid/roots/inventory/PouchHandler.java
@@ -15,12 +15,14 @@ public class PouchHandler implements INBTSerializable<NBTTagCompound> {
   public static final int APOTHECARY_POUCH_HERB_SLOTS = 9;
   public static final int APOTHECARY_POUCH_INVENTORY_SLOTS = 18;
 
-  private ItemStackHandler inventorySlots;
-  private ItemStackHandler herbSlots;
+  private PouchItemHandler inventorySlots;
+  private PouchItemHandler herbSlots;
+  private ItemStack pouch;
 
-  public PouchHandler (int inventorySlots, int herbSlots) {
-    this.inventorySlots = new ItemStackHandler(inventorySlots);
-    this.herbSlots = new ItemStackHandler(herbSlots) {
+  public PouchHandler (ItemStack pouch, int inventorySlots, int herbSlots) {
+    this.pouch = pouch;
+    this.inventorySlots = new PouchItemHandler(inventorySlots);
+    this.herbSlots = new PouchItemHandler(herbSlots) {
       @Override
       public boolean isItemValid(int slot, @Nonnull ItemStack stack) {
         return HerbRegistry.containsHerbItem(stack.getItem());
@@ -28,11 +30,11 @@ public class PouchHandler implements INBTSerializable<NBTTagCompound> {
     };
   }
 
-  public ItemStackHandler getInventory () {
+  public PouchItemHandler getInventory () {
     return inventorySlots;
   }
 
-  public ItemStackHandler getHerbs () {
+  public PouchItemHandler getHerbs () {
     return herbSlots;
   }
 
@@ -53,16 +55,43 @@ public class PouchHandler implements INBTSerializable<NBTTagCompound> {
   public static PouchHandler getHandler (ItemStack stack) {
     PouchHandler handler;
     if (stack.getItem() == ModItems.component_pouch) {
-      handler = new PouchHandler(COMPONENT_POUCH_INVENTORY_SLOTS, COMPONENT_POUCH_HERB_SLOTS);
+      handler = new PouchHandler(stack, COMPONENT_POUCH_INVENTORY_SLOTS, COMPONENT_POUCH_HERB_SLOTS);
     } else if (stack.getItem() == ModItems.apothecary_pouch) {
-      handler = new PouchHandler(APOTHECARY_POUCH_INVENTORY_SLOTS, APOTHECARY_POUCH_HERB_SLOTS);
+      handler = new PouchHandler(stack, APOTHECARY_POUCH_INVENTORY_SLOTS, APOTHECARY_POUCH_HERB_SLOTS);
     } else {
       return null;
     }
     if (stack.hasTagCompound()) {
-      handler.deserializeNBT(stack.getTagCompound());
+      NBTTagCompound tag = stack.getTagCompound();
+      if (tag.hasKey("inventory")) {
+        handler.deserializeNBT(tag.getCompoundTag("inventory"));
+      }
     }
 
     return handler;
+  }
+
+  public void saveToStack () {
+    NBTTagCompound tag = pouch.getTagCompound();
+    if (tag == null) {
+      tag = new NBTTagCompound();
+      pouch.setTagCompound(tag);
+    }
+
+    tag.setTag("inventory", serializeNBT());
+  }
+
+  public class PouchItemHandler extends ItemStackHandler {
+
+    public PouchItemHandler(int size) {
+      super(size);
+    }
+
+    @Override
+    protected void onContentsChanged(int slot) {
+      super.onContentsChanged(slot);
+
+      PouchHandler.this.saveToStack();
+    }
   }
 }
