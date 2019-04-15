7851218ff4733289897e17b10ada9b1fbd44f511
Jon McManus <due@wxwhatever.com>
2019-04-15 21:14:28 +1000

    Don't render HUD while chat is open.

--- src/main/java/epicsquid/roots/gui/hud/RenderHUD.java
+++ src/main/java/epicsquid/roots/gui/hud/RenderHUD.java
@@ -5,6 +5,7 @@ import epicsquid.roots.init.ModBlocks;
 import net.minecraft.block.Block;
 import net.minecraft.block.state.IBlockState;
 import net.minecraft.client.Minecraft;
+import net.minecraft.client.gui.GuiChat;
 import net.minecraft.util.math.RayTraceResult;
 import net.minecraftforge.client.event.RenderGameOverlayEvent;
 import net.minecraftforge.fml.common.Mod;
@@ -24,6 +25,8 @@ public class RenderHUD {
     if (event.getType() != RenderGameOverlayEvent.ElementType.ALL) return;
 
     Minecraft mc = Minecraft.getMinecraft();
+    if (mc.currentScreen instanceof GuiChat) return;
+
     RayTraceResult trace = mc.objectMouseOver;
     if (trace == null || trace.typeOfHit != RayTraceResult.Type.BLOCK) return;
 
