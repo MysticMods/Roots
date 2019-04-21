8b21530911a041bef494f4a1fe0496f1fd493f7b
Davoleo <davoleo2001@gmail.com>
2019-04-21 18:48:47 +0200

    Config Entries to disable spells

--- src/main/java/epicsquid/roots/config/SpellConfig.java
+++ src/main/java/epicsquid/roots/config/SpellConfig.java
@@ -6,6 +6,69 @@ import net.minecraftforge.common.config.Config;
 @Config(modid = Roots.MODID, name = "roots/spells")
 public class SpellConfig {
 
+  @Config.Name("Disable Acid Cloud Spell")
+  @Config.Comment("Set to true to disable Acid Cloud Spell")
+  public static boolean disableAcidCloud = false;
+  @Config.Name("Disable Dandelion Winds Spell")
+  @Config.Comment("Set to true to disable Dandelion Winds Spell")
+  public static boolean disableDandelionWinds = false;
+  @Config.Name("Disable Growth Infusion Spell")
+  @Config.Comment("Set to true to disable Growth Infusion Spell")
+  public static boolean disableGrowthInfusion = false;
+  @Config.Name("Disable Harvest Spell")
+  @Config.Comment("Set to true to disable Harvest Spell")
+  public static boolean disableHarvest = false;
+  @Config.Name("Disable Life Drain Spell")
+  @Config.Comment("Set to true to disable Life Drain Spell")
+  public static boolean disableLifeDrain = false;
+  @Config.Name("Disable Light Drifter Spell")
+  @Config.Comment("Set to true to disable Light Drifter Spell")
+  public static boolean disableLightDrifter = false;
+  @Config.Name("Disable Magnetism Spell")
+  @Config.Comment("Set to true to disable Magnetism Spell")
+  public static boolean disableMagnetism = false;
+  @Config.Name("Disable Mind Ward Spell")
+  @Config.Comment("Set to true to disable Mind Ward Spell")
+  public static boolean disableMindWard = false;
+  @Config.Name("Disable Petal Shell Spell")
+  @Config.Comment("Set to true to disable Petal Shell Spell")
+  public static boolean disablePetalShell = false;
+  @Config.Name("Disable Radiance Spell")
+  @Config.Comment("Set to true to disable Radiance Spell")
+  public static boolean disableRadiance = false;
+  @Config.Name("Disable Rampant Growth Spell")
+  @Config.Comment("Set to true to disable Rampant Growth Spell")
+  public static boolean disableRampantGrowth = false;
+  @Config.Name("Disable Rose Thorns Spell")
+  @Config.Comment("Set to true to disable Rose Thorns Spell")
+  public static boolean disableRoseThorns = false;
+  @Config.Name("Disable Sanctuary Spell")
+  @Config.Comment("Set to true to disable Sanctuary Spell")
+  public static boolean disableSanctuary = false;
+  @Config.Name("Disable Second Wind Spell")
+  @Config.Comment("Set to true to disable Second Wind Spell")
+  public static boolean disableSecondWind = false;
+  @Config.Name("Disable Sense Animals Spell")
+  @Config.Comment("Set to true to disable Sense Animals Spell")
+  public static boolean disableSenseAnimals = false;
+  @Config.Name("Disable Sense Danger Spell")
+  @Config.Comment("Set to true to disable Sense Danger Spell")
+  public static boolean disableSenseDanger = false;
+  @Config.Name("Disable Shatter Spell")
+  @Config.Comment("Set to true to disable Shatter Spell")
+  public static boolean disableShatter= false;
+  @Config.Name("Disable Sky Soarer Spell")
+  @Config.Comment("Set to true to disable Sky Soarer Spell")
+  public static boolean disableSkySoarer = false;
+  @Config.Name("Disable Time Stop Spell")
+  @Config.Comment("Set to true to disable Time Stop Spell")
+  public static boolean disableTimeStop = false;
+  @Config.Name("Disable Wild Fire Spell")
+  @Config.Comment("Set to true to disable Wild Fire Spell")
+  public static boolean disableWildFire = false;
+
+
+
   public static CategoryAcidCloud categoryAcidCloud = new CategoryAcidCloud();
   public static CategoryDandelionWinds categoryDandelionWinds = new CategoryDandelionWinds();
   public static CategoryGrowthInfusion categoryGrowthInfusion = new CategoryGrowthInfusion();
@@ -19,6 +82,8 @@ public class SpellConfig {
   public static CategoryRampantGrowth categoryRampantGrowth = new CategoryRampantGrowth();
 
   public static class CategoryAcidCloud {
+
+
 
   }
 
