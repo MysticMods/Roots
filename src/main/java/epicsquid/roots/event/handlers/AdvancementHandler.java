073f00068252d82c64f52353e32d091d80432427
Jon McManus <due@wxwhatever.com>
2019-05-05 13:45:13 +1000

    Retroactively grant Roots advancements.
    In order to sustain the viability of Patchouli for documentation, and
    given that you cannot necessarily rely entirely on people following the
    identical progression path (or buying items from villagers),
    retroactively granting advancements means that Patchouli entries will be
    properly unlocked.

--- src/main/java/epicsquid/roots/event/handlers/AdvancementHandler.java
+++ src/main/java/epicsquid/roots/event/handlers/AdvancementHandler.java
@@ -2,21 +2,27 @@ package epicsquid.roots.event.handlers;
 
 import epicsquid.roots.Roots;
 import epicsquid.roots.util.XPUtil;
+import net.minecraft.advancements.Advancement;
+import net.minecraft.advancements.AdvancementProgress;
+import net.minecraft.advancements.PlayerAdvancements;
+import net.minecraft.entity.player.EntityPlayer;
+import net.minecraft.entity.player.EntityPlayerMP;
 import net.minecraft.util.ResourceLocation;
+import net.minecraft.util.math.BlockPos;
+import net.minecraft.world.World;
 import net.minecraftforge.event.entity.player.AdvancementEvent;
 import net.minecraftforge.fml.common.Mod;
 import net.minecraftforge.fml.common.eventhandler.SubscribeEvent;
+
+import java.util.ArrayList;
+import java.util.List;
 
 @Mod.EventBusSubscriber(modid= Roots.MODID)
 @SuppressWarnings("unused")
 public class AdvancementHandler {
   public static int FIRST_CRAFT_XP = 4;
 
-  @SubscribeEvent
-  public static void onAdvancement (AdvancementEvent event) {
-    if (event.getEntityPlayer().world.isRemote) return;
-
-    ResourceLocation adv = event.getAdvancement().getId();
+  private static void grantXP (ResourceLocation adv, World world, BlockPos pos) {
     if (adv.getNamespace().equals(Roots.MODID)) {
       switch (adv.getPath()) {
         case "aubergine":
@@ -29,8 +35,36 @@ public class AdvancementHandler {
         case "spirit_herbs":
         case "stalicripe":
         case "terra_moss":
-          XPUtil.spawnXP(event.getEntityPlayer().world, event.getEntityPlayer().getPosition(), FIRST_CRAFT_XP);
+          XPUtil.spawnXP(world, pos, FIRST_CRAFT_XP);
       }
+    }
+  }
+
+  @SubscribeEvent
+  public static void onAdvancement (AdvancementEvent event) {
+    if (event.getEntityPlayer().world.isRemote) return;
+
+    ResourceLocation adv = event.getAdvancement().getId();
+    if (adv.getNamespace().equals(Roots.MODID)) {
+      World world = event.getEntityPlayer().world;
+      BlockPos pos = event.getEntityPlayer().getPosition();
+      grantXP(adv, world, pos);
+
+      Advancement advancement = event.getAdvancement();
+      EntityPlayer player = event.getEntityPlayer();
+      if (player.world.isRemote) return;
+
+      PlayerAdvancements advancements = ((EntityPlayerMP) player).getAdvancements();
+      for (Advancement parent = advancement.getParent(); parent != null; parent = parent.getParent()) {
+        AdvancementProgress progress = advancements.getProgress(parent);
+        if (progress.isDone()) continue;
+        for (String criterion : progress.getRemaningCriteria()) {
+          progress.grantCriterion(criterion);
+        }
+        advancement.getRewards().apply((EntityPlayerMP) player);
+        grantXP(parent.getId(), world, pos);
+      }
+      advancements.save();
     }
   }
 }
