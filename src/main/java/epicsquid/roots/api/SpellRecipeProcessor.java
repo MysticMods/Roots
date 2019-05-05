a1284009eecff4c8d8de645a6d74303263fad715
Jon McManus <due@wxwhatever.com>
2019-05-05 17:49:27 +1000

    Include spell costs in spell pages.
    This does remove the explanation previously included in every spell and
    replaces it with a link to the Sellcrafting & Imbuing page, which I feel
    is much more informative.

--- src/main/java/epicsquid/roots/api/SpellRecipeProcessor.java
+++ src/main/java/epicsquid/roots/api/SpellRecipeProcessor.java
@@ -2,6 +2,7 @@ package epicsquid.roots.api;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Map;
 
 import epicsquid.roots.init.ModItems;
 import epicsquid.roots.item.ItemSpellDust;
@@ -13,16 +14,23 @@ import vazkii.patchouli.api.IComponentProcessor;
 import vazkii.patchouli.api.IVariableProvider;
 import vazkii.patchouli.common.util.ItemStackUtil;
 
+@SuppressWarnings("unused")
 public class SpellRecipeProcessor implements IComponentProcessor {
 
   private List<Ingredient> ingredients = new ArrayList<>();
+  private List<ItemStack> herbs = new ArrayList<>();
+  private List<String> costs = new ArrayList<>();
   private String spellName = null;
 
   @Override
   public void setup(IVariableProvider<String> iVariableProvider) {
     spellName = iVariableProvider.get("spell");
-    SpellBase spellBase = SpellRegistry.spellRegistry.get(spellName);
-    ingredients = spellBase.getIngredients();
+    SpellBase spell = SpellRegistry.spellRegistry.get(spellName);
+    ingredients = spell.getIngredients();
+    for (Map.Entry<Herb, Double> cost : spell.getCosts().entrySet()) {
+      herbs.add(new ItemStack(cost.getKey().getItem()));
+      costs.add(String.format("x %.3f", cost.getValue()));
+    }
   }
 
   @Override
@@ -38,7 +46,20 @@ public class SpellRecipeProcessor implements IComponentProcessor {
 
       return ItemStackUtil.serializeStack(dust);
     }
+    if (s.equals("cost1")) {
+      return ItemStackUtil.serializeStack(herbs.get(0));
+    }
+    if (s.equals("cost2")) {
+      if (herbs.size() == 1) return ItemStackUtil.serializeStack(ItemStack.EMPTY);
+      return ItemStackUtil.serializeStack(herbs.get(1));
+    }
+    if (s.equals("cost1_text")) {
+      return costs.get(0);
+    }
+    if (s.equals("cost2_text")) {
+      if (costs.size() == 1) return "";
+      return costs.get(1);
+    }
     return null;
   }
-
 }
