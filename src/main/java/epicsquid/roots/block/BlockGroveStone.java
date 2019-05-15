f5c0b23b57588a9e1bb0384042bf44fb32cf4259
Jon McManus <due@wxwhatever.com>
2019-05-15 22:44:59 +1000

    Rotatations, segmentation, breaking...
    It'll probably break everything.

--- src/main/java/epicsquid/roots/block/BlockGroveStone.java
+++ src/main/java/epicsquid/roots/block/BlockGroveStone.java
@@ -3,20 +3,42 @@ package epicsquid.roots.block;
 import epicsquid.mysticallib.block.BlockTEBase;
 import net.minecraft.block.SoundType;
 import net.minecraft.block.material.Material;
-import net.minecraft.block.state.BlockFaceShape;
+import net.minecraft.block.properties.PropertyBool;
+import net.minecraft.block.properties.PropertyDirection;
+import net.minecraft.block.properties.PropertyEnum;
+import net.minecraft.block.state.BlockStateContainer;
 import net.minecraft.block.state.IBlockState;
+import net.minecraft.entity.EntityLivingBase;
+import net.minecraft.init.Items;
+import net.minecraft.item.Item;
+import net.minecraft.item.ItemStack;
 import net.minecraft.tileentity.TileEntity;
+import net.minecraft.util.BlockRenderLayer;
 import net.minecraft.util.EnumFacing;
+import net.minecraft.util.IStringSerializable;
 import net.minecraft.util.math.AxisAlignedBB;
 import net.minecraft.util.math.BlockPos;
 import net.minecraft.world.IBlockAccess;
+import net.minecraft.world.World;
 
 import javax.annotation.Nonnull;
+import java.util.Random;
 
 public class BlockGroveStone extends BlockTEBase {
+  public static final PropertyEnum<Half> HALF = PropertyEnum.create("half", Half.class);
+  public static final PropertyDirection FACING = PropertyDirection.create("facing", EnumFacing.Plane.HORIZONTAL);
+  public static final PropertyBool VALID = PropertyBool.create("valid");
 
   public BlockGroveStone(@Nonnull Material mat, @Nonnull SoundType type, float hardness, @Nonnull String name, @Nonnull Class<? extends TileEntity> teClass) {
     super(mat, type, hardness, name, teClass);
+
+    this.setDefaultState(this.blockState.getBaseState().withProperty(VALID, true).withProperty(HALF, Half.BOTTOM).withProperty(FACING, EnumFacing.NORTH));
+  }
+
+  @Nonnull
+  @Override
+  public BlockRenderLayer getRenderLayer() {
+    return BlockRenderLayer.CUTOUT;
   }
 
   @Override
@@ -32,6 +54,104 @@ public class BlockGroveStone extends BlockTEBase {
   @Nonnull
   @Override
   public AxisAlignedBB getBoundingBox(@Nonnull IBlockState state, @Nonnull IBlockAccess world, @Nonnull BlockPos pos) {
-    return new AxisAlignedBB(0, 0, 0, 1, 3, 1);
+    return new AxisAlignedBB(0, 0, 0, 1, 1, 1);
+  }
+
+  @Override
+  @SuppressWarnings("deprecation")
+  public IBlockState getStateFromMeta(int meta) {
+    return getDefaultState().withProperty(VALID, (meta & 1) == 1).withProperty(HALF, Half.fromInt((meta >> 1 & 1))).withProperty(FACING, EnumFacing.byIndex(((meta >> 2)+2)));
+  }
+
+  @Override
+  public int getMetaFromState(IBlockState state) {
+    return (state.getValue(FACING).ordinal() - 2) << 2 | state.getValue(HALF).ordinal() << 1 | (state.getValue(VALID) ? 1 : 0);
+  }
+
+  @Override
+  protected BlockStateContainer createBlockState() {
+    return new BlockStateContainer(this, HALF, VALID, FACING);
+  }
+
+  @Override
+  public boolean canPlaceBlockAt(World worldIn, BlockPos pos) {
+    if (worldIn.getBlockState(pos.down()).getBlock() == this) return false;
+
+    IBlockState up = worldIn.getBlockState(pos.up());
+    IBlockState upup = worldIn.getBlockState(pos.up().up());
+
+    return up.getBlock() != this && upup.getBlock() != this && super.canPlaceBlockAt(worldIn, pos) && up.getBlock().isReplaceable(worldIn, pos.up()) && upup.getBlock().isReplaceable(worldIn, pos.up().up());
+  }
+
+  @Override
+  public void onBlockPlacedBy(World worldIn, BlockPos pos, IBlockState state, EntityLivingBase placer, ItemStack stack) {
+    super.onBlockPlacedBy(worldIn, pos, state, placer, stack);
+    worldIn.setBlockState(pos.up(), this.getDefaultState().withProperty(HALF, Half.TOP));
+    worldIn.setBlockState(pos.up().up(), this.getDefaultState().withProperty(HALF, Half.TOP));
+  }
+
+  @Override
+  @SuppressWarnings("deprecation")
+  public IBlockState getStateForPlacement(World worldIn, BlockPos pos, EnumFacing facing, float hitX, float hitY, float hitZ, int meta, EntityLivingBase placer) {
+    // Work out the facing
+    EnumFacing f = EnumFacing.fromAngle(placer.rotationYaw).getOpposite();
+    if (f == EnumFacing.UP || f == EnumFacing.DOWN) {
+      f = EnumFacing.NORTH;
+    }
+    return this.getDefaultState().withProperty(HALF, Half.BOTTOM).withProperty(FACING, f);
+  }
+
+  @Override
+  public void breakBlock(World worldIn, BlockPos pos, IBlockState state) {
+    super.breakBlock(worldIn, pos, state);
+
+    IBlockState down = worldIn.getBlockState(pos.down());
+    IBlockState downdown = worldIn.getBlockState(pos.down().down());
+    IBlockState up = worldIn.getBlockState(pos.up());
+    IBlockState upup = worldIn.getBlockState(pos.up().up());
+
+    if (down.getBlock() == this) {
+      worldIn.setBlockToAir(pos.down());
+    }
+    if (downdown.getBlock() == this) {
+      worldIn.setBlockToAir(pos.down().down());
+    }
+    if (up.getBlock() == this) {
+      worldIn.setBlockToAir(pos.up());
+    }
+    if (upup.getBlock() == this) {
+      worldIn.setBlockToAir(pos.up());
+    }
+  }
+
+  @Override
+  public Item getItemDropped(IBlockState state, Random rand, int fortune) {
+    if (state.getValue(HALF) == Half.BOTTOM) {
+      return super.getItemDropped(state, rand, fortune);
+    }
+    return Items.AIR;
+  }
+
+  @Override
+  public int damageDropped(IBlockState state) {
+    return 0;
+  }
+
+  public enum Half implements IStringSerializable
+  {
+    TOP,
+    BOTTOM;
+
+    @Override
+    public String getName() {
+      switch (this) {
+        case TOP: return "top";
+        default: return "bottom";
+      }
+    }
+
+    public static Half fromInt (int x) {
+      return values()[x];
+    }
   }
 }
