f7f47676491143ea18b5f0fa66be7ee3a1f36c5b
Jon McManus <due@wxwhatever.com>
2019-05-27 12:25:37 +1000

    Better visual indication of valid/invalid recipes, Grove Stone status.

--- src/main/java/epicsquid/roots/gui/client/GuiGroveCrafter.java
+++ src/main/java/epicsquid/roots/gui/client/GuiGroveCrafter.java
@@ -44,13 +44,28 @@ public class GuiGroveCrafter extends GuiContainer {
     int i = (this.width - this.xSize) / 2;
     int j = (this.height - this.ySize) / 2;
     this.drawTexturedModalRect(i, j, 0, 0, 176, 207);
+    int j3 = -1;
+    int i3 = -1;
     if (container.getRecipe() != null) {
       RenderHelper.enableGUIStandardItemLighting();
       ItemStack result = container.getRecipe().getResult();
-      i += (this.xSize / 4) * 3;
-      j += (this.ySize / 5);
-      mc.getRenderItem().renderItemIntoGUI(result, i, j);
-      mc.getRenderItem().renderItemOverlayIntoGUI(mc.fontRenderer, result, i, j, null);
+      i3 = i + ((this.xSize / 4) * 3);
+      j3 = j + (this.ySize / 5);
+      mc.getRenderItem().renderItemIntoGUI(result, i3, j3);
+      mc.getRenderItem().renderItemOverlayIntoGUI(mc.fontRenderer, result, i3, j3, null);
+      RenderHelper.disableStandardItemLighting();
+    }
+    this.mc.getTextureManager().bindTexture(new ResourceLocation(Roots.MODID, "textures/gui/grove_crafting_gui.png"));
+    if (!container.getValidStone()) {
+      int i2 = i + 54;
+      int j2 = j + 33;
+      this.drawTexturedModalRect(i2, j2, 176, 0, 16, 22);
+      if (container.getRecipe() != null) {
+        GlStateManager.pushMatrix();
+        GlStateManager.translate(0, 0, 150f);
+        this.drawTexturedModalRect(i3 + 1, j3 + 1, 176 + 16, 0, 15, 15);
+        GlStateManager.popMatrix();
+      }
     }
   }
 }
