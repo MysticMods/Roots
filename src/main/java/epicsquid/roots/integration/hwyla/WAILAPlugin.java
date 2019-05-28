b7bbf375d7226f2905c078ec3f19673517c0b119
Jon McManus <due@wxwhatever.com>
2019-05-28 23:09:43 +1000

    WAILA and TOP integration.
    Here's a secret: I haven't tested any of my recent commits!

---
+++ src/main/java/epicsquid/roots/integration/hwyla/WAILAPlugin.java
@@ -0,0 +1,23 @@
+package epicsquid.roots.integration.hwyla;
+
+import epicsquid.roots.block.BlockGroveStone;
+import epicsquid.roots.integration.hwyla.providers.ProviderBonfire;
+import epicsquid.roots.integration.hwyla.providers.ProviderGroveStone;
+import epicsquid.roots.tileentity.TileEntityBonfire;
+import mcp.mobius.waila.api.IWailaPlugin;
+import mcp.mobius.waila.api.IWailaRegistrar;
+import mcp.mobius.waila.api.WailaPlugin;
+
+@WailaPlugin
+@SuppressWarnings("unused")
+public class WAILAPlugin implements IWailaPlugin {
+  @SuppressWarnings("unused")
+  public WAILAPlugin() {
+  }
+
+  @Override
+  public void register(IWailaRegistrar registrar) {
+    registrar.registerBodyProvider(new ProviderGroveStone(), BlockGroveStone.class);
+    registrar.registerBodyProvider(new ProviderBonfire(), TileEntityBonfire.class);
+  }
+}
