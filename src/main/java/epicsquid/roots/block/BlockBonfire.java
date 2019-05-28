cab5f95b5bd1941da0ab4ce2c0854c53a53fac04
Jon McManus <due@wxwhatever.com>
2019-05-28 20:37:12 +1000

    Like a proper fire, Pyre now burns when lit.

--- src/main/java/epicsquid/roots/block/BlockBonfire.java
+++ src/main/java/epicsquid/roots/block/BlockBonfire.java
@@ -9,15 +9,20 @@ import net.minecraft.block.state.BlockFaceShape;
 import net.minecraft.block.state.BlockStateContainer;
 import net.minecraft.block.state.IBlockState;
 import net.minecraft.entity.EntityLivingBase;
+import net.minecraft.init.SoundEvents;
 import net.minecraft.tileentity.TileEntity;
 import net.minecraft.util.EnumFacing;
 import net.minecraft.util.EnumHand;
+import net.minecraft.util.SoundCategory;
 import net.minecraft.util.math.AxisAlignedBB;
 import net.minecraft.util.math.BlockPos;
 import net.minecraft.world.IBlockAccess;
 import net.minecraft.world.World;
+import net.minecraftforge.fml.relauncher.Side;
+import net.minecraftforge.fml.relauncher.SideOnly;
 
 import javax.annotation.Nonnull;
+import java.util.Random;
 
 @SuppressWarnings("deprecation")
 public class BlockBonfire extends BlockTEBase {
@@ -40,12 +45,11 @@ public class BlockBonfire extends BlockTEBase {
   }
 
   @Override
-  public int getLightValue(IBlockState state, IBlockAccess world, BlockPos pos)
-  {
-      if (state.getValue(BURNING))
-        return 12;
-      else
-        return 0;
+  public int getLightValue(IBlockState state, IBlockAccess world, BlockPos pos) {
+    if (state.getValue(BURNING))
+      return 12;
+    else
+      return 0;
   }
 
   @Override
@@ -63,27 +67,23 @@ public class BlockBonfire extends BlockTEBase {
   //Concerning the blockstate --------------------
   @Nonnull
   @Override
-  protected BlockStateContainer createBlockState()
-  {
+  protected BlockStateContainer createBlockState() {
     return new BlockStateContainer(this, BURNING);
   }
 
   @Nonnull
   @Override
-  public IBlockState getStateForPlacement(@Nonnull World world, @Nonnull BlockPos pos, @Nonnull EnumFacing facing, float hitX, float hitY, float hitZ, int meta, @Nonnull EntityLivingBase placer, EnumHand hand)
-  {
+  public IBlockState getStateForPlacement(@Nonnull World world, @Nonnull BlockPos pos, @Nonnull EnumFacing facing, float hitX, float hitY, float hitZ, int meta, @Nonnull EntityLivingBase placer, EnumHand hand) {
     return this.getDefaultState().withProperty(BURNING, false);
   }
 
-  public static void setState(boolean burning, World world, BlockPos pos)
-  {
+  public static void setState(boolean burning, World world, BlockPos pos) {
     TileEntity te = world.getTileEntity(pos);
 
-    if(burning) world.setBlockState(pos, ModBlocks.bonfire.getDefaultState().withProperty(BURNING, true), 3);
+    if (burning) world.setBlockState(pos, ModBlocks.bonfire.getDefaultState().withProperty(BURNING, true), 3);
     else world.setBlockState(pos, ModBlocks.bonfire.getDefaultState().withProperty(BURNING, false), 3);
 
-    if (te != null)
-    {
+    if (te != null) {
       te.validate();
       world.setTileEntity(pos, te);
     }
@@ -92,16 +92,19 @@ public class BlockBonfire extends BlockTEBase {
 
   @Nonnull
   @Override
-  public IBlockState getStateFromMeta(int meta)
-  {
-      return this.getDefaultState().withProperty(BURNING, meta == 1);
+  public IBlockState getStateFromMeta(int meta) {
+    return this.getDefaultState().withProperty(BURNING, meta == 1);
   }
 
   @Override
-  public int getMetaFromState(IBlockState state)
-  {
+  public int getMetaFromState(IBlockState state) {
     return state.getValue(BURNING) ? 1 : 0;
   }
 
-
+  @SideOnly(Side.CLIENT)
+  public void randomDisplayTick(IBlockState stateIn, World worldIn, BlockPos pos, Random rand) {
+    if (stateIn.getValue(BURNING)) {
+      worldIn.playSound((double) pos.getX() + 0.5D, (double) pos.getY(), (double) pos.getZ() + 0.5D, SoundEvents.BLOCK_FIRE_AMBIENT, SoundCategory.BLOCKS, 0.5F, 1.0F, false);
+    }
+  }
 }
