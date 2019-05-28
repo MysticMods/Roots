4dc9338d527dfa3925b90c51986694ab95c08fe0
Jon McManus <due@wxwhatever.com>
2019-05-28 19:57:41 +1000

    Fix position of Pyre ritual in JEI.

--- src/main/java/epicsquid/roots/integration/jei/ritual/RitualCraftingCategory.java
+++ src/main/java/epicsquid/roots/integration/jei/ritual/RitualCraftingCategory.java
@@ -58,7 +58,7 @@ public class RitualCraftingCategory implements IRecipeCategory<RitualCraftingWra
     group.set(4, recipe.getRecipe().get(4));
     group.init(5, false, 99, 23);
     group.set(5, recipe.getResult());
-    group.init(6, true, 26, 30);
+    group.init(6, true, 26, 29);
     group.set(6, new ItemStack(ModBlocks.bonfire));
   }
 }
