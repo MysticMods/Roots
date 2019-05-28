1432c9f70e018b348895a30556bfb2a82959d612
Jon McManus <due@wxwhatever.com>
2019-05-29 00:26:00 +1000

    Start Chisel integration; don't harvest melon or pumpkin stems.

---
+++ src/main/java/epicsquid/roots/integration/chisel/RootsChisel.java
@@ -0,0 +1,14 @@
+package epicsquid.roots.integration.chisel;
+
+import epicsquid.roots.init.ModBlocks;
+import net.minecraft.block.Block;
+
+import java.util.Arrays;
+
+public class RootsChisel {
+  public static void integrate () {
+    for (Block block : Arrays.asList(ModBlocks.runestone, ModBlocks.runestone_brick, ModBlocks.runestone_brick_alt, ModBlocks.chiseled_runestone)) {
+
+    }
+  }
+}
