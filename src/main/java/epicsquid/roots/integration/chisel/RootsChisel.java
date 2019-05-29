4ff9c4e07bc4870169e7b7895c1cade8fdac7c96
Jon McManus <due@wxwhatever.com>
2019-05-29 11:52:26 +1000

    Conversion recipes for runic soil; Chisel integration.

--- src/main/java/epicsquid/roots/integration/chisel/RootsChisel.java
+++ src/main/java/epicsquid/roots/integration/chisel/RootsChisel.java
@@ -2,13 +2,25 @@ package epicsquid.roots.integration.chisel;
 
 import epicsquid.roots.init.ModBlocks;
 import net.minecraft.block.Block;
+import net.minecraft.nbt.NBTTagCompound;
+import net.minecraftforge.fml.common.event.FMLInterModComms;
 
 import java.util.Arrays;
 
 public class RootsChisel {
   public static void integrate () {
     for (Block block : Arrays.asList(ModBlocks.runestone, ModBlocks.runestone_brick, ModBlocks.runestone_brick_alt, ModBlocks.chiseled_runestone)) {
+      NBTTagCompound tag = new NBTTagCompound();
+      tag.setString("group", "rootsRunestone");
+      tag.setString("block", block.getRegistryName().toString());
+      FMLInterModComms.sendMessage("chisel", "add_variation", tag);
+    }
 
+    for (Block block : Arrays.asList(ModBlocks.runic_soil_air, ModBlocks.runic_soil_earth, ModBlocks.runic_soil_fire, ModBlocks.runic_soil_water)) {
+      NBTTagCompound tag = new NBTTagCompound();
+      tag.setString("group", "rootsRunicSoilTypes");
+      tag.setString("block", block.getRegistryName().toString());
+      FMLInterModComms.sendMessage("chisel", "add_variation", tag);
     }
   }
 }
