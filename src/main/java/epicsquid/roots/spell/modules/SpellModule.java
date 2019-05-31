1774ba0da1191eede87f50c368d8d01e588ea514
Jon McManus <due@wxwhatever.com>
2019-05-31 21:21:18 +1000

    Display module information for spells in tooltips.

--- src/main/java/epicsquid/roots/spell/modules/SpellModule.java
+++ src/main/java/epicsquid/roots/spell/modules/SpellModule.java
@@ -1,15 +1,18 @@
 package epicsquid.roots.spell.modules;
 
 import net.minecraft.item.ItemStack;
+import net.minecraft.util.text.TextFormatting;
 
 public class SpellModule {
 
     private ItemStack ingredient;
     private String name;
+    private TextFormatting colour;
 
-    public SpellModule(String name, ItemStack ingredient){
+    public SpellModule(String name, ItemStack ingredient, TextFormatting colour){
         this.name = name;
         this.ingredient = ingredient;
+        this.colour = colour;
     }
 
     public String getName() {
@@ -18,5 +21,9 @@ public class SpellModule {
 
     public ItemStack getIngredient() {
         return ingredient;
+    }
+
+    public TextFormatting getFormat () {
+        return colour;
     }
 }
