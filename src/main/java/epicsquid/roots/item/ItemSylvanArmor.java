b31ca0f9ecbe4524a2c0081b08726f1c713070bb
Jon McManus <due@wxwhatever.com>
2019-05-31 22:25:26 +1000

    Strip out herb usage of Sylvan armor.

--- src/main/java/epicsquid/roots/item/ItemSylvanArmor.java
+++ src/main/java/epicsquid/roots/item/ItemSylvanArmor.java
@@ -30,10 +30,6 @@ import java.util.ArrayList;
 import java.util.List;
 
 public class ItemSylvanArmor extends ItemArmor implements IModeledObject {
-
-    private List<String> herbs = new ArrayList<>();
-    private int delayTicks = 0;
-
     public ItemSylvanArmor(ArmorMaterial material, EntityEquipmentSlot slot, String name)
     {
         super(material, 0, slot);
@@ -41,13 +37,6 @@ public class ItemSylvanArmor extends ItemArmor implements IModeledObject {
         setRegistryName(new ResourceLocation(Roots.MODID, name));
         setMaxDamage(500);
         setCreativeTab(Roots.tab);
-        addHerbs();
-    }
-
-    private void addHerbs()
-    {
-        for (Herb herb : HerbRegistry.REGISTRY)
-            herbs.add(herb.getName());
     }
 
     @Override
@@ -69,57 +58,5 @@ public class ItemSylvanArmor extends ItemArmor implements IModeledObject {
     public ModelBiped getArmorModel(EntityLivingBase entityLiving, ItemStack itemStack, EntityEquipmentSlot armorSlot, ModelBiped _default)
     {
         return new ModelSylvanArmor(armorSlot);
-    }
-
-    //Temporary method to add an armor for each herb
-    @Override
-    public void getSubItems(@Nonnull CreativeTabs tab, @Nonnull NonNullList<ItemStack> items)
-    {
-        super.getSubItems(tab, items);
-
-        if (isInCreativeTab(tab)) {
-            for (String h : herbs) {
-                ItemStack stack = new ItemStack(this);
-                stack.setTagCompound(new NBTTagCompound());
-                stack.getTagCompound().setString("herb", h);
-                items.add(stack);
-            }
-        }
-    }
-
-    @Override
-    public void onArmorTick(World world, EntityPlayer player, ItemStack itemStack)
-    {
-        delayTicks++;
-
-        //One minute delay
-        if (itemStack.hasTagCompound() && delayTicks == 1200)
-        {
-            NBTTagCompound compound = itemStack.getTagCompound();
-            if (compound.hasKey("herb"))
-            {
-                Herb herb = HerbRegistry.getHerbByName(compound.getString("herb"));
-
-                if (PowderInventoryUtil.getPowderTotal(player, herb) >= 1)
-                    PowderInventoryUtil.removePowder(player, herb, 1);
-            }
-
-            delayTicks = 0;
-        }
-
-    }
-
-        @Override
-        public void addInformation(ItemStack stack, @Nullable World worldIn, List<String> tooltip, ITooltipFlag flagIn)
-        {
-            if (stack.hasTagCompound())
-            {
-                NBTTagCompound compound = stack.getTagCompound();
-                if (compound.hasKey("herb"))
-                {
-                    String herb = compound.getString("herb");
-                    tooltip.add("Infused Herb: " + herb);
-                }
-        }
     }
 }
