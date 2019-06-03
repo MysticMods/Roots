3496148126ddba6e2762cc53aa5afb93d6b87f06
Jon McManus <due@wxwhatever.com>
2019-06-03 19:57:03 +1000

    Runic Shears capability.

---
+++ src/main/java/epicsquid/roots/event/handlers/CapabilityHandler.java
@@ -0,0 +1,27 @@
+package epicsquid.roots.event.handlers;
+
+import com.google.common.collect.Sets;
+import epicsquid.mysticalworld.entity.EntityDeer;
+import epicsquid.roots.Roots;
+import epicsquid.roots.capability.runic_shears.RunicShearsCapabilityHandler;
+import net.minecraft.entity.Entity;
+import net.minecraft.entity.passive.EntityCow;
+import net.minecraft.entity.passive.EntityLlama;
+import net.minecraft.entity.passive.EntitySquid;
+import net.minecraftforge.event.AttachCapabilitiesEvent;
+import net.minecraftforge.fml.common.Mod;
+import net.minecraftforge.fml.common.eventhandler.SubscribeEvent;
+
+import java.util.Set;
+
+@Mod.EventBusSubscriber(modid = Roots.MODID)
+public class CapabilityHandler {
+  public static Set<Class<?>> entityClasses = Sets.newHashSet(EntityCow.class, EntityLlama.class, EntitySquid.class, EntityDeer.class);
+
+  @SubscribeEvent
+  public static void onAttachCapabilityEntity(AttachCapabilitiesEvent<Entity> event) {
+    if (entityClasses.contains(event.getObject().getClass())) {
+      event.addCapability(RunicShearsCapabilityHandler.IDENTIFIER, new RunicShearsCapabilityHandler());
+    }
+  }
+}
