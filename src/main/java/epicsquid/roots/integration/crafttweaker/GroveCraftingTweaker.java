2673dc810bebef6101811a1070ae7e9b46301897
Jon McManus <due@wxwhatever.com>
2019-06-05 17:45:47 +1000

    Fix a few issues with the previous PR.

--- src/main/java/epicsquid/roots/integration/crafttweaker/GroveCraftingTweaker.java
+++ src/main/java/epicsquid/roots/integration/crafttweaker/GroveCraftingTweaker.java
@@ -1,16 +1,9 @@
 package epicsquid.roots.integration.crafttweaker;
 
-import java.util.Arrays;
-import java.util.Collections;
-import java.util.HashMap;
-import java.util.Map;
-import java.util.stream.Stream;
-
 import com.blamejared.mtlib.helpers.InputHelper;
 import com.blamejared.mtlib.helpers.LogHelper;
 import com.blamejared.mtlib.utils.BaseMapAddition;
 import com.blamejared.mtlib.utils.BaseMapRemoval;
-
 import crafttweaker.CraftTweakerAPI;
 import crafttweaker.annotations.ZenRegister;
 import crafttweaker.api.item.IIngredient;
@@ -19,54 +12,60 @@ import crafttweaker.api.minecraft.CraftTweakerMC;
 import crafttweaker.mc1120.CraftTweaker;
 import epicsquid.roots.Roots;
 import epicsquid.roots.init.ModRecipes;
-import epicsquid.roots.recipe.PyreCraftingRecipe;
+import epicsquid.roots.recipe.GroveCraftingRecipe;
 import net.minecraft.item.crafting.Ingredient;
+import net.minecraft.util.ResourceLocation;
 import stanhebben.zenscript.annotations.ZenClass;
 import stanhebben.zenscript.annotations.ZenMethod;
 
+import java.util.Collections;
+import java.util.HashMap;
+import java.util.Map;
+import java.util.stream.Stream;
+
 @ZenRegister
-@ZenClass("mods." + Roots.MODID + ".Pyre")
-public class PyreCraftingTweaker {
+@ZenClass("mods." + Roots.MODID + ".Grove")
+public class GroveCraftingTweaker {
 
   @ZenMethod
   public static void addRecipe(String name, IItemStack output, IIngredient[] inputs) {
     if (inputs.length != 5) {
-      CraftTweakerAPI.getLogger().logError("Pyre Crafting Ritual must have 5 items: " + name);
+      CraftTweakerAPI.logError("Grove Crafting Ritual must have 5 items: " + name);
     }
-    CraftTweaker.LATE_ACTIONS.add(new Add(Collections.singletonMap(name + ".ct", new PyreCraftingRecipe(InputHelper.toStack(output)).addIngredients(Stream.of(inputs).map(CraftTweakerMC::getIngredient).toArray(Ingredient[]::new)).setName(name + ".ct"))));
+    CraftTweaker.LATE_ACTIONS.add(new Add(Collections.singletonMap(new ResourceLocation(Roots.MODID, name + ".ct"), new GroveCraftingRecipe(InputHelper.toStack(output)).addIngredients((Object) Stream.of(inputs).map(CraftTweakerMC::getIngredient).toArray(Ingredient[]::new)).setName(name + ".ct"))));
   }
 
   @ZenMethod
   public static void removeRecipe(IItemStack output) {
-    Map<String, PyreCraftingRecipe> recipes = new HashMap<>();
-    for(PyreCraftingRecipe modRecipe : ModRecipes.getPyreCraftingRecipes().values()) {
-      if (output.matches(InputHelper.toIItemStack(modRecipe.getResult()))) {
-        recipes.put(modRecipe.getName(), modRecipe);
+    Map<ResourceLocation, GroveCraftingRecipe> recipes = new HashMap<>();
+    for (Map.Entry<ResourceLocation, GroveCraftingRecipe> modRecipe : ModRecipes.getGroveCraftingRecipes().entrySet()) {
+      if (output.matches(InputHelper.toIItemStack(modRecipe.getValue().getResult()))) {
+        recipes.put(modRecipe.getKey(), modRecipe.getValue());
       }
     }
     CraftTweaker.LATE_ACTIONS.add(new Remove(recipes));
   }
 
-  private static class Remove extends BaseMapRemoval<String, PyreCraftingRecipe> {
+  private static class Remove extends BaseMapRemoval<ResourceLocation, GroveCraftingRecipe> {
 
-    private Remove(Map<String, PyreCraftingRecipe> map) {
-      super("Pyre Crafting Ritual", ModRecipes.getPyreCraftingRecipes(), map);
+    private Remove(Map<ResourceLocation, GroveCraftingRecipe> map) {
+      super("Grove Crafting Ritual", ModRecipes.getGroveCraftingRecipes(), map);
     }
 
     @Override
-    protected String getRecipeInfo(Map.Entry<String, PyreCraftingRecipe> recipe) {
+    protected String getRecipeInfo(Map.Entry<ResourceLocation, GroveCraftingRecipe> recipe) {
       return LogHelper.getStackDescription(recipe.getValue().getResult());
     }
   }
 
-  private static class Add extends BaseMapAddition<String, PyreCraftingRecipe> {
+  private static class Add extends BaseMapAddition<ResourceLocation, GroveCraftingRecipe> {
 
-    private Add(Map<String, PyreCraftingRecipe> map) {
-      super("Pyre Crafting Ritual", ModRecipes.getPyreCraftingRecipes(), map);
+    private Add(Map<ResourceLocation, GroveCraftingRecipe> map) {
+      super("Grove Crafting Ritual", ModRecipes.getGroveCraftingRecipes(), map);
     }
 
     @Override
-    protected String getRecipeInfo(Map.Entry<String, PyreCraftingRecipe> recipe) {
+    protected String getRecipeInfo(Map.Entry<ResourceLocation, GroveCraftingRecipe> recipe) {
       return LogHelper.getStackDescription(recipe.getValue().getResult());
     }
   }
