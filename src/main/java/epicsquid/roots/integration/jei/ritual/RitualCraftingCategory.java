6136d0750bf570cb93cc61d371fe87733600bfc9
Jon McManus <due@wxwhatever.com>
2019-06-07 21:26:53 +1000

    Update JEI integration to properly show oredict entries.

--- src/main/java/epicsquid/roots/integration/jei/ritual/RitualCraftingCategory.java
+++ src/main/java/epicsquid/roots/integration/jei/ritual/RitualCraftingCategory.java
@@ -9,10 +9,13 @@ import mezz.jei.api.gui.IDrawable;
 import mezz.jei.api.gui.IGuiItemStackGroup;
 import mezz.jei.api.gui.IRecipeLayout;
 import mezz.jei.api.ingredients.IIngredients;
+import mezz.jei.api.ingredients.VanillaTypes;
 import mezz.jei.api.recipe.IRecipeCategory;
 import net.minecraft.client.resources.I18n;
 import net.minecraft.item.ItemStack;
 import net.minecraft.util.ResourceLocation;
+
+import java.util.List;
 
 public class RitualCraftingCategory implements IRecipeCategory<RitualCraftingWrapper> {
 
@@ -45,18 +48,18 @@ public class RitualCraftingCategory implements IRecipeCategory<RitualCraftingWra
   @Override
   public void setRecipe(IRecipeLayout recipeLayout, RitualCraftingWrapper recipeWrapper, IIngredients ingredients) {
     IGuiItemStackGroup group = recipeLayout.getItemStacks();
-    PyreCraftingRecipe recipe = recipeWrapper.recipe;
+    List<List<ItemStack>> data = ingredients.getInputs(VanillaTypes.ITEM);
     group.init(0, true, 26, 2);
-    group.set(0, recipe.getRecipe().get(0));
+    group.set(0, data.get(0));
     group.init(1, true, 0, 23);
-    group.set(1, recipe.getRecipe().get(1));
+    group.set(1, data.get(1));
     group.init(2, true, 52, 23);
-    group.set(2, recipe.getRecipe().get(2));
+    group.set(2, data.get(2));
     group.init(3, true, 7, 56);
-    group.set(3, recipe.getRecipe().get(3));
+    group.set(3, data.get(3));
     group.init(4, true, 47, 56);
-    group.set(4, recipe.getRecipe().get(4));
+    group.set(4, data.get(4));
     group.init(5, false, 99, 23);
-    group.set(5, recipe.getResult());
+    group.set(5, ingredients.getOutputs(VanillaTypes.ITEM).get(0));
   }
 }
