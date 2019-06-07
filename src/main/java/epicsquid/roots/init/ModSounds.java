22a5be01afe0a486779a4d322b7280e89cbb7a0c
Jon McManus <due@wxwhatever.com>
2019-06-07 20:00:04 +1000

    Grove Crafter now has a nice little ditty!
    Not quite wind chimes, but close enough.

---
+++ src/main/java/epicsquid/roots/init/ModSounds.java
@@ -0,0 +1,23 @@
+package epicsquid.roots.init;
+
+import epicsquid.mysticallib.event.RegisterContentEvent;
+import epicsquid.mysticalworld.MysticalWorld;
+import epicsquid.roots.Roots;
+import net.minecraft.util.ResourceLocation;
+import net.minecraft.util.SoundEvent;
+
+import javax.annotation.Nonnull;
+
+public class ModSounds {
+  public static SoundEvent CHIMES;
+
+  public static void initSounds (@Nonnull RegisterContentEvent event) {
+    event.addSound(CHIMES = createSoundEvent(new ResourceLocation(Roots.MODID, "chimes")));
+  }
+
+  public static SoundEvent createSoundEvent (ResourceLocation name) {
+    SoundEvent result = new SoundEvent(name);
+    result.setRegistryName(name);
+    return result;
+  }
+}
