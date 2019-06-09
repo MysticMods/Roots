3b94ee0ce4cec1283b3be1e2ba2d78563298497c
Jon McManus <due@wxwhatever.com>
2019-06-09 21:11:25 +1000

    Get rid of the last of the ugly stuff.

--- src/main/java/epicsquid/roots/proxy/CommonProxy.java
+++ src/main/java/epicsquid/roots/proxy/CommonProxy.java
@@ -1,7 +1,5 @@
 package epicsquid.roots.proxy;
 
-import epicsquid.roots.api.RunicShearsRecipeProcessor;
-import epicsquid.roots.config.GroveCraftingConfig;
 import epicsquid.roots.effect.EffectManager;
 import epicsquid.roots.init.HerbRegistry;
 import epicsquid.roots.integration.chisel.RootsChisel;
@@ -10,14 +8,11 @@ import epicsquid.roots.integration.endercore.EndercoreHarvest;
 import epicsquid.roots.integration.harvest.HarvestIntegration;
 import epicsquid.roots.integration.jer.JERIntegration;
 import epicsquid.roots.recipe.RunicCarvingRecipes;
-import epicsquid.roots.recipe.RunicShearRecipes;
 import epicsquid.roots.ritual.RitualRegistry;
 import epicsquid.roots.rune.RuneRegistry;
 import epicsquid.roots.spell.SpellRegistry;
 import epicsquid.roots.spell.modules.ModuleRegistry;
 import epicsquid.roots.util.OfferingUtil;
-import net.minecraft.server.MinecraftServer;
-import net.minecraftforge.fml.common.FMLCommonHandler;
 import net.minecraftforge.fml.common.Loader;
 import net.minecraftforge.fml.common.event.FMLInitializationEvent;
 import net.minecraftforge.fml.common.event.FMLLoadCompleteEvent;
@@ -60,7 +55,5 @@ public class CommonProxy {
     if (Loader.isModLoaded("harvest")) {
       HarvestIntegration.init();
     }
-
-    GroveCraftingConfig.getClasses();
   }
 }
