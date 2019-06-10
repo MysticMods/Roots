3011f595bdd2465754023812916194018b6eb88a
Jon McManus <due@wxwhatever.com>
2019-06-10 11:05:14 +1000

    Vanilla-style particles for elemental soil.

--- src/main/java/epicsquid/roots/block/BlockElementalSoil.java
+++ src/main/java/epicsquid/roots/block/BlockElementalSoil.java
@@ -12,19 +12,24 @@ import net.minecraft.block.IGrowable;
 import net.minecraft.block.SoundType;
 import net.minecraft.block.material.Material;
 import net.minecraft.block.state.IBlockState;
+import net.minecraft.client.Minecraft;
 import net.minecraft.item.Item;
 import net.minecraft.util.EnumFacing;
+import net.minecraft.util.EnumParticleTypes;
 import net.minecraft.util.math.BlockPos;
 import net.minecraft.world.IBlockAccess;
 import net.minecraft.world.World;
 import net.minecraftforge.common.EnumPlantType;
 import net.minecraftforge.common.IPlantable;
+import net.minecraftforge.fml.relauncher.Side;
+import net.minecraftforge.fml.relauncher.SideOnly;
 
 import javax.annotation.Nonnull;
 import java.util.Random;
 
 public class BlockElementalSoil extends BlockBase {
-  private final @Nonnull Item itemBlock;
+  private final @Nonnull
+  Item itemBlock;
   private final EnumElementalSoilType soilType;
 
   public BlockElementalSoil(@Nonnull Material mat, @Nonnull SoundType type, @Nonnull String name, @Nonnull EnumElementalSoilType soilType) {
@@ -45,9 +50,9 @@ public class BlockElementalSoil extends BlockBase {
         return true;
     }
     return plant == CustomPlantType.ELEMENT_FIRE && soilType == EnumElementalSoilType.FIRE
-            || plant == CustomPlantType.ELEMENT_AIR && soilType == EnumElementalSoilType.AIR
-            || plant == CustomPlantType.ELEMENT_EARTH && soilType == EnumElementalSoilType.EARTH
-            || plant == CustomPlantType.ELEMENT_WATER && soilType == EnumElementalSoilType.WATER;
+        || plant == CustomPlantType.ELEMENT_AIR && soilType == EnumElementalSoilType.AIR
+        || plant == CustomPlantType.ELEMENT_EARTH && soilType == EnumElementalSoilType.EARTH
+        || plant == CustomPlantType.ELEMENT_WATER && soilType == EnumElementalSoilType.WATER;
   }
 
   @Override
@@ -66,50 +71,57 @@ public class BlockElementalSoil extends BlockBase {
     }
   }
 
+  @SideOnly(Side.CLIENT)
   @Override
   public void randomDisplayTick(IBlockState stateIn, World worldIn, BlockPos pos, Random rand) {
-      int[] color = ElementalSoilTransformFX.getColor(soilType.ordinal());
+    if (soilType == EnumElementalSoilType.FIRE && rand.nextInt(5) == 0) {
+      double d8 = pos.getX() + (double) rand.nextFloat();
+      double d4 = pos.getY() + stateIn.getBoundingBox(worldIn, pos).maxY - 0.2f;
+      double d6 = pos.getZ() + (double) rand.nextFloat();
+      worldIn.spawnParticle(EnumParticleTypes.LAVA, d8, d4, d6, 0.0D, 0.0D, 0.0D);
+    }
 
-      if (soilType == EnumElementalSoilType.FIRE && worldIn.getTotalWorldTime() % 7 == 0)
-      {
-        ParticleUtil.spawnParticleFiery(worldIn, pos.getX() + 0.5F, pos.getY() + 1.5F, pos.getZ() + 0.5F,
-                0, -0.025F, 0,
-                color[0], color[1], color[2], 0.75F, 7, 100);
+    if (soilType == EnumElementalSoilType.WATER) {
+      for (int i = 0; i < 2; i++) {
+        double x_offset = Math.min(Math.max(0.05, rand.nextDouble()), 0.95);
+        double y_offset = Math.min(Math.max(0.05, rand.nextDouble()), 0.95);
+
+        double d0 = (double) pos.getX() + x_offset;
+        double d1 = (double) pos.getY() + 1D;
+        double d2 = (double) pos.getZ() + y_offset;
+        worldIn.spawnParticle(EnumParticleTypes.WATER_SPLASH, d0, d1, d2, 0, 1.5D, 0);
       }
+    }
 
-      if (soilType == EnumElementalSoilType.WATER)
-      {
-        ParticleUtil.spawnParticleGlow(worldIn, pos.getX() + rand.nextFloat(), pos.getY() + 1F, pos.getZ() + rand.nextFloat(),
-                0, rand.nextFloat() * 0.05F, 0,
-                color[0], color[1], color[2], 0.75F, 2, 300);
+    if (soilType == EnumElementalSoilType.AIR && rand.nextInt(13) == 0) {
+      for (int i = 0; i < 2; i++) {
+        double x_offset = Math.min(Math.max(0.05, rand.nextDouble()), 0.95);
+        double y_offset = Math.min(Math.max(0.05, rand.nextDouble()), 0.95);
+
+        double d0 = (double) pos.getX() + x_offset;
+        double d1 = (double) pos.getY() + 0.5D;
+        double d2 = (double) pos.getZ() + y_offset;
+
+        worldIn.spawnParticle(EnumParticleTypes.CLOUD, d0, d1, d2, 0.0D, 0.05D, 0.0D);
       }
+    }
 
-      if (soilType == EnumElementalSoilType.AIR)
-      {
-        ParticleUtil.spawnParticleGlow(worldIn, pos.getX() + rand.nextFloat(), pos.getY() + 1F, pos.getZ() + rand.nextFloat(),
-                0, rand.nextFloat() * 0.4F, 0,
-                color[0], color[1], color[2], 0.75F, 2, 80);
+    if (soilType == EnumElementalSoilType.EARTH && rand.nextInt(6) == 0) {
+      for (int i = 0; i < 2 + rand.nextInt(3); i++) {
+        double x_offset = Math.min(Math.max(0.05, rand.nextDouble()), 0.95);
+        double y_offset = Math.min(Math.max(0.05, rand.nextDouble()), 0.95);
+
+        double d0 = (double) pos.getX() + x_offset;
+        double d1 = (double) pos.getY() + 1D;
+        double d2 = (double) pos.getZ() + y_offset;
+        worldIn.spawnParticle(EnumParticleTypes.BLOCK_CRACK, d0, d1, d2, 0, 1D, 0, Block.getStateId(stateIn));
       }
-
-      if (soilType == EnumElementalSoilType.EARTH)
-      {
-        ParticleUtil.spawnParticleFiery(worldIn, pos.getX() + rand.nextFloat(), pos.getY() + 1.1F, pos.getZ() + rand.nextFloat(),
-                0, 0, 0,
-                color[0], color[1], color[2], 1F, 2, 300);
-      }
-
-
-//      ParticleUtil.spawnParticleGlow(worldIn, pos.getX() + rand.nextFloat(), pos.getY() + 0.75F, pos.getZ() + rand.nextFloat(),
-//              0, 0.05F, 0, color[0], color[1], color[2], 0.75F, 2, 80);
+    }
   }
 
   @Override
   public Item getItemBlock() {
     return itemBlock;
-  }
-
-  public EnumElementalSoilType getSoilType() {
-    return soilType;
   }
 }
 
