2177d9678c6bd263c1959c23ee5d21025cb7aa76
Jon McManus <due@wxwhatever.com>
2019-06-15 22:32:57 +1000

    Grove activation advancement, tweak advancement ordering.

---
+++ src/main/java/epicsquid/roots/advancements/ActivatePredicate.java
@@ -0,0 +1,19 @@
+package epicsquid.roots.advancements;
+
+import com.google.gson.JsonElement;
+import epicsquid.mysticallib.advancement.IGenericPredicate;
+import net.minecraft.entity.player.EntityPlayerMP;
+
+import javax.annotation.Nullable;
+
+public class ActivatePredicate implements IGenericPredicate<Void> {
+  @Override
+  public boolean test(EntityPlayerMP player, Void condition) {
+    return true;
+  }
+
+  @Override
+  public IGenericPredicate<Void> deserialize(@Nullable JsonElement element) {
+    return new ActivatePredicate();
+  }
+}
