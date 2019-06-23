d866be080aff5c0eaf568c2d2e69eda78ecb0d93
Jon McManus <due@wxwhatever.com>
2019-06-23 22:52:10 +1000

    CraftTweaker support for Pacifist mobs.

---
+++ src/main/java/epicsquid/roots/integration/crafttweaker/PacifistTweaker.java
@@ -0,0 +1,65 @@
+package epicsquid.roots.integration.crafttweaker;
+
+import com.blamejared.mtlib.utils.BaseAction;
+import crafttweaker.annotations.ZenRegister;
+import crafttweaker.api.entity.IEntityDefinition;
+import crafttweaker.mc1120.CraftTweaker;
+import epicsquid.roots.Roots;
+import epicsquid.roots.init.ModRecipes;
+import net.minecraft.entity.Entity;
+import net.minecraftforge.fml.common.registry.EntityEntry;
+import stanhebben.zenscript.annotations.ZenClass;
+import stanhebben.zenscript.annotations.ZenMethod;
+
+@ZenRegister
+@ZenClass("mods." + Roots.MODID + ".Pacifist")
+public class PacifistTweaker {
+
+  @ZenMethod
+  public static void addEntity(IEntityDefinition entity) {
+    CraftTweaker.LATE_ACTIONS.add(new Add((EntityEntry) entity.getInternal()));
+  }
+
+  @ZenMethod
+  public static void removeEntity(IEntityDefinition entity) {
+    CraftTweaker.LATE_ACTIONS.add(new Remove((EntityEntry) entity.getInternal()));
+  }
+
+  private static class Remove extends BaseAction {
+    private final EntityEntry entry;
+
+    public Remove(EntityEntry entry) {
+      super("remove_pacifist");
+      this.entry = entry;
+    }
+
+    @Override
+    public void apply() {
+      ModRecipes.removePacifistEntry(entry.getName());
+    }
+
+    @Override
+    protected String getRecipeInfo() {
+      return String.format("Recipe to remove %s from Pacifist", entry.getName());
+    }
+  }
+
+  private static class Add extends BaseAction {
+    private final EntityEntry entry;
+
+    public Add(EntityEntry entry) {
+      super("add_pacifist");
+      this.entry = entry;
+    }
+
+    @Override
+    public void apply() {
+      ModRecipes.addPacifistEntry(entry.getName(), entry.getEntityClass());
+    }
+
+    @Override
+    protected String getRecipeInfo() {
+      return String.format("Recipe to add %s to Pacifism", entry.getName());
+    }
+  }
+}
