0f56ce9df9a3974d7c869f1b6084d1bc6200b2e4
Jon McManus <due@wxwhatever.com>
2019-06-28 20:52:03 +1000

    Any leaf is suitable for cloud berry.

--- src/main/java/epicsquid/roots/init/ModRecipes.java
+++ src/main/java/epicsquid/roots/init/ModRecipes.java
@@ -666,7 +666,7 @@ public class ModRecipes {
 
     addCraftingRecipe("cloud_berry",
         new PyreCraftingRecipe(new ItemStack(ModItems.cloud_berry, 3), 1).addIngredients(
-            new ItemStack(ItemBlock.getItemFromBlock(Blocks.LEAVES)),
+            new OreIngredient("treeLeaves"),
             new ItemStack(Item.getItemFromBlock(Blocks.TALLGRASS), 1, 1),
             new OreIngredient("blockWool"),
             new ItemStack(ModItems.terra_moss),
