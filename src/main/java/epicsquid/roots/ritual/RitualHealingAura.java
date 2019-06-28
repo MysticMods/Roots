c1eb33e77ac107361ada94d0c74c4ab633c50f2a
Jon McManus <due@wxwhatever.com>
2019-06-28 15:55:23 +1000

    compileOnly Botania; update the requirements for Healing Aura.

--- src/main/java/epicsquid/roots/ritual/RitualHealingAura.java
+++ src/main/java/epicsquid/roots/ritual/RitualHealingAura.java
@@ -4,6 +4,7 @@ import epicsquid.roots.entity.ritual.EntityRitualBase;
 import epicsquid.roots.entity.ritual.EntityRitualHealingAura;
 import epicsquid.roots.init.ModItems;
 import epicsquid.roots.recipe.conditions.ConditionItems;
+import epicsquid.roots.recipe.conditions.ConditionStandingStones;
 import epicsquid.roots.recipe.conditions.ConditionTrees;
 import net.minecraft.block.BlockPlanks;
 import net.minecraft.init.Blocks;
@@ -23,7 +24,8 @@ public class RitualHealingAura extends RitualBase {
             new ItemStack(ModItems.wildroot), 
             new ItemStack(Blocks.SAPLING, 1, 2)
     ));
-    addCondition(new ConditionTrees(BlockPlanks.EnumType.BIRCH, 4));
+    addCondition(new ConditionStandingStones(3, 1));
+    addCondition(new ConditionTrees(BlockPlanks.EnumType.BIRCH, 1));
     setIcon(ModItems.ritual_healing_aura);
     setColor(TextFormatting.GOLD);
   }
