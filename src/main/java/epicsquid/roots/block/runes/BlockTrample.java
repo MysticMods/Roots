14f312a688038eb672b8d049afb9b281f33a80f0
Jon McManus <due@wxwhatever.com>
2019-07-01 10:41:50 +1000

    Fix #123: convert sure-footed stone to a ticking tile entity.
    Yes, this was the only solution to retain the originally described
    functionality.

--- src/main/java/epicsquid/roots/block/runes/BlockTrample.java
+++ src/main/java/epicsquid/roots/block/runes/BlockTrample.java
@@ -1,17 +1,18 @@
 package epicsquid.roots.block.runes;
 
-import epicsquid.mysticallib.block.BlockBase;
+import epicsquid.mysticallib.block.BlockTEBase;
 import net.minecraft.block.SoundType;
 import net.minecraft.block.material.Material;
+import net.minecraft.tileentity.TileEntity;
 
 import javax.annotation.Nonnull;
 
-public class BlockTrample extends BlockBase {
+public class BlockTrample extends BlockTEBase {
   public static int SAFE_RANGE_X = 30;
   public static int SAFE_RANGE_Y = 5;
   public static int SAFE_RANGE_Z = 30;
 
-  public BlockTrample(@Nonnull Material mat, @Nonnull SoundType type, float hardness, @Nonnull String name) {
-    super(mat, type, hardness, name);
+  public BlockTrample(@Nonnull Material mat, @Nonnull SoundType type, float hardness, @Nonnull String name, @Nonnull Class<? extends TileEntity> teClass) {
+    super(mat, type, hardness, name, teClass);
   }
 }
