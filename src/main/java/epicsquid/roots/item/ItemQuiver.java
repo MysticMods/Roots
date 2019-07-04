88c271bfa4f6e01bd2d4d67754d064c0517374fb
Jon McManus <due@wxwhatever.com>
2019-07-04 13:01:32 +1000

    Fix #124: living arrows retain their status as living arrows.
    The quiver should also detect living arrows shot by other people when
    you try to pick them up and return those instead. It will always pick up
    living arrows (i.e., never fail to break them).

--- src/main/java/epicsquid/roots/item/ItemQuiver.java
+++ src/main/java/epicsquid/roots/item/ItemQuiver.java
@@ -3,26 +3,31 @@ package epicsquid.roots.item;
 import epicsquid.mysticallib.item.ItemArrowBase;
 import epicsquid.mysticallib.util.Util;
 import epicsquid.roots.Roots;
+import epicsquid.roots.entity.item.EntityLivingArrow;
 import epicsquid.roots.gui.GuiHandler;
 import epicsquid.roots.handler.QuiverHandler;
 import epicsquid.roots.init.ModItems;
 import epicsquid.roots.util.QuiverInventoryUtil;
-import net.minecraft.entity.Entity;
 import net.minecraft.entity.EntityLivingBase;
 import net.minecraft.entity.player.EntityPlayer;
 import net.minecraft.entity.projectile.EntityArrow;
 import net.minecraft.entity.projectile.EntityTippedArrow;
+import net.minecraft.init.Items;
 import net.minecraft.item.EnumRarity;
 import net.minecraft.item.ItemArrow;
 import net.minecraft.item.ItemStack;
 import net.minecraft.util.ActionResult;
 import net.minecraft.util.EnumActionResult;
+import net.minecraft.util.EnumFacing;
 import net.minecraft.util.EnumHand;
 import net.minecraft.util.math.AxisAlignedBB;
 import net.minecraft.util.text.Style;
 import net.minecraft.util.text.TextComponentTranslation;
 import net.minecraft.util.text.TextFormatting;
 import net.minecraft.world.World;
+import net.minecraftforge.items.CapabilityItemHandler;
+import net.minecraftforge.items.IItemHandler;
+import net.minecraftforge.items.ItemHandlerHelper;
 
 import javax.annotation.Nonnull;
 import java.util.List;
@@ -40,7 +45,7 @@ public class ItemQuiver extends ItemArrowBase {
   public EntityArrow createArrow(World worldIn, ItemStack stack, EntityLivingBase shooter) {
     ItemStack livingArrow = findLivingArrow(stack);
     if (!livingArrow.isEmpty()) {
-      return ((ItemArrowBase) livingArrow.getItem()).createArrow(worldIn, livingArrow, shooter);
+      return ((ItemLivingArrow) livingArrow.getItem()).createArrow(worldIn, livingArrow, shooter);
     }
 
     ItemStack normalArrow = findOtherArrow(stack);
@@ -61,12 +66,12 @@ public class ItemQuiver extends ItemArrowBase {
     return true;
   }
 
-  public static ItemStack findLivingArrow (ItemStack quiver) {
+  public static ItemStack findLivingArrow(ItemStack quiver) {
     QuiverHandler handler = QuiverHandler.getHandler(quiver);
     ItemStack result = ItemStack.EMPTY;
     for (int i = 0; i < handler.getInventory().getSlots(); i++) {
       ItemStack stack = handler.getInventory().getStackInSlot(i);
-      if (stack.isEmpty() || !(stack.getItem() instanceof ItemArrow)) continue;
+      if (stack.isEmpty()) continue;
       if (stack.getItem() != ModItems.living_arrow) continue;
       result = handler.getInventory().extractItem(i, 1, false);
       break;
@@ -75,7 +80,7 @@ public class ItemQuiver extends ItemArrowBase {
     return result;
   }
 
-  public static ItemStack findOtherArrow (ItemStack quiver) {
+  public static ItemStack findOtherArrow(ItemStack quiver) {
     QuiverHandler handler = QuiverHandler.getHandler(quiver);
     ItemStack result = ItemStack.EMPTY;
     for (int i = 0; i < handler.getInventory().getSlots(); i++) {
@@ -95,7 +100,7 @@ public class ItemQuiver extends ItemArrowBase {
     return new ActionResult<>(EnumActionResult.SUCCESS, player.getHeldItem(hand));
   }
 
-  public static void tryPickupArrows (EntityPlayer player) {
+  public static void tryPickupArrows(EntityPlayer player) {
     ItemStack quiver = QuiverInventoryUtil.getQuiver(player);
     if (quiver.isEmpty()) return;
 
@@ -111,13 +116,19 @@ public class ItemQuiver extends ItemArrowBase {
     int consumed = 0;
     int generated = 0;
     for (EntityArrow arrow : arrows) {
+      ItemStack stack = new ItemStack(Items.ARROW);
+
+      if (arrow instanceof EntityLivingArrow) {
+        stack = new ItemStack(ModItems.living_arrow);
+      }
+
       arrow.setDead();
       if (arrow.getEntityData().hasKey("generated")) {
         generated++;
         continue;
       }
-      if (Util.rand.nextInt(3) != 0) {
-        if (handler.consume()) {
+      if (Util.rand.nextInt(3) != 0 || stack.getItem() == ModItems.living_arrow) {
+        if (ItemHandlerHelper.insertItemStacked(handler.getInventory(), stack, false).isEmpty()) {
           consumed++;
         }
       }
@@ -125,7 +136,7 @@ public class ItemQuiver extends ItemArrowBase {
 
     if (consumed > 0) {
       player.sendStatusMessage(new TextComponentTranslation("roots.quiver.picked_up_arrow").setStyle(new Style().setColor(TextFormatting.GREEN).setBold(true)), true);
-    } else if (consumed == 0 && generated > 0){
+    } else if (consumed == 0 && generated > 0) {
       player.sendStatusMessage(new TextComponentTranslation("roots.quiver.fragile").setStyle(new Style().setColor(TextFormatting.DARK_GREEN).setBold(true)), true);
     } else {
       player.sendStatusMessage(new TextComponentTranslation("roots.quiver.broke").setStyle(new Style().setColor(TextFormatting.DARK_GREEN).setBold(true)), true);
