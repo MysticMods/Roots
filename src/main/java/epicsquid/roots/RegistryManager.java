9f76fb2e7f25626c5461ccccf31bc01cc3d22800
EpicSquid <epicsquid315@gmail.com>
2019-07-11 09:41:09 +1000

    Added potion effect for freeze rather than the effect effect

--- src/main/java/epicsquid/roots/RegistryManager.java
+++ src/main/java/epicsquid/roots/RegistryManager.java
@@ -5,11 +5,13 @@ import epicsquid.mysticallib.event.RegisterContentEvent;
 import epicsquid.mysticallib.event.RegisterModRecipesEvent;
 import epicsquid.mysticallib.event.RegisterParticleEvent;
 import epicsquid.mysticallib.event.RegisterWorldGenEvent;
+import epicsquid.roots.effect.PotionFreeze;
 import epicsquid.roots.init.*;
 import epicsquid.roots.item.ItemStaff;
 import epicsquid.roots.network.PacketHandler;
 import net.minecraft.client.Minecraft;
 import net.minecraft.item.Item;
+import net.minecraft.potion.Potion;
 import net.minecraftforge.event.RegistryEvent;
 import net.minecraftforge.fml.common.eventhandler.EventPriority;
 import net.minecraftforge.fml.common.eventhandler.SubscribeEvent;
@@ -20,51 +22,58 @@ import javax.annotation.Nonnull;
 
 public class RegistryManager {
 
-  @SubscribeEvent
-  public void init(@Nonnull RegisterContentEvent event) {
-    LibRegistry.setActiveMod(Roots.MODID, Roots.CONTAINER);
+	public static Potion freeze;
 
-    ModBlocks.registerBlocks(event);
-    ModSounds.initSounds(event);
-    ModItems.registerItems(event);
+	@SubscribeEvent
+	public void init(@Nonnull RegisterContentEvent event) {
+		LibRegistry.setActiveMod(Roots.MODID, Roots.CONTAINER);
 
-    ModEntities.registerMobs();
-    ModEntities.registerMobSpawn();
-    PacketHandler.registerMessages();
-  }
+		ModBlocks.registerBlocks(event);
+		ModSounds.initSounds(event);
+		ModItems.registerItems(event);
 
-  @SubscribeEvent
-  public void initRecipes(@Nonnull RegisterModRecipesEvent event) {
-    LibRegistry.setActiveMod(Roots.MODID, Roots.CONTAINER);
+		ModEntities.registerMobs();
+		ModEntities.registerMobSpawn();
+		PacketHandler.registerMessages();
+	}
 
-    ModItems.registerOredict();
-    ModRecipes.initRecipes(event);
-  }
+	@SubscribeEvent
+	public void initRecipes(@Nonnull RegisterModRecipesEvent event) {
+		LibRegistry.setActiveMod(Roots.MODID, Roots.CONTAINER);
 
-  @SubscribeEvent
-  public void worldGenInit(RegisterWorldGenEvent event) {
-    LibRegistry.setActiveMod(Roots.MODID, Roots.CONTAINER);
+		ModItems.registerOredict();
+		ModRecipes.initRecipes(event);
+	}
+
+	@SubscribeEvent
+	public void worldGenInit(RegisterWorldGenEvent event) {
+		LibRegistry.setActiveMod(Roots.MODID, Roots.CONTAINER);
 
     /*GameRegistry.registerWorldGenerator(new WorldGenWildlandGrove(), 102);
     GameRegistry.registerWorldGenerator(new WorldGenNaturalGrove(), 103);*/
-  }
+	}
 
-  @SideOnly(Side.CLIENT)
-  @SubscribeEvent
-  public void onRegisterCustomModels(@Nonnull RegisterParticleEvent event) {
-    ModParticles.init();
-  }
+	@SideOnly(Side.CLIENT)
+	@SubscribeEvent
+	public void onRegisterCustomModels(@Nonnull RegisterParticleEvent event) {
+		ModParticles.init();
+	}
 
-  @SubscribeEvent(priority = EventPriority.LOWEST)
-  public void registerOredict(@Nonnull RegistryEvent.Register<Item> event) {
-    LibRegistry.setActiveMod(Roots.MODID, Roots.CONTAINER);
+	@SubscribeEvent(priority = EventPriority.LOWEST)
+	public void registerOredict(@Nonnull RegistryEvent.Register<Item> event) {
+		LibRegistry.setActiveMod(Roots.MODID, Roots.CONTAINER);
 
-    ModItems.registerOredict();
-  }
+		ModItems.registerOredict();
+	}
 
-  @SideOnly(Side.CLIENT)
-  public static void registerColorHandlers() {
-    Minecraft.getMinecraft().getItemColors().registerItemColorHandler(new ItemStaff.StaffColorHandler(), ModItems.staff);
-  }
+	@SideOnly(Side.CLIENT)
+	public static void registerColorHandlers() {
+		Minecraft.getMinecraft().getItemColors().registerItemColorHandler(new ItemStaff.StaffColorHandler(), ModItems.staff);
+	}
+
+	@SubscribeEvent
+	public void registerPotions(RegistryEvent.Register<Potion> event) {
+		event.getRegistry().register(freeze = new PotionFreeze(0xFFFFFF).setRegistryName("freeze"));
+	}
 
 }
---
+++ src/main/java/epicsquid/roots/effect/PotionFreeze.java
@@ -0,0 +1,72 @@
+package epicsquid.roots.effect;
+
+import epicsquid.mysticallib.network.PacketHandler;
+import epicsquid.roots.Roots;
+import epicsquid.roots.network.fx.MessageFrostTouchFX;
+import net.minecraft.block.state.IBlockState;
+import net.minecraft.client.Minecraft;
+import net.minecraft.entity.EntityLivingBase;
+import net.minecraft.init.Blocks;
+import net.minecraft.potion.Potion;
+import net.minecraft.potion.PotionEffect;
+import net.minecraft.util.ResourceLocation;
+import net.minecraft.util.math.BlockPos;
+import net.minecraft.world.World;
+import net.minecraftforge.fml.relauncher.Side;
+import net.minecraftforge.fml.relauncher.SideOnly;
+
+import javax.annotation.Nonnull;
+
+public class PotionFreeze extends Potion {
+
+	private ResourceLocation texture = new ResourceLocation(Roots.MODID, "textures/gui/potion/freeze");
+
+	public PotionFreeze(int liquidColorIn) {
+		super(false, liquidColorIn);
+		setPotionName("Freeze");
+		setIconIndex(0, 0);
+	}
+
+	@Override
+	public boolean isReady(int duration, int amplifier) {
+		return true;
+	}
+
+	@Override
+	public void performEffect(@Nonnull EntityLivingBase entity, int amplifier) {
+		World world = entity.getEntityWorld();
+		BlockPos posDown = entity.getPosition().down();
+
+		for (int i = -1; i <= 1; i++) {
+			for (int j = -1; j <= 1; j++) {
+				BlockPos pos = posDown.add(i, 0, j);
+				IBlockState state = world.getBlockState(pos);
+				if (!world.isRemote) {
+					if (state.getBlock() == Blocks.FIRE) {
+						world.setBlockState(pos, Blocks.AIR.getDefaultState());
+					} else if (state.getBlock() == Blocks.LAVA) {
+						world.setBlockState(pos, Blocks.OBSIDIAN.getDefaultState());
+					} else if (state.getBlock() == Blocks.WATER) {
+						world.setBlockState(pos, Blocks.ICE.getDefaultState());
+					}
+				}
+			}
+		}
+
+		if (entity.ticksExisted % 4 == 0) {
+			PacketHandler.sendToAllTracking(new MessageFrostTouchFX(entity.posX, entity.posY + entity.getEyeHeight(), entity.posZ), entity);
+		}
+	}
+
+	@Override
+	public boolean shouldRender(PotionEffect effect) {
+		return true;
+	}
+
+	@SideOnly(Side.CLIENT)
+	@Override
+	public int getStatusIconIndex() {
+		Minecraft.getMinecraft().getTextureManager().bindTexture(texture);
+		return super.getStatusIconIndex();
+	}
+}
