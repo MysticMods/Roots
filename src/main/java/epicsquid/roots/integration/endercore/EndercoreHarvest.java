f9c4bd7ab313e66d0849f95be1216b21e4deb543
Jon McManus <due@wxwhatever.com>
2019-07-11 22:16:16 +1000

    Move aubergine integration to Mystic World.

--- src/main/java/epicsquid/roots/integration/endercore/EndercoreHarvest.java
+++ src/main/java/epicsquid/roots/integration/endercore/EndercoreHarvest.java
@@ -11,8 +11,8 @@ import java.util.List;
 
 public class EndercoreHarvest {
   public static void init() {
-    List<BlockCropBase> crops = Arrays.asList(ModBlocks.moonglow, epicsquid.mysticalworld.init.ModBlocks.aubergine, ModBlocks.pereskia, ModBlocks.wildroot, ModBlocks.spirit_herb, ModBlocks.wildewheet, ModBlocks.cloud_berry, ModBlocks.infernal_bulb, ModBlocks.dewgonia, ModBlocks.stalicripe);
-    List<Item> seeds = Arrays.asList(ModItems.moonglow_seed, epicsquid.mysticalworld.init.ModItems.aubergine_seed, ModItems.pereskia_bulb, ModItems.wildroot, ModItems.spirit_herb_seed, ModItems.wildewheet_seed, ModItems.cloud_berry, ModItems.infernal_bulb, ModItems.dewgonia, ModItems.stalicripe);
+    List<BlockCropBase> crops = Arrays.asList(ModBlocks.moonglow, ModBlocks.pereskia, ModBlocks.wildroot, ModBlocks.spirit_herb, ModBlocks.wildewheet, ModBlocks.cloud_berry, ModBlocks.infernal_bulb, ModBlocks.dewgonia, ModBlocks.stalicripe);
+    List<Item> seeds = Arrays.asList(ModItems.moonglow_seed, ModItems.pereskia_bulb, ModItems.wildroot, ModItems.spirit_herb_seed, ModItems.wildewheet_seed, ModItems.cloud_berry, ModItems.infernal_bulb, ModItems.dewgonia, ModItems.stalicripe);
 
     assert crops.size() == seeds.size();
 
