0409abb3079dd8f28bcfa3ee091f370df07a2e04
Jon McManus <due@wxwhatever.com>
2019-08-04 22:11:43 +1000

    Remove standing stone condition for Summon Creatures ritual. (fix #157)
    It only summons one creature and needs to be redesigned anyway.

--- src/main/java/epicsquid/roots/ritual/wild/RitualSummonCreatures.java
+++ src/main/java/epicsquid/roots/ritual/wild/RitualSummonCreatures.java
@@ -46,7 +46,6 @@ public class RitualSummonCreatures extends RitualBase {
             new ItemStack(ModItems.moonglow_leaf)
     ));
 //    addCondition(new ConditionGroveFaith(GroveType.WILD, 0));
-    addCondition(new ConditionStandingStones(3, 3));
 
     // TODO: CraftTweaker support
     entityItem.put(EntityChicken.class, Lists.newArrayList(new ItemStack(Items.CHICKEN)));
