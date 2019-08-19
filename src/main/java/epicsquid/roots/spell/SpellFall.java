881c77969a70725a17d99adab89d92e05fa69e30
Davoleo <davoleo2001@gmail.com>
2019-08-19 16:15:58 +0200

    Fall Spell Works, yay
    - I have a really cool idea for its FX :thinking:

--- src/main/java/epicsquid/roots/spell/SpellFall.java
+++ src/main/java/epicsquid/roots/spell/SpellFall.java
@@ -5,12 +5,15 @@ import epicsquid.mysticalworld.init.ModBlocks;
 import epicsquid.roots.init.HerbRegistry;
 import epicsquid.roots.init.ModItems;
 import epicsquid.roots.spell.modules.SpellModule;
-import jdk.nashorn.internal.ir.BlockStatement;
 import net.minecraft.block.Block;
+import net.minecraft.block.BlockGrass;
+import net.minecraft.block.BlockLeaves;
+import net.minecraft.block.BlockTallGrass;
 import net.minecraft.block.state.IBlockState;
 import net.minecraft.entity.player.EntityPlayer;
-import net.minecraft.init.Blocks;
+import net.minecraft.init.SoundEvents;
 import net.minecraft.item.ItemStack;
+import net.minecraft.util.SoundCategory;
 import net.minecraft.util.math.BlockPos;
 import net.minecraft.util.text.TextFormatting;
 import net.minecraft.world.World;
@@ -30,14 +33,16 @@ public class SpellFall extends SpellBase {
     public static String spellName = "spell_fall";
     public static SpellFall instance = new SpellFall(spellName);
 
+    private int count;
+
     public SpellFall(String name) {
-        super(name, TextFormatting.GOLD, 227, 179, 66, 209, 113, 10);
+        super(name, TextFormatting.GOLD, 227/255F, 179/255F, 66/255F, 209/255F, 113/255F, 10/255F);
 
         this.castType = EnumCastType.CONTINUOUS;
         this.cooldown = 120;
 
-        addCost(HerbRegistry.getHerbByName("stalicripe"), 0.5F);
-        addCost(HerbRegistry.getHerbByName("wildewheet"), 0.25F);
+        addCost(HerbRegistry.getHerbByName("stalicripe"), 0.05F);
+        addCost(HerbRegistry.getHerbByName("wildewheet"), 0.025F);
 
         addIngredients(
                 new ItemStack(ModItems.stalicripe),
@@ -49,14 +54,30 @@ public class SpellFall extends SpellBase {
 
     @Override
     public boolean cast(EntityPlayer caster, List<SpellModule> modules) {
-        caster.world.setBlockState(caster.getPosition().up(2), Blocks.GRASS.getDefaultState());
-
+        //caster.world.setBlockState(caster.getPosition().up(2), Blocks.GRASS.getDefaultState());
+        count++;
         boolean hadEffect = false;
-        List<BlockPos> blocks =  Util.getBlocksWithinRadius(caster.world, caster.getPosition(), 10, 10, 10, blockPos -> isAffectedByFallSpell(caster.world, blockPos) );
+        List<BlockPos> blocks = Util.getBlocksWithinRadius(caster.world, caster.getPosition(), 10, 10, 10, blockPos -> isAffectedByFallSpell(caster.world, blockPos));
 
-        for (BlockPos pos : blocks)
+        BlockPos pos;
+        if (blocks.size() > 1)
+            pos = blocks.get(Util.rand.nextInt(blocks.size() - 1));
+        else if (!blocks.isEmpty())
+            pos = blocks.get(0);
+        else
+            return false;
+
+        IBlockState blockstate = caster.world.getBlockState(pos);
+        Block block = blockstate.getBlock();
+
+        if (count % 3 == 0)
         {
-
+            if (block instanceof BlockLeaves || block instanceof BlockGrass) {
+                block.dropBlockAsItemWithChance(caster.world, pos, blockstate, 0.75F, 0);
+                caster.world.setBlockToAir(pos);
+                caster.world.playSound(caster, pos, SoundEvents.BLOCK_GRASS_BREAK, SoundCategory.BLOCKS, 1F, 1F);
+                return true;
+            }
         }
         return false;
     }
@@ -66,15 +87,6 @@ public class SpellFall extends SpellBase {
         IBlockState state = world.getBlockState(pos);
         Block block = state.getBlock();
 
-        if (block == Blocks.GRASS || block == Blocks.TALLGRASS)
-            return true;
-        else if (block == Blocks.LEAVES || block == Blocks.LEAVES2)
-            return true;
-        else if (block == Blocks.DIRT)
-        {
-            // TODO: 17/08/2019 Complete This condition
-            return true;
-        }
-        else return false;
+        return block instanceof BlockLeaves || block instanceof BlockGrass || block instanceof BlockTallGrass;
     }
 }
