af8793657120b6d06829471057f0160bed7ccd1f
Jon McManus <due@wxwhatever.com>
2019-09-04 20:16:50 +1000

    Remove the time constraints on Divine Protection, they make no sense.
    "Oh hey here's this ritual to make night tick faster... TOO BAD YOU CAN
    ONLY START IT DURING THE DAY LOL SUCKS TO BE YOU."

--- src/main/java/epicsquid/roots/ritual/RitualDivineProtection.java
+++ src/main/java/epicsquid/roots/ritual/RitualDivineProtection.java
@@ -21,7 +21,6 @@ public class RitualDivineProtection extends RitualBase {
         new ItemStack(ModItems.bark_oak),
         new ItemStack(Items.GLOWSTONE_DUST)
     ));
-    addCondition(new ConditionWorldTime(0, 13000));
     setIcon(ModItems.ritual_divine_protection);
     setColor(TextFormatting.YELLOW);
     setBold(true);
