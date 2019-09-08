4016ecff6e020f2beeaf346d523104471e030359
Jon McManus <due@wxwhatever.com>
2019-09-08 11:21:03 +1000

    Finish the Terra Moss overhaul.
    Currently it still uses a configuration file rather than CraftTweaker,
    but as it is a core feature of the mod it feels strange not to be able
    to adjust it outside of CraftTweaker.
    
    I'll probably change my mind.

---
+++ src/main/java/epicsquid/roots/config/MossConfig.java
@@ -0,0 +1,86 @@
+package epicsquid.roots.config;
+
+import com.google.common.collect.BiMap;
+import com.google.common.collect.HashBiMap;
+import epicsquid.mysticallib.util.ConfigUtil;
+import epicsquid.roots.Roots;
+import net.minecraft.block.Block;
+import net.minecraft.block.state.IBlockState;
+import net.minecraft.item.ItemBlock;
+import net.minecraft.item.ItemStack;
+import net.minecraftforge.common.config.Config;
+
+import java.util.HashMap;
+import java.util.Map;
+
+@Config.LangKey("config.roots.category.moss")
+@Config(modid = Roots.MODID, name = "roots/moss")
+@SuppressWarnings("unused")
+public class MossConfig {
+  @Config.Comment(("List of mod:item:meta,mod:item:meta (meta optional) of mossy blocks and what to convert them into when scraping with knives [note that logs or blocks with positional data are unsuited for this purpose]"))
+  public static String[] MossyCobblestones = new String[]{"minecraft:mossy_cobblestone,minecraft:cobblestone", "minecraft:stonebrick:1,minecraft:stonebrick", "minecraft:monster_egg:3,minecraft:monster_egg:2"};
+
+  @Config.Ignore
+  private static Map<ItemStack, ItemStack> mossyCobblestones = null;
+
+  @Config.Ignore
+  private static BiMap<Block, Block> mossyBlocks = HashBiMap.create();
+
+  @Config.Ignore
+  private static BiMap<IBlockState, IBlockState> mossyStates = HashBiMap.create();
+
+  @SuppressWarnings("deprecation")
+  public static Map<ItemStack, ItemStack> getMossyCobblestones() {
+    if (mossyCobblestones == null) {
+      mossyCobblestones = ConfigUtil.parseMap(new HashMap<>(), ConfigUtil::parseItemStack, ConfigUtil::parseItemStack, ",", MossyCobblestones);
+
+      mossyBlocks.clear();
+      mossyStates.clear();
+      for (Map.Entry<ItemStack, ItemStack> entry : mossyCobblestones.entrySet()) {
+        ItemStack in = entry.getKey();
+        ItemStack out = entry.getValue();
+        if (in.getItem() instanceof ItemBlock && out.getItem() instanceof ItemBlock) {
+          Block blockIn = ((ItemBlock) in.getItem()).getBlock();
+          Block blockOut = ((ItemBlock) out.getItem()).getBlock();
+          if (in.getMetadata() != 0 || out.getMetadata() != 0) {
+            IBlockState stateIn = blockIn.getStateFromMeta(in.getMetadata());
+            IBlockState stateOut = blockOut.getStateFromMeta(out.getMetadata());
+            mossyStates.put(stateIn, stateOut);
+          } else {
+            mossyBlocks.put(blockIn, blockOut);
+          }
+        }
+      }
+    }
+
+    return mossyCobblestones;
+  }
+
+  public static IBlockState scrapeResult (IBlockState state) {
+    Map<ItemStack, ItemStack> mossy = getMossyCobblestones();
+
+    if (mossyStates.containsKey(state)) {
+      return mossyStates.get(state);
+    }
+
+    if (mossyBlocks.containsKey(state.getBlock())) {
+      return mossyBlocks.get(state.getBlock()).getDefaultState();
+    }
+
+    return null;
+  }
+
+  public static IBlockState mossConversion (IBlockState state) {
+    Map<ItemStack, ItemStack> mossy = getMossyCobblestones();
+
+    if (mossyStates.inverse().containsKey(state)) {
+      return mossyStates.inverse().get(state);
+    }
+
+    if (mossyBlocks.inverse().containsKey(state.getBlock())) {
+      return mossyBlocks.inverse().get(state.getBlock()).getDefaultState();
+    }
+
+    return null;
+  }
+}
