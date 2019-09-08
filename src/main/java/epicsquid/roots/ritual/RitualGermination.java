216c4c43179e21c907bb99c37043c2c07acdb06f
Jon McManus <due@wxwhatever.com>
2019-09-08 17:43:00 +1000

    Reduce the bone meal cost on Germination.

--- src/main/java/epicsquid/roots/ritual/RitualGermination.java
+++ src/main/java/epicsquid/roots/ritual/RitualGermination.java
@@ -10,6 +10,7 @@ import net.minecraft.item.ItemStack;
 import net.minecraft.util.math.BlockPos;
 import net.minecraft.util.text.TextFormatting;
 import net.minecraft.world.World;
+import net.minecraftforge.oredict.OreIngredient;
 
 public class RitualGermination extends RitualBase {
 
@@ -20,7 +21,7 @@ public class RitualGermination extends RitualBase {
         new ItemStack(ModItems.wildroot),
         new ItemStack(epicsquid.mysticalworld.init.ModItems.aubergine),
         new ItemStack(Items.DYE, 1, 15),
-        new ItemStack(Items.DYE, 1, 15)
+        new OreIngredient("rootsBark")
     ));
     addCondition(new ConditionStandingStones(3, 2));
     setIcon(ModItems.ritual_germination);
