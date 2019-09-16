ffc7960938a878191f46011cd8de6518aebefb89
Jon McManus <due@wxwhatever.com>
2019-09-16 17:52:34 +1000

    Remove unused constants.

--- src/main/java/epicsquid/roots/util/Constants.java
+++ src/main/java/epicsquid/roots/util/Constants.java
@@ -3,19 +3,9 @@ package epicsquid.roots.util;
 import epicsquid.roots.Roots;
 
 public class Constants {
-  public static String TIME_STOP_TAG = Roots.MODID + ":time_stop";
-  public static String GEAS_TAG = Roots.MODID + ":geas";
   public static String LIGHT_DRIFTER_TAG = Roots.MODID + ":light_drifter";
   public static String LIGHT_DRIFTER_X = Roots.MODID + ":light_drifter_x";
   public static String LIGHT_DRIFTER_Y = Roots.MODID + ":light_drifter_y";
   public static String LIGHT_DRIFTER_Z = Roots.MODID + ":light_drifter_z";
   public static String LIGHT_DRIFTER_MODE = Roots.MODID + ":light_drifter_mode";
-  public static String INVULNERABILITY_RITUAL_TAG = Roots.MODID + ":invulnerability_ritual";
-  public static String INVULNERABILITY_RITUAL_TAG_ID = Roots.MODID + ":invulnerability_ritual_id";
-  public static String TRAP_PAPER_ENTITY_TAG = Roots.MODID + ":trapped_entity";
-  public static String TRAP_PAPER_ENTITY_DATA_TAG = Roots.MODID + ":trapped_entity_data";
-  public static String EFFECT_TAG = Roots.MODID + ":effects";
-  public static String BOOK_OF_THE_DEAD_LIST = Roots.MODID + ":book_dead_list";
-  public static String FOLLOW_EFFECT_TARGET = Roots.MODID + ":follow_target";
-  public static String KNOWLEDGE = Roots.MODID + ":knowledge";
 }
