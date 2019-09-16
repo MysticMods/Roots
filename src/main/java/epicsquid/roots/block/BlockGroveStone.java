f39d6d79264527bb4e2d3b23c7a44d2deb6f9588
Jon McManus <due@wxwhatever.com>
2019-09-16 14:23:28 +1000

    Convert Grove Stone into multipart model
    Hopefully this resolves issues some users have with Optifine.

--- src/main/java/epicsquid/roots/block/BlockGroveStone.java
+++ src/main/java/epicsquid/roots/block/BlockGroveStone.java
@@ -3,6 +3,7 @@ package epicsquid.roots.block;
 import epicsquid.mysticallib.block.BlockTEBase;
 import epicsquid.mysticallib.network.PacketHandler;
 import epicsquid.mysticallib.util.Util;
+import epicsquid.roots.Roots;
 import epicsquid.roots.config.GeneralConfig;
 import epicsquid.roots.network.fx.MessageOvergrowthEffectFX;
 import net.minecraft.block.BlockDoublePlant;
@@ -45,7 +46,7 @@ import java.util.Random;
 @SuppressWarnings("deprecation")
 public class BlockGroveStone extends BlockTEBase {
   public static final PropertyEnum<Half> HALF = PropertyEnum.create("half", Half.class);
-  public static final PropertyDirection FACING = PropertyDirection.create("facing", EnumFacing.Plane.HORIZONTAL);
+  public static final PropertyDirection FACING = PropertyDirection.create("facing", (facing) -> facing == EnumFacing.NORTH || facing == EnumFacing.EAST);
   public static final PropertyBool VALID = PropertyBool.create("valid");
 
   public BlockGroveStone(@Nonnull Material mat, @Nonnull SoundType type, float hardness, @Nonnull String name, @Nonnull Class<? extends TileEntity> teClass) {
@@ -81,12 +82,13 @@ public class BlockGroveStone extends BlockTEBase {
   @Override
   @SuppressWarnings("deprecation")
   public IBlockState getStateFromMeta(int meta) {
-    return getDefaultState().withProperty(VALID, (meta & 1) == 1).withProperty(HALF, Half.fromInt((meta >> 1 & 1))).withProperty(FACING, EnumFacing.byIndex(((meta >> 2) + 2)));
+    return getDefaultState().withProperty(VALID, (meta & 1) == 1).withProperty(HALF, Half.fromInt((meta & 7) >> 1)).withProperty(FACING, (meta >> 3) == 0 ? EnumFacing.NORTH : EnumFacing.EAST);
   }
 
   @Override
   public int getMetaFromState(IBlockState state) {
-    return (state.getValue(FACING).ordinal() - 2) << 2 | state.getValue(HALF).ordinal() << 1 | (state.getValue(VALID) ? 1 : 0);
+    int meta = (((state.getValue(FACING) == EnumFacing.NORTH ? 0 : 1) << 2 ^ state.getValue(HALF).ordinal())) << 1 ^ (state.getValue(VALID) ? 1 : 0);
+    return meta;
   }
 
   @Override
@@ -107,8 +109,8 @@ public class BlockGroveStone extends BlockTEBase {
   @Override
   public void onBlockPlacedBy(World worldIn, BlockPos pos, IBlockState state, EntityLivingBase placer, ItemStack stack) {
     super.onBlockPlacedBy(worldIn, pos, state, placer, stack);
-    worldIn.setBlockState(pos.up(), this.getDefaultState().withProperty(HALF, Half.TOP));
-    worldIn.setBlockState(pos.up().up(), this.getDefaultState().withProperty(HALF, Half.TOP));
+    worldIn.setBlockState(pos.up(), this.getDefaultState().withProperty(HALF, Half.MIDDLE).withProperty(FACING, state.getValue(FACING)));
+    worldIn.setBlockState(pos.up().up(), this.getDefaultState().withProperty(HALF, Half.TOP).withProperty(FACING, state.getValue(FACING)));
   }
 
   @Override
@@ -116,8 +118,10 @@ public class BlockGroveStone extends BlockTEBase {
   public IBlockState getStateForPlacement(World worldIn, BlockPos pos, EnumFacing facing, float hitX, float hitY, float hitZ, int meta, EntityLivingBase placer) {
     // Work out the facing
     EnumFacing f = EnumFacing.fromAngle(placer.rotationYaw).getOpposite();
-    if (f == EnumFacing.UP || f == EnumFacing.DOWN) {
+    if (f == EnumFacing.NORTH || f == EnumFacing.SOUTH || f == EnumFacing.DOWN || f == EnumFacing.UP) {
       f = EnumFacing.NORTH;
+    } else {
+      f = EnumFacing.EAST;
     }
     return this.getDefaultState().withProperty(HALF, Half.BOTTOM).withProperty(FACING, f);
   }
@@ -152,6 +156,7 @@ public class BlockGroveStone extends BlockTEBase {
 
   public enum Half implements IStringSerializable {
     TOP,
+    MIDDLE,
     BOTTOM;
 
     @Override
@@ -159,13 +164,20 @@ public class BlockGroveStone extends BlockTEBase {
       switch (this) {
         case TOP:
           return "top";
+        case MIDDLE:
+          return "middle";
         default:
           return "bottom";
       }
     }
 
     public static Half fromInt(int x) {
-      return values()[x];
+      for (Half half : values()) {
+        if (half.ordinal() == x) {
+          return half;
+        }
+      }
+      return BOTTOM;
     }
   }
 
@@ -232,7 +244,7 @@ public class BlockGroveStone extends BlockTEBase {
   public void randomDisplayTick(IBlockState stateIn, World worldIn, BlockPos pos, Random rand) {
     super.randomDisplayTick(stateIn, worldIn, pos, rand);
 
-    if (stateIn.getValue(VALID)) {
+    if (stateIn.getValue(VALID) && stateIn.getValue(HALF) == Half.MIDDLE) {
       for (int i = -2; i <= 2; ++i) {
         for (int j = -2; j <= 2; ++j) {
           if (i > -2 && i < 2 && j == -1) {
@@ -243,7 +255,7 @@ public class BlockGroveStone extends BlockTEBase {
               if (!worldIn.isAirBlock(pos.add(i / 2, 0, j / 2))) {
                 break;
               }
-              ClientProxy.particleRenderer.spawnParticle(worldIn, Util.getLowercaseClassName(ParticleLeafArc.class), (double) pos.getX() + 0.5D, (double) pos.getY() + 1.0D, (double) pos.getZ() + 0.5D, (i + rand.nextDouble() - 0.05) * 0.04, -0.0001, (j + rand.nextFloat() - 0.05) * 0.04,
+              ClientProxy.particleRenderer.spawnParticle(worldIn, Util.getLowercaseClassName(ParticleLeafArc.class), (double) pos.getX() + 0.5D, (double) pos.getY() + 0.5D, (double) pos.getZ() + 0.5D, (i + rand.nextDouble() - 0.05) * 0.04, -0.0001, (j + rand.nextFloat() - 0.05) * 0.04,
                   100, 0.14 + rand.nextDouble() * 0.05, 0.385, 0.117 + rand.nextDouble() * 0.05, 1, rand.nextDouble() + 0.5, rand.nextDouble() * 2);
             }
           }
