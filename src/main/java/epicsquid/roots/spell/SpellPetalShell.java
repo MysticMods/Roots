c22767b468617ef726f923c8c89cece01c892645
Jon McManus <due@wxwhatever.com>
2019-09-26 18:39:47 +1000

    Unify petal shell into the new system.

--- src/main/java/epicsquid/roots/spell/SpellPetalShell.java
+++ src/main/java/epicsquid/roots/spell/SpellPetalShell.java
@@ -1,7 +1,6 @@
 package epicsquid.roots.spell;
 
 import epicsquid.mysticallib.network.PacketHandler;
-import epicsquid.roots.init.HerbRegistry;
 import epicsquid.roots.init.ModItems;
 import epicsquid.roots.init.ModPotions;
 import epicsquid.roots.network.fx.MessagePetalShellBurstFX;
@@ -19,12 +18,14 @@ public class SpellPetalShell extends SpellBase {
   public static Property.PropertyCooldown PROP_COOLDOWN = new Property.PropertyCooldown(120);
   public static Property.PropertyCastType PROP_CAST_TYPE = new Property.PropertyCastType(EnumCastType.INSTANTANEOUS);
   public static Property.PropertyCost PROP_COST_1 = new Property.PropertyCost(0, new SpellCost("spirit_herb", 0.75));
+  public static Property.PropertyDuration PROP_DURATION = new Property.PropertyDuration(60 * 20);
   public static Property<Integer> PROP_MAXIMUM = new Property<>("maximum_shells", 3);
 
   public static String spellName = "spell_petal_shell";
   public static SpellPetalShell instance = new SpellPetalShell(spellName);
 
   private int maxShells;
+  private int duration;
 
   public SpellPetalShell(String name) {
     super(name, TextFormatting.LIGHT_PURPLE, 255f / 255f, 192f / 255f, 240f / 255f, 255f / 255f, 255f / 255f, 255f / 255f);
@@ -32,7 +33,7 @@ public class SpellPetalShell extends SpellBase {
   }
 
   @Override
-  public void init () {
+  public void init() {
     addIngredients(
         new ItemStack(Items.MELON),
         new ItemStack(epicsquid.mysticalworld.init.ModItems.aubergine),
@@ -45,15 +46,12 @@ public class SpellPetalShell extends SpellBase {
   @Override
   public boolean cast(EntityPlayer player, List<SpellModule> modules) {
     PotionEffect shell = player.getActivePotionEffect(ModPotions.petal_shell);
-    int amp = shell == null ? 0 : shell.getAmplifier() + 1;
-    if (amp < 3) {
-      if (!player.world.isRemote) {
-        player.addPotionEffect(new PotionEffect(ModPotions.petal_shell, 60 * 20, amp, false, false));
-        PacketHandler.sendToAllTracking(new MessagePetalShellBurstFX(player.posX, player.posY + 1.0f, player.posZ), player);
-      }
-      return true;
+    int amp = shell == null ? 0 : shell.getAmplifier() == maxShells ? maxShells : shell.getAmplifier() + 1;
+    if (!player.world.isRemote) {
+      player.addPotionEffect(new PotionEffect(ModPotions.petal_shell, duration, amp, false, false));
+      PacketHandler.sendToAllTracking(new MessagePetalShellBurstFX(player.posX, player.posY + 1.0f, player.posZ), player);
     }
-    return false;
+    return true;
   }
 
   @Override
@@ -61,5 +59,6 @@ public class SpellPetalShell extends SpellBase {
     this.castType = properties.getProperty(PROP_CAST_TYPE);
     this.cooldown = properties.getProperty(PROP_COOLDOWN);
     this.maxShells = properties.getProperty(PROP_MAXIMUM);
+    this.duration = properties.getProperty(PROP_DURATION);
   }
 }
