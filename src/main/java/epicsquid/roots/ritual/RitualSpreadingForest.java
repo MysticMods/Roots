1e31bc9749358e27a3574794ceb97bff0802b70b
Jon McManus <due@wxwhatever.com>
2019-09-26 19:42:17 +1000

    Spreading Forest.

--- src/main/java/epicsquid/roots/ritual/RitualSpreadingForest.java
+++ src/main/java/epicsquid/roots/ritual/RitualSpreadingForest.java
@@ -4,6 +4,7 @@ import epicsquid.roots.entity.ritual.EntityRitualBase;
 import epicsquid.roots.entity.ritual.EntityRitualSpreadingForest;
 import epicsquid.roots.init.ModItems;
 import epicsquid.roots.recipe.conditions.ConditionItems;
+import epicsquid.roots.util.types.Property;
 import net.minecraft.init.Blocks;
 import net.minecraft.item.ItemStack;
 import net.minecraft.util.math.BlockPos;
@@ -12,8 +13,19 @@ import net.minecraft.world.World;
 import net.minecraftforge.oredict.OreIngredient;
 
 public class RitualSpreadingForest extends RitualBase {
-  public RitualSpreadingForest(String name, int duration, boolean disabled) {
+  public static Property.PropertyDuration PROP_DURATION = new Property.PropertyDuration(2400);
+  public static Property<Integer> PROP_RADIUS_X = new Property<>("radius_x", 35);
+  public static Property<Integer> PROP_RADIUS_Y = new Property<>("radius_y", 30);
+  public static Property<Integer> PROP_RADIUS_Z = new Property<>("radius_z", 35);
+  public static Property<Integer> PROP_PLACE_INTERVAL = new Property<>("place_interval", 30);
+  public static Property<Integer> PROP_GROWTH_INTERVAL = new Property<>("growth_interval", 20);
+  public static Property<Integer> PROP_DOUBLE_CHANCE = new Property<>("double_chance", 20);
+
+  public int radius_x, radius_y, radius_z, place_interval, growth_interval, double_chance;
+
+  public RitualSpreadingForest(String name, boolean disabled) {
     super(name, disabled);
+    properties.addProperties(PROP_DURATION, PROP_RADIUS_X, PROP_RADIUS_Y, PROP_RADIUS_Z, PROP_PLACE_INTERVAL, PROP_GROWTH_INTERVAL, PROP_DOUBLE_CHANCE);
   }
 
   @Override
@@ -32,7 +44,13 @@ public class RitualSpreadingForest extends RitualBase {
 
   @Override
   public void finalise() {
-
+    int[] radius = properties.getRadius();
+    radius_x = radius[0];
+    radius_y = radius[1];
+    radius_z = radius[2];
+    place_interval = properties.getProperty(PROP_PLACE_INTERVAL);
+    growth_interval = properties.getProperty(PROP_GROWTH_INTERVAL);
+    double_chance = properties.getProperty(PROP_DOUBLE_CHANCE);
   }
 
   @Override
