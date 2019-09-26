50b72c6e53dce3ae74b2d5fbf52a82d14c76c449
Jon McManus <due@wxwhatever.com>
2019-09-26 19:16:04 +1000

    Germination ritual converted!

--- src/main/java/epicsquid/roots/ritual/RitualRegistry.java
+++ src/main/java/epicsquid/roots/ritual/RitualRegistry.java
@@ -46,7 +46,7 @@ public class RitualRegistry {
     addRitual(ritual_flower_growth = new RitualFlowerGrowth("ritual_flower_growth", RitualConfig.disableRitualCategory.disableFlowerGrowth));
     addRitual(ritual_frost_lands = new RitualFrostLands("ritual_frost_lands", RitualConfig.disableRitualCategory.disableFrostLands));
     addRitual(ritual_gathering = new RitualGathering("ritual_gathering", RitualConfig.disableRitualCategory.disableGathering));
-    addRitual(ritual_germination = new RitualGermination("ritual_germination", 6400, RitualConfig.disableRitualCategory.disableNaturalAura));
+    addRitual(ritual_germination = new RitualGermination("ritual_germination", RitualConfig.disableRitualCategory.disableNaturalAura));
     addRitual(ritual_heavy_storms = new RitualHeavyStorms("ritual_heavy_storms", 2400, RitualConfig.disableRitualCategory.disableHeavyStorms));
     addRitual(ritual_life = new RitualHealingAura("ritual_healing_aura", 800, RitualConfig.disableRitualCategory.disableHealingAura));
     addRitual(ritual_overgrowth = new RitualOvergrowth("ritual_overgrowth", 3000, RitualConfig.disableRitualCategory.disableOvergrowth));
