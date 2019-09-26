4e17bee7c5269a5fce1a5ad3364f291c17ff982e
Jon McManus <due@wxwhatever.com>
2019-09-27 09:40:58 +1000

    Warding Protection!

--- src/main/java/epicsquid/roots/ritual/RitualRegistry.java
+++ src/main/java/epicsquid/roots/ritual/RitualRegistry.java
@@ -55,7 +55,7 @@ public class RitualRegistry {
     // TODO:
     addRitual(ritual_summon_creatures = new RitualSummonCreatures("ritual_summon_creatures", 0, RitualConfig.disableRitualCategory.disableSummonCreatures));
     addRitual(ritual_transmutation = new RitualTransmutation("ritual_transmutation", RitualConfig.disableRitualCategory.disableTransmutation));
-    addRitual(ritual_warding_protection = new RitualWardingProtection("ritual_warding_protection", 1200, RitualConfig.disableRitualCategory.disableWardingProtection));
+    addRitual(ritual_warding_protection = new RitualWardingProtection("ritual_warding_protection", RitualConfig.disableRitualCategory.disableWardingProtection));
     addRitual(ritual_wild_growth = new RitualWildGrowth("ritual_wild_growth", 300, RitualConfig.disableRitualCategory.disableWildGrowth));
     addRitual(ritual_windwall = new RitualWindwall("ritual_windwall", 3000, RitualConfig.disableRitualCategory.disableWindwall));
   }
