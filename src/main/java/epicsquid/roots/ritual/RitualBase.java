6caa9336f487503a3e52c64c38cbb9202c16db60
Jon McManus <due@wxwhatever.com>
2019-09-29 13:12:11 +1000

    Mark some Nullables and Deprecated.

--- src/main/java/epicsquid/roots/ritual/RitualBase.java
+++ src/main/java/epicsquid/roots/ritual/RitualBase.java
@@ -93,7 +93,7 @@ public abstract class RitualBase {
     return false;
   }
 
-  public boolean canFire(TileEntityBonfire tileEntityBonfire, EntityPlayer player) {
+  public boolean canFire(TileEntityBonfire tileEntityBonfire, @Nullable EntityPlayer player) {
     IBlockState state = tileEntityBonfire.getWorld().getBlockState(tileEntityBonfire.getPos());
     if (state.getValue(BlockBonfire.BURNING)) {
       return false;
