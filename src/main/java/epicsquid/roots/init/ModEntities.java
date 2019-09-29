652074aa1eb90fa08fd800a13e74575ef9eb9b35
Jon McManus <due@wxwhatever.com>
2019-09-29 13:02:38 +1000

    More succinctly register entities.

--- src/main/java/epicsquid/roots/init/ModEntities.java
+++ src/main/java/epicsquid/roots/init/ModEntities.java
@@ -12,8 +12,11 @@ import epicsquid.roots.entity.spell.EntityFireJet;
 import epicsquid.roots.entity.spell.EntityThornTrap;
 import epicsquid.roots.entity.spell.EntityTimeStop;
 import epicsquid.roots.proxy.ClientProxy;
+import net.minecraft.entity.Entity;
 import net.minecraft.world.storage.loot.LootTableList;
 
+import java.util.Arrays;
+import java.util.List;
 import java.util.stream.Stream;
 
 public class ModEntities {
@@ -31,45 +34,47 @@ public class ModEntities {
    * LibRegistry.registerEntityRenderer(Entity.class, new RenderEntity.Factory());
    */
   public static void registerMobs() {
-    //Spell & Ritual Entities
+    // Helper entities
     LibRegistry.registerEntity(EntityFireJet.class);
     LibRegistry.registerEntity(EntityThornTrap.class);
     LibRegistry.registerEntity(EntityTimeStop.class);
     LibRegistry.registerEntity(EntityBoost.class);
-
-    LibRegistry.registerEntity(EntityRitualHealingAura.class);
-    LibRegistry.registerEntity(EntityRitualHeavyStorms.class);
-    LibRegistry.registerEntity(EntityRitualDivineProtection.class);
-    LibRegistry.registerEntity(EntityRitualFireStorm.class);
     LibRegistry.registerEntity(EntityFlare.class);
-    LibRegistry.registerEntity(EntityRitualSpreadingForest.class);
-    LibRegistry.registerEntity(EntityRitualWindwall.class);
-    LibRegistry.registerEntity(EntityRitualWardingProtection.class);
-    LibRegistry.registerEntity(EntityRitualOvergrowth.class);
-    LibRegistry.registerEntity(EntityRitualFrostLands.class);
-    LibRegistry.registerEntity(EntityRitualFlowerGrowth.class);
 
+    // Actual entities
     LibRegistry.registerEntity(EntityFairy.class, 0xf542e3, 0xdb7fa1);
+
+    // Ritual entities
+    List<Class<? extends Entity>> ritualClasses = Arrays.asList(
+        EntityRitualAnimalHarvest.class,
+        EntityRitualDivineProtection.class,
+        EntityRitualFireStorm.class,
+        EntityRitualFlowerGrowth.class,
+        EntityRitualFrostLands.class,
+        EntityRitualGathering.class,
+        EntityRitualGermination.class,
+        EntityRitualHealingAura.class,
+        EntityRitualHeavyStorms.class,
+        EntityRitualOvergrowth.class,
+        EntityRitualPurity.class,
+        EntityRitualSpreadingForest.class,
+        EntityRitualTransmutation.class,
+        EntityRitualWardingProtection.class,
+        EntityRitualWildGrowth.class,
+        EntityRitualWindwall.class
+    );
+
+    ritualClasses.forEach(LibRegistry::registerEntity);
 
     if (Roots.proxy instanceof ClientProxy) {
       LibRegistry.registerEntityRenderer(EntityFireJet.class, new RenderNull.Factory());
       LibRegistry.registerEntityRenderer(EntityThornTrap.class, new RenderNull.Factory());
       LibRegistry.registerEntityRenderer(EntityTimeStop.class, new RenderNull.Factory());
       LibRegistry.registerEntityRenderer(EntityBoost.class, new RenderNull.Factory());
-
-      LibRegistry.registerEntityRenderer(EntityRitualHealingAura.class, new RenderNull.Factory());
-      LibRegistry.registerEntityRenderer(EntityRitualHeavyStorms.class, new RenderNull.Factory());
-      LibRegistry.registerEntityRenderer(EntityRitualDivineProtection.class, new RenderNull.Factory());
-      LibRegistry.registerEntityRenderer(EntityRitualFireStorm.class, new RenderNull.Factory());
       LibRegistry.registerEntityRenderer(EntityFlare.class, new RenderNull.Factory());
-      LibRegistry.registerEntityRenderer(EntityRitualSpreadingForest.class, new RenderNull.Factory());
-      LibRegistry.registerEntityRenderer(EntityRitualWindwall.class, new RenderNull.Factory());
-      LibRegistry.registerEntityRenderer(EntityRitualWardingProtection.class, new RenderNull.Factory());
-      LibRegistry.registerEntityRenderer(EntityRitualOvergrowth.class, new RenderNull.Factory());
-      LibRegistry.registerEntityRenderer(EntityRitualFrostLands.class, new RenderNull.Factory());
-      LibRegistry.registerEntityRenderer(EntityRitualFlowerGrowth.class, new RenderNull.Factory());
-
       LibRegistry.registerEntityRenderer(EntityFairy.class, new RenderFairy.Factory());
+
+      ritualClasses.forEach(c -> LibRegistry.registerEntityRenderer(c, new RenderNull.Factory()));
     }
   }
 
