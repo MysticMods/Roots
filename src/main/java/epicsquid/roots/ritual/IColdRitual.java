45c31e1b9a820aa8e84c660694579ddac6d9b555
Jon McManus <due@wxwhatever.com>
2019-09-29 12:31:55 +1000

    Generify the handling of Frost Lands into IColdRitual.
    Now Rituals & Ritual Entities that implement this will get the same
    "cold" effect without having it hard-coded.

---
+++ src/main/java/epicsquid/roots/ritual/IColdRitual.java
@@ -0,0 +1,7 @@
+package epicsquid.roots.ritual;
+
+// A simple interface to denote that the ritual is "cold"
+// and should produce neither light nor heat, while also
+// burning a blue fire.
+public interface IColdRitual {
+}
