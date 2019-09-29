4076ed716185d09f67103fa4d2c35bce36fb311f
Jon McManus <due@wxwhatever.com>
2019-09-29 12:47:04 +1000

    Fix #260: gathering places into the first inventory found within 3x3 of the pyre.
    Conflicts:
    	src/main/java/epicsquid/roots/entity/ritual/EntityRitualGathering.java

--- src/main/java/epicsquid/roots/entity/ritual/EntityRitualGathering.java
+++ src/main/java/epicsquid/roots/entity/ritual/EntityRitualGathering.java
@@ -14,6 +14,7 @@ import net.minecraft.network.datasync.DataSerializers;
 import net.minecraft.network.datasync.EntityDataManager;
 import net.minecraft.tileentity.TileEntity;
 import net.minecraft.util.EnumFacing;
+import net.minecraft.util.math.AxisAlignedBB;
 import net.minecraft.util.math.BlockPos;
 import net.minecraft.world.World;
 import net.minecraftforge.items.CapabilityItemHandler;
@@ -24,9 +25,10 @@ import java.util.Random;
 import java.util.stream.Collectors;
 
 public class EntityRitualGathering extends EntityRitualBase {
-
   protected static Random random = new Random();
   protected static final DataParameter<Integer> lifetime = EntityDataManager.createKey(EntityRitualGathering.class, DataSerializers.VARINT);
+  public static AxisAlignedBB bounding = new AxisAlignedBB(-1, -1, -1, 1, 1, 1);
+
 
   public EntityRitualGathering(World worldIn) {
     super(worldIn);
@@ -44,12 +46,25 @@ public class EntityRitualGathering extends EntityRitualBase {
     }
     if (!world.isRemote) {
       if (this.ticksExisted % 80 == 0) {
-        TileEntity te = world.getTileEntity(getPosition().down()); // Hope that it's in the right position these days
-        IItemHandler cap = te.getCapability(CapabilityItemHandler.ITEM_HANDLER_CAPABILITY, null);
-        if (cap != null) {
-          List<BlockPos> transferredItems = Magnetize.store(world, getPosition(), cap, 15, 15, 15);
-          if (!transferredItems.isEmpty()) {
-            PacketHandler.sendToAllTracking(new MessageItemGatheredFX(transferredItems), this);
+        AxisAlignedBB bounds = bounding.offset(getPosition());
+        BlockPos start = new BlockPos(bounds.minX, bounds.minY, bounds.minZ);
+        BlockPos stop = new BlockPos(bounds.maxX, bounds.maxY, bounds.maxZ);
+        TileEntity te = null;
+        boolean found = false;
+        for (BlockPos.MutableBlockPos pos : BlockPos.getAllInBoxMutable(start, stop)) {
+          te = world.getTileEntity(pos);
+          if (te != null && te.hasCapability(CapabilityItemHandler.ITEM_HANDLER_CAPABILITY, null)) {
+            found = true;
+            break;
+          }
+        }
+        if (found && te != null) {
+          IItemHandler cap = te.getCapability(CapabilityItemHandler.ITEM_HANDLER_CAPABILITY, null);
+          if (cap != null) {
+            List<BlockPos> transferredItems = Magnetize.store(world, getPosition(), cap, 15, 15, 15);
+            if (!transferredItems.isEmpty()) {
+              PacketHandler.sendToAllTracking(new MessageItemGatheredFX(transferredItems), this);
+            }
           }
         }
       }
