9533f409e7c54a5eb56b482b60813d88a5675ee3
Jon McManus <due@wxwhatever.com>
2019-10-05 14:13:34 +1000

    Specify in the tooltip what the cooldown is.

--- src/main/java/epicsquid/roots/integration/jei/shears/RunicShearsEntityCategory.java
+++ src/main/java/epicsquid/roots/integration/jei/shears/RunicShearsEntityCategory.java
@@ -2,16 +2,19 @@ package epicsquid.roots.integration.jei.shears;
 
 import epicsquid.roots.Roots;
 import epicsquid.roots.integration.jei.JEIRootsPlugin;
-import epicsquid.roots.recipe.RunicShearRecipe;
 import mezz.jei.api.IGuiHelper;
 import mezz.jei.api.gui.IDrawable;
 import mezz.jei.api.gui.IGuiItemStackGroup;
 import mezz.jei.api.gui.IRecipeLayout;
+import mezz.jei.api.gui.ITooltipCallback;
 import mezz.jei.api.ingredients.IIngredients;
 import mezz.jei.api.ingredients.VanillaTypes;
 import mezz.jei.api.recipe.IRecipeCategory;
 import net.minecraft.client.resources.I18n;
+import net.minecraft.item.ItemStack;
 import net.minecraft.util.ResourceLocation;
+
+import java.util.List;
 
 public class RunicShearsEntityCategory implements IRecipeCategory<RunicShearsEntityWrapper> {
 
@@ -44,6 +47,10 @@ public class RunicShearsEntityCategory implements IRecipeCategory<RunicShearsEnt
   @Override
   public void setRecipe(IRecipeLayout recipeLayout, RunicShearsEntityWrapper recipeWrapper, IIngredients ingredients) {
     IGuiItemStackGroup group = recipeLayout.getItemStacks();
+    group.addTooltipCallback((slotIndex, input, ingredient, tooltip) -> {
+      tooltip.add("");
+      tooltip.add(I18n.format("jei.roots.runic_shears.cooldown", recipeWrapper.getCooldown()));
+    });
     group.init(0, true, 104, 32);
     group.set(0, ingredients.getOutputs(VanillaTypes.ITEM).get(0));
   }
