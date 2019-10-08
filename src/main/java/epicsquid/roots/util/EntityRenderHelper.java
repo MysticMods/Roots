7ead5570e7658f8b40c49d3552a0f27e3a30bdff
Jon McManus <due@wxwhatever.com>
2019-10-09 09:49:38 +1000

    Fix #329: turns out that RenderHelper isn't GuiScreenInventory.drawEntityOnScreen...
    Whoops, my bad.

---
+++ src/main/java/epicsquid/roots/util/EntityRenderHelper.java
@@ -0,0 +1,53 @@
+package epicsquid.roots.util;
+
+import net.minecraft.client.Minecraft;
+import net.minecraft.client.renderer.GlStateManager;
+import net.minecraft.client.renderer.OpenGlHelper;
+import net.minecraft.client.renderer.RenderHelper;
+import net.minecraft.client.renderer.entity.RenderManager;
+import net.minecraft.entity.EntityLivingBase;
+
+public class EntityRenderHelper {
+  public static void drawEntityOnScreen(int posX, int posY, float scale, float mouseX, float mouseY, EntityLivingBase ent) {
+    if (ent.world == null) {
+      ent.world = Minecraft.getMinecraft().world;
+    }
+
+    GlStateManager.enableColorMaterial();
+    GlStateManager.pushMatrix();
+    GlStateManager.translate((float) posX, (float) posY, 50.0F);
+    GlStateManager.scale(-scale, scale, scale);
+    GlStateManager.rotate(180.0F, 0.0F, 0.0F, 1.0F);
+    float f = ent.renderYawOffset;
+    float f1 = ent.rotationYaw;
+    float f2 = ent.rotationPitch;
+    float f3 = ent.prevRotationYawHead;
+    float f4 = ent.rotationYawHead;
+    GlStateManager.rotate(135.0F, 0.0F, 1.0F, 0.0F);
+    RenderHelper.enableStandardItemLighting();
+    GlStateManager.rotate(-135.0F, 0.0F, 1.0F, 0.0F);
+    GlStateManager.rotate(-((float) Math.atan((double) (mouseY / 40.0F))) * 20.0F, 1.0F, 0.0F, 0.0F);
+    ent.renderYawOffset = (float) Math.atan((double) (mouseX / 40.0F)) * 20.0F;
+    ent.rotationYaw = (float) Math.atan((double) (mouseX / 40.0F)) * 40.0F;
+    ent.rotationPitch = -((float) Math.atan((double) (mouseY / 40.0F))) * 20.0F;
+    ent.rotationYawHead = ent.rotationYaw;
+    ent.prevRotationYawHead = ent.rotationYaw;
+    GlStateManager.translate(0.0F, 0.0F, 0.0F);
+    RenderManager rendermanager = Minecraft.getMinecraft().getRenderManager();
+    rendermanager.setPlayerViewY(180.0F);
+    rendermanager.setRenderShadow(false);
+    rendermanager.renderEntity(ent, 0.0D, 0.0D, 0.0D, 0.0F, 1.0F, false);
+    rendermanager.setRenderShadow(true);
+    ent.renderYawOffset = f;
+    ent.rotationYaw = f1;
+    ent.rotationPitch = f2;
+    ent.prevRotationYawHead = f3;
+    ent.rotationYawHead = f4;
+    GlStateManager.popMatrix();
+    RenderHelper.disableStandardItemLighting();
+    GlStateManager.disableRescaleNormal();
+    GlStateManager.setActiveTexture(OpenGlHelper.lightmapTexUnit);
+    GlStateManager.disableTexture2D();
+    GlStateManager.setActiveTexture(OpenGlHelper.defaultTexUnit);
+  }
+}
