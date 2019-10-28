07c2a18bb1f05b1ab2e74b439b1c26e312647ecb
Jon McManus <due@wxwhatever.com>
2019-10-28 21:21:11 +1000

    Fix the thing that I thought was broken but wasn't broken.

--- src/main/java/epicsquid/roots/init/ModRecipes.java
+++ src/main/java/epicsquid/roots/init/ModRecipes.java
@@ -1061,7 +1061,7 @@ public class ModRecipes {
             new ItemStack(ModItems.dewgonia)));
 
     addCraftingRecipe("elemental_soil",
-        new FeyCraftingRecipe(new ItemStack(ModBlocks.elemental_soil), 4).addIngredients(
+        new FeyCraftingRecipe(new ItemStack(ModBlocks.elemental_soil, 4), 4).addIngredients(
             new ItemStack(Blocks.DIRT),
             new ItemStack(ModItems.terra_moss),
             new ItemStack(ModItems.wildroot),
