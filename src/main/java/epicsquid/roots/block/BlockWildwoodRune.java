6dafd349c46520ee2b8cb8b5a5db16510d6f1b45
Jon McManus <due@wxwhatever.com>
2019-12-24 14:09:50 +1000

    Fix #249: remove wildwood rune functionality.
    They now serve as a purely decorative block.
    
    Conflicts:
    	src/main/java/epicsquid/roots/block/BlockWildwoodRune.java
    	src/main/java/epicsquid/roots/block/runes/BlockWildwoodRune.java
    	src/main/java/epicsquid/roots/proxy/CommonProxy.java
    	src/main/java/epicsquid/roots/rune/RuneRegistry.java
    	src/main/java/epicsquid/roots/tileentity/TileEntityWildrootRune.java

---
+++ src/main/java/epicsquid/roots/block/BlockWildwoodRune.java
@@ -0,0 +1,24 @@
+package epicsquid.roots.block;
+
+import epicsquid.mysticallib.block.BlockBase;
+import epicsquid.roots.init.ModBlocks;
+import net.minecraft.block.SoundType;
+import net.minecraft.block.material.Material;
+import net.minecraft.block.state.IBlockState;
+import net.minecraft.item.Item;
+
+import javax.annotation.Nonnull;
+import java.util.Random;
+
+public class BlockWildwoodRune extends BlockBase {
+
+  public BlockWildwoodRune(@Nonnull Material mat, @Nonnull SoundType type, float hardness, @Nonnull String name) {
+    super(mat, type, hardness, name);
+  }
+
+  @Override
+  public Item getItemDropped(IBlockState state, Random rand, int fortune) {
+    return Item.getItemFromBlock(ModBlocks.wildwood_log);
+  }
+}
+
