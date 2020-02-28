9fe9752c8894c9bdb1e454c99aa83227d230e26e
Jon McManus <due@wxwhatever.com>
2020-02-28 22:39:06 +1000

    Fix #327.

--- src/main/java/epicsquid/roots/advancements/Advancements.java
+++ src/main/java/epicsquid/roots/advancements/Advancements.java
@@ -2,6 +2,7 @@ package epicsquid.roots.advancements;
 
 import epicsquid.mysticallib.advancement.GenericTrigger;
 import epicsquid.roots.Roots;
+import epicsquid.roots.recipe.FeyCraftingRecipe;
 import net.minecraft.advancements.CriteriaTriggers;
 import net.minecraft.util.ResourceLocation;
 import net.minecraftforge.event.entity.living.LivingDeathEvent;
@@ -9,11 +10,14 @@ import net.minecraftforge.event.entity.living.LivingDeathEvent;
 public class Advancements {
   public static final ResourceLocation PACIFIST_ID = new ResourceLocation(Roots.MODID, "pacifist");
   public static final ResourceLocation ACTIVATE_ID = new ResourceLocation(Roots.MODID, "grove_activate");
+  public static final ResourceLocation CRAFTING_ID = new ResourceLocation(Roots.MODID, "fey_crafting");
+  public static GenericTrigger<FeyCraftingRecipe> CRAFTING_TRIGGER;
   public static GenericTrigger<LivingDeathEvent> PACIFIST_TRIGGER;
   public static GenericTrigger<Void> GROVE_TRIGGER;
 
   public static void init() {
     PACIFIST_TRIGGER = CriteriaTriggers.register(new GenericTrigger<>(PACIFIST_ID, new KillPredicate()));
     GROVE_TRIGGER = CriteriaTriggers.register(new GenericTrigger<>(ACTIVATE_ID, new ActivatePredicate()));
+    CRAFTING_TRIGGER = CriteriaTriggers.register(new GenericTrigger<>(CRAFTING_ID, new CraftingPredicate()));
   }
 }
---
+++ src/main/java/epicsquid/roots/advancements/CraftingPredicate.java
@@ -0,0 +1,46 @@
+package epicsquid.roots.advancements;
+
+import com.google.gson.JsonElement;
+import com.google.gson.JsonObject;
+import com.google.gson.JsonSyntaxException;
+import epicsquid.mysticallib.advancement.IGenericPredicate;
+import epicsquid.roots.init.ModRecipes;
+import epicsquid.roots.recipe.FeyCraftingRecipe;
+import net.minecraft.entity.player.EntityPlayerMP;
+import net.minecraft.util.JsonUtils;
+import net.minecraft.util.ResourceLocation;
+
+import javax.annotation.Nullable;
+
+public class CraftingPredicate implements IGenericPredicate<FeyCraftingRecipe> {
+  private FeyCraftingRecipe recipe;
+
+  public CraftingPredicate() {
+  }
+
+  public CraftingPredicate(FeyCraftingRecipe recipe) {
+    this.recipe = recipe;
+  }
+
+  @Override
+  public boolean test(EntityPlayerMP player, FeyCraftingRecipe condition) {
+    return recipe.equals(condition);
+  }
+
+  @Override
+  public IGenericPredicate<FeyCraftingRecipe> deserialize(@Nullable JsonElement element) {
+    if (element != null) {
+      JsonObject object = element.getAsJsonObject();
+      if (JsonUtils.isString(object, "recipe")) {
+        ResourceLocation rl = new ResourceLocation(JsonUtils.getString(object, "recipe"));
+        FeyCraftingRecipe recipe = ModRecipes.getFeyCraftingRecipe(rl);
+        if (recipe != null) {
+          return new CraftingPredicate(recipe);
+        } else {
+          throw new JsonSyntaxException("Unknown recipe name: '" + rl + "'");
+        }
+      }
+    }
+    return new CraftingPredicate(null);
+  }
+}
