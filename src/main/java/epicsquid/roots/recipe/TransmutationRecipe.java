a96fa13a189304bddc74fe557964d6a4bd714bdd
Jon McManus <due@wxwhatever.com>
2020-03-05 08:58:40 +1000

    "New transmutation" recipe.

--- src/main/java/epicsquid/roots/recipe/TransmutationRecipe.java
+++ src/main/java/epicsquid/roots/recipe/TransmutationRecipe.java
@@ -1,107 +1,173 @@
 package epicsquid.roots.recipe;
 
-import epicsquid.roots.util.StateUtil;
+import com.google.common.collect.Sets;
+import epicsquid.mysticallib.util.NullableSupplier;
 import epicsquid.roots.util.types.RegistryItem;
-import epicsquid.roots.util.types.WorldPosStatePredicate;
 import net.minecraft.block.Block;
+import net.minecraft.block.properties.IProperty;
 import net.minecraft.block.state.IBlockState;
 import net.minecraft.item.ItemStack;
-import net.minecraft.util.ResourceLocation;
 import net.minecraft.util.math.BlockPos;
 import net.minecraft.world.World;
 
+import javax.annotation.Nullable;
+import java.util.Collections;
+import java.util.List;
+import java.util.Set;
+
+@SuppressWarnings("unchecked")
 public class TransmutationRecipe extends RegistryItem {
-  private IBlockState startState;
-  private Block startBlock;
-  private IBlockState endState;
-  private ItemStack endStack;
-  private WorldPosStatePredicate condition = (a, b, c) -> true;
+  private BlockStatePredicate start;
+  private NullableSupplier<ItemStack> stack;
+  private NullableSupplier<IBlockState> state;
+  private WorldBlockStatePredicate condition;
 
-  public TransmutationRecipe(ResourceLocation name, Block startBlock, ItemStack endState, WorldPosStatePredicate condition) {
-    this.setRegistryName(name);
-    this.startBlock = startBlock;
-    this.endStack = endState;
-    this.endState = null;
-    if (condition != null)
-      this.condition = condition;
-  }
-
-  public TransmutationRecipe(ResourceLocation name, Block startBlock, IBlockState endState, WorldPosStatePredicate condition) {
-    this.setRegistryName(name);
-    this.startBlock = startBlock;
-    this.endState = endState;
-    if (condition != null) {
-      this.condition = condition;
-    }
-  }
-
-  public TransmutationRecipe(ResourceLocation name, IBlockState startState, IBlockState endState, WorldPosStatePredicate condition) {
-    this.setRegistryName(name);
-    this.startBlock = null;
-    this.startState = startState;
-    this.endState = endState;
-    if (condition != null) {
-      this.condition = condition;
-    }
-  }
-
-  public TransmutationRecipe(ResourceLocation name, IBlockState startState, ItemStack endState, WorldPosStatePredicate condition) {
-    this.setRegistryName(name);
-    this.startBlock = null;
-    this.startState = startState;
-    this.endStack = endState;
-    this.endState = null;
-    if (condition != null) {
-      this.condition = condition;
-    }
-  }
-
-  public boolean itemOutput() {
-    return this.endStack != null;
-  }
-
-  public Block getStartBlock() {
-    return startBlock;
-  }
-
-  public IBlockState getStartState() {
-    if (startState == null && startBlock != null) return startBlock.getDefaultState();
-    return startState;
-  }
-
-  public ItemStack getEndStack() {
-    return endStack;
-  }
-
-  public IBlockState getEndState() {
-    return endState;
-  }
-
-  public WorldPosStatePredicate getCondition() {
-    return condition;
-  }
-
-  public boolean matches(Block block) {
-    return matches(block.getDefaultState());
-  }
-
-  public boolean matches(IBlockState start) {
-    if (this.startState != null) {
-      return StateUtil.compareStates(this.startState, start);
+  public TransmutationRecipe(BlockStatePredicate start, @Nullable NullableSupplier<ItemStack> stack, @Nullable NullableSupplier<IBlockState> state, @Nullable WorldBlockStatePredicate condition) {
+    this.start = start;
+    if (stack == null) {
+      this.stack = NullableSupplier.nullable(ItemStack.class);
     } else {
-      return this.startBlock == start.getBlock();
+      this.stack = stack;
+    }
+    if (state == null) {
+      this.state = NullableSupplier.nullable(IBlockState.class);
+    } else {
+      this.state = state;
+    }
+    if (this.stack.isNull() && this.state.isNull()) {
+      throw new IllegalStateException("Can't have both state and stack output as null");
+    }
+    if (condition == null) {
+      this.condition = WorldBlockStatePredicate.TRUE;
+    } else {
+      this.condition = condition;
     }
   }
 
-  public boolean matches(World world, BlockPos pos, IBlockState state) {
-    return (this.startBlock != null && this.startBlock == state.getBlock() || this.startState != null && StateUtil.compareStates(this.startState, state)) && (this.getCondition() == null || this.getCondition().test(world, pos, state));
+  @Nullable
+  public IBlockState getEndState() {
+    return state.get();
+  }
+
+  @Nullable
+  public ItemStack getEndStack() {
+    return stack.get();
+  }
+
+  public boolean isItem() {
+    return !stack.isNull();
+  }
+
+  public boolean isState() {
+    return !state.isNull();
+  }
+
+  public boolean matches(IBlockState state, World world, BlockPos pos) {
+    return start.test(state) && condition.test(state, world, pos);
   }
 
   public boolean matches(World world, BlockPos pos) {
-    return matches(world, pos, world.getBlockState(pos));
+    return matches(world.getBlockState(pos), world, pos);
   }
 
-  public String getKey() {
-    return "roots.ritual.transmutation." + getRegistryName().getPath();
+  public interface MatchingStates {
+    default List<IBlockState> matchingStates() {
+      return Collections.emptyList();
+    }
+  }
+
+  @FunctionalInterface
+  private interface BlockStatePredicate extends MatchingStates {
+    BlockStatePredicate TRUE = (o) -> true;
+
+    boolean test(IBlockState state);
+  }
+
+  public class StatePredicate implements BlockStatePredicate {
+    protected IBlockState state;
+
+    public StatePredicate(IBlockState state) {
+      this.state = state;
+    }
+
+    @Override
+    public List<IBlockState> matchingStates() {
+      return Collections.singletonList(state);
+    }
+
+    @Override
+    public boolean test(IBlockState state) {
+      return state.getBlock() == this.state.getBlock();
+    }
+  }
+
+  public class PropertyPredicate extends StatePredicate {
+    protected List<IProperty<?>> props;
+
+    public PropertyPredicate(IBlockState state, IProperty<?> prop) {
+      super(state);
+      this.props = Collections.singletonList(prop);
+    }
+
+    public PropertyPredicate(IBlockState state, List<IProperty<?>> props) {
+      super(state);
+      this.props = props;
+    }
+
+    @Override
+    public boolean test(IBlockState state) {
+      return super.test(state) && props.stream().allMatch(prop -> state.getValue(prop) == this.state.getValue(prop));
+    }
+  }
+
+  public class BlocksPredicate implements BlockStatePredicate {
+    private Set<Block> blocks;
+
+    public BlocksPredicate(Block... blocks) {
+      this.blocks = Sets.newHashSet(blocks);
+    }
+
+    @Override
+    public boolean test(IBlockState state) {
+      return blocks.contains(state.getBlock());
+    }
+  }
+
+  @FunctionalInterface
+  public interface WorldBlockStatePredicate extends MatchingStates {
+    WorldBlockStatePredicate TRUE = (a, b, c) -> true;
+
+    boolean test(IBlockState state, World world, BlockPos pos);
+  }
+
+  @SuppressWarnings("deprecation")
+  public class BlockStateBelow implements WorldBlockStatePredicate {
+    protected BlockStatePredicate state;
+
+    public BlockStateBelow(BlockStatePredicate state) {
+      this.state = state;
+    }
+
+    @Override
+    public boolean test(IBlockState state, World world, BlockPos pos) {
+      return this.state.test(world.getBlockState(pos.down()));
+    }
+
+    @Override
+    public List<IBlockState> matchingStates() {
+      return this.state.matchingStates();
+    }
+  }
+
+  public class BlockStateAbove extends BlockStateBelow {
+    public BlockStateAbove(BlockStatePredicate state) {
+      super(state);
+    }
+
+    @Override
+    public boolean test(IBlockState state, World world, BlockPos pos) {
+      return this.state.test(world.getBlockState(pos.up()));
+    }
   }
 }
+
