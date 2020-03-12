a4f1bfaf3ea8d896ecc2b3b5e0cf9096236bbe0e
Jon McManus <due@wxwhatever.com>
2020-03-12 13:45:00 +1000

    Tweaks some docs. Better runed textures.

--- src/main/java/epicsquid/roots/client/PatchouliHack.java
+++ src/main/java/epicsquid/roots/client/PatchouliHack.java
@@ -6,8 +6,8 @@ import epicsquid.roots.ritual.RitualBase;
 import epicsquid.roots.ritual.RitualRegistry;
 import epicsquid.roots.spell.SpellBase;
 import epicsquid.roots.spell.SpellRegistry;
-import epicsquid.roots.util.types.Property;
 import epicsquid.roots.util.types.PropertyTable;
+import net.minecraft.client.Minecraft;
 import net.minecraft.client.resources.I18n;
 import net.minecraft.item.ItemStack;
 import net.minecraft.util.ResourceLocation;
@@ -17,11 +17,14 @@ import net.minecraftforge.fml.common.ObfuscationReflectionHelper;
 import net.minecraftforge.fml.relauncher.Side;
 import net.minecraftforge.fml.relauncher.SideOnly;
 import vazkii.patchouli.client.book.BookCategory;
+import vazkii.patchouli.client.book.BookEntry;
 import vazkii.patchouli.client.book.gui.GuiBook;
 import vazkii.patchouli.client.book.gui.GuiBookCategory;
+import vazkii.patchouli.client.book.gui.GuiBookEntry;
 import vazkii.patchouli.client.book.text.BookTextParser;
 import vazkii.patchouli.common.book.Book;
 
+import java.util.LinkedList;
 import java.util.Map;
 import java.util.function.Function;
 
@@ -30,6 +33,7 @@ public class PatchouliHack {
   public static void init() {
     Map<String, BookTextParser.CommandProcessor> COMMANDS = ObfuscationReflectionHelper.getPrivateValue(BookTextParser.class, null, "COMMANDS");
     Map<String, BookTextParser.FunctionProcessor> FUNCTIONS = ObfuscationReflectionHelper.getPrivateValue(BookTextParser.class, null, "FUNCTIONS");
+    int spaceWidth = Minecraft.getMinecraft().fontRenderer.getStringWidth(" ");
 
     BookTextParser.FunctionProcessor categoryFunction = (parameter, state) -> {
       state.prevColor = state.color;
@@ -83,47 +87,102 @@ public class PatchouliHack {
     COMMANDS.put("/uses", reset);
 
     Function<String, BookTextParser.FunctionProcessor> prop = (type) ->
-      (parameter, state) -> {
-        if (parameter.contains("/")) {
-          String[] parts = parameter.split("/");
-          RitualBase ritual = RitualRegistry.getRitual(parts[0]);
-          SpellBase spell = SpellRegistry.getSpell(parts[0]);
-          PropertyTable props;
-          String propName = parts[1];
-          Object value = null;
-          if (type.equals("ritual") && ritual != null) {
-            props = ritual.getProperties();
-            if (props.hasProperty(propName)) {
-              value = props.getValue(propName);
-            } else {
-              return "INVALID PROPERTY: " + propName;
+        (parameter, state) -> {
+          if (parameter.contains("/")) {
+            String[] parts = parameter.split("/");
+            RitualBase ritual = RitualRegistry.getRitual(parts[0]);
+            SpellBase spell = SpellRegistry.getSpell(parts[0]);
+            PropertyTable props;
+            String propName = parts[1];
+            Object value = null;
+            if (type.equals("ritual") && ritual != null) {
+              props = ritual.getProperties();
+              if (props.hasProperty(propName)) {
+                value = props.getValue(propName);
+              } else {
+                return "INVALID PROPERTY: " + propName;
+              }
+            } else if (type.equals("ritual")) {
+              return "INVALID RITUAL";
+            } else if (type.equals("spell") && spell != null) {
+              props = spell.getProperties();
+              if (props.hasProperty(propName)) {
+                value = props.getValue(propName);
+              } else {
+                return "INVALID PROPERTY: " + propName;
+              }
+            } else if (type.equals("spell")) {
+              return "INVALID SPELL";
             }
-          } else if (type.equals("ritual")) {
-            return "INVALID RITUAL";
-          } else if (type.equals("spell") && spell != null) {
-            props = spell.getProperties();
-            if (props.hasProperty(propName)) {
-              value = props.getValue(propName);
+            if (value != null) {
+              return value.toString();
             } else {
-              return "INVALID PROPERTY: " + propName;
+              return "INVALID COMMAND";
             }
-          } else if (type.equals("spell")) {
-            return "INVALID SPELL";
-          }
-          if (value != null) {
-            return value.toString();
           } else {
-            return "INVALID COMMAND";
+            return "INVALID " + (type.equals("spell") ? "SPELL" : "RITUAL") + "COMMAND, REQUIRES PROPERTY TOO";
           }
-        } else {
-          return "INVALID " + (type.equals("spell") ? "SPELL" : "RITUAL") + "COMMAND, REQUIRES PROPERTY TOO";
-        }
-    };
+        };
 
     FUNCTIONS.put("spell", prop.apply("spell"));
     COMMANDS.put("/spell", reset);
 
     FUNCTIONS.put("ritual", prop.apply("ritual"));
     COMMANDS.put("/ritual", reset);
+
+/*    BookTextParser.FunctionProcessor li_link = (parameter, state) -> {
+      state.lineBreaks = 1;
+      state.spacingLeft = 4;
+      state.spacingRight = spaceWidth;
+
+      state.cluster = new LinkedList<>();
+
+      state.prevColor = state.color;
+      state.color = state.book.linkColor;
+      boolean isExternal = parameter.matches("^https?\\:.*");
+
+      if (isExternal) {
+        String url = parameter;
+        state.tooltip = I18n.format("patchouli.gui.lexicon.external_link");
+        state.isExternalLink = true;
+        state.onClick = () -> GuiBook.openWebLink(url);
+      } else {
+        int hash = parameter.indexOf('#');
+        String anchor = null;
+        if (hash >= 0) {
+          anchor = parameter.substring(hash + 1);
+          parameter = parameter.substring(0, hash);
+        }
+
+        ResourceLocation href = new ResourceLocation(state.book.getModNamespace(), parameter);
+        BookEntry entry = state.book.contents.entries.get(href);
+        if (entry != null) {
+          state.tooltip = entry.isLocked() ? (TextFormatting.GRAY + I18n.format("patchouli.gui.lexicon.locked")) : entry.getName();
+          GuiBook gui = state.gui;
+          Book book = state.book;
+          int page = 0;
+          if (anchor != null) {
+            int anchorPage = entry.getPageFromAnchor(anchor);
+            if (anchorPage >= 0)
+              page = anchorPage / 2;
+            else
+              state.tooltip += " (INVALID ANCHOR:" + anchor + ")";
+          }
+          int finalPage = page;
+          state.onClick = () -> {
+            GuiBookEntry entryGui = new GuiBookEntry(book, entry, finalPage);
+            gui.displayLexiconGui(entryGui, true);
+            GuiBook.playBookFlipSound(book);
+          };
+        } else {
+          state.tooltip = "BAD LINK: " + parameter;
+        }
+      }
+      char bullet = '\u2022';
+      return TextFormatting.BLACK.toString() + bullet + TextFormatting.RESET.toString();
+    };
+
+    FUNCTIONS.put("lil", li_link);
+    COMMANDS.put("/lil", reset);*/
   }
 }
