c7674c22d8274c8c52149f6fce6bf67ef3a0a0a2
Jon McManus <due@wxwhatever.com>
2020-03-22 10:30:30 +1000

    Fix #439: inconsistency with repair.

--- src/main/java/epicsquid/roots/item/ILivingRepair.java
+++ src/main/java/epicsquid/roots/item/ILivingRepair.java
@@ -7,17 +7,16 @@ import net.minecraft.item.ItemStack;
 import net.minecraft.world.World;
 
 public interface ILivingRepair {
-  default void update(ItemStack stack, World worldIn, Entity entityIn, int itemSlot, boolean isSelected) {
-    update(stack, worldIn, entityIn, itemSlot, isSelected, 40);
-  }
-
   default void update(ItemStack stack, World worldIn, Entity entityIn, int itemSlot, boolean isSelected, int bound) {
-    if (entityIn instanceof EntityPlayer) {
-      if (stack.equals(((EntityPlayer) entityIn).getActiveItemStack())) return;
-    }
-    if (stack.getItemDamage() > 0) {
-      if (Util.rand.nextInt(Math.max(1, bound)) == 0) {
-        stack.setItemDamage(stack.getItemDamage() - 1);
+    if (!worldIn.isRemote && entityIn instanceof EntityPlayer) {
+      EntityPlayer player = (EntityPlayer) entityIn;
+      if (player.isHandActive() && isSelected) {
+        return;
+      }
+      if (stack.getItemDamage() > 0) {
+        if (Util.rand.nextInt(Math.max(1, bound)) == 0) {
+          stack.setItemDamage(stack.getItemDamage() - 1);
+        }
       }
     }
   }
