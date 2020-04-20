08af62a0f62ca93c6c531c4be35c67611a349c14
Jon McManus <due@wxwhatever.com>
2020-04-20 17:45:33 +1000

    Work on #451: address Nature's Blades.

--- src/main/java/epicsquid/roots/spell/SpellNaturesBlades.java
+++ src/main/java/epicsquid/roots/spell/SpellNaturesBlades.java
@@ -6,6 +6,7 @@ import epicsquid.roots.Roots;
 import epicsquid.roots.modifiers.instance.ModifierInstanceList;
 import epicsquid.roots.network.fx.MessageFallBladesFX;
 import epicsquid.roots.util.types.Property;
+import net.minecraft.block.Block;
 import net.minecraft.block.BlockDirt;
 import net.minecraft.block.BlockDoublePlant;
 import net.minecraft.block.BlockTallGrass;
@@ -22,6 +23,7 @@ import net.minecraft.util.text.TextFormatting;
 import net.minecraft.world.World;
 import net.minecraftforge.oredict.OreIngredient;
 
+import java.util.ArrayList;
 import java.util.List;
 
 public class SpellNaturesBlades extends SpellBase {
@@ -31,17 +33,18 @@ public class SpellNaturesBlades extends SpellBase {
   public static Property.PropertyCost PROP_COST = new Property.PropertyCost(0, new SpellCost("terra_moss", 0.08));
   public static Property<Integer> PROP_RADIUS = new Property<>("radius", 5).setDescription("horizontal radius of the area in which this spell takes effect");
   public static Property<Integer> PROP_RADIUS_Y = new Property<>("radius_y", 3).setDescription("radius on the Y axis of the area in which this spell takes effect");
-  public static Property<Integer> PROP_GRASS_CHANCE = new Property<>("grass_chance", 12).setDescription("chance for the spell to generate tall grass in the area (the higher the number is the lower the chance is: 1/x) [default: 1/15]");
-  public static Property<Integer> PROP_UPGRADE_CHANCE = new Property<>("upgrade_chance", 40).setDescription("chance for the spell to upgrade tall grass in the area to a double tall grass (the higher the number is the lower the chance is: 1/x) [default: 1/30]");
+  public static Property<Integer> PROP_GRASS_CHANCE = new Property<>("grass_chance", 17).setDescription("chance for the spell to generate tall grass in the area (the higher the number is the lower the chance is: 1/x) [default: 1/17");
+  public static Property<Integer> PROP_UPGRADE_CHANCE = new Property<>("upgrade_chance", 90).setDescription("chance for the spell to upgrade tall grass in the area to a double tall grass (the higher the number is the lower the chance is: 1/x) [default: 1/90]");
+  public static Property<Integer> PROP_CONVERSION_CHANCE = new Property<>("conversion_chance", 6).setDescription("chance for the spell to convert top-level dirt blocks into grass blocks (the higher the number is the lower the chance is: 1/x) [default: 1/6]");
 
   public static ResourceLocation spellName = new ResourceLocation(Roots.MODID, "spell_natures_blades");
   public static SpellNaturesBlades instance = new SpellNaturesBlades(spellName);
 
-  private int radius, radius_y, grass_chance, upgrade_chance;
+  private int radius, radius_y, grass_chance, upgrade_chance, conversion_chance;
 
   public SpellNaturesBlades(ResourceLocation name) {
     super(name, TextFormatting.GREEN, 64 / 255F, 240 / 255F, 24 / 255F, 26 / 255F, 110 / 255F, 13 / 255F);
-    properties.addProperties(PROP_COOLDOWN, PROP_CAST_TYPE, PROP_COST, PROP_RADIUS, PROP_RADIUS_Y, PROP_GRASS_CHANCE, PROP_UPGRADE_CHANCE);
+    properties.addProperties(PROP_COOLDOWN, PROP_CAST_TYPE, PROP_COST, PROP_RADIUS, PROP_RADIUS_Y, PROP_GRASS_CHANCE, PROP_UPGRADE_CHANCE, PROP_CONVERSION_CHANCE);
   }
 
   @Override
@@ -58,50 +61,75 @@ public class SpellNaturesBlades extends SpellBase {
   @Override
   public boolean cast(EntityPlayer caster, ModifierInstanceList modifiers, int ticks) {
     World world = caster.world;
-    List<BlockPos> blocks = Util.getBlocksWithinRadius(caster.world, caster.getPosition(), radius, radius_y, radius, pos -> (world.getBlockState(pos).getBlock() == Blocks.TALLGRASS && world.getBlockState(pos).getValue(BlockTallGrass.TYPE) != BlockTallGrass.EnumType.DEAD_BUSH && world.isAirBlock(pos.up())) || (world.getBlockState(pos) == Blocks.DIRT && world.getBlockState(pos).getValue(BlockDirt.VARIANT) == BlockDirt.DirtType.DIRT && world.isAirBlock(pos.up())) || (world.getBlockState(pos).getBlock() == Blocks.GRASS && world.isAirBlock(pos.up())));
+    List<BlockPos> tallgrass = new ArrayList<>();
+    List<BlockPos> dirt = new ArrayList<>();
+    List<BlockPos> grass = new ArrayList<>();
+    Util.getBlocksWithinRadius(caster.world, caster.getPosition(), radius, radius_y, radius, pos -> {
+      IBlockState state = world.getBlockState(pos);
+      Block block = state.getBlock();
+      if (block == Blocks.TALLGRASS) {
+        if (state.getValue(BlockTallGrass.TYPE) != BlockTallGrass.EnumType.DEAD_BUSH) {
+          if (world.isAirBlock(pos.up())) {
+            tallgrass.add(pos);
+          }
+        }
+      }
+      if (block == Blocks.DIRT) {
+        if (state.getValue(BlockDirt.VARIANT) == BlockDirt.DirtType.DIRT) {
+          if (world.isAirBlock(pos.up())) {
+            dirt.add(pos);
+          }
+        }
+      }
+      if (block == Blocks.GRASS) {
+        if (world.isAirBlock(pos.up())) {
+          grass.add(pos);
+        }
+      }
+      return false;
+    });
 
-    if (blocks.isEmpty()) {
+    if (tallgrass.isEmpty() && grass.isEmpty() && dirt.isEmpty()) {
       return false;
     }
 
-    BlockPos pos = blocks.get(Util.rand.nextInt(blocks.size()));
-    IBlockState state = world.getBlockState(pos);
-
-    if (state.getBlock() == Blocks.DIRT) {
+    BlockPos pos = null;
+    if (!dirt.isEmpty() && Util.rand.nextInt(conversion_chance) == 0) {
+      pos = dirt.get(Util.rand.nextInt(dirt.size()));
       if (!world.isRemote) {
         world.setBlockState(pos, Blocks.GRASS.getDefaultState());
       }
-    } else if (state.getBlock() == Blocks.GRASS) {
-      if (Util.rand.nextInt(grass_chance) == 0) {
-        if (!world.isRemote) {
-          world.setBlockState(pos.up(), Blocks.TALLGRASS.getDefaultState().withProperty(BlockTallGrass.TYPE, BlockTallGrass.EnumType.GRASS));
-        }
-      } else {
-        return false;
-      }
-    } else {
-      if (Util.rand.nextInt(upgrade_chance) == 0) {
-        if (!world.isRemote) {
-          BlockDoublePlant.EnumPlantType grass_type;
-          if (state.getValue(BlockTallGrass.TYPE) == BlockTallGrass.EnumType.FERN) {
-            grass_type = BlockDoublePlant.EnumPlantType.FERN;
-          } else {
-            grass_type = BlockDoublePlant.EnumPlantType.GRASS;
-          }
-          IBlockState grass = Blocks.DOUBLE_PLANT.getDefaultState().withProperty(BlockDoublePlant.VARIANT, grass_type).withProperty(BlockDoublePlant.HALF, BlockDoublePlant.EnumBlockHalf.LOWER);
-          world.setBlockState(pos, grass);
-          grass = Blocks.DOUBLE_PLANT.getDefaultState().withProperty(BlockDoublePlant.VARIANT, grass_type).withProperty(BlockDoublePlant.HALF, BlockDoublePlant.EnumBlockHalf.UPPER);
-          world.setBlockState(pos.up(), grass);
-        }
-      } else {
-        return false;
+    }
+
+    if (!grass.isEmpty() && Util.rand.nextInt(grass_chance) == 0) {
+      pos = grass.get(Util.rand.nextInt(grass.size()));
+      if (!world.isRemote) {
+        world.setBlockState(pos.up(), Blocks.TALLGRASS.getDefaultState().withProperty(BlockTallGrass.TYPE, BlockTallGrass.EnumType.GRASS));
       }
     }
 
-    if (!world.isRemote) {
+    if (!tallgrass.isEmpty() && Util.rand.nextInt(upgrade_chance) == 0) {
+      pos = tallgrass.get(Util.rand.nextInt(tallgrass.size()));
+      IBlockState state = world.getBlockState(pos);
+      if (!world.isRemote) {
+        BlockDoublePlant.EnumPlantType grass_type;
+        if (state.getValue(BlockTallGrass.TYPE) == BlockTallGrass.EnumType.FERN) {
+          grass_type = BlockDoublePlant.EnumPlantType.FERN;
+        } else {
+          grass_type = BlockDoublePlant.EnumPlantType.GRASS;
+        }
+        IBlockState grassState = Blocks.DOUBLE_PLANT.getDefaultState().withProperty(BlockDoublePlant.VARIANT, grass_type).withProperty(BlockDoublePlant.HALF, BlockDoublePlant.EnumBlockHalf.LOWER);
+        world.setBlockState(pos, grassState);
+        grassState = Blocks.DOUBLE_PLANT.getDefaultState().withProperty(BlockDoublePlant.VARIANT, grass_type).withProperty(BlockDoublePlant.HALF, BlockDoublePlant.EnumBlockHalf.UPPER);
+        world.setBlockState(pos.up(), grassState);
+      }
+    }
+
+    if (!world.isRemote && pos != null) {
       world.playSound(null, pos, SoundEvents.BLOCK_GRASS_BREAK, SoundCategory.BLOCKS, 0.5f, 1f);
       PacketHandler.sendToAllTracking(new MessageFallBladesFX(pos.getX(), pos.getY(), pos.getZ(), false), world, pos);
     }
+
     return true;
   }
 
@@ -113,5 +141,6 @@ public class SpellNaturesBlades extends SpellBase {
     this.radius_y = properties.get(PROP_RADIUS_Y);
     this.grass_chance = properties.get(PROP_GRASS_CHANCE);
     this.upgrade_chance = properties.get(PROP_UPGRADE_CHANCE);
+    this.conversion_chance = properties.get(PROP_CONVERSION_CHANCE);
   }
 }
