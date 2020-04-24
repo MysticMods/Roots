91dd887858e093d6d473193f24a6d6564f939b78
Jon McManus <due@wxwhatever.com>
2020-04-24 21:19:19 +1000

    Glass Eye.

---
+++ src/main/java/epicsquid/roots/item/ItemGlassEye.java
@@ -0,0 +1,70 @@
+package epicsquid.roots.item;
+
+import epicsquid.mysticallib.item.ItemBase;
+import epicsquid.mysticallib.network.PacketHandler;
+import epicsquid.roots.Roots;
+import epicsquid.roots.network.MessageClearToasts;
+import net.minecraft.advancements.Advancement;
+import net.minecraft.advancements.AdvancementManager;
+import net.minecraft.advancements.AdvancementProgress;
+import net.minecraft.advancements.PlayerAdvancements;
+import net.minecraft.client.resources.I18n;
+import net.minecraft.client.util.ITooltipFlag;
+import net.minecraft.creativetab.CreativeTabs;
+import net.minecraft.entity.EntityLivingBase;
+import net.minecraft.entity.player.EntityPlayer;
+import net.minecraft.entity.player.EntityPlayerMP;
+import net.minecraft.init.MobEffects;
+import net.minecraft.init.SoundEvents;
+import net.minecraft.item.EnumAction;
+import net.minecraft.item.EnumRarity;
+import net.minecraft.item.ItemStack;
+import net.minecraft.nbt.NBTTagCompound;
+import net.minecraft.nbt.NBTTagList;
+import net.minecraft.nbt.NBTTagString;
+import net.minecraft.potion.PotionEffect;
+import net.minecraft.util.*;
+import net.minecraft.util.math.BlockPos;
+import net.minecraft.util.text.Style;
+import net.minecraft.util.text.TextComponentTranslation;
+import net.minecraft.util.text.TextFormatting;
+import net.minecraft.world.World;
+import net.minecraft.world.WorldServer;
+import net.minecraftforge.common.util.Constants;
+import net.minecraftforge.fml.relauncher.Side;
+import net.minecraftforge.fml.relauncher.SideOnly;
+import vazkii.patchouli.common.handler.AdvancementSyncHandler;
+
+import javax.annotation.Nonnull;
+import javax.annotation.Nullable;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.StringJoiner;
+
+@SuppressWarnings("deprecation")
+public class ItemGlassEye extends ItemBase {
+  public ItemGlassEye(@Nonnull String name) {
+    super(name);
+    setMaxStackSize(64);
+  }
+
+  @Override
+  public ActionResult<ItemStack> onItemRightClick(World worldIn, EntityPlayer playerIn, EnumHand handIn) {
+    playerIn.addPotionEffect(new PotionEffect(MobEffects.NIGHT_VISION, 15 * 20, 0, false, false));
+    return ActionResult.newResult(EnumActionResult.SUCCESS, playerIn.getHeldItem(handIn));
+  }
+
+  @Override
+  public EnumRarity getRarity(ItemStack stack) {
+    return EnumRarity.UNCOMMON;
+  }
+
+  @Override
+  @SideOnly(Side.CLIENT)
+  public void addInformation(ItemStack stack, @Nullable World worldIn, List<String> tooltip, ITooltipFlag flagIn) {
+    super.addInformation(stack, worldIn, tooltip, flagIn);
+
+    tooltip.add("");
+    tooltip.add(TextFormatting.YELLOW + I18n.format("roots.glass_eye.tooltip"));
+  }
+}
