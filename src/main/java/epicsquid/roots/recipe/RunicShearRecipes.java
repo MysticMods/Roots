cf9ac0ba2d636ddcfd4b03f87d5c8182ab29e542
Jon McManus <due@wxwhatever.com>
2020-04-26 12:57:23 +1000

    Sprout support.

--- src/main/java/epicsquid/roots/recipe/RunicShearRecipes.java
+++ src/main/java/epicsquid/roots/recipe/RunicShearRecipes.java
@@ -1,5 +1,6 @@
 package epicsquid.roots.recipe;
 
+import com.google.common.collect.Sets;
 import epicsquid.mysticalworld.entity.*;
 import epicsquid.roots.Roots;
 import epicsquid.roots.init.ModItems;
@@ -7,11 +8,14 @@ import epicsquid.roots.init.ModRecipes;
 import epicsquid.roots.recipe.transmutation.PropertyPredicate;
 import net.minecraft.block.BlockBeetroot;
 import net.minecraft.block.BlockCrops;
+import net.minecraft.entity.EntityLivingBase;
 import net.minecraft.entity.passive.*;
 import net.minecraft.init.Blocks;
 import net.minecraft.init.Items;
 import net.minecraft.item.ItemStack;
 import net.minecraft.util.ResourceLocation;
+
+import java.util.function.Function;
 
 public class RunicShearRecipes {
 
@@ -37,11 +41,27 @@ public class RunicShearRecipes {
     ModRecipes.addRunicShearRecipe(new RunicShearEntityRecipe(new ResourceLocation(Roots.MODID, "frog_strange_slime"), new ItemStack(ModItems.strange_slime, 1), EntityFrog.class, 20 * 120));
     ModRecipes.addRunicShearRecipe(new RunicShearEntityRecipe(new ResourceLocation(Roots.MODID, "squid_strange_slime"), new ItemStack(ModItems.strange_slime, 1), EntitySquid.class, 20 * 120));
 
+    ModRecipes.addRunicShearRecipe(new RunicShearConditionalEntityRecipe(new ResourceLocation(Roots.MODID, "sprout_stuff"), entityLivingBase -> {
+      EntitySprout sprout = (EntitySprout) entityLivingBase;
+      switch (sprout.getDataManager().get(EntitySprout.variant)) {
+        default:
+        case 0: {
+          return new ItemStack(Items.MELON_SEEDS);
+        }
+        case 1: {
+          return new ItemStack(ModItems.terra_spores);
+        }
+        case 2: {
+          return new ItemStack(Items.BEETROOT_SEEDS);
+        }
+        case 3: {
+          return new ItemStack(epicsquid.mysticalworld.init.ModItems.aubergine_seed);
+        }
+      }
+    }, Sets.newHashSet(new ItemStack(Items.MELON_SEEDS), new ItemStack(ModItems.terra_spores), new ItemStack(Items.BEETROOT_SEEDS), new ItemStack(epicsquid.mysticalworld.init.ModItems.aubergine_seed)), EntitySprout.class, 20 * 180));
 
-
-/*    addPacifistEntry("pig", EntityPig.class);
-    addPacifistEntry("sheep", EntitySheep.class);
-    addPacifistEntry("wolf", EntityWolf.class);
-    addPacifistEntry("sprout", EntitySprout.class);*/
+//    addPacifistEntry("pig", EntityPig.class);
+//    addPacifistEntry("sheep", EntitySheep.class);
+//    addPacifistEntry("wolf", EntityWolf.class);
   }
 }
