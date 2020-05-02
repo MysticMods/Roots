54904db5f45c998aaf4ce690534787a830774730
Jon McManus <due@wxwhatever.com>
2020-05-02 13:50:44 +1000

    Fix #488: don't add creative pouches to the pouch list.

--- src/main/java/epicsquid/roots/util/CommonHerbUtil.java
+++ src/main/java/epicsquid/roots/util/CommonHerbUtil.java
@@ -2,6 +2,7 @@ package epicsquid.roots.util;
 
 import epicsquid.roots.integration.baubles.pouch.BaublePowderInventoryUtil;
 import epicsquid.roots.item.ItemPouch;
+import epicsquid.roots.item.PouchType;
 import net.minecraft.entity.player.EntityPlayer;
 import net.minecraft.item.ItemStack;
 import net.minecraftforge.fml.common.Loader;
@@ -24,13 +25,18 @@ public class CommonHerbUtil {
     if (Loader.isModLoaded("baubles")) {
       ItemStack stack = BaublePowderInventoryUtil.getPouch(player);
       if (!stack.isEmpty()) {
-        result.add(stack);
+        if (((ItemPouch) stack.getItem()).getType() != PouchType.CREATIVE) {
+          result.add(stack);
+        }
       }
     }
 
     for (int i = 0; i < 36; i++) {
       if (player.inventory.getStackInSlot(i).getItem() instanceof ItemPouch) {
-        result.add(player.inventory.getStackInSlot(i));
+        ItemStack pouch = player.inventory.getStackInSlot(i);
+        if (((ItemPouch) pouch.getItem()).getType() != PouchType.CREATIVE) {
+          result.add(pouch);
+        }
       }
     }
 
