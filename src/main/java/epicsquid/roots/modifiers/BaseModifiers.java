52faf9e802a5c05e8026a7d7814d5cd3095e4e94
Jon McManus <due@wxwhatever.com>
2020-05-14 12:49:46 +1000

    Temporarily rename base modifiers.

--- src/main/java/epicsquid/roots/modifiers/BaseModifiers.java
+++ src/main/java/epicsquid/roots/modifiers/BaseModifiers.java
@@ -6,10 +6,10 @@ import epicsquid.roots.modifiers.modifier.ModifierCores;
 import net.minecraft.util.ResourceLocation;
 
 public class BaseModifiers {
-  public static Modifier REDSTONE = ModifierRegistry.register(new Modifier(new ResourceLocation(Roots.MODID, "redstone"), ModifierType.ALL_COST_MULTIPLIER, ModifierCores.REDSTONE, 0.05));
-  public static Modifier GLOWSTONE = ModifierRegistry.register(new Modifier(new ResourceLocation(Roots.MODID, "glowstone"), ModifierType.ALL_COST_MULTIPLIER, ModifierCores.GLOWSTONE, 0.05));
-  public static Modifier GUNPOWDER = ModifierRegistry.register(new Modifier(new ResourceLocation(Roots.MODID, "gunpowder"), ModifierType.ALL_COST_MULTIPLIER, ModifierCores.GUNPOWDER, 0.05));
-  public static Modifier LAPIS = ModifierRegistry.register(new Modifier(new ResourceLocation(Roots.MODID, "lapis"), ModifierType.ALL_COST_MULTIPLIER, ModifierCores.LAPIS, 0.05));
-  public static Modifier RUNIC_DUST = ModifierRegistry.register(new Modifier(new ResourceLocation(Roots.MODID, "runic_dust"), ModifierType.ALL_COST_MULTIPLIER, ModifierCores.RUNIC_DUST, 0.05));
-  public static Modifier BLAZE_POWDER = ModifierRegistry.register(new Modifier(new ResourceLocation(Roots.MODID, "blaze_powder"), ModifierType.ALL_COST_MULTIPLIER, ModifierCores.BLAZE_POWDER, 0.05));
+  public static Modifier SPEEDY = ModifierRegistry.register(new Modifier(new ResourceLocation(Roots.MODID, "redstone"), ModifierType.ALL_COST_MULTIPLIER, ModifierCores.REDSTONE, 0.05)); // Decreases the cooldown or interval of the spell
+  public static Modifier REDUCTION = ModifierRegistry.register(new Modifier(new ResourceLocation(Roots.MODID, "glowstone"), ModifierType.ALL_COST_MULTIPLIER, ModifierCores.GLOWSTONE, -0.05)); // Reduces the cost of the spell.
+  public static Modifier GREATER_EMPOWER = ModifierRegistry.register(new Modifier(new ResourceLocation(Roots.MODID, "gunpowder"), ModifierType.ALL_COST_MULTIPLIER, ModifierCores.GUNPOWDER, 0.10)); // Additionally increases the duration or damage of the spell if applicable
+  public static Modifier GREATER_REDUCTION = ModifierRegistry.register(new Modifier(new ResourceLocation(Roots.MODID, "lapis"), ModifierType.ALL_COST_MULTIPLIER, ModifierCores.LAPIS, -0.10)); // Additionally reduces the cost of the spell
+  public static Modifier GREATER_SPEEDY = ModifierRegistry.register(new Modifier(new ResourceLocation(Roots.MODID, "runic_dust"), ModifierType.ALL_COST_MULTIPLIER, ModifierCores.RUNIC_DUST, 0.10)); // Decreases the cooldown or interval of the spell again
+  public static Modifier EMPOWER = ModifierRegistry.register(new Modifier(new ResourceLocation(Roots.MODID, "blaze_powder"), ModifierType.ALL_COST_MULTIPLIER, ModifierCores.BLAZE_POWDER, 0.05)); // Increases duration or damage of the spell if applicable.
 }
