b2fa5b400d921aa04ca2f2f2b5475bdcf4ab839e
Jon McManus <due@wxwhatever.com>
2020-07-28 19:42:19 +1000

    Peaceful modifier for petal shell.

--- src/main/java/epicsquid/roots/spell/SpellPetalShell.java
+++ src/main/java/epicsquid/roots/spell/SpellPetalShell.java
@@ -1,6 +1,7 @@
 package epicsquid.roots.spell;
 
 import epicsquid.mysticallib.network.PacketHandler;
+import epicsquid.mysticallib.util.Util;
 import epicsquid.roots.Roots;
 import epicsquid.roots.init.ModItems;
 import epicsquid.roots.init.ModPotions;
@@ -8,12 +9,18 @@ import epicsquid.roots.modifiers.*;
 import epicsquid.roots.modifiers.instance.staff.StaffModifierInstanceList;
 import epicsquid.roots.network.fx.MessagePetalShellBurstFX;
 import epicsquid.roots.properties.Property;
+import epicsquid.roots.util.EntityUtil;
+import net.minecraft.entity.EntityLivingBase;
 import net.minecraft.entity.player.EntityPlayer;
 import net.minecraft.init.Items;
 import net.minecraft.item.ItemStack;
 import net.minecraft.potion.PotionEffect;
 import net.minecraft.util.ResourceLocation;
+import net.minecraft.util.math.AxisAlignedBB;
 import net.minecraft.util.text.TextFormatting;
+import net.minecraft.world.World;
+
+import java.util.List;
 
 public class SpellPetalShell extends SpellBase {
   public static Property.PropertyCooldown PROP_COOLDOWN = new Property.PropertyCooldown(120);
@@ -21,6 +28,9 @@ public class SpellPetalShell extends SpellBase {
   public static Property.PropertyCost PROP_COST_1 = new Property.PropertyCost(0, new SpellCost("spirit_herb", 0.75));
   public static Property.PropertyDuration PROP_DURATION = new Property.PropertyDuration(120 * 20);
   public static Property<Integer> PROP_MAXIMUM = new Property<>("maximum_shells", 3).setDescription("maximum number of shells (attack blockers) a player can have");
+  public static Property<Integer> PROP_RADIUS_X = new Property<>("radius_x", 5).setDescription("radius on the X axis of the area the spell has effect on");
+  public static Property<Integer> PROP_RADIUS_Y = new Property<>("radius_y", 5).setDescription("radius on the Y axis of the area the spell has effect on");
+  public static Property<Integer> PROP_RADIUS_Z = new Property<>("radius_z", 5).setDescription("radius on the Z axis of the area the spell has effect on");
 
   public static Modifier PERESKIA = ModifierRegistry.register(new Modifier(new ResourceLocation(Roots.MODID, "react_radiance"), ModifierCores.PERESKIA, ModifierCost.of(CostType.ADDITIONAL_COST, ModifierCores.PERESKIA, 1)));
   public static Modifier WILDEWHEET = ModifierRegistry.register(new Modifier(new ResourceLocation(Roots.MODID, "peaceful_dance"), ModifierCores.WILDEWHEET, ModifierCost.of(CostType.ADDITIONAL_COST, ModifierCores.WILDEWHEET, 1)));
@@ -38,11 +48,14 @@ public class SpellPetalShell extends SpellBase {
 
   private int maxShells;
   private int duration;
+  private int radius_x, radius_y, radius_z;
 
   public SpellPetalShell(ResourceLocation name) {
     super(name, TextFormatting.LIGHT_PURPLE, 255f / 255f, 192f / 255f, 240f / 255f, 255f / 255f, 255f / 255f, 255f / 255f);
-    properties.addProperties(PROP_COOLDOWN, PROP_CAST_TYPE, PROP_COST_1, PROP_MAXIMUM);
+    properties.addProperties(PROP_COOLDOWN, PROP_CAST_TYPE, PROP_COST_1, PROP_MAXIMUM, PROP_RADIUS_X, PROP_RADIUS_Y, PROP_RADIUS_Z);
     acceptsModifiers(PERESKIA, WILDEWHEET, WILDROOT, MOONGLOW_LEAF, TERRA_MOSS, BAFFLE_CAP, CLOUD_BERRY, INFERNAL_BULB, STALICRIPE, DEWGONIA);
+    setPeaceful(WILDEWHEET);
+    setFire(INFERNAL_BULB);
   }
 
   @Override
@@ -56,11 +69,23 @@ public class SpellPetalShell extends SpellBase {
     );
   }
 
+  private AxisAlignedBB bb;
+
   @Override
   public boolean cast(EntityPlayer player, StaffModifierInstanceList modifiers, int ticks) {
     if (!player.world.isRemote) {
-      player.addPotionEffect(new PotionEffect(ModPotions.petal_shell, ampInt(duration), ampInt(maxShells), false, false));
-      PacketHandler.sendToAllTracking(new MessagePetalShellBurstFX(player.posX, player.posY + 1.0f, player.posZ), player);
+      if (modifiers != null && peaceful(modifiers)) {
+        World world = player.world;
+        List<EntityLivingBase> entities = world.getEntitiesWithinAABB(EntityLivingBase.class, bb.offset(player.getPosition()));
+        entities.removeIf(o -> !EntityUtil.isFamiliar(player, o));
+        if (!entities.isEmpty()) {
+          EntityLivingBase entity = entities.get(Util.rand.nextInt(entities.size()));
+          entity.addPotionEffect(new PotionEffect(ModPotions.petal_shell, ampInt(duration), ampInt(maxShells), false, false));
+          PacketHandler.sendToAllTracking(new MessagePetalShellBurstFX(entity.posX, entity.posY + 1.0f, entity.posZ), entity);
+        }
+        player.addPotionEffect(new PotionEffect(ModPotions.petal_shell, ampInt(duration), ampInt(maxShells), false, false));
+        PacketHandler.sendToAllTracking(new MessagePetalShellBurstFX(player.posX, player.posY + 1.0f, player.posZ), player);
+      }
     }
     return true;
   }
@@ -71,5 +96,9 @@ public class SpellPetalShell extends SpellBase {
     this.cooldown = properties.get(PROP_COOLDOWN);
     this.maxShells = properties.get(PROP_MAXIMUM);
     this.duration = properties.get(PROP_DURATION);
+    this.radius_x = properties.get(PROP_RADIUS_X);
+    this.radius_y = properties.get(PROP_RADIUS_Y);
+    this.radius_z = properties.get(PROP_RADIUS_Z);
+    this.bb = new AxisAlignedBB(-radius_x, -radius_y, -radius_z, 1 + radius_x, 1 + radius_y, 1 + radius_z);
   }
 }
