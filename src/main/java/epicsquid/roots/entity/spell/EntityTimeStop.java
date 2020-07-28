3b50d62eb84648314f2aefdfe3d1b1146dd64029
Jon McManus <due@wxwhatever.com>
2020-07-28 20:22:58 +1000

    Time Stop peacefuls!

--- src/main/java/epicsquid/roots/entity/spell/EntityTimeStop.java
+++ src/main/java/epicsquid/roots/entity/spell/EntityTimeStop.java
@@ -3,8 +3,10 @@ package epicsquid.roots.entity.spell;
 import epicsquid.roots.init.ModPotions;
 import epicsquid.roots.particle.ParticleUtil;
 import epicsquid.roots.spell.SpellTimeStop;
+import epicsquid.roots.util.EntityUtil;
 import net.minecraft.entity.Entity;
 import net.minecraft.entity.EntityLivingBase;
+import net.minecraft.entity.player.EntityPlayer;
 import net.minecraft.nbt.NBTTagCompound;
 import net.minecraft.nbt.NBTUtil;
 import net.minecraft.network.datasync.DataParameter;
@@ -17,80 +19,38 @@ import net.minecraft.world.World;
 import java.util.List;
 import java.util.UUID;
 
-public class EntityTimeStop extends Entity {
-  private static final DataParameter<Integer> lifetime = EntityDataManager.createKey(EntityTimeStop.class, DataSerializers.VARINT);
-  private UUID playerId = null;
-
-  private int duration;
+public class EntityTimeStop extends EntitySpellModifiable<SpellTimeStop> {
 
   public EntityTimeStop(World worldIn) {
-    this(worldIn, SpellTimeStop.duration);
+    this(worldIn, SpellTimeStop.instance.duration);
   }
 
   public EntityTimeStop(World world, int duration) {
-    super(world);
-    this.setInvisible(true);
-    this.setSize(1, 1);
-    this.duration = duration;
-    getDataManager().register(lifetime, duration);
-  }
-
-  public void setPlayer(UUID id) {
-    this.playerId = id;
-  }
-
-  @Override
-  public boolean isEntityInsideOpaqueBlock() {
-    return false;
-  }
-
-  @Override
-  protected void entityInit() {
-
+    super(world, SpellTimeStop.instance, duration);
   }
 
   @Override
   public void onUpdate() {
-    getDataManager().set(lifetime, getDataManager().get(lifetime) - 1);
-    getDataManager().setDirty(lifetime);
-    if (getDataManager().get(lifetime) <= 0) {
-      setDead();
-    }
+    super.onUpdate();
     if (world.isRemote) {
       for (float i = 0; i < 360; i += 24.0f) {
         if (rand.nextInt(4) == 0) {
           if (rand.nextBoolean()) {
-            ParticleUtil
-                .spawnParticleSmoke(world, (float) posX + rand.nextFloat() * rand.nextFloat() * 8.0f * (float) Math.sin(Math.toRadians(i)), (float) posY - 0.5f,
-                    (float) posZ + rand.nextFloat() * rand.nextFloat() * 8.0f * (float) Math.cos(Math.toRadians(i)), 0, rand.nextFloat() * 0.05f, 0,
-                    SpellTimeStop.instance.getRed1(), SpellTimeStop.instance.getGreen1(), SpellTimeStop.instance.getBlue1(), rand.nextFloat() * 0.25f,
-                    rand.nextFloat() * 24f, 120, true);
+            ParticleUtil.spawnParticleSmoke(world, (float) posX + rand.nextFloat() * rand.nextFloat() * 8.0f * (float) Math.sin(Math.toRadians(i)), (float) posY - 0.5f, (float) posZ + rand.nextFloat() * rand.nextFloat() * 8.0f * (float) Math.cos(Math.toRadians(i)), 0, rand.nextFloat() * 0.05f, 0, SpellTimeStop.instance.getRed1(), SpellTimeStop.instance.getGreen1(), SpellTimeStop.instance.getBlue1(), rand.nextFloat() * 0.25f, rand.nextFloat() * 24f, 120, true);
           } else {
-            ParticleUtil
-                .spawnParticleSmoke(world, (float) posX + rand.nextFloat() * rand.nextFloat() * 8.0f * (float) Math.sin(Math.toRadians(i)), (float) posY - 0.5f,
-                    (float) posZ + rand.nextFloat() * rand.nextFloat() * 8.0f * (float) Math.cos(Math.toRadians(i)), 0, rand.nextFloat() * 0.05f, 0,
-                    SpellTimeStop.instance.getRed2(), SpellTimeStop.instance.getRed2(), SpellTimeStop.instance.getBlue2(), rand.nextFloat() * 0.25f,
-                    rand.nextFloat() * 24f, 120, true);
+            ParticleUtil.spawnParticleSmoke(world, (float) posX + rand.nextFloat() * rand.nextFloat() * 8.0f * (float) Math.sin(Math.toRadians(i)), (float) posY - 0.5f, (float) posZ + rand.nextFloat() * rand.nextFloat() * 8.0f * (float) Math.cos(Math.toRadians(i)), 0, rand.nextFloat() * 0.05f, 0, SpellTimeStop.instance.getRed2(), SpellTimeStop.instance.getRed2(), SpellTimeStop.instance.getBlue2(), rand.nextFloat() * 0.25f, rand.nextFloat() * 24f, 120, true);
           }
         }
       }
     }
     List<EntityLivingBase> entities = world.getEntitiesWithinAABB(EntityLivingBase.class, new AxisAlignedBB(posX - 9.0, posY - 1.0, posZ - 9.0, posX + 9.0, posY + 19.0, posZ + 9.0));
     for (EntityLivingBase e : entities) {
-      if (playerId != null && e.getUniqueID() != playerId) {
+      if (!(e instanceof EntityPlayer)) {
+        if (modifiers != null && instance.peaceful(modifiers) && EntityUtil.isFriendly(e)) {
+          continue;
+        }
         e.addPotionEffect(new PotionEffect(ModPotions.time_stop, 40, 0, false, false));
       }
     }
   }
-
-  @Override
-  protected void readEntityFromNBT(NBTTagCompound compound) {
-    this.playerId = NBTUtil.getUUIDFromTag(compound.getCompoundTag("id"));
-  }
-
-  @Override
-  protected void writeEntityToNBT(NBTTagCompound compound) {
-    compound.setTag("id", NBTUtil.createUUIDTag(playerId));
-  }
-
 }
