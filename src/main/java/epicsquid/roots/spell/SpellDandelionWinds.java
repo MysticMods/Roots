628c78404d2331dfd69cc2741ab562396d2c42bd
Jon McManus <due@wxwhatever.com>
2020-09-12 21:25:07 +1000

    Finalise Dandelion Winds.

--- src/main/java/epicsquid/roots/spell/SpellDandelionWinds.java
+++ src/main/java/epicsquid/roots/spell/SpellDandelionWinds.java
@@ -3,18 +3,27 @@ package epicsquid.roots.spell;
 import epicsquid.mysticallib.network.PacketHandler;
 import epicsquid.roots.Roots;
 import epicsquid.roots.init.ModItems;
+import epicsquid.roots.init.ModPotions;
 import epicsquid.roots.modifiers.*;
 import epicsquid.roots.modifiers.instance.staff.StaffModifierInstanceList;
 import epicsquid.roots.network.fx.MessageDandelionCastFX;
 import epicsquid.roots.properties.Property;
 import epicsquid.roots.util.EntityUtil;
+import net.minecraft.entity.Entity;
 import net.minecraft.entity.EntityLivingBase;
+import net.minecraft.entity.item.EntityBoat;
+import net.minecraft.entity.item.EntityItem;
 import net.minecraft.entity.player.EntityPlayer;
 import net.minecraft.init.Blocks;
+import net.minecraft.init.MobEffects;
 import net.minecraft.item.ItemStack;
+import net.minecraft.potion.PotionEffect;
 import net.minecraft.util.ResourceLocation;
 import net.minecraft.util.math.AxisAlignedBB;
+import net.minecraft.util.math.BlockPos;
+import net.minecraft.util.math.Vec3d;
 import net.minecraft.util.text.TextFormatting;
+import net.minecraft.world.World;
 import net.minecraftforge.oredict.OreIngredient;
 
 import java.util.List;
@@ -24,6 +33,15 @@ public class SpellDandelionWinds extends SpellBase {
   public static Property.PropertyCastType PROP_CAST_TYPE = new Property.PropertyCastType(EnumCastType.INSTANTANEOUS);
   public static Property.PropertyCost PROP_COST_1 = new Property.PropertyCost(0, new SpellCost("cloud_berry", 0.125));
   public static Property<Float> PROP_DISTANCE = new Property<>("distance", 0.75f).setDescription("the vertical component of the vector that determines the direction of the entity");
+  public static Property<Double> PROP_RANGE_1 = new Property<>("range_1", 6.0).setDescription("the first range increment for calculating the bounding box");
+  public static Property<Double> PROP_RANGE_2 = new Property<>("range_2", 4.0).setDescription("the first range increment for calculating the bounding box");
+  public static Property<Float> PROP_ADDITIONAL_DISTANCE = new Property<>("additional_distance", 0.25f).setDescription("the additional vertical component of the vector that determines the direction of the entity");
+  public static Property<Float> PROP_ADDITIONAL_FALL = new Property<>("additional_fall", 1.35f).setDescription("the fall distance modifier");
+  public static Property<Integer> PROP_SLOW_DURATION = new Property<>("slow_duration", 60).setDescription("the duration of the slow fall effect");
+  public static Property<Integer> PROP_POISON_DURATION = new Property<>("posion_duration", 5 * 20).setDescription("the duration of the poison effect to apply");
+  public static Property<Integer> PROP_POISON_AMPLIFIER = new Property<>("poison_amplifier", 0).setDescription("the amplifier to apply to the poison effect");
+  public static Property<Integer> PROP_FIRE_DURATION = new Property<>("fire_duration", 4).setDescription("the duration (in seconds) that entities should be set on fire for");
+  public static Property<Double> PROP_BOAT_SPEED = new Property<>("boat_speed", 1.5).setDescription("the multiplier that should be applied to boat speed");
 
   public static Modifier STRONGER = ModifierRegistry.register(new Modifier(new ResourceLocation(Roots.MODID, "strong_gusts"), ModifierCores.PERESKIA, ModifierCost.of(CostType.ADDITIONAL_COST, ModifierCores.PERESKIA, 1)));
   public static Modifier PEACEFUL = ModifierRegistry.register(new Modifier(new ResourceLocation(Roots.MODID, "peaceful_winds"), ModifierCores.WILDEWHEET, ModifierCost.of(CostType.ADDITIONAL_COST, ModifierCores.WILDEWHEET, 1)));
@@ -47,11 +65,13 @@ public class SpellDandelionWinds extends SpellBase {
   public static ResourceLocation spellName = new ResourceLocation(Roots.MODID, "spell_dandelion_winds");
   public static SpellDandelionWinds instance = new SpellDandelionWinds(spellName);
 
-  private float distance;
+  private int slow_duration, poison_amplifier, poison_duration, fire_duration;
+  private double r1, r2, boat_speed;
+  private float distance, additional_distance, additional_fall;
 
   public SpellDandelionWinds(ResourceLocation name) {
     super(name, TextFormatting.YELLOW, 255f / 255f, 255f / 255f, 32f / 255f, 255f / 255f, 176f / 255f, 32f / 255f);
-    properties.addProperties(PROP_COOLDOWN, PROP_CAST_TYPE, PROP_COST_1, PROP_DISTANCE);
+    properties.addProperties(PROP_COOLDOWN, PROP_CAST_TYPE, PROP_COST_1, PROP_DISTANCE, PROP_RANGE_1, PROP_RANGE_2, PROP_ADDITIONAL_DISTANCE, PROP_ADDITIONAL_DISTANCE, PROP_POISON_AMPLIFIER, PROP_POISON_DURATION, PROP_BOAT_SPEED);
     acceptsModifiers(STRONGER, PEACEFUL, GROUNDED, SLOW_FALL, CIRCLE, ITEMS, POISON, FIRE, SUCTION, BOAT_SPEED);
   }
 
@@ -68,25 +88,149 @@ public class SpellDandelionWinds extends SpellBase {
 
   @Override
   public boolean cast(EntityPlayer player, StaffModifierInstanceList info, int ticks) {
-    PacketHandler.sendToAllTracking(new MessageDandelionCastFX(player.getUniqueID(), player.posX, player.posY + player.getEyeHeight(), player.posZ), player);
-    List<EntityLivingBase> entities = player.world.getEntitiesWithinAABB(EntityLivingBase.class,
-        new AxisAlignedBB(player.posX + player.getLookVec().x * 6.0 - 6.0, player.posY + player.getLookVec().y * 6.0 - 6.0,
-            player.posZ + player.getLookVec().z * 6.0 - 4.0, player.posX + player.getLookVec().x * 6.0 + 6.0, player.posY + player.getLookVec().y * 6.0 + 6.0,
-            player.posZ + player.getLookVec().z * 6.0 + 6.0));
-    if (entities.size() > 0) {
-      for (EntityLivingBase e : entities) {
-        if (e != player) {
-          if (info.has(PEACEFUL) && EntityUtil.isFriendly(e)) {
-            continue;
+    // TODO: Circle, Suction
+    if (info.has(BOAT_SPEED)) {
+      Entity entity = player.getLowestRidingEntity();
+      if (entity instanceof EntityBoat) {
+        if (!player.world.isRemote) {
+          entity.motionX *= ampDouble(boat_speed);
+          entity.motionZ *= ampDouble(boat_speed);
+          PacketHandler.sendToAllTracking(new MessageDandelionCastFX(player.getUniqueID(), player.posX, player.posY + player.getEyeHeight(), player.posZ), player);
+        }
+        return true;
+      }
+    }
+
+    Vec3d lookVec = player.getLookVec();
+    float d = info.has(STRONGER) ? distance + additional_distance : distance;
+    float motion = ampFloat(d * d + d);
+    Vec3d playVec = player.getPositionVector();
+
+    int count = 0;
+
+    if (!info.has(CIRCLE)) {
+      AxisAlignedBB bounding = new AxisAlignedBB(player.posX + lookVec.x * r1 - r1, player.posY + lookVec.y * r1 - r1, player.posZ + lookVec.z * r1 - r2, player.posX + lookVec.x * r1 + r1, player.posY + lookVec.y * r1 + r1, player.posZ + lookVec.z * r1 + r1);
+      List<EntityLivingBase> entities = player.world.getEntitiesWithinAABB(EntityLivingBase.class, bounding);
+      count = entities.size() - 1;
+      if (!player.world.isRemote) {
+        entities.forEach(o -> {
+          if (o != player) {
+            BlockPos pos = o.getPosition();
+            Vec3d vec = lookVec;
+            if (info.has(SUCTION)) {
+              vec = playVec.subtract(o.getPositionVector()).normalize();
+            }
+            flingLivingEntity(o, vec, motion, info);
+            if (info.has(FIRE)) {
+              trySetFire(player.world, pos);
+            }
           }
-          e.motionX += (player.getLookVec().x);
-          e.motionY += (distance + distance * distance);
-          e.motionZ += (player.getLookVec().z);
-          e.velocityChanged = true;
+        });
+      }
+
+      if (info.has(ITEMS)) {
+        List<EntityItem> items = player.world.getEntitiesWithinAABB(EntityItem.class, bounding);
+        count += items.size();
+        if (!player.world.isRemote) {
+          items.forEach(o -> {
+            BlockPos pos = o.getPosition();
+            Vec3d vec = lookVec;
+            if (info.has(SUCTION)) {
+              vec = playVec.subtract(o.getPositionVector()).normalize();
+            }
+            flingEntity(o, vec, motion, info);
+            if (info.has(FIRE)) {
+              trySetFire(player.world, pos);
+            }
+          });
+        }
+      }
+    } else {
+      double val = (r1 + r2) / 2;
+      AxisAlignedBB bounding = new AxisAlignedBB(val, val, val, -(val) - 0.5, -(val) - 0.5, -(val) - 0.5).offset(player.getPosition());
+      List<EntityLivingBase> entities = player.world.getEntitiesWithinAABB(EntityLivingBase.class, bounding);
+      if (!player.world.isRemote) {
+        entities.forEach(o -> {
+          if (o != player) {
+            BlockPos pos = o.getPosition();
+            Vec3d vec;
+            if (info.has(SUCTION)) {
+              vec = playVec.subtract(o.getPositionVector()).normalize();
+            } else {
+              vec = o.getPositionVector().subtract(playVec).normalize();
+            }
+            flingLivingEntity(o, vec, motion, info);
+            if (info.has(FIRE)) {
+              trySetFire(player.world, pos);
+            }
+          }
+        });
+        if (info.has(ITEMS)) {
+          List<EntityItem> items = player.world.getEntitiesWithinAABB(EntityItem.class, bounding);
+          count += items.size();
+          if (!player.world.isRemote) {
+            items.forEach(o -> {
+              BlockPos pos = o.getPosition();
+              Vec3d vec;
+              if (info.has(SUCTION)) {
+                vec = playVec.subtract(o.getPositionVector()).normalize();
+              } else {
+                vec = o.getPositionVector().subtract(playVec).normalize();
+              }
+              flingEntity(o, vec, motion, info);
+              if (info.has(FIRE)) {
+                trySetFire(player.world, pos);
+              }
+            });
+          }
         }
       }
     }
-    return true;
+
+    if (count > 0) {
+      if (!player.world.isRemote) {
+        PacketHandler.sendToAllTracking(new MessageDandelionCastFX(player.getUniqueID(), player.posX, player.posY + player.getEyeHeight(), player.posZ), player);
+      }
+      return true;
+    } else {
+      return false;
+    }
+  }
+
+  private void trySetFire(World world, BlockPos pos) {
+    if (world.isRemote) {
+      return;
+    }
+
+    if (world.isAirBlock(pos) && Blocks.FIRE.canPlaceBlockAt(world, pos)) {
+      world.setBlockState(pos, Blocks.FIRE.getDefaultState());
+    }
+  }
+
+  private void flingEntity(Entity e, Vec3d lookVec, double motion, StaffModifierInstanceList info) {
+    e.motionX += lookVec.x;
+    e.motionY += (motion * 0.7);
+    e.motionZ += lookVec.z;
+    if (info.has(GROUNDED)) {
+      e.fallDistance *= additional_fall;
+    }
+    e.velocityChanged = true;
+  }
+
+  private void flingLivingEntity(EntityLivingBase e, Vec3d lookVec, double motion, StaffModifierInstanceList info) {
+    if (info.has(PEACEFUL) && EntityUtil.isFriendly(e)) {
+      return;
+    }
+    flingEntity(e, lookVec, motion, info);
+    if (info.has(SLOW_FALL)) {
+      e.addPotionEffect(new PotionEffect(ModPotions.slow_fall, slow_duration, 0, false, false));
+    }
+    if (info.has(POISON)) {
+      e.addPotionEffect(new PotionEffect(MobEffects.POISON, poison_duration, poison_amplifier));
+    }
+    if (info.has(FIRE)) {
+      e.setFire(fire_duration);
+    }
   }
 
   @Override
@@ -94,5 +238,13 @@ public class SpellDandelionWinds extends SpellBase {
     this.castType = properties.get(PROP_CAST_TYPE);
     this.cooldown = properties.get(PROP_COOLDOWN);
     this.distance = properties.get(PROP_DISTANCE);
+    this.additional_distance = properties.get(PROP_ADDITIONAL_DISTANCE);
+    this.additional_fall = properties.get(PROP_ADDITIONAL_FALL);
+    this.slow_duration = properties.get(PROP_SLOW_DURATION);
+    this.poison_amplifier = properties.get(PROP_POISON_AMPLIFIER);
+    this.poison_duration = properties.get(PROP_POISON_DURATION);
+    this.fire_duration = properties.get(PROP_FIRE_DURATION);
+    this.r1 = properties.get(PROP_RANGE_1);
+    this.r2 = properties.get(PROP_RANGE_2);
   }
 }
