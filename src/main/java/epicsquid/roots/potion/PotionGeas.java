ac1db695be3f9a3a6fec4f0ecdb782ffc05da596
Jon McManus <due@wxwhatever.com>
2020-09-14 12:04:26 +1000

    Start working on Cows with Guns.

--- src/main/java/epicsquid/roots/potion/PotionGeas.java
+++ src/main/java/epicsquid/roots/potion/PotionGeas.java
@@ -1,7 +1,26 @@
 package epicsquid.roots.potion;
 
+import epicsquid.roots.modifiers.instance.staff.ModifierSnapshot;
+import epicsquid.roots.modifiers.instance.staff.StaffModifierInstanceList;
+import epicsquid.roots.spell.SpellGeas;
+import epicsquid.roots.util.EntityUtil;
+import net.minecraft.entity.EntityCreature;
+import net.minecraft.entity.EntityLiving;
+import net.minecraft.entity.EntityLivingBase;
+import net.minecraft.entity.SharedMonsterAttributes;
+import net.minecraft.entity.ai.EntityAIAttackMelee;
+import net.minecraft.entity.ai.EntityAIBase;
+import net.minecraft.entity.ai.EntityAINearestAttackableTarget;
+import net.minecraft.entity.ai.EntityAITasks;
+import net.minecraft.entity.ai.attributes.AbstractAttributeMap;
+import net.minecraft.entity.ai.attributes.IAttributeInstance;
+import net.minecraft.entity.monster.AbstractSkeleton;
+import net.minecraft.entity.monster.EntityMob;
+import net.minecraft.entity.passive.EntityWolf;
 import net.minecraft.potion.Potion;
 import net.minecraft.potion.PotionEffect;
+
+import java.util.function.Predicate;
 
 public class PotionGeas extends Potion {
   public PotionGeas() {
@@ -14,6 +33,89 @@ public class PotionGeas extends Potion {
   @Override
   public boolean isReady(int duration, int amplifier) {
     return true;
+  }
+
+  @Override
+  public void applyAttributesModifiersToEntity(EntityLivingBase target, AbstractAttributeMap attributeMapIn, int amplifier) {
+    super.applyAttributesModifiersToEntity(target, attributeMapIn, amplifier);
+    if (target instanceof EntityCreature && EntityUtil.isFriendly(target)) {
+      EntityCreature entity = (EntityCreature) target;
+      ModifierSnapshot mods = StaffModifierInstanceList.fromSnapshot(target.getEntityData(), SpellGeas.instance);
+      if (mods.has(SpellGeas.PEACEFUL)) {
+        boolean hadAttack = false;
+        for (EntityAITasks.EntityAITaskEntry task : entity.tasks.taskEntries) {
+          if (task.action instanceof EntityAIAttackMelee) {
+            hadAttack = true;
+            break;
+          }
+        }
+        if (!hadAttack) {
+          entity.tasks.addTask(5, new EntityAIAttackMelee(entity, 1.0d, false));
+        }
+        boolean hadTarget = false;
+        for (EntityAITasks.EntityAITaskEntry task : entity.targetTasks.taskEntries) {
+          if (task.action instanceof EntityAINearestAttackableTarget) {
+            hadTarget = true;
+            break;
+          }
+        }
+        if (!hadTarget) {
+          entity.targetTasks.addTask(5, new EntityAINearestAttackableTarget<>(entity, EntityMob.class, 10, true, true, EntityUtil::isHostile));
+        }
+        boolean hadDamage = false;
+        //noinspection ConstantConditions
+        if (entity.getAttributeMap().getAttributeInstance(SharedMonsterAttributes.ATTACK_DAMAGE) == null) {
+          entity.getAttributeMap().registerAttribute(SharedMonsterAttributes.ATTACK_DAMAGE).setBaseValue(2);
+        } else {
+          hadDamage = true;
+        }
+        target.getEntityData().setBoolean("hadAttack", hadAttack);
+        target.getEntityData().setBoolean("hadTarget", hadTarget);
+        target.getEntityData().setBoolean("hadDamage", hadDamage);
+      }
+    }
+  }
+
+  @Override
+  public void removeAttributesModifiersFromEntity(EntityLivingBase target, AbstractAttributeMap attributeMapIn, int amplifier) {
+    super.removeAttributesModifiersFromEntity(target, attributeMapIn, amplifier);
+    if (target instanceof EntityCreature && EntityUtil.isFriendly(target)) {
+      EntityCreature entity = (EntityCreature) target;
+      ModifierSnapshot mods = StaffModifierInstanceList.fromSnapshot(target.getEntityData(), SpellGeas.instance);
+      if (mods.has(SpellGeas.PEACEFUL)) {
+        EntityAIBase attack = null;
+        for (EntityAITasks.EntityAITaskEntry task : entity.tasks.taskEntries) {
+          if (task.action instanceof EntityAIAttackMelee) {
+            attack = task.action;
+            break;
+          }
+        }
+        if (attack != null && !entity.getEntityData().getBoolean("hadAttack")) {
+          entity.tasks.removeTask(attack);
+        }
+        EntityAIBase targAi = null;
+        for (EntityAITasks.EntityAITaskEntry task : entity.targetTasks.taskEntries) {
+          if (task.action instanceof EntityAINearestAttackableTarget) {
+            targAi = task.action;
+            break;
+          }
+        }
+        if (targAi != null && !entity.getEntityData().getBoolean("hadTarget")) {
+          entity.targetTasks.removeTask(targAi);
+        }
+
+        if (!entity.getEntityData().getBoolean("hadDamage")) {
+          IAttributeInstance attr = entity.getAttributeMap().getAttributeInstanceByName(SharedMonsterAttributes.ATTACK_DAMAGE.getName());
+          if (attr != null) {
+            attr.setBaseValue(0);
+          }
+        }
+
+        target.getEntityData().removeTag("hadAttack");
+        target.getEntityData().removeTag("hadTarget");
+        target.getEntityData().removeTag("hadDamage");
+      }
+    }
   }
 
   @Override
