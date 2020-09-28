9b99197b836377491c0c2e6cadcbc7db9978eebe
Jon McManus <due@wxwhatever.com>
2020-09-28 10:33:25 +1000

    Finalise the Magnetism modifier for Radiance.

--- src/main/java/epicsquid/roots/event/DropsHandler.java
+++ src/main/java/epicsquid/roots/event/DropsHandler.java
@@ -1,16 +1,20 @@
 package epicsquid.roots.event;
 
 import epicsquid.roots.Roots;
+import epicsquid.roots.config.SpellConfig;
 import net.minecraft.entity.Entity;
 import net.minecraft.entity.EntityLivingBase;
 import net.minecraft.entity.item.EntityItem;
+import net.minecraft.entity.item.EntityXPOrb;
 import net.minecraft.entity.player.EntityPlayer;
 import net.minecraft.nbt.NBTTagCompound;
 import net.minecraft.util.DamageSource;
+import net.minecraft.world.World;
 import net.minecraftforge.common.util.Constants;
 import net.minecraftforge.event.entity.living.LivingDropsEvent;
 import net.minecraftforge.event.entity.living.LivingExperienceDropEvent;
 import net.minecraftforge.fml.common.Mod;
+import net.minecraftforge.fml.common.eventhandler.EventPriority;
 import net.minecraftforge.fml.common.eventhandler.SubscribeEvent;
 
 import java.util.UUID;
@@ -42,10 +46,30 @@ public class DropsHandler {
     }
   }
 
-  @SubscribeEvent
-  public static void onExperienceDrop (LivingExperienceDropEvent event) {
-    if () {
-
+  @SubscribeEvent(priority = EventPriority.LOWEST)
+  public static void onExperienceDrop(LivingExperienceDropEvent event) {
+    if (SpellConfig.spellFeaturesCategory.shouldMagnetismAttractXP) {
+      EntityLivingBase dying = event.getEntityLiving();
+      NBTTagCompound data = dying.getEntityData();
+      if (data.hasKey("magnetic_ticks", Constants.NBT.TAG_INT)) {
+        int ticks = data.getInteger("magnetic_ticks");
+        if (ticks < dying.ticksExisted || (dying.ticksExisted - ticks) < 3 * 20) {
+          if (data.hasUniqueId("magnetic")) {
+            UUID id = data.getUniqueId("magnetic");
+            EntityPlayer player = event.getAttackingPlayer();
+            if (id != null && id.equals(player.getUniqueID())) {
+              int i = event.getDroppedExperience();
+              World world = event.getAttackingPlayer().world;
+              while (i > 0) {
+                int j = EntityXPOrb.getXPSplit(i);
+                i -= j;
+                world.spawnEntity(new EntityXPOrb(world, player.posX, player.posY, player.posZ, j));
+              }
+              event.setCanceled(true);
+            }
+          }
+        }
+      }
     }
   }
 }
