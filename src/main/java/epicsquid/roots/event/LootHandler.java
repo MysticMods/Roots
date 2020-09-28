f8af474e18e1239321525ae1c8946d5adfd01f52
Jon McManus <due@wxwhatever.com>
2020-09-28 10:22:45 +1000

    Cache the output of isOre.

--- src/main/java/epicsquid/roots/event/LootHandler.java
+++ src/main/java/epicsquid/roots/event/LootHandler.java
@@ -16,7 +16,9 @@ import net.minecraft.world.storage.loot.conditions.LootCondition;
 import net.minecraftforge.common.util.Constants;
 import net.minecraftforge.event.LootTableLoadEvent;
 import net.minecraftforge.event.entity.living.LivingDropsEvent;
+import net.minecraftforge.event.entity.living.LivingExperienceDropEvent;
 import net.minecraftforge.fml.common.Mod;
+import net.minecraftforge.fml.common.eventhandler.EventPriority;
 import net.minecraftforge.fml.common.eventhandler.SubscribeEvent;
 
 import java.util.Set;
@@ -41,27 +43,7 @@ public class LootHandler {
   }
 
   @SubscribeEvent
-  public static void onLivingDrop(LivingDropsEvent event) {
-    DamageSource source = event.getSource();
-    Entity entity = source.getTrueSource();
-    if (entity instanceof EntityPlayer) {
-      EntityLivingBase dying = event.getEntityLiving();
-      NBTTagCompound data = dying.getEntityData();
-      if (data.hasKey("magnetic_ticks", Constants.NBT.TAG_INT)) {
-        int ticks = data.getInteger("magnetic_ticks");
-        if (ticks < dying.ticksExisted || (dying.ticksExisted - ticks) < 5 * 20) {
-          if (data.hasUniqueId("magnetic")) {
-            UUID id = data.getUniqueId("magnetic");
-            if (id != null && id.equals(entity.getUniqueID())) {
-              for (EntityItem item : event.getDrops()) {
-                item.setPosition(entity.posX, entity.posY, entity.posZ);
-                entity.world.spawnEntity(item);
-              }
-              event.setCanceled(true);
-            }
-          }
-        }
-      }
-    }
+  public static void onExperienceDrop (LivingExperienceDropEvent event) {
+    if (
   }
 }
