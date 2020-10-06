5559ac41ec486e727f50ba6eb2e544e974a15081
Jon McManus <due@wxwhatever.com>
2020-10-06 20:21:11 +1000

    Reactants for the geas potion.

--- src/main/java/epicsquid/roots/potion/PotionGeas.java
+++ src/main/java/epicsquid/roots/potion/PotionGeas.java
@@ -1,5 +1,6 @@
 package epicsquid.roots.potion;
 
+import epicsquid.roots.init.ModDamage;
 import epicsquid.roots.modifiers.instance.staff.ModifierSnapshot;
 import epicsquid.roots.modifiers.instance.staff.StaffModifierInstanceList;
 import epicsquid.roots.spell.SpellGeas;
@@ -15,8 +16,10 @@ import net.minecraft.entity.ai.EntityAITasks;
 import net.minecraft.entity.ai.attributes.AbstractAttributeMap;
 import net.minecraft.entity.ai.attributes.IAttributeInstance;
 import net.minecraft.entity.monster.EntityMob;
+import net.minecraft.init.MobEffects;
 import net.minecraft.potion.Potion;
 import net.minecraft.potion.PotionEffect;
+import net.minecraft.util.DamageSource;
 
 public class PotionGeas extends Potion {
   public PotionGeas() {
@@ -116,6 +119,20 @@ public class PotionGeas extends Potion {
         target.getEntityData().removeTag("hadAttack");
         target.getEntityData().removeTag("hadTarget");
         target.getEntityData().removeTag("hadDamage");
+      }
+    } else {
+      ModifierSnapshot mods = StaffModifierInstanceList.fromSnapshot(target.getEntityData(), SpellGeas.instance);
+      if (!mods.has(SpellGeas.PEACEFUL) || !EntityUtil.isFriendly(target)) {
+        if (mods.has(SpellGeas.PARALYSIS)) {
+          target.addPotionEffect(new PotionEffect(MobEffects.SLOWNESS, SpellGeas.instance.root_duration, 10));
+        } else if (mods.has(SpellGeas.FIRE)) {
+          target.setFire(SpellGeas.instance.fire_duration);
+          target.attackEntityFrom(DamageSource.IN_FIRE, SpellGeas.instance.fire_damage);
+        } else if (mods.has(SpellGeas.PHYSICAL)) {
+          target.attackEntityFrom(ModDamage.PHYSICAL_DAMAGE, SpellGeas.instance.physical_damage);
+        } else if (mods.has(SpellGeas.WATER)) {
+          target.attackEntityFrom(ModDamage.waterDamageFrom(null), SpellGeas.instance.water_damage);
+        }
       }
     }
   }
