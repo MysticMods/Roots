ed78c4ce6e6d274632c4fba68cb03deb540f6010
Jon McManus <due@wxwhatever.com>
2020-10-06 19:10:32 +1000

    Some effects for Geas.

--- src/main/java/epicsquid/roots/network/fx/MessageTargetedGeasFX.java
+++ src/main/java/epicsquid/roots/network/fx/MessageTargetedGeasFX.java
@@ -1,5 +1,6 @@
 package epicsquid.roots.network.fx;
 
+import epicsquid.mysticallib.util.Util;
 import epicsquid.mysticallib.util.VecUtil;
 import epicsquid.roots.particle.ParticleUtil;
 import epicsquid.roots.spell.SpellGeas;
@@ -7,7 +8,6 @@ import io.netty.buffer.ByteBuf;
 import net.minecraft.client.Minecraft;
 import net.minecraft.entity.player.EntityPlayer;
 import net.minecraft.util.math.BlockPos;
-import net.minecraft.util.math.MathHelper;
 import net.minecraft.util.math.Vec3d;
 import net.minecraft.world.World;
 import net.minecraftforge.fml.common.network.simpleimpl.IMessage;
@@ -16,43 +16,33 @@ import net.minecraftforge.fml.common.network.simpleimpl.MessageContext;
 import net.minecraftforge.fml.relauncher.Side;
 import net.minecraftforge.fml.relauncher.SideOnly;
 
-import java.util.Random;
-
-public class MessageSenseHomeFX implements IMessage {
-  private BlockPos home;
-
-  public MessageSenseHomeFX() {
+public class MessageTargetedGeasFX implements IMessage {
+  public MessageTargetedGeasFX() {
     super();
-  }
-
-  public MessageSenseHomeFX(BlockPos home) {
-    this.home = home;
   }
 
   @Override
   public void fromBytes(ByteBuf buf) {
-    this.home = new BlockPos(buf.readInt(), buf.readInt(), buf.readInt());
   }
 
   @Override
   public void toBytes(ByteBuf buf) {
-    buf.writeInt(home.getX());
-    buf.writeInt(home.getY());
-    buf.writeInt(home.getZ());
   }
 
-  private static final float[] color = new float[]{182/255.0f, 109/255.0f, 191/255.0f, 0.5f};
-
-  public static class MessageHolder implements IMessageHandler<MessageSenseHomeFX, IMessage> {
+  public static class MessageHolder implements IMessageHandler<MessageTargetedGeasFX, IMessage> {
     @SideOnly(Side.CLIENT)
     @Override
-    public IMessage onMessage(final MessageSenseHomeFX message, final MessageContext ctx) {
+    public IMessage onMessage(final MessageTargetedGeasFX message, final MessageContext ctx) {
       World world = Minecraft.getMinecraft().world;
       EntityPlayer player = Minecraft.getMinecraft().player;
       Vec3d playerPos = player.getPositionVector().add(0, 1, 0);
-      Vec3d line = new Vec3d(message.home.getX(), player.posY, message.home.getZ()).add(0.5, 0.5, 0.5);
-      for (Vec3d vec : VecUtil.pointsBetween(playerPos, line, 15)) {
-        ParticleUtil.spawnParticlePetal(world, (float) vec.x, (float) vec.y, (float) vec.z, 0, 0, 0, color, 15, 5 * 20);
+      Vec3d lookVec = player.getLookVec().scale(SpellGeas.instance.distance);
+      for (Vec3d vec : VecUtil.pointsFrom(playerPos, lookVec)) {
+        if (Util.rand.nextBoolean()) {
+          ParticleUtil.spawnParticleGlow(world, (float) vec.x, (float) vec.y, (float) vec.z, 0, 0.125f * (Util.rand.nextFloat() - 0.5f), 0, SpellGeas.instance.getFirstColours(0.75f), 2f + Util.rand.nextFloat() * 2f, 20);
+        } else {
+          ParticleUtil.spawnParticleGlow(world, (float) vec.x, (float) vec.y, (float) vec.z, 0, 0.125f * (Util.rand.nextFloat() - 0.5f), 0, SpellGeas.instance.getSecondColours(0.75f), 2f + Util.rand.nextFloat() * 2f, 20);
+        }
       }
       return null;
     }
