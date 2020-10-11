fad2ece2a0d2691acda88bf87c8aafc4ef410d4d
Jon McManus <due@wxwhatever.com>
2020-10-11 22:30:30 +1000

    Finalise Chem Trails.

--- src/main/java/epicsquid/roots/network/fx/MessageChemTrailsFX.java
+++ src/main/java/epicsquid/roots/network/fx/MessageChemTrailsFX.java
@@ -3,6 +3,7 @@ package epicsquid.roots.network.fx;
 import epicsquid.mysticallib.util.Util;
 import epicsquid.roots.particle.ParticleUtil;
 import epicsquid.roots.spell.SpellNaturesScythe;
+import epicsquid.roots.spell.SpellSkySoarer;
 import io.netty.buffer.ByteBuf;
 import net.minecraft.client.Minecraft;
 import net.minecraft.world.World;
@@ -41,11 +42,17 @@ public class MessageChemTrailsFX implements IMessage {
     }
   }
 
+  private static final float[] smoke = new float[]{87/255.0f, 79/255.0f, 75/255.0f, 1.0f};
+
   public static class Handler implements IMessageHandler<MessageChemTrailsFX, IMessage> {
     @SideOnly(Side.CLIENT)
     @Override
     public IMessage onMessage(MessageChemTrailsFX message, MessageContext ctx) {
       World world = Minecraft.getMinecraft().world;
+
+      for (int i = 0; i < 4; i++) {
+        ParticleUtil.spawnParticleSmoke(world, (float) message.pos[0] + (Util.rand.nextFloat()) - 0.5f, (float) (message.pos[1] - 1f) + (Util.rand.nextFloat()) + 0.5f, (float) message.pos[2] + (Util.rand.nextFloat()) - 0.5f, -0.125f * (float) message.motion[0], -0.125f * (float) message.motion[1], -0.125f * (float) message.motion[2], smoke, 5.0f * Util.rand.nextFloat() + 5.0f, 190, false);
+      }
       return null;
     }
   }
