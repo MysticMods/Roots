fb6ea787cafe8d39d52c16cfb0f756428f9d8b84
Jon McManus <due@wxwhatever.com>
2020-10-11 16:47:17 +1000

    Minor code clean up.

--- src/main/java/epicsquid/roots/particle/ParticleUtil.java
+++ src/main/java/epicsquid/roots/particle/ParticleUtil.java
@@ -28,6 +28,10 @@ public class ParticleUtil {
     }
   }
 
+  public static void spawnParticleStar(World world, float x, float y, float z, float vx, float vy, float vz, float[] rgba, float scale, int lifetime) {
+    spawnParticleStar(world, x, y, z, vx, vy, vz, rgba[0], rgba[1], rgba[2], rgba[3], scale, lifetime);
+  }
+
   public static void spawnParticleStar(World world, float x, float y, float z, float vx, float vy, float vz, float r, float g, float b, float a, float scale, int lifetime) {
     if (MysticalLib.proxy instanceof ClientProxy) {
       counter += random.nextInt(3);
