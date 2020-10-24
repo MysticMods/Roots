58857cbe40891604ff059ff13bfc359534b6303e
Jon McManus <due@wxwhatever.com>
2020-10-24 20:25:07 +1000

    Fix #585, adjust Sky Soarer duration and some of the amplifier methods.
    i.e., all of the amplifier methods.

--- src/main/java/epicsquid/roots/spell/ISpellMulitipliers.java
+++ src/main/java/epicsquid/roots/spell/ISpellMulitipliers.java
@@ -1,5 +1,7 @@
 package epicsquid.roots.spell;
 
+import net.minecraft.util.math.MathHelper;
+
 public interface ISpellMulitipliers {
   Buff getAmplify ();
   Buff getSpeedy ();
@@ -29,111 +31,111 @@ public interface ISpellMulitipliers {
   }
 
   default int ampSubInt(int value) {
-    return (int) (value - Math.floor((double) value * getAmplifyValue()));
+    return MathHelper.floor(value - value * getAmplifyValue());
   }
 
   default int ampSubInt(float value) {
-    return (int) (value - Math.floor((double) value * getAmplifyValue()));
+    return MathHelper.floor(value - value * getAmplifyValue());
   }
 
   default int ampSubInt(double value) {
-    return (int) (value - Math.floor(value * getAmplifyValue()));
+    return MathHelper.floor(value - value * getAmplifyValue());
   }
 
   default float ampSubFloat(int value) {
-    return (float) (value - Math.floor(value * getAmplifyValue()));
+    return (float) (value - value * getAmplifyValue());
   }
 
   default float ampSubFloat(float value) {
-    return (float) (value - Math.floor(value * getAmplifyValue()));
+    return (float) (value - value * getAmplifyValue());
   }
 
   default float ampSubFloat(double value) {
-    return (float) (value - Math.floor(value * getAmplifyValue()));
+    return (float) (value - value * getAmplifyValue());
   }
 
   default double ampSubDouble(int value) {
-    return value - Math.floor(value * getAmplifyValue());
+    return value - value * getAmplifyValue();
   }
 
   default double ampSubDouble(double value) {
-    return value - Math.floor(value * getAmplifyValue());
+    return value - value * getAmplifyValue();
   }
 
   default double ampSubDouble(float value) {
-    return value - Math.floor(value * getAmplifyValue());
+    return value - value * getAmplifyValue();
   }
 
   default int ampInt(int value) {
-    return (int) (value + Math.floor((double) value * getAmplifyValue()));
+    return MathHelper.floor(value + value * getAmplifyValue());
   }
 
   default int ampInt(float value) {
-    return (int) (value + Math.floor((double) value * getAmplifyValue()));
+    return MathHelper.floor(value + value * getAmplifyValue());
   }
 
   default int ampInt(double value) {
-    return (int) (value + Math.floor(value * getAmplifyValue()));
+    return MathHelper.floor(value + value * getAmplifyValue());
   }
 
   default float ampFloat(int value) {
-    return (float) (value + Math.floor(value * getAmplifyValue()));
+    return (float) (value + value * getAmplifyValue());
   }
 
   default float ampFloat(float value) {
-    return (float) (value + Math.floor(value * getAmplifyValue()));
+    return (float) (value + value * getAmplifyValue());
   }
 
   default float ampFloat(double value) {
-    return (float) (value + Math.floor(value * getAmplifyValue()));
+    return (float) (value + value * getAmplifyValue());
   }
 
   default double ampDouble(int value) {
-    return value + Math.floor(value * getAmplifyValue());
+    return value + value * getAmplifyValue();
   }
 
   default double ampDouble(double value) {
-    return value + Math.floor(value * getAmplifyValue());
+    return value + value * getAmplifyValue();
   }
 
   default double ampDouble(float value) {
-    return value + Math.floor(value * getAmplifyValue());
+    return value + value * getAmplifyValue();
   }
 
   default float speedSubFloat(int value) {
-    return (float) (value - Math.floor((float) value * getSpeedValue()));
+    return (float) (value - value * getSpeedValue());
   }
 
   default float speedSubFloat(double value) {
-    return (float) (value - Math.floor(value * getSpeedValue()));
+    return (float) (value - value * getSpeedValue());
   }
 
   default float speedSubFloat(float value) {
-    return (float) (value - Math.floor(value * getSpeedValue()));
+    return (float) (value - value * getSpeedValue());
   }
 
   default double speedSubDouble(int value) {
-    return value - Math.floor((double) value * getSpeedValue());
+    return value - value * getSpeedValue();
   }
 
   default double speedSubDouble(float value) {
-    return value - Math.floor(value * getSpeedValue());
+    return value - value * getSpeedValue();
   }
 
   default double speedSubDouble(double value) {
-    return value - Math.floor(value * getSpeedValue());
+    return value - value * getSpeedValue();
   }
 
   default int speedSubInt(int value) {
-    return (int) Math.floor((double) value * getSpeedValue());
+    return MathHelper.floor(value - value * getSpeedValue());
   }
 
   default int speedSubInt(float value) {
-    return (int) Math.floor(value * getSpeedValue());
+    return MathHelper.floor(value - value * getSpeedValue());
   }
 
   default int speedSubInt(double value) {
-    return (int) Math.floor(value * getSpeedValue());
+    return MathHelper.floor(value - value * getSpeedValue());
   }
 
   enum Buff {
