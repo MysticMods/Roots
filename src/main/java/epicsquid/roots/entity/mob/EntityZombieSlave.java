ba98a1223cb920786b7926d44b8b000e884d4143
Jon McManus <due@wxwhatever.com>
2020-11-03 17:10:08 +1000

    Fix slaves attacking each other.

--- src/main/java/epicsquid/roots/entity/mob/EntityZombieSlave.java
+++ src/main/java/epicsquid/roots/entity/mob/EntityZombieSlave.java
@@ -1,6 +1,7 @@
 package epicsquid.roots.entity.mob;
 
 import epicsquid.roots.util.EntityUtil;
+import epicsquid.roots.util.SlaveUtil;
 import net.minecraft.entity.ai.EntityAINearestAttackableTarget;
 import net.minecraft.entity.monster.EntityMob;
 import net.minecraft.entity.monster.EntityZombie;
@@ -24,6 +25,6 @@ public class EntityZombieSlave extends EntityZombie {
 
   @Override
   protected void applyEntityAI() {
-    this.targetTasks.addTask(2, new EntityAINearestAttackableTarget<>(this, EntityMob.class, 10, false, false, EntityUtil::isHostile));
+    this.targetTasks.addTask(2, new EntityAINearestAttackableTarget<>(this, EntityMob.class, 10, false, false, o -> EntityUtil.isHostile(o) && !SlaveUtil.isSlave(o)));
   }
 }
