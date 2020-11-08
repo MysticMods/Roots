2ba1a1c123a05af7bf2055860fa2e83d4c171f92
Jon McManus <due@wxwhatever.com>
2020-11-08 19:05:40 +1000

    Fix #618: reversed numbers for gunpowder/blaze.

--- src/main/java/epicsquid/roots/modifiers/BaseModifiers.java
+++ src/main/java/epicsquid/roots/modifiers/BaseModifiers.java
@@ -49,10 +49,10 @@ public class BaseModifiers {
   public static Modifier NULL = new Modifier(new ResourceLocation(Roots.MODID, "null"), AIR, Cost.noCost());
 
   public static Modifier SPEEDY = ModifierRegistry.register(new Modifier(new ResourceLocation(Roots.MODID, "speedy"), ModifierCores.REDSTONE, Cost.single(CostType.ALL_COST_MULTIPLIER, 0.05))); // Decreases the cooldownTotal or interval of the spell
-  public static Modifier EMPOWER = ModifierRegistry.register(new Modifier(new ResourceLocation(Roots.MODID, "empower"), ModifierCores.GUNPOWDER, Cost.single(CostType.ALL_COST_MULTIPLIER, 0.10))); // Additionally increases the duration or damage of the spell if applicable
+  public static Modifier EMPOWER = ModifierRegistry.register(new Modifier(new ResourceLocation(Roots.MODID, "empower"), ModifierCores.GUNPOWDER, Cost.single(CostType.ALL_COST_MULTIPLIER, 0.05))); // Additionally increases the duration or damage of the spell if applicable
   public static Modifier REDUCTION = ModifierRegistry.register(new Modifier(new ResourceLocation(Roots.MODID, "reduction"), ModifierCores.GLOWSTONE, Cost.single(CostType.ALL_COST_MULTIPLIER, -0.05))); // Reduces the cost of the spell.
 
   public static Modifier GREATER_SPEEDY = ModifierRegistry.register(new Modifier(new ResourceLocation(Roots.MODID, "greater_speedy"), ModifierCores.NETHER_WART, Cost.single(CostType.ALL_COST_MULTIPLIER, 0.10))); // Decreases the cooldownTotal or interval of the spell again
-  public static Modifier GREATER_EMPOWER = ModifierRegistry.register(new Modifier(new ResourceLocation(Roots.MODID, "greater_empower"), ModifierCores.BLAZE_POWDER, Cost.single(CostType.ALL_COST_MULTIPLIER, 0.05))); // Increases duration or damage of the spell if applicable.
+  public static Modifier GREATER_EMPOWER = ModifierRegistry.register(new Modifier(new ResourceLocation(Roots.MODID, "greater_empower"), ModifierCores.BLAZE_POWDER, Cost.single(CostType.ALL_COST_MULTIPLIER, 0.10))); // Increases duration or damage of the spell if applicable.
   public static Modifier GREATER_REDUCTION = ModifierRegistry.register(new Modifier(new ResourceLocation(Roots.MODID, "greater_reduction"), ModifierCores.RUNIC_DUST, Cost.single(CostType.ALL_COST_MULTIPLIER, -0.10))); // Additionally reduces the cost of the spell
 }
