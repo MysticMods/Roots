0ed5bf931f66081914fabf29a6059daf12f9578f
Jon McManus <due@wxwhatever.com>
2020-11-10 19:38:53 +1000

    Make animal servants slightly more functional.
    They attack but ... don't do any damage.

--- src/main/java/epicsquid/roots/potion/PotionGeas.java
+++ src/main/java/epicsquid/roots/potion/PotionGeas.java
@@ -1,5 +1,6 @@
 package epicsquid.roots.potion;
 
+import epicsquid.roots.entity.ai.EntityAIPassiveAttackMelee;
 import epicsquid.roots.init.ModDamage;
 import epicsquid.roots.modifiers.instance.staff.ModifierSnapshot;
 import epicsquid.roots.modifiers.instance.staff.StaffModifierInstanceList;
@@ -10,12 +11,11 @@ import net.minecraft.entity.EntityCreature;
 import net.minecraft.entity.EntityLivingBase;
 import net.minecraft.entity.SharedMonsterAttributes;
 import net.minecraft.entity.ai.EntityAIAttackMelee;
-import net.minecraft.entity.ai.EntityAIBase;
 import net.minecraft.entity.ai.EntityAINearestAttackableTarget;
-import net.minecraft.entity.ai.EntityAITasks;
 import net.minecraft.entity.ai.attributes.AbstractAttributeMap;
 import net.minecraft.entity.ai.attributes.IAttributeInstance;
 import net.minecraft.entity.monster.EntityMob;
+import net.minecraft.entity.passive.EntityCow;
 import net.minecraft.init.MobEffects;
 import net.minecraft.potion.Potion;
 import net.minecraft.potion.PotionEffect;
@@ -43,22 +43,14 @@ public class PotionGeas extends Potion {
       if (mods.has(SpellGeas.PEACEFUL)) {
         boolean hadAttack = entity.tasks.taskEntries.stream().anyMatch(o -> o.action instanceof EntityAIAttackMelee);
         if (!hadAttack) {
-          entity.tasks.addTask(5, new EntityAIAttackMelee(entity, 1.0d, false));
+           entity.tasks.addTask(5, new EntityAIPassiveAttackMelee(entity, 1.0d, false));
         }
         boolean hadTarget = entity.targetTasks.taskEntries.stream().anyMatch(o -> o.action instanceof EntityAINearestAttackableTarget);
         if (!hadTarget) {
-          entity.targetTasks.addTask(5, new EntityAINearestAttackableTarget<>(entity, EntityMob.class, 10, false, false, o -> EntityUtil.isHostile(o) && !SlaveUtil.isSlave(o)));
-        }
-        boolean hadDamage = false;
-        //noinspection ConstantConditions
-        if (entity.getAttributeMap().getAttributeInstance(SharedMonsterAttributes.ATTACK_DAMAGE) == null) {
-          entity.getAttributeMap().registerAttribute(SharedMonsterAttributes.ATTACK_DAMAGE).setBaseValue(2);
-        } else {
-          hadDamage = true;
+          entity.targetTasks.addTask(5, new EntityAINearestAttackableTarget<>(entity, EntityMob.class, 10, false, false, o -> !SlaveUtil.isSlave(o)));
         }
         target.getEntityData().setBoolean("hadAttack", hadAttack);
         target.getEntityData().setBoolean("hadTarget", hadTarget);
-        target.getEntityData().setBoolean("hadDamage", hadDamage);
       }
     }
   }
@@ -84,16 +76,8 @@ public class PotionGeas extends Potion {
           entity.targetTasks.taskEntries.removeIf(o -> o.action instanceof EntityAINearestAttackableTarget);
         }
 
-        if (!entity.getEntityData().getBoolean("hadDamage")) {
-          IAttributeInstance attr = entity.getAttributeMap().getAttributeInstanceByName(SharedMonsterAttributes.ATTACK_DAMAGE.getName());
-          if (attr != null) {
-            attr.setBaseValue(0);
-          }
-        }
-
         target.getEntityData().removeTag("hadAttack");
         target.getEntityData().removeTag("hadTarget");
-        target.getEntityData().removeTag("hadDamage");
       } else if (!EntityUtil.isFriendly(target) || !mods.has(SpellGeas.PEACEFUL)) {
         if (mods.has(SpellGeas.WEAKNESS)) {
           target.addPotionEffect(new PotionEffect(MobEffects.WEAKNESS, mods.ampInt(SpellGeas.instance.weakness_duration), SpellGeas.instance.weakness_amplifier));
@@ -104,7 +88,7 @@ public class PotionGeas extends Potion {
           // TODO: VISUAL EFFECT
           target.attackEntityFrom(ModDamage.PHYSICAL_DAMAGE, mods.ampFloat(SpellGeas.instance.physical_damage));
         } else if (mods.has(SpellGeas.WATER)) {
-          // TODO: ROCK FALL e VERYONE DIE
+          // TODO: ROCK FALL EVERYONE DIE
           target.attackEntityFrom(ModDamage.waterDamageFrom(null), mods.ampFloat(SpellGeas.instance.water_damage));
         }
       }
