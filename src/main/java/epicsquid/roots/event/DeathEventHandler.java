eba34e8ac00045c3c74668a1620040bdbd327d8a
Jon McManus <due@wxwhatever.com>
2020-12-18 18:05:52 +1000

    Fix #294: implement check for nearby hostiles.
    Should hopefully prevent some proximity failures.

--- src/main/java/epicsquid/roots/event/DeathEventHandler.java
+++ src/main/java/epicsquid/roots/event/DeathEventHandler.java
@@ -5,19 +5,25 @@ import epicsquid.roots.advancements.Advancements;
 import epicsquid.roots.config.GeneralConfig;
 import epicsquid.roots.init.ModRecipes;
 import epicsquid.roots.recipe.PacifistEntry;
+import epicsquid.roots.util.EntityUtil;
 import net.minecraft.entity.Entity;
 import net.minecraft.entity.EntityLivingBase;
 import net.minecraft.entity.player.EntityPlayer;
 import net.minecraft.entity.player.EntityPlayerMP;
 import net.minecraft.util.DamageSource;
 import net.minecraft.util.EntityDamageSource;
+import net.minecraft.util.math.AxisAlignedBB;
 import net.minecraftforge.event.entity.living.LivingDeathEvent;
 import net.minecraftforge.fml.common.Mod;
 import net.minecraftforge.fml.common.eventhandler.SubscribeEvent;
 
+import java.util.List;
+
 @Mod.EventBusSubscriber(modid = Roots.MODID)
 @SuppressWarnings("unused")
 public class DeathEventHandler {
+  public static AxisAlignedBB BOUNDING_BOX = new AxisAlignedBB(-0.75, -0.75, -0.75, 1.75, 1.75, 1.75);
+
   @SubscribeEvent
   public static void onDeath(LivingDeathEvent event) {
     if (!GeneralConfig.UntruePacifist) {
@@ -35,6 +41,11 @@ public class DeathEventHandler {
     if (!entry.matches(entity, (EntityPlayer) trueSource)) return;
 
     if (entity.getControllingPassenger() != null) return;
+
+    List<Entity> entities = entity.getEntityWorld().getEntitiesInAABBexcluding(entity, BOUNDING_BOX.offset(entity.getPosition()), EntityUtil::isHostile);
+    if (!entities.isEmpty()) {
+      return;
+    }
 
     Advancements.PACIFIST_TRIGGER.trigger((EntityPlayerMP) trueSource, event);
   }
