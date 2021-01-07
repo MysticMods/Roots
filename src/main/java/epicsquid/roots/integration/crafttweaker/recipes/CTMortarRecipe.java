f2c496b68214a0feb17da4a6b902d12585d1e2d3
Jon McManus <due@wxwhatever.com>
2021-01-07 12:40:15 +1000

    Convert Mortar CraftTweaker integration to use recipe names.

--- src/main/java/epicsquid/roots/integration/crafttweaker/recipes/CTMortarRecipe.java
+++ src/main/java/epicsquid/roots/integration/crafttweaker/recipes/CTMortarRecipe.java
@@ -2,10 +2,12 @@ package epicsquid.roots.integration.crafttweaker.recipes;
 
 import crafttweaker.api.item.IIngredient;
 import crafttweaker.api.minecraft.CraftTweakerMC;
+import epicsquid.roots.Roots;
 import epicsquid.roots.recipe.MortarRecipe;
 import epicsquid.roots.tileentity.TileEntityMortar;
 import net.minecraft.item.ItemStack;
 import net.minecraft.item.crafting.Ingredient;
+import net.minecraft.util.ResourceLocation;
 
 import java.util.List;
 import java.util.stream.Collectors;
@@ -13,15 +15,18 @@ import java.util.stream.Collectors;
 public class CTMortarRecipe extends MortarRecipe {
   private List<IIngredient> ingredients;
   private List<Ingredient> convertedIngredients;
+  private ResourceLocation name;
 
-  public CTMortarRecipe(ItemStack result, List<IIngredient> ingredients) {
+  public CTMortarRecipe(String name, ItemStack result, List<IIngredient> ingredients) {
     super(result, new Ingredient[]{});
+    this.name = new ResourceLocation(Roots.MODID, name);
     this.ingredients = ingredients;
     this.convertedIngredients = ingredients.stream().map(CraftTweakerMC::getIngredient).collect(Collectors.toList());
   }
 
-  public CTMortarRecipe(ItemStack result, List<IIngredient> ingredients, float red1, float green1, float blue1, float red2, float green2, float blue2) {
+  public CTMortarRecipe(String name, ItemStack result, List<IIngredient> ingredients, float red1, float green1, float blue1, float red2, float green2, float blue2) {
     super(result, new Ingredient[]{}, red1, green1, blue1, red2, green2, blue2);
+    this.name = new ResourceLocation(Roots.MODID, name);
     this.ingredients = ingredients;
     this.convertedIngredients = ingredients.stream().map(CraftTweakerMC::getIngredient).collect(Collectors.toList());
   }
