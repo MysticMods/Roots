10eba2f7c4b9d5a193a4f0b3e0ebd4bb05c2bfbe
Jon McManus <due@wxwhatever.com>
2021-03-04 14:27:25 +1000

    Cayalyst Plates, Fire-starting.

---
+++ src/main/java/epicsquid/roots/integration/patchouli/OredictSpotlightProcessor.java
@@ -0,0 +1,26 @@
+package epicsquid.roots.integration.patchouli;
+
+import net.minecraft.item.crafting.Ingredient;
+import net.minecraftforge.oredict.OreIngredient;
+import vazkii.patchouli.api.IComponentProcessor;
+import vazkii.patchouli.api.IVariableProvider;
+import vazkii.patchouli.common.util.ItemStackUtil;
+
+@SuppressWarnings("unused")
+public class OredictSpotlightProcessor implements IComponentProcessor {
+
+  private Ingredient ingredient;
+
+  @Override
+  public void setup(IVariableProvider<String> iVariableProvider) {
+    ingredient = new OreIngredient(iVariableProvider.get("oredict"));
+  }
+
+  @Override
+  public String process(String s) {
+    if (s.equals("input")) {
+      return ItemStackUtil.serializeIngredient(ingredient);
+    }
+    return null;
+  }
+}
