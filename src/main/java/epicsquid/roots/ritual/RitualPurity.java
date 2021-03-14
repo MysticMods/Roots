8ad03b583e8030c702e71415d6c4b54f3e2e5fce
Jon McManus <due@wxwhatever.com>
2021-03-14 20:02:23 +1000

    Overgrowth, Purity, Spreading Forest. Tweaks.

--- src/main/java/epicsquid/roots/ritual/RitualPurity.java
+++ src/main/java/epicsquid/roots/ritual/RitualPurity.java
@@ -16,7 +16,7 @@ public class RitualPurity extends RitualBase {
   public static Property<Integer> PROP_RADIUS_X = new Property<>("radius_x", 15).setDescription("Radius on the X Axis of the cube in which the ritual takes place");
   public static Property<Integer> PROP_RADIUS_Y = new Property<>("radius_y", 15).setDescription("Radius on the Y Axis of the cube in which the ritual takes place");
   public static Property<Integer> PROP_RADIUS_Z = new Property<>("radius_z", 15).setDescription("Radius on the Z Axis of the cube in which the ritual takes place");
-  public static Property.PropertyInterval PROP_INTERVAL = new Property.PropertyInterval(20).setDescription("interval in ticks between each conversion boost");
+  public static Property.PropertyInterval PROP_INTERVAL = new Property.PropertyInterval(20).setDescription("interval in ticks between each purification or conversion boost");
   public static Property<Integer> PROP_ZOMBIE_COUNT = new Property<>("zombie_count", 1).setDescription("the rate at which the zombie villager is converted back to a villager");
 
   public double radius_x, radius_y, radius_z;
