47e8787b9a52cdadea24a3774860af2cd892f262
Jon McManus <due@wxwhatever.com>
2021-03-17 10:24:49 +1000

    More bark processing, fey crafting templates.

--- src/main/java/epicsquid/roots/integration/patchouli/BarkRecipeProcessor.java
+++ src/main/java/epicsquid/roots/integration/patchouli/BarkRecipeProcessor.java
@@ -10,54 +10,85 @@ import vazkii.patchouli.common.util.ItemStackUtil;
 public class BarkRecipeProcessor implements IComponentProcessor {
   private BarkRecipe recipe1 = null;
   private BarkRecipe recipe2 = null;
+  private BarkRecipe recipe3 = null;
+  private BarkRecipe recipe4 = null;
+  private BarkRecipe recipe5 = null;
 
   @Override
   public void setup(IVariableProvider<String> iVariableProvider) {
-    String recipe1 = null;
-    String recipe2 = null;
-    if (iVariableProvider.has("recipe")) {
-      recipe1 = iVariableProvider.get("recipe");
-    } else if (iVariableProvider.has("recipe1")) {
-      recipe1 = iVariableProvider.get("recipe1");
-    }
-    if (iVariableProvider.has("recipe2")) {
-      recipe2 = iVariableProvider.get("recipe2");
-    }
-    if (recipe1 == null) {
-      throw new IllegalStateException("must have at least one recipe");
+    String recipe1 = iVariableProvider.get("recipe1");
+    String recipe2 = iVariableProvider.get("recipe2");
+    if (recipe1 == null || recipe2 == null) {
+      throw new IllegalStateException("must have at least two recipes");
     }
     this.recipe1 = ModRecipes.getBarkRecipeByName(new ResourceLocation(recipe1));
-    if (recipe2 != null) {
-      this.recipe2 = ModRecipes.getBarkRecipeByName(new ResourceLocation(recipe2));
-      if (this.recipe2 == null) {
-        throw new IllegalArgumentException("invalid argument provided for recipe2: " + recipe2);
-      }
+    this.recipe2 = ModRecipes.getBarkRecipeByName(new ResourceLocation(recipe2));
+    if (this.recipe1 == null || this.recipe2 == null) {
+      throw new IllegalArgumentException("invalid argumnet provided for recipes: " + recipe1 + ", " + recipe2);
     }
-    if (this.recipe1 == null) {
-      throw new IllegalArgumentException("invalid argumnet provided for recipe1: " + recipe1);
+
+    String recipe3 = null;
+    String recipe4 = null;
+    String recipe5 = null;
+    boolean quint = false;
+    if (iVariableProvider.has("recipe3")) {
+      recipe3 = iVariableProvider.get("recipe3");
+      quint = true;
+    }
+    if (iVariableProvider.has("recipe4")) {
+      recipe4 = iVariableProvider.get("recipe4");
+      quint = true;
+    }
+    if (iVariableProvider.has("recipe5")) {
+      recipe5 = iVariableProvider.get("recipe5");
+      quint = true;
+    }
+    if (quint && (recipe3 == null || recipe4 == null || recipe5 == null)) {
+      throw new IllegalStateException("must have five total recipes recipes");
+    }
+    if (quint) {
+      this.recipe3 = ModRecipes.getBarkRecipeByName(new ResourceLocation(recipe3));
+      this.recipe4 = ModRecipes.getBarkRecipeByName(new ResourceLocation(recipe4));
+      this.recipe5 = ModRecipes.getBarkRecipeByName(new ResourceLocation(recipe5));
+      if (this.recipe3 == null || this.recipe4 == null || this.recipe5 == null) {
+        throw new IllegalArgumentException("invalid argument provided for recipes: " + recipe3 + ", " + recipe4 + ", " + recipe5);
+      }
     }
   }
 
   @Override
   public String process(String s) {
-    if (s.equals("input") || s.equals("input1")) {
+    if (s.equals("input1")) {
       return ItemStackUtil.serializeStack(recipe1.getBlockStack());
     }
     if (s.equals("input2")) {
-      if (recipe1 == null) {
-        throw new IllegalStateException("this is not a dual-recipe template");
-      }
       return ItemStackUtil.serializeStack(recipe2.getBlockStack());
     }
+    if (s.equals("input3")) {
+      return ItemStackUtil.serializeStack(recipe3.getBlockStack());
+    }
+    if (s.equals("input4")) {
+      return ItemStackUtil.serializeStack(recipe4.getBlockStack());
+    }
+    if (s.equals("input5")) {
+      return ItemStackUtil.serializeStack(recipe5.getBlockStack());
+    }
 
-    if (s.equals("output") || s.equals("output1")) {
+    if (s.equals("output1")) {
       return ItemStackUtil.serializeStack(recipe1.getItem());
     }
     if (s.equals("output2")) {
-      if (recipe1 == null) {
-        throw new IllegalStateException("this is not a dual-recipe template");
-      }
       return ItemStackUtil.serializeStack(recipe2.getItem());
+    }
+
+    if (s.equals("output3")) {
+      return ItemStackUtil.serializeStack(recipe3.getItem());
+    }
+    if (s.equals("output4")) {
+      return ItemStackUtil.serializeStack(recipe4.getItem());
+    }
+    if (s.equals("output5")) {
+      return ItemStackUtil.serializeStack(recipe5.getItem());
     }
 
     return null;
