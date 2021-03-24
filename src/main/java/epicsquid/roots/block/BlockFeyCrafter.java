0f375b418d5634c74fd792c241da7539f5d6b48c
Jon McManus <due@wxwhatever.com>
2021-03-24 13:03:18 +1000

    Comparator support for both crafters.

--- src/main/java/epicsquid/roots/block/BlockFeyCrafter.java
+++ src/main/java/epicsquid/roots/block/BlockFeyCrafter.java
@@ -1,19 +1,30 @@
 package epicsquid.roots.block;
 
 import epicsquid.mysticallib.block.BlockTEBase;
+import epicsquid.roots.init.ModRecipes;
+import epicsquid.roots.recipe.FeyCraftingRecipe;
+import epicsquid.roots.recipe.MortarRecipe;
+import epicsquid.roots.spell.SpellBase;
+import epicsquid.roots.tileentity.TileEntityFeyCrafter;
+import epicsquid.roots.tileentity.TileEntityMortar;
+import epicsquid.roots.tileentity.TileEntityRunicCrafter;
 import net.minecraft.block.SoundType;
 import net.minecraft.block.material.Material;
 import net.minecraft.block.state.BlockFaceShape;
 import net.minecraft.block.state.IBlockState;
+import net.minecraft.item.ItemStack;
 import net.minecraft.tileentity.TileEntity;
 import net.minecraft.util.BlockRenderLayer;
 import net.minecraft.util.EnumFacing;
 import net.minecraft.util.math.AxisAlignedBB;
 import net.minecraft.util.math.BlockPos;
 import net.minecraft.world.IBlockAccess;
+import net.minecraft.world.World;
 
 import javax.annotation.Nonnull;
+import java.util.List;
 
+@SuppressWarnings("deprecation")
 public class BlockFeyCrafter extends BlockTEBase {
 
   public BlockFeyCrafter(@Nonnull Material mat, @Nonnull SoundType type, float hardness, @Nonnull String name, @Nonnull Class<? extends TileEntity> teClass) {
@@ -52,5 +63,35 @@ public class BlockFeyCrafter extends BlockTEBase {
       return BlockFaceShape.UNDEFINED;
     }
     return super.getBlockFaceShape(worldIn, state, pos, face);
+  }
+
+  @Override
+  public boolean hasComparatorInputOverride(IBlockState state) {
+    return true;
+  }
+
+  @Override
+  public int getComparatorInputOverride(IBlockState blockState, World worldIn, BlockPos pos) {
+    TileEntity te = worldIn.getTileEntity(pos);
+    if (te instanceof TileEntityRunicCrafter) {
+      TileEntityRunicCrafter tile = (TileEntityRunicCrafter) te;
+      FeyCraftingRecipe recipe = tile.getRecipe();
+      if (recipe == null) {
+        return 0;
+      }
+      if (recipe.matches(tile.getContents())) {
+        return 15;
+      }
+    }
+    if (te instanceof TileEntityFeyCrafter) {
+      TileEntityFeyCrafter tile = (TileEntityFeyCrafter) te;
+      if (tile.getRecipe() == null) {
+        return 0;
+      } else {
+        return 15;
+      }
+    }
+
+    return 0;
   }
 }
