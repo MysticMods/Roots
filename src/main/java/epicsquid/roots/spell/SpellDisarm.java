2b6388e0fe02fdfd532160df9e505c4d2d415111
Jon McManus <due@wxwhatever.com>
2021-07-23 19:55:14 +1000

    Fix #717: only one spell had identical colours.

--- src/main/java/epicsquid/roots/spell/SpellDisarm.java
+++ src/main/java/epicsquid/roots/spell/SpellDisarm.java
@@ -82,7 +82,7 @@ public class SpellDisarm extends SpellBase {
   private float drop_chance, armor_chance, chance_increase, knockback;
 
   private SpellDisarm(ResourceLocation name) {
-    super(name, TextFormatting.DARK_RED, 122F / 255F, 0F, 0F, 58F / 255F, 58F / 255F, 58F / 255F);
+    super(name, TextFormatting.DARK_RED, 58F / 255F, 58F / 255F, 58F / 255F, 122F / 255F, 0F, 0F);
     properties.add(PROP_COOLDOWN, PROP_CAST_TYPE, PROP_COST_1, PROP_COST_2, PROP_RADIUS_X, PROP_RADIUS_Y, PROP_RADIUS_Z, PROP_CHANCE_INCREASE, PROP_ARMOR, PROP_POISON_AMPLIFIER, PROP_POISON_DURATION, PROP_FIRE_DURATION, PROP_KNOCKBACK, PROP_WEAKNESS_AMPLIFIER, PROP_WEAKNESS_DURATION);
     acceptsModifiers(DROP_CHANCE, ARMOR1, DUO, ARMOR2, POISON, FLOWERS, FIRE, WEAKNESS, KNOCKBACK);
   }
