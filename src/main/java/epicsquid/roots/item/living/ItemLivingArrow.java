9d5c57f53b0fe5922c1915db5109c91973e2b3a2
Jon McManus <due@wxwhatever.com>
2021-08-02 14:59:53 +1000

    Fix #730: add dispenser behaviour for living arrows.

--- src/main/java/epicsquid/roots/item/living/ItemLivingArrow.java
+++ src/main/java/epicsquid/roots/item/living/ItemLivingArrow.java
@@ -2,14 +2,27 @@ package epicsquid.roots.item.living;
 
 import epicsquid.mysticallib.item.ItemArrowBase;
 import epicsquid.roots.entity.projectile.EntityLivingArrow;
+import net.minecraft.block.BlockDispenser;
+import net.minecraft.dispenser.BehaviorProjectileDispense;
+import net.minecraft.dispenser.IPosition;
 import net.minecraft.entity.EntityLivingBase;
+import net.minecraft.entity.IProjectile;
 import net.minecraft.entity.projectile.EntityArrow;
+import net.minecraft.entity.projectile.EntityTippedArrow;
+import net.minecraft.init.Items;
 import net.minecraft.item.ItemStack;
 import net.minecraft.world.World;
 
 public class ItemLivingArrow extends ItemArrowBase {
   public ItemLivingArrow(String name) {
     super(name);
+    BlockDispenser.DISPENSE_BEHAVIOR_REGISTRY.putObject(this, new BehaviorProjectileDispense() {
+      protected IProjectile getProjectileEntity(World worldIn, IPosition position, ItemStack stackIn) {
+        EntityLivingArrow entitytippedarrow = new EntityLivingArrow(worldIn, position.getX(), position.getY(), position.getZ());
+        entitytippedarrow.pickupStatus = EntityArrow.PickupStatus.ALLOWED;
+        return entitytippedarrow;
+      }
+    });
   }
 
   @Override
