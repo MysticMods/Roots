20b0bb8826fb37f5246c4ab4b3733e6021985529
Jon McManus <due@wxwhatever.com>
2021-10-27 09:15:26 +1000

    Move Blockstates to a datagen folder.

--- src/main/java/mysticmods/roots/blocks/RunedObsidianBlocks.java
+++ src/main/java/mysticmods/roots/blocks/RunedObsidianBlocks.java
@@ -1,53 +1,65 @@
 package mysticmods.roots.blocks;
 
-import net.minecraft.block.BlockState;
-import net.minecraft.block.SlabBlock;
-import net.minecraft.block.StairsBlock;
-import net.minecraft.block.WallBlock;
+import com.tterrag.registrate.util.nullness.NonNullUnaryOperator;
+import mysticmods.roots.init.ModBlocks;
+import net.minecraft.block.*;
 import noobanidus.libs.noobutil.block.BaseBlocks;
 
 import java.util.function.Supplier;
 
-public class RunedObsidianBlocks  {
-    public static class Button extends BaseBlocks.StoneButtonBlock {
-        public Button(Properties properties) {
-            super(properties);
-        }
-    }
+/**
+ * These classes are specifically designed to be wither- and ender-dragon-proof
+ * along with a high resistance to explosion damage.
+ */
+public class RunedObsidianBlocks {
+  public static NonNullUnaryOperator<AbstractBlock.Properties> RUNED_PROPERTIES = r -> AbstractBlock.Properties.copy(Blocks.OBSIDIAN);
 
-    public static class PressurePlate extends BaseBlocks.PressurePlateBlock {
-        public PressurePlate(Sensitivity sensitivityIn, Properties propertiesIn) {
-            super(sensitivityIn, propertiesIn);
-        }
+  public static class Block extends net.minecraft.block.Block {
+    public Block(Properties p_i48440_1_) {
+      super(p_i48440_1_);
     }
+  }
 
-    public static class Slab extends SlabBlock {
-        public Slab(Properties p_i48331_1_) {
-            super(p_i48331_1_);
-        }
+  public static class Button extends BaseBlocks.StoneButtonBlock {
+    public Button(Properties properties) {
+      super(properties);
     }
+  }
 
-    public static class Stairs extends StairsBlock {
-        public Stairs(Supplier<BlockState> state, Properties properties) {
-            super(state, properties);
-        }
+  public static class PressurePlate extends BaseBlocks.PressurePlateBlock {
+    public PressurePlate(Sensitivity sensitivityIn, Properties propertiesIn) {
+      super(sensitivityIn, propertiesIn);
     }
+  }
 
-    public static class Wall extends WallBlock {
-        public Wall(Properties p_i48301_1_) {
-            super(p_i48301_1_);
-        }
+  public static class Slab extends SlabBlock {
+    public Slab(Properties p_i48331_1_) {
+      super(p_i48331_1_);
     }
+  }
 
-    public static class NarrowPost extends BaseBlocks.NarrowPostBlock {
-        public NarrowPost(Properties properties) {
-            super(properties);
-        }
+  public static class Stairs extends StairsBlock {
+    public Stairs(Properties properties) {
+      // TODO
+      super(() -> null, properties);
     }
+  }
 
-    public static class WidePost extends BaseBlocks.WidePostBlock {
-        public WidePost(Properties properties) {
-            super(properties);
-        }
+  public static class Wall extends WallBlock {
+    public Wall(Properties p_i48301_1_) {
+      super(p_i48301_1_);
     }
+  }
+
+  public static class NarrowPost extends BaseBlocks.NarrowPostBlock {
+    public NarrowPost(Properties properties) {
+      super(properties);
+    }
+  }
+
+  public static class WidePost extends BaseBlocks.WidePostBlock {
+    public WidePost(Properties properties) {
+      super(properties);
+    }
+  }
 }
--- src/main/java/mysticmods/roots/init/ModBlocks.java
+++ src/main/java/mysticmods/roots/init/ModBlocks.java
@@ -1,7 +1,5 @@
 package mysticmods.roots.init;
 
-import com.tterrag.registrate.providers.DataGenContext;
-import com.tterrag.registrate.providers.RegistrateBlockstateProvider;
 import com.tterrag.registrate.util.entry.BlockEntry;
 import mysticmods.roots.blocks.*;
 import mysticmods.roots.RootsTags;
@@ -9,93 +7,64 @@ import mysticmods.roots.blocks.crops.CropBlock;
 import mysticmods.roots.blocks.crops.ElementalCropBlock;
 import mysticmods.roots.blocks.crops.ThreeStageCropBlock;
 import mysticmods.roots.blocks.crops.WaterElementalCropBlock;
+import mysticmods.roots.datagen.Blockstates;
 import net.minecraft.block.AbstractBlock;
-import net.minecraft.block.Block;
 import net.minecraft.block.Blocks;
-import net.minecraft.util.ResourceLocation;
-import net.minecraftforge.client.model.generators.ConfiguredModel;
-import net.minecraftforge.client.model.generators.ModelFile;
 
 import static mysticmods.roots.Roots.REGISTRATE;
 
 public class ModBlocks {
-  private static <T extends Block> void cropBlockstate(DataGenContext<Block, T> ctx, RegistrateBlockstateProvider p) {
-    String prefix = ctx.getName().replace("crop", "");
-    ModelFile crop = p.models().getExistingFile(new ResourceLocation("minecraft", "block/crop"));
-    p.getVariantBuilder(ctx.getEntry())
-        .forAllStates(state -> {
-          String file = prefix + state.getValue(((CropBlock) state.getBlock()).getAgeProperty());
-          ModelFile stage = p.models().getBuilder(file)
-              .parent(crop)
-              .texture("crop", p.modLoc("block/crops/" + file));
-          return ConfiguredModel.builder().modelFile(stage).build();
-        });
-  }
-
-  private static <T extends Block> void crossBlockstate(DataGenContext<Block, T> ctx, RegistrateBlockstateProvider p) {
-    String prefix = ctx.getName().replace("crop", "");
-    ModelFile crop = p.models().getExistingFile(new ResourceLocation("minecraft", "block/cross"));
-    p.getVariantBuilder(ctx.getEntry())
-        .forAllStates(state -> {
-          String file = prefix + state.getValue(((CropBlock) state.getBlock()).getAgeProperty());
-          ModelFile stage = p.models().getBuilder(file)
-              .parent(crop)
-              .texture("cross", p.modLoc("block/crops/" + file));
-          return ConfiguredModel.builder().modelFile(stage).build();
-        });
-  }
-
   public static class Crops {
     public static BlockEntry<ThreeStageCropBlock> WILDROOT_CROP = REGISTRATE.block("wildroot_crop", (p) -> new ThreeStageCropBlock(p, ModItems.Herbs.WILDROOT))
         .properties(o -> AbstractBlock.Properties.copy(Blocks.WHEAT))
-        .blockstate(ModBlocks::cropBlockstate)
+        .blockstate(Blockstates::cropBlockstate)
         .tag(RootsTags.Blocks.WILDROOT_CROP)
         .register();
 
     public static BlockEntry<ElementalCropBlock> CLOUD_BERRY_CROP = REGISTRATE.block("cloud_berry_crop", (p) -> new ElementalCropBlock(p, ModItems.Herbs.CLOUD_BERRY))
         .properties(o -> AbstractBlock.Properties.copy(Blocks.WHEAT))
-        .blockstate(ModBlocks::cropBlockstate)
+        .blockstate(Blockstates::cropBlockstate)
         .tag(RootsTags.Blocks.CLOUD_BERRY_CROP)
         .register();
     public static BlockEntry<WaterElementalCropBlock> DEWGONIA_CROP = REGISTRATE.block("dewgonia_crop", (p) -> new WaterElementalCropBlock(p, ModItems.Herbs.DEWGONIA))
         .properties(o -> AbstractBlock.Properties.copy(Blocks.WHEAT))
-        .blockstate(ModBlocks::cropBlockstate)
+        .blockstate(Blockstates::cropBlockstate)
         .tag(RootsTags.Blocks.DEWGONIA_CROP)
         .register();
     public static BlockEntry<ElementalCropBlock> INFERNAL_BULB_CROP = REGISTRATE.block("infernal_bulb_crop", (p) -> new ElementalCropBlock(p, ModItems.Herbs.INFERNAL_BULB))
         .properties(o -> AbstractBlock.Properties.copy(Blocks.WHEAT))
-        .blockstate(ModBlocks::crossBlockstate)
+        .blockstate(Blockstates::crossBlockstate)
         .tag(RootsTags.Blocks.INFERNAL_BULB_CROP)
         .register();
     public static BlockEntry<ElementalCropBlock> STALICRIPE_CROP = REGISTRATE.block("stalicripe_crop", (p) -> new ElementalCropBlock(p, ModItems.Herbs.STALICRIPE))
         .properties(o -> AbstractBlock.Properties.copy(Blocks.WHEAT))
-        .blockstate(ModBlocks::cropBlockstate)
+        .blockstate(Blockstates::cropBlockstate)
         .tag(RootsTags.Blocks.STALICRIPE_CROP)
         .register();
 
     public static BlockEntry<CropBlock> MOONGLOW_LEAF_CROP = REGISTRATE.block("moonglow_leaf_crop", (p) -> new CropBlock(p, ModItems.Seeds.MOONGLOW_LEAF_SEEDS))
         .properties(o -> AbstractBlock.Properties.copy(Blocks.WHEAT))
-        .blockstate(ModBlocks::cropBlockstate)
+        .blockstate(Blockstates::cropBlockstate)
         .tag(RootsTags.Blocks.MOONGLOW_LEAF_CROP)
         .register();
     public static BlockEntry<CropBlock> PERESKIA_CROP = REGISTRATE.block("pereskia_crop", (p) -> new CropBlock(p, ModItems.Seeds.PERESKIA_BULB))
         .properties(o -> AbstractBlock.Properties.copy(Blocks.WHEAT))
-        .blockstate(ModBlocks::crossBlockstate)
+        .blockstate(Blockstates::crossBlockstate)
         .tag(RootsTags.Blocks.PERESKIA_CROP)
         .register();
     public static BlockEntry<ThreeStageCropBlock> SPIRIT_HERB_CROP = REGISTRATE.block("spirit_herb_crop", (p) -> new ThreeStageCropBlock(p, ModItems.Seeds.SPIRIT_HERB_SEEDS))
         .properties(o -> AbstractBlock.Properties.copy(Blocks.WHEAT))
-        .blockstate(ModBlocks::cropBlockstate)
+        .blockstate(Blockstates::cropBlockstate)
         .tag(RootsTags.Blocks.SPIRIT_HERB_CROP)
         .register();
     public static BlockEntry<CropBlock> WILDEWHEET_CROP = REGISTRATE.block("wildewheet_crop", (p) -> new CropBlock(p, ModItems.Seeds.WILDEWHEET_SEEDS))
         .properties(o -> AbstractBlock.Properties.copy(Blocks.WHEAT))
-        .blockstate(ModBlocks::cropBlockstate)
+        .blockstate(Blockstates::cropBlockstate)
         .tag(RootsTags.Blocks.WILDEWHEET_CROP)
         .register();
   }
 
-   public static BlockEntry<FeyLightBlock> FEYLIGHT = REGISTRATE.block("feylight", FeyLightBlock::new)
+   public static BlockEntry<FeyLightBlock> FEY_LIGHT = REGISTRATE.block("fey_light", FeyLightBlock::new)
         .properties(o -> AbstractBlock.Properties.copy(Blocks.TORCH))
         .register();
   public static BlockEntry<CatalystPlateBlock> CATALYST_PLATE = REGISTRATE.block("catalyst_plate", CatalystPlateBlock::new).register();
