f63f7f93fbc87ebbf418148deae710715b11f069
Jon McManus <due@wxwhatever.com>
2021-10-27 09:22:05 +1000

    Don't declare runed obsidian blocks.
    We only need to use tags and properties now, hooray!

--- src/main/java/mysticmods/roots/init/ModBlocks.java
+++ src/main/java/mysticmods/roots/init/ModBlocks.java
@@ -1,6 +1,7 @@
 package mysticmods.roots.init;
 
 import com.tterrag.registrate.util.entry.BlockEntry;
+import com.tterrag.registrate.util.nullness.NonNullUnaryOperator;
 import mysticmods.roots.blocks.*;
 import mysticmods.roots.RootsTags;
 import mysticmods.roots.blocks.crops.CropBlock;
@@ -8,12 +9,59 @@ import mysticmods.roots.blocks.crops.ElementalCropBlock;
 import mysticmods.roots.blocks.crops.ThreeStageCropBlock;
 import mysticmods.roots.blocks.crops.WaterElementalCropBlock;
 import mysticmods.roots.datagen.Blockstates;
-import net.minecraft.block.AbstractBlock;
-import net.minecraft.block.Blocks;
+import net.minecraft.block.*;
+import net.minecraft.tags.BlockTags;
+import noobanidus.libs.noobutil.block.BaseBlocks;
 
 import static mysticmods.roots.Roots.REGISTRATE;
 
 public class ModBlocks {
+  public static class Decoration {
+    public static class RunedObsidian {
+      public static NonNullUnaryOperator<AbstractBlock.Properties> RUNED_PROPERTIES = r -> AbstractBlock.Properties.copy(Blocks.OBSIDIAN);
+
+      public static BlockEntry<Block> RUNED_OBSIDIAN = REGISTRATE.block("runed_obsidian", Block::new)
+          .properties(RUNED_PROPERTIES)
+          .tag(BlockTags.DRAGON_IMMUNE, BlockTags.WITHER_IMMUNE)
+          .register();
+
+      public static BlockEntry<BaseBlocks.StoneButtonBlock> RUNED_BUTTON = REGISTRATE.block("runed_button", BaseBlocks.StoneButtonBlock::new)
+          .properties(RUNED_PROPERTIES)
+          .tag(BlockTags.DRAGON_IMMUNE, BlockTags.WITHER_IMMUNE)
+          .register();
+
+      public static BlockEntry<BaseBlocks.PressurePlateBlock> RUNED_PRESSURE_PLATE = REGISTRATE.block("runed_pressure_plate", (p) -> new BaseBlocks.PressurePlateBlock(PressurePlateBlock.Sensitivity.MOBS, p))
+          .properties(RUNED_PROPERTIES)
+          .tag(BlockTags.DRAGON_IMMUNE, BlockTags.WITHER_IMMUNE)
+          .register();
+
+      public static BlockEntry<SlabBlock> RUNED_SLAB = REGISTRATE.block("runed_slab", SlabBlock::new)
+          .properties(RUNED_PROPERTIES)
+          .tag(BlockTags.DRAGON_IMMUNE, BlockTags.WITHER_IMMUNE)
+          .register();
+
+      public static BlockEntry<StairsBlock> RUNED_STAIRS = REGISTRATE.block("runed_stairs", (p) -> new StairsBlock(() -> RunedObsidian.RUNED_OBSIDIAN.getDefaultState(), p))
+          .properties(RUNED_PROPERTIES)
+          .tag(BlockTags.DRAGON_IMMUNE, BlockTags.WITHER_IMMUNE)
+          .register();
+
+      public static BlockEntry<WallBlock> RUNED_WALL = REGISTRATE.block("runed_wall", WallBlock::new)
+          .properties(RUNED_PROPERTIES)
+          .tag(BlockTags.DRAGON_IMMUNE, BlockTags.WITHER_IMMUNE)
+          .register();
+
+      public static BlockEntry<BaseBlocks.NarrowPostBlock> RUNED_NARROW_POST = REGISTRATE.block("runed_narrow_post", BaseBlocks.NarrowPostBlock::new)
+          .properties(RUNED_PROPERTIES)
+          .tag(BlockTags.DRAGON_IMMUNE, BlockTags.WITHER_IMMUNE)
+          .register();
+
+      public static BlockEntry<BaseBlocks.WidePostBlock> RUNED_WIDE_POST = REGISTRATE.block("runed_wide_post", BaseBlocks.WidePostBlock::new)
+          .properties(RUNED_PROPERTIES)
+          .tag(BlockTags.DRAGON_IMMUNE, BlockTags.WITHER_IMMUNE)
+          .register();
+    }
+  }
+
   public static class Crops {
     public static BlockEntry<ThreeStageCropBlock> WILDROOT_CROP = REGISTRATE.block("wildroot_crop", (p) -> new ThreeStageCropBlock(p, ModItems.Herbs.WILDROOT))
         .properties(o -> AbstractBlock.Properties.copy(Blocks.WHEAT))
