87158d2e2b7d3ba02fd7672d561dc7647824a135
Kevin Kragenbrink <kevin@writh.net>
2021-10-26 02:19:28 -0700

    scaffold for feylight

---
+++ src/main/java/mysticmods/roots/blocks/FeyLightBlock.java
@@ -0,0 +1,9 @@
+package mysticmods.roots.blocks;
+
+import net.minecraft.block.Block;
+
+public class FeyLightBlock extends Block {
+    public FeyLightBlock(Properties builder) {
+        super(builder);
+    }
+}
