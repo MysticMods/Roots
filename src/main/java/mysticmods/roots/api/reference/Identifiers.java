3e698b375b42e88d4df401d0c24dea5fe27871be
Jon McManus <due@wxwhatever.com>
2021-11-30 11:03:59 +1000

    Some reference classes.

---
+++ src/main/java/mysticmods/roots/api/reference/Identifiers.java
@@ -0,0 +1,4 @@
+package mysticmods.roots.api.reference;
+
+public interface Identifiers {
+}
---
+++ src/main/java/mysticmods/roots/api/reference/Shapes.java
@@ -0,0 +1,8 @@
+package mysticmods.roots.api.reference;
+
+import net.minecraft.block.Block;
+import net.minecraft.util.math.shapes.VoxelShape;
+
+public interface Shapes {
+  VoxelShape FEY_LIGHT = Block.box(0.33, 0.33, 0.33, 0.66, 0.66, 0.66);
+}
