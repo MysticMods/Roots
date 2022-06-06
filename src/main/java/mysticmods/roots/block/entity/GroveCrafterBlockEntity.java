a5a835e802b150ed286326c800025ade571b7ce3
Jon McManus <due@wxwhatever.com>
2022-06-06 10:09:05 +1000

    Slightly more dynamic pedestal positions.

--- src/main/java/mysticmods/roots/block/entity/GroveCrafterBlockEntity.java
+++ src/main/java/mysticmods/roots/block/entity/GroveCrafterBlockEntity.java
@@ -11,7 +11,12 @@ import net.minecraft.server.level.ServerLevel;
 import net.minecraft.world.level.block.entity.BlockEntityType;
 import net.minecraft.world.level.block.state.BlockState;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class GroveCrafterBlockEntity extends BaseBlockEntity implements MonitoringBlockEntity {
+  private List<BlockPos> pedestalPositions = null;
+
   public GroveCrafterBlockEntity(BlockEntityType<?> pType, BlockPos pWorldPosition, BlockState pBlockState) {
     super(pType, pWorldPosition, pBlockState);
   }
@@ -44,15 +49,40 @@ public class GroveCrafterBlockEntity extends BaseBlockEntity implements Monitori
     }
   }
 
+  public List<BlockPos> pedestalPositions () {
+    if (pedestalPositions != null) {
+      return pedestalPositions;
+    } else {
+      pedestalPositions = new ArrayList<>();
+      BlockPos.betweenClosedStream(getBoundingBox()).forEach(pos -> {
+        BlockState state = getLevel().getBlockState(pos);
+        if (state.is(RootsTags.Blocks.GROVE_PEDESTALS) && state.getValue(PedestalBlock.VALID)) {
+          pedestalPositions.add(pos.immutable());
+        }
+      });
+      return pedestalPositions;
+    }
+  }
+
   @Override
   public void notify(ServerLevel pLevel, BlockPos pPos) {
+    if (pedestalPositions == null) {
+      pedestalPositions = new ArrayList<>();
+    } else {
+      pedestalPositions.clear();
+    }
     BlockPos.betweenClosedStream(getBoundingBox()).forEach(pos -> {
       BlockState state = pLevel.getBlockState(pos);
-      if (state.is(RootsTags.Blocks.GROVE_PEDESTALS) && !state.getValue(PedestalBlock.VALID)) {
-        if (pLevel.getBlockEntity(pos) instanceof PedestalBlockEntity pedestal) {
-          if (!pedestal.getHeldItem().isEmpty()) {
-            pLevel.setBlock(pos, state.setValue(PedestalBlock.VALID, true), 1 | 2 | 8);
+      if (state.is(RootsTags.Blocks.GROVE_PEDESTALS)) {
+        if (!state.getValue(PedestalBlock.VALID)) {
+          if (pLevel.getBlockEntity(pos) instanceof PedestalBlockEntity pedestal) {
+            if (!pedestal.getHeldItem().isEmpty()) {
+              pLevel.setBlock(pos, state.setValue(PedestalBlock.VALID, true), 1 | 2 | 8);
+              pedestalPositions.add(pos.immutable());
+            }
           }
+        } else {
+          pedestalPositions.add(pos.immutable());
         }
       }
     });
