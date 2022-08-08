bd91c2e61c091addca65b6472f34be2e036fdc2f
Jon McManus <noobanidus@users.noreply.github.com>
2022-08-08 10:51:50 +1000

    Encode the tag in herbs.

--- src/main/java/mysticmods/roots/api/herbs/Herb.java
+++ src/main/java/mysticmods/roots/api/herbs/Herb.java
@@ -3,19 +3,27 @@ package mysticmods.roots.api.herbs;
 import mysticmods.roots.api.DescribedRegistryEntry;
 import mysticmods.roots.api.registry.Registries;
 import net.minecraft.resources.ResourceLocation;
+import net.minecraft.tags.TagKey;
+import net.minecraft.world.item.Item;
 import net.minecraft.world.level.ItemLike;
 
 import java.util.function.Supplier;
 
 public class Herb extends DescribedRegistryEntry<Herb> {
   private final Supplier<? extends ItemLike> item;
+  private final TagKey<Item> tag;
 
-  public Herb(Supplier<? extends ItemLike> item) {
+  public Herb(Supplier<? extends ItemLike> item, TagKey<Item> tag) {
     this.item = item;
+    this.tag = tag;
   }
 
   public ItemLike getItem() {
     return item.get();
+  }
+
+  public TagKey<Item> getTag() {
+    return tag;
   }
 
   @Override
--- src/main/java/mysticmods/roots/init/ModHerbs.java
+++ src/main/java/mysticmods/roots/init/ModHerbs.java
@@ -1,21 +1,22 @@
 package mysticmods.roots.init;
 
 import com.tterrag.registrate.util.entry.RegistryEntry;
+import mysticmods.roots.RootsTags;
 import mysticmods.roots.api.herbs.Herb;
 
 import static mysticmods.roots.Roots.REGISTRATE;
 
 public class ModHerbs {
-  public static final RegistryEntry<Herb> SACRED_MOSS = REGISTRATE.simple("sacred_moss", Herb.class, () -> new Herb(ModItems.Herbs.SACRED_MOSS));
-  public static final RegistryEntry<Herb> WILDROOT = REGISTRATE.simple("wildroot", Herb.class, () -> new Herb(ModItems.Herbs.WILDROOT));
-  public static final RegistryEntry<Herb> CLOUD_BERRY = REGISTRATE.simple("cloud_berry", Herb.class, () -> new Herb(ModItems.Herbs.CLOUD_BERRY));
-  public static final RegistryEntry<Herb> DEWGONIA = REGISTRATE.simple("dewgonia", Herb.class, () -> new Herb(ModItems.Herbs.DEWGONIA));
-  public static final RegistryEntry<Herb> INFERNAL_BULB = REGISTRATE.simple("infernal_bulb", Herb.class, () -> new Herb(ModItems.Herbs.INFERNAL_BULB));
-  public static final RegistryEntry<Herb> STALICRIPE = REGISTRATE.simple("stalicripe", Herb.class, () -> new Herb(ModItems.Herbs.STALICRIPE));
-  public static final RegistryEntry<Herb> MOONGLOW = REGISTRATE.simple("moonglow", Herb.class, () -> new Herb(ModItems.Herbs.MOONGLOW));
-  public static final RegistryEntry<Herb> PERESKIA = REGISTRATE.simple("pereskia", Herb.class, () -> new Herb(ModItems.Herbs.PERESKIA));
-  public static final RegistryEntry<Herb> SPIRIT_LEAF = REGISTRATE.simple("spirit_leaf", Herb.class, () -> new Herb(ModItems.Herbs.SPIRIT_LEAF));
-  public static final RegistryEntry<Herb> WILDEWHEET = REGISTRATE.simple("wildewheet", Herb.class, () -> new Herb(ModItems.Herbs.WILDEWHEET));
+  public static final RegistryEntry<Herb> SACRED_MOSS = REGISTRATE.simple("sacred_moss", Herb.class, () -> new Herb(ModItems.Herbs.SACRED_MOSS, RootsTags.Herbs.SACRED_MOSS));
+  public static final RegistryEntry<Herb> WILDROOT = REGISTRATE.simple("wildroot", Herb.class, () -> new Herb(ModItems.Herbs.WILDROOT, RootsTags.Herbs.WILDROOT));
+  public static final RegistryEntry<Herb> CLOUD_BERRY = REGISTRATE.simple("cloud_berry", Herb.class, () -> new Herb(ModItems.Herbs.CLOUD_BERRY, RootsTags.Herbs.CLOUD_BERRY));
+  public static final RegistryEntry<Herb> DEWGONIA = REGISTRATE.simple("dewgonia", Herb.class, () -> new Herb(ModItems.Herbs.DEWGONIA, RootsTags.Herbs.DEWGONIA));
+  public static final RegistryEntry<Herb> INFERNAL_BULB = REGISTRATE.simple("infernal_bulb", Herb.class, () -> new Herb(ModItems.Herbs.INFERNAL_BULB, RootsTags.Herbs.INFERNAL_BULB));
+  public static final RegistryEntry<Herb> STALICRIPE = REGISTRATE.simple("stalicripe", Herb.class, () -> new Herb(ModItems.Herbs.STALICRIPE, RootsTags.Herbs.STALICRIPE));
+  public static final RegistryEntry<Herb> MOONGLOW = REGISTRATE.simple("moonglow", Herb.class, () -> new Herb(ModItems.Herbs.MOONGLOW, RootsTags.Herbs.MOONGLOW));
+  public static final RegistryEntry<Herb> PERESKIA = REGISTRATE.simple("pereskia", Herb.class, () -> new Herb(ModItems.Herbs.PERESKIA, RootsTags.Herbs.PERESKIA));
+  public static final RegistryEntry<Herb> SPIRIT_LEAF = REGISTRATE.simple("spirit_leaf", Herb.class, () -> new Herb(ModItems.Herbs.SPIRIT_LEAF, RootsTags.Herbs.SPIRIT_LEAF));
+  public static final RegistryEntry<Herb> WILDEWHEET = REGISTRATE.simple("wildewheet", Herb.class, () -> new Herb(ModItems.Herbs.WILDEWHEET, RootsTags.Herbs.WILDEWHEET));
 
   public static void load() {
   }
