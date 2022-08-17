9a3d1c92085cfbce9e366c2239fef099ff782840
Jon McManus <noobanidus@users.noreply.github.com>
2022-08-17 13:06:08 +1000

    More ritual conditions.

--- src/main/java/mysticmods/roots/api/ritual/RitualCondition.java
+++ src/main/java/mysticmods/roots/api/ritual/RitualCondition.java
@@ -1,20 +1,20 @@
 package mysticmods.roots.api.ritual;
 
 import mysticmods.roots.api.DescribedRegistryEntry;
+import mysticmods.roots.api.registry.Registries;
 import net.minecraft.resources.ResourceLocation;
+import net.minecraft.world.item.Item;
 
 public class RitualCondition extends DescribedRegistryEntry<RitualCondition> {
   protected String descriptionId;
 
-
-
   @Override
   protected String getDescriptor() {
-    return null;
+    return "ritual_condition";
   }
 
   @Override
   public ResourceLocation getKey() {
-    return null;
+    return Registries.RITUAL_CONDITION_REGISTRY.get().getKey(this);
   }
 }
