b406cb358a964aa9d4e628dfb594c87192981c41
Jon McManus <noobanidus@users.noreply.github.com>
2022-08-20 12:47:59 +1000

    Stub in rituals and spells.

--- src/main/java/mysticmods/roots/api/reference/Rituals.java
+++ src/main/java/mysticmods/roots/api/reference/Rituals.java
@@ -6,9 +6,27 @@ import net.minecraft.resources.ResourceKey;
 import net.minecraft.resources.ResourceLocation;
 
 public interface Rituals {
+  // PLACEHOLDER
   ResourceKey<Ritual> CRAFTING = ritual("crafting");
-  ResourceKey<Ritual> TRANSMUTATION = ritual("transmutation");
+
+  // ACTUAL RITUALS, NOT ALL CONFIRMED
   ResourceKey<Ritual> ANIMAL_HARVEST = ritual("animal_harvest");
+  ResourceKey<Ritual> DIVINE_PROTECTION = ritual("divine_protection");
+  ResourceKey<Ritual> FIRE_STORM = ritual("fire_storm");
+  ResourceKey<Ritual> FLOWER_GROWTH = ritual("flower_growth");
+  ResourceKey<Ritual> FROST_LANDS = ritual("frost_lands");
+  ResourceKey<Ritual> GATHERING = ritual("gathering");
+  ResourceKey<Ritual> GERMINATION = ritual("germination");
+  ResourceKey<Ritual> HEALING_AURA = ritual("healing_aura");
+  ResourceKey<Ritual> HEAVY_STORMS = ritual("heavy_storms");
+  ResourceKey<Ritual> OVERGROWTH = ritual("overgrowth");
+  ResourceKey<Ritual> PURITY = ritual("purity");
+  ResourceKey<Ritual> SPREADING_FOREST = ritual("spreading_forest");
+  ResourceKey<Ritual> SUMMON_CREATURES = ritual("summon_creatures");
+  ResourceKey<Ritual> TRANSMUTATION = ritual("transmutation");
+  ResourceKey<Ritual> WARDING_PROTECTION = ritual("warding_protection");
+  ResourceKey<Ritual> WILDROOT_GROWTH = ritual("wildroot_growth");
+  ResourceKey<Ritual> WINDWALL = ritual("windwall");
 
   static ResourceKey<Ritual> ritual(String name) {
     return ResourceKey.create(RootsAPI.RITUAL_REGISTRY, new ResourceLocation(RootsAPI.MODID, name));
--- src/main/java/mysticmods/roots/api/reference/Spells.java
+++ src/main/java/mysticmods/roots/api/reference/Spells.java
@@ -6,7 +6,33 @@ import net.minecraft.resources.ResourceKey;
 import net.minecraft.resources.ResourceLocation;
 
 public interface Spells {
+  ResourceKey<Spell> ACID_CLOUD = spell("acid_cloud");
+  ResourceKey<Spell> AQUA_BUBBLE = spell("aqua_bubble");
+  ResourceKey<Spell> AUGMENT = spell("augment");
+  ResourceKey<Spell> LIGHT_DRIFTER = spell("light_drifter");
+  ResourceKey<Spell> MAGNETISM = spell("magnetism");
+  ResourceKey<Spell> CHRYSOPOEIA = spell("chrysopoeia");
+  ResourceKey<Spell> DANDELION_WINDS = spell("dandelion_winds");
+  ResourceKey<Spell> DESATURATE = spell("desaturate");
+  ResourceKey<Spell> DISARM = spell("disarm");
+  ResourceKey<Spell> EXTENSION = spell("extension");
+  ResourceKey<Spell> NONDETECTION = spell("nondetection");
+  ResourceKey<Spell> FEY_LIGHT = spell("fey_light");
+  ResourceKey<Spell> GEAS = spell("geas");
+  ResourceKey<Spell> CONTROL_UNDEAD = spell("control_undead");
   ResourceKey<Spell> GROWTH_INFUSION = spell("growth_infusion");
+  ResourceKey<Spell> HARVEST = spell("harvest");
+  ResourceKey<Spell> LIFE_DRAIN = spell("life_drain");
+  ResourceKey<Spell> PETAL_SHELL = spell("petal_shell");
+  ResourceKey<Spell> RADIANCE = spell("radiance");
+  ResourceKey<Spell> ROSE_THORNS = spell("rose_thorns");
+  ResourceKey<Spell> SANCTUARY = spell("sanctuary");
+  ResourceKey<Spell> SHATTER = spell("shatter");
+  ResourceKey<Spell> SKY_SOARER = spell("sky_soarer");
+  ResourceKey<Spell> JAUNT = spell("jaunt");
+  ResourceKey<Spell> STORM_CLOUD = spell("storm_cloud");
+  ResourceKey<Spell> TIME_STOP = spell("time_stop");
+  ResourceKey<Spell> WILDFIRE = spell("wildfire");
 
   static ResourceKey<Spell> spell(String name) {
     return ResourceKey.create(RootsAPI.SPELL_REGISTRY, new ResourceLocation(RootsAPI.MODID, name));
