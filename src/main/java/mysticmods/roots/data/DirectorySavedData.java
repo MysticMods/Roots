287d5999cc8c9cae5a4566f2f670c17ffde65f8b
Jon McManus <noobanidus@users.noreply.github.com>
2022-08-21 14:47:40 +1000

    Future TODO.

--- src/main/java/mysticmods/roots/data/DirectorySavedData.java
+++ src/main/java/mysticmods/roots/data/DirectorySavedData.java
@@ -4,6 +4,7 @@ import net.minecraft.world.level.saveddata.SavedData;
 
 import java.io.File;
 
+// TODO: Move to noobutil, remove from this
 public abstract class DirectorySavedData extends SavedData {
   @Override
   public void save(File pFile) {
