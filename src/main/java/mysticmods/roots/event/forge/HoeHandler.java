cb759ed4b815819655126ecdb258ef50be35a520
Jon McManus <noobanidus@users.noreply.github.com>
2022-09-13 14:19:22 +1000

    Reinstating hoeing underwater.

--- src/main/java/mysticmods/roots/event/forge/HoeHandler.java
+++ src/main/java/mysticmods/roots/event/forge/HoeHandler.java
@@ -1,26 +1,34 @@
 package mysticmods.roots.event.forge;
 
 import mysticmods.roots.api.RootsAPI;
+import net.minecraft.core.BlockPos;
+import net.minecraft.core.Direction;
+import net.minecraft.tags.FluidTags;
+import net.minecraft.world.item.context.UseOnContext;
+import net.minecraft.world.level.LevelAccessor;
+import net.minecraft.world.level.block.Blocks;
+import net.minecraft.world.level.block.state.BlockState;
+import net.minecraft.world.level.material.FluidState;
+import net.minecraftforge.common.ToolActions;
+import net.minecraftforge.event.level.BlockEvent;
+import net.minecraftforge.eventbus.api.SubscribeEvent;
 import net.minecraftforge.fml.common.Mod;
 
 @Mod.EventBusSubscriber(modid = RootsAPI.MODID)
 public class HoeHandler {
-/*  @SubscribeEvent
-  public static void onHoeUse(UseHoeEvent event) {
-    Level world = event.getPlayer().level;
-    UseOnContext pContext = event.getContext();
-    BlockPos blockpos = pContext.getClickedPos();
-    FluidState fluidstate = world.getFluidState(blockpos.above());
-    if (pContext.getClickedFace() != Direction.DOWN && fluidstate.is(FluidTags.WATER)) {
-      BlockState blockstate = world.getBlockState(blockpos).getToolModifiedState(world, blockpos, pContext.getPlayer(), pContext.getItemInHand(), ToolActions.HOE_TILL);
-      if (blockstate != null) {
-        Player playerentity = pContext.getPlayer();
-        world.playSound(playerentity, blockpos, SoundEvents.HOE_TILL, SoundSource.BLOCKS, 1.0F, 1.0F);
-        if (!world.isClientSide()) {
-          world.setBlock(blockpos, blockstate, 11);
+  @SubscribeEvent
+  public static void onHoeUse(BlockEvent.BlockToolModificationEvent event) {
+    if (event.getToolAction() == ToolActions.HOE_TILL) {
+      LevelAccessor world = event.getLevel();
+      UseOnContext pContext = event.getContext();
+      BlockPos blockpos = pContext.getClickedPos();
+      FluidState fluidstate = world.getFluidState(blockpos.above());
+      if (pContext.getClickedFace() != Direction.DOWN && fluidstate.is(FluidTags.WATER)) {
+        BlockState blockstate = world.getBlockState(blockpos);
+        if (blockstate.is(Blocks.GRASS) || blockstate.is(Blocks.DIRT_PATH) || blockstate.is(Blocks.DIRT)) {
+          event.setFinalState(Blocks.FARMLAND.defaultBlockState());
         }
-        event.setResult(Event.Result.ALLOW);
       }
     }
-  }*/
+  }
 }
