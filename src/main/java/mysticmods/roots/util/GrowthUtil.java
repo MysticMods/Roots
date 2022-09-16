71b34b703ffaffaa101fafff10bae20ade12a370
Jon McManus <noobanidus@users.noreply.github.com>
2022-09-16 12:04:09 +1000

    Begin work on growth util for the Growth Infusion spell.

--- src/main/java/mysticmods/roots/util/GrowthUtil.java
+++ src/main/java/mysticmods/roots/util/GrowthUtil.java
@@ -1,4 +1,35 @@
 package mysticmods.roots.util;
 
+import mysticmods.roots.api.RootsAPI;
+import net.minecraft.core.BlockPos;
+import net.minecraft.world.entity.player.Player;
+import net.minecraft.world.level.Level;
+import net.minecraft.world.level.block.Block;
+import net.minecraft.world.level.block.BonemealableBlock;
+import net.minecraft.world.level.block.CropBlock;
+import net.minecraft.world.level.block.state.BlockState;
+import org.jetbrains.annotations.Nullable;
+
 public class GrowthUtil {
+  public boolean canGrow(Level level, BlockPos pos, @Nullable BlockState state, @Nullable Player player) {
+    if (state == null) {
+      state = level.getBlockState(pos);
+    }
+
+    if (state.is(RootsAPI.Tags.Blocks.GROWTH_BLACKLIST)) {
+      return false;
+    }
+
+    if (state.is(RootsAPI.Tags.Blocks.GROWTH_FORCE)) {
+      return true;
+    }
+
+    Block block = state.getBlock();
+    if (block instanceof CropBlock crop) {
+      return !crop.isMaxAge(state);
+    }
+
+    // TODO: This should probably be checked
+    return block instanceof BonemealableBlock;
+  }
 }
