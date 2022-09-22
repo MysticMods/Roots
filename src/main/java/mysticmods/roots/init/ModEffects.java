a2a6ec844f4884b732e28f42babd9373bf4f9b81
Jon McManus <noobanidus@users.noreply.github.com>
2022-09-22 13:35:19 +1000

    Revert "I'm not really happy with this."
    This reverts commit fd32fed34e49892d4a1048c815e19e83f2e38a3a.

--- src/main/java/mysticmods/roots/init/ModEffects.java
+++ src/main/java/mysticmods/roots/init/ModEffects.java
@@ -2,11 +2,15 @@ package mysticmods.roots.init;
 
 import com.tterrag.registrate.util.entry.RegistryEntry;
 import mysticmods.roots.effect.FriendlyEarthEffect;
+import mysticmods.roots.effect.SkySoarerEffect;
+import net.minecraftforge.registries.RegistryObject;
 
 import static mysticmods.roots.Roots.REGISTRATE;
 
 public class ModEffects {
   public static final RegistryEntry<FriendlyEarthEffect> FRIENDLY_EARTH = REGISTRATE.effect("friendly_earth", FriendlyEarthEffect::new).register();
+
+  public static final RegistryEntry<SkySoarerEffect> SKY_SOARER = REGISTRATE.effect("sky_soarer", SkySoarerEffect::new).register();
 
   public static void load() {
   }
