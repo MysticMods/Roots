5154d33560e5d681834950b1701c673bb46fbd89
ByThePowerOfScience <public.bythepowerofscience@gmail.com>
2023-02-18 22:11:21 -0500

    Fixes issue where changing dimensions while under the effects of Light Drifter will apply your original coords in the new dimension

--- src/main/java/epicsquid/roots/ExportDocumentation.java
+++ src/main/java/epicsquid/roots/ExportDocumentation.java
@@ -6,6 +6,7 @@ import epicsquid.roots.integration.crafttweaker.tweaks.predicates.*;
 import epicsquid.roots.util.zen.ZenDocExporter;
 
 import java.io.IOException;
+import java.io.InputStream;
 import java.nio.file.Files;
 import java.nio.file.Path;
 import java.nio.file.Paths;
