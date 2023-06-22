1b01f25d517c850933fe96f64f67cd8fbe546013
Jon McManus <noobanidus@users.noreply.github.com>
2023-06-22 13:26:11 +1000

    Add "NYI" to relevant not-yet-implemented block tooltips.

--- src/main/java/mysticmods/roots/event/forge/TooltipHandler.java
+++ src/main/java/mysticmods/roots/event/forge/TooltipHandler.java
@@ -1,2 +1,20 @@
-package mysticmods.roots.event.forge;public class TooltipHandler {
+package mysticmods.roots.event.forge;
+
+import com.google.common.eventbus.Subscribe;
+import mysticmods.roots.api.RootsAPI;
+import net.minecraft.network.chat.Component;
+import net.minecraft.world.item.ItemStack;
+import net.minecraftforge.event.entity.player.ItemTooltipEvent;
+import net.minecraftforge.eventbus.api.SubscribeEvent;
+import net.minecraftforge.fml.common.Mod;
+
+@Mod.EventBusSubscriber(modid= RootsAPI.MODID)
+public class TooltipHandler {
+  @SubscribeEvent
+  public static void onItemTooltip (ItemTooltipEvent event) {
+    ItemStack stack = event.getItemStack();
+    if (stack.is(RootsAPI.Tags.Items.NYI)) {
+      event.getToolTip().add(Component.literal("Not Yet Implemented"));
+    }
+  }
 }
