715a9835f75799c938436d01fa43d53e1a565eec
Jon McManus <noobanidus@users.noreply.github.com>
2023-08-16 13:51:17 +1000

    Implement the friendly earth effect.

--- src/main/java/mysticmods/roots/effect/FriendlyEarthEffect.java
+++ src/main/java/mysticmods/roots/effect/FriendlyEarthEffect.java
@@ -2,9 +2,20 @@ package mysticmods.roots.effect;
 
 import net.minecraft.world.effect.MobEffect;
 import net.minecraft.world.effect.MobEffectCategory;
+import net.minecraft.world.entity.LivingEntity;
 
 public class FriendlyEarthEffect extends MobEffect {
   public FriendlyEarthEffect() {
     super(MobEffectCategory.BENEFICIAL, 0x946434);
+  }
+
+  @Override
+  public void applyEffectTick(LivingEntity pLivingEntity, int pAmplifier) {
+    pLivingEntity.fallDistance = 0;
+  }
+
+  @Override
+  public boolean isDurationEffectTick(int pDuration, int pAmplifier) {
+    return true;
   }
 }
--- src/main/java/mysticmods/roots/init/ModEffects.java
+++ src/main/java/mysticmods/roots/init/ModEffects.java
@@ -1,17 +1,14 @@
 package mysticmods.roots.init;
 
 import com.tterrag.registrate.util.entry.RegistryEntry;
-import mysticmods.roots.effect.SenseDangerEffect;
-import mysticmods.roots.effect.SimpleEffect;
-import mysticmods.roots.effect.SkySoarerEffect;
-import mysticmods.roots.effect.WakefulEffect;
+import mysticmods.roots.effect.*;
 import net.minecraft.world.effect.MobEffectCategory;
 
 import static mysticmods.roots.Roots.REGISTRATE;
 
 public class ModEffects {
   public static final RegistryEntry<WakefulEffect> WAKEFUL = REGISTRATE.effect("wakeful", WakefulEffect::new).register();
-  public static final RegistryEntry<SimpleEffect> FRIENDLY_EARTH = REGISTRATE.effect("friendly_earth", () -> new SimpleEffect(MobEffectCategory.BENEFICIAL, 0x946434)).register();
+  public static final RegistryEntry<FriendlyEarthEffect> FRIENDLY_EARTH = REGISTRATE.effect("friendly_earth", FriendlyEarthEffect::new).register();
   public static final RegistryEntry<SkySoarerEffect> SKY_SOARER = REGISTRATE.effect("sky_soarer", SkySoarerEffect::new).register();
   public static final RegistryEntry<SimpleEffect> PETAL_SHELL = REGISTRATE.effect("petal_shell", () -> new SimpleEffect(MobEffectCategory.BENEFICIAL, 0xcc5ec8)).register();
   public static final RegistryEntry<SenseDangerEffect> SENSE_DANGER = REGISTRATE.effect("sense_danger", SenseDangerEffect::new).register();
