8f368be076843db6a0163b744fe96563ecdd30f3
Jon <due@wxwhatever.com>
2025-01-05 16:01:41 +1000

    More adjustments to data.

--- neoforge/src/main/java/mysticmods/roots/api/data/DataMaps.java
+++ neoforge/src/main/java/mysticmods/roots/api/data/DataMaps.java
@@ -2,6 +2,7 @@ package mysticmods.roots.api.data;
 
 import mysticmods.roots.api.RootsAPI;
 import mysticmods.roots.api.herb.Herb;
+import mysticmods.roots.api.modifier.SpellModifier;
 import mysticmods.roots.api.registry.RootsRegistries;
 import mysticmods.roots.api.spell.Spell;
 import net.minecraft.core.registries.Registries;
@@ -14,9 +15,13 @@ import net.neoforged.neoforge.registries.datamaps.RegisterDataMapTypesEvent;
 
 @EventBusSubscriber(modid = RootsAPI.MODID)
 public class DataMaps {
-  public static final AdvancedDataMapType<Spell, SpellCostDataMap.SpellCostData, SpellCostDataMap.CostRemover> SPELL_COST_DATA = AdvancedDataMapType.builder(RootsAPI.rl("spell_cost_data"), RootsRegistries.Keys.SPELLS, SpellCostDataMap.SpellCostData.CODEC)
-      .merger(new SpellCostDataMap.CostListMerger())
-      .remover(SpellCostDataMap.CostRemover.CODEC)
+  public static final AdvancedDataMapType<Spell, CostDataMap.CostData, CostDataMap.SpellCostRemover> SPELL_COST_DATA = AdvancedDataMapType.builder(RootsAPI.rl("spell_cost_data"), RootsRegistries.Keys.SPELLS, CostDataMap.CostData.CODEC)
+      .merger(CostDataMap.listMerger())
+      .remover(CostDataMap.SpellCostRemover.CODEC)
+      .build();
+  public static final AdvancedDataMapType<SpellModifier, CostDataMap.CostData, CostDataMap.SpellModifierCostRemover> SPELL_MODIFIER_COST_DATA = AdvancedDataMapType.builder(RootsAPI.rl("spell_modifier_cost_data"), RootsRegistries.Keys.SPELL_MODIFIERS, CostDataMap.CostData.CODEC)
+      .merger(CostDataMap.listMerger())
+      .remover(CostDataMap.SpellModifierCostRemover.CODEC)
       .build();
   public static final DataMapType<Item, Herb> HERB_ITEM_DATA = DataMapType.builder(RootsAPI.rl("herb_item_data"), Registries.ITEM, RootsRegistries.HERBS.byNameCodec())
       .build();
@@ -24,5 +29,7 @@ public class DataMaps {
   @SubscribeEvent
   public static void registerDataMaps (RegisterDataMapTypesEvent event) {
     event.register(SPELL_COST_DATA);
+    event.register(SPELL_MODIFIER_COST_DATA);
+    event.register(HERB_ITEM_DATA);
   }
 }
--- neoforge/src/main/java/mysticmods/roots/api/data/RootsDataMapProvider.java
+++ neoforge/src/main/java/mysticmods/roots/api/data/RootsDataMapProvider.java
@@ -1,17 +1,14 @@
 package mysticmods.roots.api.data;
 
-import mysticmods.roots.api.RootsAPI;
 import mysticmods.roots.api.herb.Herb;
+import mysticmods.roots.api.modifier.SpellModifier;
 import mysticmods.roots.api.registry.RootsRegistries;
 import mysticmods.roots.api.spell.Spell;
 import net.minecraft.core.HolderLookup;
 import net.minecraft.data.PackOutput;
 import net.minecraft.world.item.Item;
 import net.neoforged.neoforge.common.data.DataMapProvider;
-import net.neoforged.neoforge.registries.datamaps.AdvancedDataMapType;
-
 import java.util.concurrent.CompletableFuture;
-
 
 public class RootsDataMapProvider extends DataMapProvider {
   public RootsDataMapProvider(PackOutput packOutput, CompletableFuture<HolderLookup.Provider> lookupProvider) {
@@ -20,11 +17,11 @@ public class RootsDataMapProvider extends DataMapProvider {
 
   @Override
   protected void gather() {
-    Builder<SpellCostDataMap.SpellCostData, Spell> builder = builder(DataMaps.SPELL_COST_DATA)
+    Builder<CostDataMap.CostData, Spell> builder = builder(DataMaps.SPELL_COST_DATA)
         .replace(false);
 
     RootsRegistries.SPELLS.stream().forEach(spell -> {
-      builder.add(spell.builtInRegistryHolder(), new SpellCostDataMap.SpellCostData(spell.getCosts()), false);
+      builder.add(spell.builtInRegistryHolder(), new CostDataMap.CostData(spell.getCosts()), false);
     });
 
     Builder<Herb, Item> builder2 = builder(DataMaps.HERB_ITEM_DATA)
@@ -32,6 +29,13 @@ public class RootsDataMapProvider extends DataMapProvider {
 
     RootsRegistries.HERBS.stream().forEach(herb -> {
       builder2.add(herb.getItem(), herb, false);
+    });
+
+    Builder<CostDataMap.CostData, SpellModifier> builder3 = builder(DataMaps.SPELL_MODIFIER_COST_DATA)
+        .replace(false);
+
+    RootsRegistries.SPELL_MODIFIERS.stream().forEach(modifier -> {
+      builder3.add(modifier.builtInRegistryHolder(), new CostDataMap.CostData(modifier.getCosts()), false);
     });
   }
 }
