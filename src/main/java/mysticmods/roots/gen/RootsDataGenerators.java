a31d29a6cf5470d307c589ccc2ffd14aedb8ae73
Jon <due@wxwhatever.com>
2025-01-10 13:59:30 +1000

    Update structures!

--- src/main/java/mysticmods/roots/gen/RootsDataGenerators.java
+++ src/main/java/mysticmods/roots/gen/RootsDataGenerators.java
@@ -51,6 +51,7 @@ public class RootsDataGenerators {
     generator.addProvider(event.includeClient(), new RootsItemModelProvider(output, helper));
     generator.addProvider(event.includeClient(), new RootsBlockModelProvider(output, helper));
     generator.addProvider(event.includeServer(), new RootsGlobalLootModifierProvider(output, provider));
+    generator.addProvider(event.includeServer(), new StructureNbtUpdater("structures", RootsAPI.MODID, helper, output));
     generator.addProvider(true, new PackMetadataGenerator(output).add(PackMetadataSection.TYPE, new PackMetadataSection(Component.literal("Roots resources"), DetectedVersion.BUILT_IN.getPackVersion(PackType.SERVER_DATA), Optional.of(new InclusiveRange<>(0, Integer.MAX_VALUE)))));
   }
 }
