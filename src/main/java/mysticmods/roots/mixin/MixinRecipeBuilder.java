d9097721cd74117fe5ce0a47915e3b9e5cf3d66f
Jon <due@wxwhatever.com>
2025-01-22 17:15:23 +1000

    Log changed recipes names.

--- src/main/java/mysticmods/roots/mixin/MixinRecipeBuilder.java
+++ src/main/java/mysticmods/roots/mixin/MixinRecipeBuilder.java
@@ -1,5 +1,6 @@
 package mysticmods.roots.mixin;
 
+import mysticmods.roots.api.RootsAPI;
 import net.minecraft.data.recipes.RecipeBuilder;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.injection.At;
@@ -10,6 +11,7 @@ public interface MixinRecipeBuilder {
   @ModifyArg(method = "save(Lnet/minecraft/data/recipes/RecipeOutput;Ljava/lang/String;)V", at = @At(value = "INVOKE", target = "Lnet/minecraft/resources/ResourceLocation;parse(Ljava/lang/String;)Lnet/minecraft/resources/ResourceLocation;"))
   default String saveChangeThing(String id) {
     if (!id.startsWith("roots:")) {
+      RootsAPI.LOG.error("Had to fix recipe id: {}", id);
       return "roots:" + id;
     }
 
