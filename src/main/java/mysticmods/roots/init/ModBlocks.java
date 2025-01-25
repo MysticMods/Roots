6a4d06d3a8caba48ad30433a3f7b46d0f990e432
Jon <due@wxwhatever.com>
2025-01-25 13:53:24 +1000

    Hopefully fix blocks being washed away.

--- src/main/java/mysticmods/roots/init/ModBlocks.java
+++ src/main/java/mysticmods/roots/init/ModBlocks.java
@@ -32,9 +32,9 @@ public class ModBlocks {
   private static final BlockBehaviour.Properties RUNED_STEM_PROPERTIES = BlockBehaviour.Properties.ofFullCopy(net.minecraft.world.level.block.Blocks.CRIMSON_STEM);
   private static final BlockBehaviour.Properties RUNESTONE_BUTTON_PROPERTIES = BlockBehaviour.Properties.ofFullCopy(net.minecraft.world.level.block.Blocks.STONE_BUTTON);
   public static BlockBehaviour.Properties SOIL_PROPERTIES = BlockBehaviour.Properties.ofFullCopy(net.minecraft.world.level.block.Blocks.DIRT);
-  public static final BlockBehaviour.Properties BASE_PROPERTIES = BlockBehaviour.Properties.of().dynamicShape().noOcclusion().strength(1.5f).sound(SoundType.STONE);
-  public static final BlockBehaviour.Properties BASE_WOODEN_PROPERTIES = BlockBehaviour.Properties.of().dynamicShape().noOcclusion().strength(1.5f).sound(SoundType.WOOD);
-  public static final BlockBehaviour.Properties BASE_REINFORCED_PROPERTIES = BlockBehaviour.Properties.of().dynamicShape().noOcclusion().strength(1.5f).sound(SoundType.STONE).requiresCorrectToolForDrops().strength(50.0F, 1200.0F);
+  public static final BlockBehaviour.Properties BASE_PROPERTIES = BlockBehaviour.Properties.of().dynamicShape().forceSolidOn().noOcclusion().strength(1.5f).sound(SoundType.STONE);
+  public static final BlockBehaviour.Properties BASE_WOODEN_PROPERTIES = BlockBehaviour.Properties.of().dynamicShape().forceSolidOn().noOcclusion().strength(1.5f).sound(SoundType.WOOD);
+  public static final BlockBehaviour.Properties BASE_REINFORCED_PROPERTIES = BlockBehaviour.Properties.of().dynamicShape().forceSolidOn().noOcclusion().strength(1.5f).sound(SoundType.STONE).requiresCorrectToolForDrops().strength(50.0F, 1200.0F);
   public static BlockBehaviour.Properties CROP_PROPERTIES = BlockBehaviour.Properties.ofFullCopy(Blocks.WHEAT);
 
   public static DeferredHolder<Block, WaterloggedBlock> THATCH = BLOCKS.register("thatch", () -> new WaterloggedBlock(BlockBehaviour.Properties.ofFullCopy(Blocks.HAY_BLOCK)));
@@ -137,7 +137,7 @@ public class ModBlocks {
   public static DeferredHolder<Block, GroveStoneBlock> PRIMAL_GROVE_STONE = BLOCKS.register("primal_grove_stone", () -> new GroveStoneBlock(BASE_PROPERTIES));
   public static DeferredHolder<Block, IncenseBurnerBlock> INCENSE_BURNER = BLOCKS.register("incense_burner", () -> new IncenseBurnerBlock(BASE_PROPERTIES));
   public static DeferredHolder<Block, MortarBlock> MORTAR = BLOCKS.register("mortar", () -> new MortarBlock(BASE_PROPERTIES));
-  public static DeferredHolder<Block, PyreBlock> PYRE = BLOCKS.register("pyre", () -> new PyreBlock( BlockBehaviour.Properties.of().dynamicShape().noOcclusion().strength(1.5f).sound(SoundType.WOOD).lightLevel(o -> o.getValue(PyreBlock.LIT) ? 15 : 0)));
+  public static DeferredHolder<Block, PyreBlock> PYRE = BLOCKS.register("pyre", () -> new PyreBlock( BlockBehaviour.Properties.of().dynamicShape().forceSolidOn().noOcclusion().strength(1.5f).sound(SoundType.WOOD).lightLevel(o -> o.getValue(PyreBlock.LIT) ? 15 : 0)));
   public static DeferredHolder<Block, PyreBlock> REINFORCED_PYRE = BLOCKS.register("reinforced_pyre", () -> new PyreBlock(BASE_REINFORCED_PROPERTIES));
   public static DeferredHolder<Block, DecorativePyreBlock> DECORATIVE_PYRE = BLOCKS.register("decorative_pyre", () -> new DecorativePyreBlock(BASE_WOODEN_PROPERTIES.lightLevel((o) -> 15)));
   public static DeferredHolder<Block, UnendingBowlBlock> UNENDING_BOWL = BLOCKS.register("unending_bowl", () -> new UnendingBowlBlock(BASE_PROPERTIES));
