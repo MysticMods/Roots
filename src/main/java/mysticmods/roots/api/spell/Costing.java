caf01e5d9540cd498479b14fd945346f3cc02cd9
Jon <due@wxwhatever.com>
2025-01-26 22:34:48 +1000

    Actually use herbMap!

--- src/main/java/mysticmods/roots/api/spell/Costing.java
+++ src/main/java/mysticmods/roots/api/spell/Costing.java
@@ -13,6 +13,7 @@ import net.minecraft.util.Mth;
 import net.minecraft.world.entity.player.Inventory;
 import net.minecraft.world.entity.player.Player;
 import net.minecraft.world.item.ItemStack;
+import net.neoforged.neoforge.capabilities.Capabilities;
 import net.neoforged.neoforge.items.IItemHandler;
 
 import java.util.ArrayList;
@@ -27,15 +28,23 @@ public class Costing {
 
   private final Object2BooleanMap<SpellModifier> modifierMap = new Object2BooleanLinkedOpenHashMap<>();
 
+  private final Map<Herb, List<HerbEntry>> herbMapCache;
+
   private boolean noCharge = false;
 
-  public Costing(ISpellInstance spell) {
+  public Costing (ISpellInstance spell) {
+    this.spell = spell;
+    modifierMap.defaultReturnValue(false);
+    herbMapCache = new HashMap<>();
+  }
+
+  public Costing(ISpellInstance spell, Player player) {
     this.spell = spell;
     // base cost is always a cost
     modifierMap.defaultReturnValue(false);
+    herbMapCache = herbMap(player);
   }
 
-  // TODO: IF you weren't charged should it trigger a cooldown?
   public void noCharge() {
     this.noCharge = true;
   }
@@ -51,47 +60,27 @@ public class Costing {
 
   private Map<Herb, List<HerbEntry>> herbMap(Player player) {
     Inventory playerInventory = player.getInventory();
-    // TODO: make this a function?
     Map<Herb, List<HerbEntry>> herbMap = new HashMap<>();
     for (int i = 0; i < playerInventory.getContainerSize(); i++) {
       ItemStack inSlot = playerInventory.getItem(i);
       Herb herb = Herb.getHerb(inSlot);
       if (herb != null) {
-        herbMap.computeIfAbsent(herb, k -> new ArrayList<>()).add(new HerbEntry(HerbEntryType.INVENTORY, herb, i, inSlot.getCount(), -1));
-        // TODO: Pouches are just a cap?
-        /*      } else if (inSlot.is(RootsTags.Items.POUCHES)) {*/
+        List<HerbEntry> entries = herbMap.get(herb);
+        if (entries == null) {
+          entries = new ArrayList<>();
+          herbMap.put(herb, entries);
+        }
+        entries.add(new HerbEntry(HerbEntryType.INVENTORY, herb, i, inSlot.getCount(), -1));
       } else {
-        IItemHandler cap = inSlot.getCapability(net.neoforged.neoforge.capabilities.Capabilities.ItemHandler.ITEM, null);
+        IItemHandler cap = inSlot.getCapability(Capabilities.ItemHandler.ITEM, null);
         if (cap != null) {
-          // TODO: check the item cap
           for (int j = 0; j < cap.getSlots(); j++) {
             ItemStack inSlot2 = cap.getStackInSlot(j);
             Herb herb2 = Herb.getHerb(inSlot2);
             if (herb2 != null) {
-              herbMap.computeIfAbsent(herb2, k -> new ArrayList<>()).add(new HerbEntry(HerbEntryType.POUCH, herb2, i, inSlot2.getCount(), j));
+              herbMap.computeIfAbsent(herb2, k -> new ArrayList<>()).add(new HerbEntry(HerbEntryType.CAPABILITY, herb2, i, inSlot2.getCount(), j));
             }
           }
-        } else {
-/*          CompoundTag tag = inSlot.getTag();
-          // PROBLEMATIC ASSUMPTIONS HERE
-          if (tag != null && tag.contains("Items", Tag.TAG_LIST)) {
-            // TODO: try to read it like a shulker box
-            ListTag itemListTag;
-            try {
-              itemListTag = tag.getList("Items", Tag.TAG_COMPOUND);
-            } catch (ClassCastException exception) {
-              continue;
-            }
-            NonNullList<ItemStack> items = NonNullList.withSize(itemListTag.size(), ItemStack.EMPTY);
-            ContainerHelper.loadAllItems(tag, items);
-            for (int j = 0; j < items.size(); j++) {
-              ItemStack inSlot2 = items.get(j);
-              Herb herb2 = Herb.getHerb(inSlot2);
-              if (herb2 != null) {
-                herbMap.computeIfAbsent(herb2, k -> new ArrayList<>()).add(new HerbEntry(HerbEntryType.POUCH, herb2, i, inSlot2.getCount(), j));
-              }
-            }
-          }*/
         }
       }
     }
@@ -105,21 +94,17 @@ public class Costing {
     calculateCosts(checkModifiers, false);
 
 
-    Inventory playerInventory = player.getInventory();
     HerbStorage cap = player.getData(ModAttachments.HERB_STORAGE);
-
-    //Map<Herb, List<HerbEntry>> herbMap = herbMap(player);
 
     for (Object2DoubleMap.Entry<Herb> entry : totalCosts.object2DoubleEntrySet()) {
       double remainder = cap.drain(entry.getKey(), entry.getDoubleValue(), true);
       if (remainder != 0) {
         double count = 0;
-        for (int i = 0; i < playerInventory.getContainerSize(); i++) {
-          ItemStack stack = playerInventory.getItem(i);
-          if (stack.is(entry.getKey().getTag())) {
-            count += stack.getCount();
+        List<HerbEntry> entries = herbMapCache.get(entry.getKey());
+        if (entries != null) {
+          for (HerbEntry herbEntry : entries) {
+            count += herbEntry.count();
           }
-          // TODO: Item capabilities, shulker boxes, pouches
         }
         if (remainder > count) {
           return false;
@@ -128,17 +113,6 @@ public class Costing {
     }
 
     return true;
-  }
-
-  // herbs can be directly in the inventory
-  // herbs can be in an item capability in the inventory
-  // herbs can be in a shulker box in the inventory
-  // herbs can be in a pouch in the inventory
-  private record HerbEntry(HerbEntryType type, Herb herb, int slot, int count, int subindex) {
-  }
-
-  private enum HerbEntryType {
-    POUCH, SHULKER, CAPABILITY, INVENTORY
   }
 
   // NOTE: THIS DOES NOT CHECK AMOUNTS, MERELY CHARGES
@@ -151,7 +125,6 @@ public class Costing {
     }
     calculateCosts(true, false);
 
-    // TODO: ???
     if (noCharge) {
       return false;
     }
@@ -159,54 +132,80 @@ public class Costing {
     Inventory playerInventory = player.getInventory();
     HerbStorage cap = player.getData(ModAttachments.HERB_STORAGE);
 
-    //Map<Herb, List<HerbEntry>> herbMap = herbMap(player);
-
+    // TODO: Curios
     for (Object2DoubleMap.Entry<Herb> entry : totalCosts.object2DoubleEntrySet()) {
       double remainder = cap.drain(entry.getKey(), entry.getDoubleValue(), false);
       if (remainder != 0) {
         int toConsume = Mth.ceil(remainder);
-        for (int i = 0; i < playerInventory.getContainerSize(); i++) {
-          ItemStack stack = playerInventory.getItem(i);
-          if (stack.is(entry.getKey().getTag())) {
+        for (HerbEntry herbEntry : herbMapCache.getOrDefault(entry.getKey(), List.of())) {
+          if (herbEntry.type() == HerbEntryType.INVENTORY) {
+            ItemStack stack = playerInventory.getItem(herbEntry.slot());
             if (stack.getCount() >= toConsume) {
-              /*RootsAPI.LOG.info("Shrunk stack of {} by {}", stack, toConsume);*/
               stack.shrink(toConsume);
               toConsume = 0;
+              playerInventory.setItem(herbEntry.slot(), stack);
               break;
             } else {
-              /*RootsAPI.LOG.info("Shrunk stack of {} by {} to 0", stack, stack.getCount());*/
               toConsume -= stack.getCount();
               stack.setCount(0);
+              playerInventory.setItem(herbEntry.slot(), stack);
             }
-            if (toConsume <= 0) {
+          } else {
+            ItemStack capStack = playerInventory.getItem(herbEntry.slot());
+            IItemHandler thisCap = capStack.getCapability(Capabilities.ItemHandler.ITEM, null);
+            if (thisCap == null) {
+              RootsAPI.LOG.error("No capability found for {}", capStack);
+              continue;
+            }
+            ItemStack capItem = thisCap.getStackInSlot(herbEntry.subindex());
+            if (capItem.getCount() >= toConsume) {
+              thisCap.extractItem(herbEntry.subindex(), toConsume, false);
+              toConsume = 0;
+              playerInventory.setItem(herbEntry.slot(), capStack);
               break;
+            } else {
+              thisCap.extractItem(herbEntry.subindex(), capItem.getCount(), false);
+              toConsume -= capItem.getCount();
+              playerInventory.setItem(herbEntry.slot(), capStack);
             }
+          }
+          if (toConsume <= 0) {
+            break;
           }
         }
         if (toConsume > 0) {
-          // HOUSTON WE HAVE A PROBLEM
           RootsAPI.LOG.info("Remainder left over! OH NO! {}", toConsume);
         }
         cap.fill(entry.getKey(), (double) Mth.ceil(remainder) - remainder);
         playerInventory.setChanged();
+        player.inventoryMenu.sendAllDataToRemote();
       }
     }
 
     return true;
   }
 
-  // TODO: Really need to come up with a cleaner way of doing this
   private void calculateCosts(boolean checkModifiers, boolean skipModifiers) {
     totalCosts.clear();
     Map<Herb, List<Cost>> herbCosts = new HashMap<>();
     for (Cost cost : spell.getSpell().getDefaultCosts()) {
-      herbCosts.computeIfAbsent(cost.getHerb(), k -> new ArrayList<>()).add(cost);
+      List<Cost> costs = herbCosts.get(cost.getHerb());
+      if (costs == null) {
+        costs = new ArrayList<>();
+        herbCosts.put(cost.getHerb(), costs);
+      }
+      costs.add(cost);
     }
     if (!skipModifiers) {
       for (SpellModifier modifier : spell.getEnabledModifiers()) {
         if (!checkModifiers || modifierMap.getBoolean(modifier)) {
           for (Cost cost : modifier.getDefaultCosts()) {
-            herbCosts.computeIfAbsent(cost.getHerb(), k -> new ArrayList<>()).add(cost);
+            List<Cost> costs = herbCosts.get(cost.getHerb());
+            if (costs == null) {
+              costs = new ArrayList<>();
+              herbCosts.put(cost.getHerb(), costs);
+            }
+            costs.add(cost);
           }
         }
       }
@@ -244,5 +243,12 @@ public class Costing {
   public Object2DoubleMap<Herb> getMaximumCost() {
     calculateCosts(false, false);
     return new Object2DoubleLinkedOpenHashMap<>(totalCosts);
+  }
+
+  private record HerbEntry(HerbEntryType type, Herb herb, int slot, int count, int subindex) {
+  }
+
+  private enum HerbEntryType {
+    CAPABILITY, INVENTORY
   }
 }
