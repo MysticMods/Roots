b6d73156c09b149968397a4cbed6e7112d736efa
Jon <due@wxwhatever.com>
2025-03-04 13:50:46 +1000

    Improve AllAroundLogPlacement.
    This may not actually be thread safe.

--- src/main/java/mysticmods/roots/worldgen/features/placements/AllAroundLogPlacement.java
+++ src/main/java/mysticmods/roots/worldgen/features/placements/AllAroundLogPlacement.java
@@ -3,11 +3,16 @@ package mysticmods.roots.worldgen.features.placements;
 import com.mojang.serialization.MapCodec;
 import mysticmods.roots.init.ModFeatures;
 import net.minecraft.core.BlockPos;
+import net.minecraft.core.Direction;
 import net.minecraft.util.RandomSource;
 import net.minecraft.world.level.levelgen.placement.PlacementContext;
 import net.minecraft.world.level.levelgen.placement.PlacementModifier;
 import net.minecraft.world.level.levelgen.placement.PlacementModifierType;
 
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Collections;
+import java.util.List;
 import java.util.stream.Stream;
 
 public class AllAroundLogPlacement extends PlacementModifier {
@@ -19,9 +24,17 @@ public class AllAroundLogPlacement extends PlacementModifier {
 
   public static MapCodec<AllAroundLogPlacement> CODEC = MapCodec.unit(INSTANCE);
 
+  private final List<Direction> DIRECTIONS = new ArrayList<>(Arrays.asList(Direction.NORTH, Direction.SOUTH, Direction.EAST, Direction.WEST));
+
   @Override
   public Stream<BlockPos> getPositions(PlacementContext context, RandomSource random, BlockPos pos) {
-    return Stream.of(pos.north(), pos.south(), pos.east(), pos.west());
+    Collections.shuffle(DIRECTIONS);
+    for (Direction direction : DIRECTIONS) {
+      if (context.getBlockState(pos.relative(direction)).isAir()) {
+        return Stream.of(pos.relative(direction));
+      }
+    }
+    return Stream.empty();
   }
 
   @Override
