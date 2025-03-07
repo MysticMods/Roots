9f72429aa47a257371a82acf7cdb09e9f5da6e3c
Jon <due@wxwhatever.com>
2025-03-07 12:46:36 +1000

    Improve knife recipes.

--- src/main/java/mysticmods/roots/integration/jei/categories/KnifeCategory.java
+++ src/main/java/mysticmods/roots/integration/jei/categories/KnifeCategory.java
@@ -17,7 +17,13 @@ import net.minecraft.client.gui.GuiGraphics;
 import net.minecraft.core.HolderLookup;
 import net.minecraft.network.chat.Component;
 import net.minecraft.world.item.ItemStack;
+import net.minecraft.world.item.crafting.Ingredient;
+import net.minecraft.world.level.ItemLike;
+import net.minecraft.world.level.block.Block;
 import net.minecraft.world.level.block.state.BlockState;
+
+import java.util.ArrayList;
+import java.util.List;
 
 public class KnifeCategory extends RootsRecipeBaseCategory<KnifeRecipe> {
   public KnifeCategory(IGuiHelper helper) {
@@ -26,9 +32,28 @@ public class KnifeCategory extends RootsRecipeBaseCategory<KnifeRecipe> {
 
   // TODO: State mappers are just block -> block so these can be blocks
   // TODO: The dynamic recipe could just have a canonical representation of a block tag
+  // but it would also need to know what blocks aren't being included
   @Override
   public void setRecipe(IRecipeLayoutBuilder builder, KnifeRecipe recipe, IFocusGroup iFocusGroup) {
     HolderLookup.Provider provider = Minecraft.getInstance().getConnection().registryAccess();
+    if (recipe != DynamicBarkRecipe.INSTANCE) {
+      if (recipe.getStateMapper() != null) {
+        OutputStateMapper mapper = recipe.getStateMapper();
+        List<ItemLike> inputs = new ArrayList<>();
+        List<ItemLike> outputs = new ArrayList<>();
+
+        mapper.mapBlock().forEach((a, b) -> {
+          inputs.add(a);
+          outputs.add(b);
+        });
+
+        builder.addSlot(RecipeIngredientRole.INPUT, 5, 5)
+            .addIngredients(Ingredient.of(inputs.toArray(new ItemLike[0])));
+        builder.addSlot(RecipeIngredientRole.INPUT, 71, 5)
+            .addIngredients(Ingredient.of(outputs.toArray(new ItemLike[0])));
+      }
+    }
+
     builder.addSlot(RecipeIngredientRole.OUTPUT, 71, 28)
         .addItemStack(recipe.getResultItem(provider));
   }
@@ -41,13 +66,7 @@ public class KnifeCategory extends RootsRecipeBaseCategory<KnifeRecipe> {
     if (recipe == DynamicBarkRecipe.INSTANCE) {
 
     } else {
-
       if (recipe.getStateMapper() != null) {
-        OutputStateMapper mapper = recipe.getStateMapper();
-        mapper.mapBlock().entrySet().stream().findFirst().ifPresent(o -> {
-          RenderUtil.renderBlock(guiGraphics, o.getKey().defaultBlockState(), 12.5f, 9, 0, 45, 12f);
-          RenderUtil.renderBlock(guiGraphics, o.getValue().defaultBlockState(), 79.5f, 9, 0, 45, 12f);
-        });
       } else if (recipe.getOutputState() != null) {
         BlockState output = recipe.getOutputState().build();
         BlockState input = recipe.getConditions().getFirst().test().getBlockState(provider);
