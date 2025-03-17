098c58a2ee40bc2b235defb40766157bb4c6466c
Jon <due@wxwhatever.com>
2025-03-17 14:52:30 +1000

    Fix sidedness violation.

--- src/main/java/mysticmods/roots/item/RunicShearsItem.java
+++ src/main/java/mysticmods/roots/item/RunicShearsItem.java
@@ -154,20 +154,24 @@ public class RunicShearsItem extends ShearsItem {
       ConditionResult conditionResult = recipe.value()
           .checkConditions(level, player, PyreBlockEntity.getPyreBoundingBox(), blockpos);
       if (conditionResult.anyFailed()) {
-        RootsAPI.LOG.info("Conditions failed.");
-        conditionResult.failedLevelConditions().forEach(o -> RootsAPI.LOG.info("Failed: " + o.getDescriptionId()));
-        conditionResult.failedPlayerConditions().forEach(o -> RootsAPI.LOG.info("Failed: " + o.getDescriptionId()));
-        conditionResult.report(player);
+        if (!level.isClientSide()) {
+          RootsAPI.LOG.info("Conditions failed.");
+          conditionResult.failedLevelConditions().forEach(o -> RootsAPI.LOG.info("Failed: " + o.getDescriptionId()));
+          conditionResult.failedPlayerConditions().forEach(o -> RootsAPI.LOG.info("Failed: " + o.getDescriptionId()));
+          conditionResult.report(player);
+        }
         return InteractionResult.FAIL;
       }
 
-      UnlockResult failedGrants = recipe.value().checkUnlocks(level, (ServerPlayer) player);
-      if (failedGrants.anyFailed() && !recipe.value().hasOutput(level.registryAccess())) {
-        RootsAPI.LOG.info("Grants failed and recipe has no output");
-        // TODO:
-        /*        failedUnlocks.failedUnlocks().forEach(o -> RootsAPI.LOG.info("Failed grant of type " + o.type().name() + " with id " + o.id()));*/
-        failedGrants.report();
-        return InteractionResult.FAIL;
+      if (!level.isClientSide()) {
+        UnlockResult failedGrants = recipe.value().checkUnlocks(level, (ServerPlayer) player);
+        if (failedGrants.anyFailed() && !recipe.value().hasOutput(level.registryAccess())) {
+          RootsAPI.LOG.info("Grants failed and recipe has no output");
+          // TODO:
+          /*        failedUnlocks.failedUnlocks().forEach(o -> RootsAPI.LOG.info("Failed grant of type " + o.type().name() + " with id " + o.id()));*/
+          failedGrants.report();
+          return InteractionResult.FAIL;
+        }
       }
 
 
