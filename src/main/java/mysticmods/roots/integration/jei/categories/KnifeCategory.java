2752502772902819143409d37754d38035643af8
Jon <due@wxwhatever.com>
2025-03-22 15:45:20 +1000

    Fix #836: JEI support for dynamic bark recipe.

--- src/main/java/mysticmods/roots/integration/jei/categories/KnifeCategory.java
+++ src/main/java/mysticmods/roots/integration/jei/categories/KnifeCategory.java
@@ -6,12 +6,13 @@ import mezz.jei.api.helpers.IGuiHelper;
 import mezz.jei.api.recipe.IFocusGroup;
 import mezz.jei.api.recipe.RecipeIngredientRole;
 import mysticmods.roots.api.RootsAPI;
+import mysticmods.roots.api.RootsTags;
 import mysticmods.roots.api.condition.CanonicalRepresentation;
 import mysticmods.roots.api.condition.LevelCondition;
 import mysticmods.roots.api.recipe.output.ChanceOutput;
 import mysticmods.roots.init.ModItems;
+import mysticmods.roots.init.ResolvedRecipes;
 import mysticmods.roots.integration.jei.RootsJEIPlugin;
-import mysticmods.roots.integration.jei.categories.drawable.DrawableComponent;
 import mysticmods.roots.integration.jei.categories.widget.ConditionWidget;
 import mysticmods.roots.integration.jei.categories.widget.DurabilityWidget;
 import mysticmods.roots.integration.jei.categories.widget.WorldTestWidget;
@@ -20,20 +21,99 @@ import mysticmods.roots.recipe.knife.KnifeOffHandRecipe;
 import mysticmods.roots.recipe.knife.KnifeRecipe;
 import mysticmods.roots.recipe.knife.OutputStateMapper;
 import net.minecraft.client.Minecraft;
+import net.minecraft.core.BlockPos;
+import net.minecraft.core.Direction;
+import net.minecraft.core.Holder;
 import net.minecraft.core.HolderLookup;
+import net.minecraft.core.registries.BuiltInRegistries;
 import net.minecraft.network.chat.Component;
+import net.minecraft.tags.BlockTags;
+import net.minecraft.world.InteractionHand;
+import net.minecraft.world.item.AxeItem;
 import net.minecraft.world.item.ItemStack;
+import net.minecraft.world.item.context.UseOnContext;
 import net.minecraft.world.item.crafting.Ingredient;
+import net.minecraft.world.item.crafting.RecipeHolder;
 import net.minecraft.world.level.ItemLike;
+import net.minecraft.world.level.block.Block;
 import net.minecraft.world.level.block.state.BlockState;
+import net.minecraft.world.phys.BlockHitResult;
+import net.minecraft.world.phys.Vec3;
+import net.neoforged.neoforge.common.ItemAbilities;
 
-import java.util.ArrayList;
-import java.util.List;
+import java.util.*;
 
 // TODO: Durability cost
 public class KnifeCategory extends RootsRecipeBaseCategory<KnifeRecipe> {
+  private static ItemStack knife;
+
+  private static Ingredient dynamicInputs;
+  private static Ingredient dynamicOutputs;
+
   public KnifeCategory(IGuiHelper helper) {
     super(RootsJEIPlugin.KNIFE_RECIPE_TYPE, helper, 166, 124, RootsAPI.rl("textures/gui/jei/bark_carving.png"), () -> new ItemStack(ModItems.SILVER_KNIFE.get()), Component.translatable("roots.jei.knife_crafting"));
+  }
+
+  public static void generateDynamicIngredients() {
+    if (knife == null) {
+      knife = new ItemStack(ModItems.NETHERITE_KNIFE);
+    }
+
+    HolderLookup.Provider provider = Minecraft.getInstance().getConnection().registryAccess();
+
+    Set<Block> skippedInputs = new HashSet<>();
+    Set<Block> skippedOutputs = new HashSet<>();
+    for (RecipeHolder<KnifeRecipe> recipe2 : ResolvedRecipes.KNIFE.getRecipes()) {
+      if (recipe2.value().equals(DynamicBarkRecipe.INSTANCE)) {
+        continue;
+      }
+
+      KnifeRecipe recipe3 = recipe2.value();
+      ItemStack result = recipe3.getResultItem(provider);
+      if (!result.is(RootsTags.Items.BARKS)) {
+        continue;
+      }
+
+      if (recipe3.getStateMapper() != null) {
+        for (Map.Entry<Block, Block> entry : recipe3.getStateMapper().mapBlock().entrySet()) {
+          skippedInputs.add(entry.getKey());
+          skippedOutputs.add(entry.getValue());
+        }
+      } else if (recipe3.getTest() != null && recipe3.getOutputState() != null) {
+        skippedInputs.add(recipe3.getTest().getBlockState(provider).getBlock());
+        skippedOutputs.add(recipe3.getOutputState().getBlock());
+      } else {
+        continue;
+      }
+    }
+
+    List<ItemStack> inputs = new ArrayList<>();
+    List<ItemStack> outputs = new ArrayList<>();
+
+    if (BuiltInRegistries.BLOCK.getTag(BlockTags.LOGS).isPresent()) {
+      UseOnContext fakeContext = new UseOnContext(Minecraft.getInstance().level, null, InteractionHand.MAIN_HAND, knife, BlockHitResult.miss(Vec3.ZERO, Direction.UP, BlockPos.ZERO));
+
+      for (Holder<Block> holder : BuiltInRegistries.BLOCK.getTag(BlockTags.LOGS).get()) {
+        Block block = holder.value();
+        if (skippedInputs.contains(block)) {
+          continue;
+        }
+
+        BlockState result = block.defaultBlockState().getToolModifiedState(fakeContext, ItemAbilities.AXE_STRIP, true);
+        if (result == null) {
+          result = AxeItem.getAxeStrippingState(block.defaultBlockState());
+          if (result == null) {
+            continue;
+          }
+        }
+
+        inputs.add(new ItemStack(block));
+        outputs.add(new ItemStack(result.getBlock()));
+      }
+
+      dynamicInputs = Ingredient.of(inputs.toArray(ItemStack[]::new));
+      dynamicOutputs = Ingredient.of(outputs.toArray(ItemStack[]::new));
+    }
   }
 
   // TODO: State mappers are just block -> block so these can be blocks
@@ -44,23 +124,30 @@ public class KnifeCategory extends RootsRecipeBaseCategory<KnifeRecipe> {
     super.setRecipe(builder, recipe, iFocusGroup);
 
     HolderLookup.Provider provider = Minecraft.getInstance().getConnection().registryAccess();
-    if (recipe != DynamicBarkRecipe.INSTANCE) {
-      if (recipe.getStateMapper() != null) {
-        OutputStateMapper mapper = recipe.getStateMapper();
-        List<ItemLike> inputs = new ArrayList<>();
-        List<ItemLike> outputs = new ArrayList<>();
+    if (recipe.getStateMapper() != null) {
+      OutputStateMapper mapper = recipe.getStateMapper();
+      List<ItemLike> inputs = new ArrayList<>();
+      List<ItemLike> outputs = new ArrayList<>();
 
-        mapper.mapBlock().forEach((a, b) -> {
-          inputs.add(a);
-          outputs.add(b);
-        });
+      mapper.mapBlock().forEach((a, b) -> {
+        inputs.add(a);
+        outputs.add(b);
+      });
 
-        // 35 -> 39
+      builder.addSlot(RecipeIngredientRole.INPUT, 7, 34)
+          .addIngredients(Ingredient.of(inputs.toArray(new ItemLike[0])));
+      builder.addSlot(RecipeIngredientRole.OUTPUT, 73, 34)
+          .addIngredients(Ingredient.of(outputs.toArray(new ItemLike[0])));
+    } else {
+      if (recipe == DynamicBarkRecipe.INSTANCE) {
+        if (dynamicInputs == null) {
+          generateDynamicIngredients();
+        }
 
         builder.addSlot(RecipeIngredientRole.INPUT, 7, 34)
-            .addIngredients(Ingredient.of(inputs.toArray(new ItemLike[0])));
+            .addIngredients(dynamicInputs);
         builder.addSlot(RecipeIngredientRole.OUTPUT, 73, 34)
-            .addIngredients(Ingredient.of(outputs.toArray(new ItemLike[0])));
+            .addIngredients(dynamicOutputs);
       } else {
         var acceptor = builder.addInvisibleIngredients(RecipeIngredientRole.OUTPUT);
 
@@ -86,22 +173,22 @@ public class KnifeCategory extends RootsRecipeBaseCategory<KnifeRecipe> {
               .addIngredients(Ingredient.of(offHandRecipe.getOffHandTag()));
         }
       }
+    }
 
-      List<ChanceOutput> outputs = recipe.getCachedOutputs();
+    List<ChanceOutput> outputs = recipe.getCachedOutputs();
 
-      int row = 0;
-      int column = 0;
+    int row = 0;
+    int column = 0;
 
-      for (int i = 0; i < outputs.size(); i++) {
-        if (i % 4 == 0 && i != 0) {
-          row++;
-          column = 0;
-        }
-        builder.addSlot(RecipeIngredientRole.OUTPUT, 97 + column * 17, 2 + row * 17)
-            .addItemStack(outputs.get(i).getOutput()).setSlotName(String.valueOf(i))
-            .addRichTooltipCallback(this.richestTooltip(recipe));
-        column++;
+    for (int i = 0; i < outputs.size(); i++) {
+      if (i % 4 == 0 && i != 0) {
+        row++;
+        column = 0;
       }
+      builder.addSlot(RecipeIngredientRole.OUTPUT, 97 + column * 17, 2 + row * 17)
+          .addItemStack(outputs.get(i).getOutput()).setSlotName(String.valueOf(i))
+          .addRichTooltipCallback(this.richestTooltip(recipe));
+      column++;
     }
   }
 
