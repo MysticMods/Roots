3e90e7c3eda5d7d2828a5c22e31aec4148ffb0e7
Jon <due@wxwhatever.com>
2025-04-02 15:12:31 +1000

    Rampant Growth now triggers the crop growth action.

--- src/main/java/mysticmods/roots/spell/RampantGrowthSpell.java
+++ src/main/java/mysticmods/roots/spell/RampantGrowthSpell.java
@@ -2,41 +2,38 @@ package mysticmods.roots.spell;
 
 import it.unimi.dsi.fastutil.ints.Int2IntMap;
 import it.unimi.dsi.fastutil.objects.Object2IntMap;
+import mysticmods.roots.action.CropGrowthAction;
 import mysticmods.roots.api.RootsTags;
 import mysticmods.roots.api.datamap.DataMaps;
-import mysticmods.roots.api.herb.Cost;
 import mysticmods.roots.api.herb.CostInstance;
 import mysticmods.roots.api.property.Property;
 import mysticmods.roots.api.property.PropertyHolder;
 import mysticmods.roots.api.spell.Costing;
 import mysticmods.roots.api.spell.ISpellInstance;
 import mysticmods.roots.api.spell.Spell;
+import mysticmods.roots.init.ModActions;
 import mysticmods.roots.init.ModSpells;
-import mysticmods.roots.network.client.fx.GrowthFXPacket;
 import mysticmods.roots.network.client.fx.RampantGrowthFXPacket;
 import mysticmods.roots.util.GrowthUtil;
 import net.minecraft.ChatFormatting;
 import net.minecraft.core.BlockPos;
 import net.minecraft.core.Holder;
 import net.minecraft.server.level.ServerLevel;
+import net.minecraft.server.level.ServerPlayer;
 import net.minecraft.world.InteractionHand;
 import net.minecraft.world.entity.player.Player;
 import net.minecraft.world.item.ItemStack;
 import net.minecraft.world.level.Level;
 import net.minecraft.world.level.block.state.BlockState;
 import net.minecraft.world.level.levelgen.structure.BoundingBox;
-import net.minecraft.world.phys.Vec3;
 import net.neoforged.neoforge.network.PacketDistributor;
 
 import java.util.ArrayList;
 import java.util.List;
-import java.util.function.BiPredicate;
 
 public class RampantGrowthSpell extends TwoRadiusSpell {
   private int interval, count;
   private BoundingBox box;
-
-  private static final BiPredicate<Level, BlockPos> GROWABLE_CROP = (level, pos) -> GrowthUtil.growthTicks(level, pos, null, null) > 0;
 
   public RampantGrowthSpell(ChatFormatting color, CostInstance costs) {
     super(Type.CONTINUOUS, color, costs, 0x157318, 0x13c3eb);
@@ -115,8 +112,12 @@ public class RampantGrowthSpell extends TwoRadiusSpell {
         int doTicks = GrowthUtil.growthTicks(pLevel, pos, null, pPlayer);
         if (doTicks > 0) {
           if (pLevel.random.nextInt(doTicks) == 0) {
-            pLevel.getBlockState(pos).randomTick((ServerLevel) pLevel, pos, pLevel.random);
+            BlockState oldState = pLevel.getBlockState(pos);
+            oldState.randomTick((ServerLevel) pLevel, pos, pLevel.random);
             PacketDistributor.sendToPlayersTrackingEntityAndSelf(pPlayer, new RampantGrowthFXPacket(pos));
+            BlockState newState = pLevel.getBlockState(pos);
+            CropGrowthAction.Context context = new CropGrowthAction.Context((ServerLevel) pLevel, (ServerPlayer) pPlayer, pos, newState, oldState, pHand, pStack, instance);
+            ModActions.CROP_GROWTH.get().accept(context);
             growCount++;
           }
         }
