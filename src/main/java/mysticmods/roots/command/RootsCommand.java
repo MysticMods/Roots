97a2b9fec3d936f5999dbc0df7bd237ab4a68da4
Jon <due@wxwhatever.com>
2025-04-05 14:23:41 +1000

    Handle ranks properly.

--- src/main/java/mysticmods/roots/command/RootsCommand.java
+++ src/main/java/mysticmods/roots/command/RootsCommand.java
@@ -322,8 +322,9 @@ public class RootsCommand {
       }
 
       int reputation = storage.getReputation(grove);
+      int rank = storage.getRank(grove);
       c.getSource()
-          .sendSuccess(() -> Component.translatable("roots.commands.reputation.current_reputation", grove.getName(), reputation), false);
+          .sendSuccess(() -> Component.translatable("roots.commands.reputation.current_reputation", grove.getName(), rank, reputation), false);
       return 1;
     }).then(Commands.literal("add").executes(c -> {
       c.getSource().sendSuccess(() -> Component.translatable("roots.commands.reputation.add.usage"), false);
@@ -345,7 +346,7 @@ public class RootsCommand {
       int amount = IntegerArgumentType.getInteger(c, "amount");
       storage.increaseReputation(grove, amount);
       PacketDistributor.sendToPlayer(player, new ClientboundReputationSyncPacket(storage));
-      c.getSource().sendSuccess(() -> Component.translatable("roots.commands.reputation.add", player.getDisplayName(), grove.getStyledName(), amount, storage.getReputation(grove)), false);
+      c.getSource().sendSuccess(() -> Component.translatable("roots.commands.reputation.add", player.getDisplayName(), grove.getStyledName(), amount, storage.getRank(grove), storage.getReputation(grove)), false);
       return 1;
     }))).then(Commands.literal("remove").executes(c -> {
       c.getSource().sendSuccess(() -> Component.translatable("roots.commands.reputation.remove.usage"), false);
@@ -367,7 +368,7 @@ public class RootsCommand {
       int amount = IntegerArgumentType.getInteger(c, "amount");
       storage.decreaseReputation(grove, amount);
       PacketDistributor.sendToPlayer(player, new ClientboundReputationSyncPacket(storage));
-      c.getSource().sendSuccess(() -> Component.translatable("roots.commands.reputation.remove", player.getDisplayName(), grove.getStyledName(), amount, storage.getReputation(grove)), false);
+      c.getSource().sendSuccess(() -> Component.translatable("roots.commands.reputation.remove", player.getDisplayName(), grove.getStyledName(), amount, storage.getRank(grove), storage.getReputation(grove)), false);
       return 1;
     }))).then(Commands.literal("set").executes(c -> {
       c.getSource().sendSuccess(() -> Component.translatable("roots.commands.reputation.set.usage"), false);
@@ -389,7 +390,7 @@ public class RootsCommand {
       int amount = IntegerArgumentType.getInteger(c, "amount");
       storage.setReputation(grove, amount);
       PacketDistributor.sendToPlayer(player, new ClientboundReputationSyncPacket(storage));
-      c.getSource().sendSuccess(() -> Component.translatable("roots.commands.reputation.set", player.getDisplayName(), grove.getStyledName(), amount), false);
+      c.getSource().sendSuccess(() -> Component.translatable("roots.commands.reputation.set", player.getDisplayName(), grove.getStyledName(), storage.getRank(grove), amount), false);
       return 1;
     }))))));
     return builder;
