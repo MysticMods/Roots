9110fa9f5e4ff4d141840b3f9bb9413562b3077b
Jon <due@wxwhatever.com>
2025-04-11 12:23:44 +1000

    Clean up.

--- src/main/java/mysticmods/roots/client/particle/SproutPortalParticle.java
+++ src/main/java/mysticmods/roots/client/particle/SproutPortalParticle.java
@@ -1,19 +1,17 @@
 package mysticmods.roots.client.particle;
 
 
-import mysticmods.roots.client.particle.render.RootsParticleRenderTypes;
 import net.minecraft.client.multiplayer.ClientLevel;
-import net.minecraft.client.particle.*;
+import net.minecraft.client.particle.Particle;
+import net.minecraft.client.particle.ParticleProvider;
+import net.minecraft.client.particle.PortalParticle;
+import net.minecraft.client.particle.SpriteSet;
 import net.minecraft.core.particles.SimpleParticleType;
 
 public class SproutPortalParticle extends PortalParticle {
   protected SproutPortalParticle(ClientLevel level, double x, double y, double z, double xSpeed, double ySpeed, double zSpeed) {
     super(level, x, y, z, xSpeed, ySpeed, zSpeed);
     this.quadSize = 0.35F * (this.random.nextFloat() * 0.2F + 0.5F);
-  }
-
-  public ParticleRenderType getRenderType() {
-    return super.getRenderType();
   }
 
   public static class Provider implements ParticleProvider<SimpleParticleType> {
