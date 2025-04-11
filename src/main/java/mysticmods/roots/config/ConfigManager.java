02214c32a1b70aa793a7accb733ef66ec01805d9
Jon <due@wxwhatever.com>
2025-04-11 13:09:18 +1000

    Fix #969: remove old hat configuration.

--- src/main/java/mysticmods/roots/config/ConfigManager.java
+++ src/main/java/mysticmods/roots/config/ConfigManager.java
@@ -17,8 +17,6 @@ public class ConfigManager {
 
   public static List<AbstractConfig> CONFIGS = new ArrayList<>();
 
-  public static HatConfig HAT_CONFIG = new HatConfig();
-
   public static ModConfigSpec.BooleanValue ALERTNESS_VISUAL;
   public static ModConfigSpec.IntValue ALERTNESS_DURATION;
   public static ModConfigSpec.BooleanValue ALERTNESS_SOUND;
@@ -47,9 +45,6 @@ public class ConfigManager {
     COMMON_BUILDER.comment("magnetism-related configuration").push("magnetism");
     EXPERIENCE_ORBS = COMMON_BUILDER.comment("whether or not experience orbs should be teleported when using magnetism")
         .define("move_experience_orbs", true);
-    COMMON_BUILDER.pop();
-    COMMON_BUILDER.comment("Hat configuration").push("hat_config");
-    HAT_CONFIG.apply(COMMON_BUILDER);
     COMMON_BUILDER.pop();
     COMMON_BUILDER.comment("Reputation-related configurations").push("reputation_config");
     PACIFIST_DISABLED = COMMON_BUILDER.comment("whether or not the Untrue Pacifist advancement is granted or utilized")
