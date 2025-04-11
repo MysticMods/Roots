616aa67fded4cdd5da62f25707d894be27c1323a
Jon <due@wxwhatever.com>
2025-04-11 16:05:00 +1000

    Improve stem block check.

--- src/main/java/mysticmods/roots/growth/growable/StemBlockCanGrow.java
+++ src/main/java/mysticmods/roots/growth/growable/StemBlockCanGrow.java
@@ -3,7 +3,9 @@ package mysticmods.roots.growth.growable;
 import mysticmods.roots.api.growth.CanGrowFunction;
 import net.minecraft.core.BlockPos;
 import net.minecraft.core.Direction;
+import net.minecraft.tags.BlockTags;
 import net.minecraft.world.level.Level;
+import net.minecraft.world.level.block.FarmBlock;
 import net.minecraft.world.level.block.state.BlockState;
 import net.minecraft.world.level.block.state.properties.IntegerProperty;
 import org.jetbrains.annotations.Nullable;
@@ -19,7 +21,11 @@ public record StemBlockCanGrow () implements CanGrowFunction {
       BlockPos offset = blockPos.relative(dir);
       BlockState offsetState = level.getBlockState(offset);
       if (offsetState.canBeReplaced() || offsetState.isAir()) {
-        return true;
+        BlockPos downPos = offset.below();
+        BlockState downState = level.getBlockState(downPos);
+        if (downState.is(BlockTags.DIRT) || downState.getBlock() instanceof FarmBlock) {
+          return true;
+        }
       }
     }
 
