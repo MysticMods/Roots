7205b81eaf0eae05af11a261ad852a6c01e72d2c
Jon <due@wxwhatever.com>
2025-04-12 14:07:52 +1000

    Solve some todos.

--- src/main/java/mysticmods/roots/ritual/BloomingRitual.java
+++ src/main/java/mysticmods/roots/ritual/BloomingRitual.java
@@ -35,10 +35,7 @@ import java.util.function.BiPredicate;
 
 public class BloomingRitual extends Ritual {
   private int count;
-  private int nextTick;
-  // TODO: Data map for block costs that increase the interval
 
-  // TODO: Caching of positions based on predicate
   private static final BiPredicate<Level, BlockPos> TWO_AIR_ABOVE = (level, pos) -> {
     BlockPos above = pos.above();
     return level.getFluidState(pos).isEmpty() && level.getFluidState(pos.above())
