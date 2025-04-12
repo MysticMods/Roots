a94e52f4845ee78de0907c7a5d6d4fbac2fa18f9
Jon <due@wxwhatever.com>
2025-04-12 14:07:07 +1000

    Fix ritual bounds being out by one.

--- src/main/java/mysticmods/roots/api/ritual/Ritual.java
+++ src/main/java/mysticmods/roots/api/ritual/Ritual.java
@@ -81,7 +81,7 @@ public abstract class Ritual implements IDescribed, TooltipComponent {
   protected abstract void animationTick(Level pLevel, BlockPos pPos, BlockState pState, BoundingBox pBoundingBox, PyreBlockEntity blockEntity, int duration, RandomSource randomSource);
 
   protected void rebuildBounds() {
-    boundingBox = new BoundingBox(-getRadiusXZ(), -getRadiusY(), -getRadiusXZ(), getRadiusXZ() + 1, getRadiusY() + 1, getRadiusXZ() + 1);
+    boundingBox = new BoundingBox(-getRadiusXZ(), -getRadiusY(), -getRadiusXZ(), getRadiusXZ(), getRadiusY(), getRadiusXZ());
     aabb = AABB.of(getBoundingBox());
   }
 
