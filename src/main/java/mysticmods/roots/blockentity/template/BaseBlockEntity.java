018bfed475d389176d7c5639570c87ee0e15eb3d
Jon <due@wxwhatever.com>
2025-04-14 18:11:57 +1000

    Fix #989 again: moved, not move.
    `move` mutilates the bounding box constantly which is not desirable.

--- src/main/java/mysticmods/roots/blockentity/template/BaseBlockEntity.java
+++ src/main/java/mysticmods/roots/blockentity/template/BaseBlockEntity.java
@@ -53,7 +53,9 @@ public abstract class BaseBlockEntity extends BlockEntity implements BoundedBloc
   public List<Pair<BlockPos, PedestalBlockEntity>> pedestals(TagKey<Block> include, TagKey<Block> exclude) {
     List<Pair<BlockPos, PedestalBlockEntity>> pedestalPositions = new ArrayList<>();
     if (getBoundingBox() != null) {
-      BlockPos.betweenClosedStream(getBoundingBox().move(getBlockPos())).forEach(pos -> {
+      BlockPos start = getBlockPos();
+      BoundingBox box = getBoundingBox().moved(start.getX(), start.getY(), start.getZ());
+      BlockPos.betweenClosedStream(box).forEach(pos -> {
         BlockState state = getLevel().getBlockState(pos);
         if (state.is(include) && !state.is(exclude)) {
           if (getLevel().getBlockEntity(pos) instanceof PedestalBlockEntity pedestal) {
