6e5b479a2156b08c789945cc6905a4edaa84b1de
Jon <due@wxwhatever.com>
2025-05-11 15:55:26 +1000

    Improve the entity emitters.

--- src/main/java/mysticmods/roots/client/particle/emitter/EntityEmitter.java
+++ src/main/java/mysticmods/roots/client/particle/emitter/EntityEmitter.java
@@ -10,49 +10,35 @@ import net.minecraft.client.particle.ParticleProvider;
 import net.minecraft.client.particle.ParticleRenderType;
 import net.minecraft.world.entity.Entity;
 
-public class EntityEmitter extends Particle {
-  private final RootsParticleOptions options;
-  private final Entity entity;
-  private final int count;
-  private final double chance;
+public abstract class EntityEmitter extends Particle {
+  protected final RootsParticleOptions options;
+  protected final Entity entity;
 
-  protected EntityEmitter(ClientLevel level, double x, double y, double z, RootsParticleOptions options, Entity entity, int lifetime, int count, double chance) {
-    super(level, x, y, z);
+  protected EntityEmitter(ClientLevel level, double x, double y, double z, RootsParticleOptions options, Entity entity, int lifetime) {
+    super(level, entity.getX(), entity.getY() + entity.getEyeHeight(), entity.getZ());
     this.xd = 0;
     this.yd = 0;
     this.zd = 0;
-    this.chance = chance;
     this.entity = entity;
     this.options = options;
     this.lifetime = lifetime;
-    this.count = count;
   }
 
   @Override
   public void tick() {
+    if (this.entity == null || this.entity.isRemoved()) {
+      this.remove();
+      return;
+    }
     this.xo = this.x;
     this.yo = this.y;
     this.zo = this.z;
-
     if (this.age++ >= this.lifetime) {
       this.remove();
     }
-
     this.x = entity.getX();
     this.y = entity.getY() + entity.getEyeHeight();
     this.z = entity.getZ();
-
-    for (int i = 0; i < count; i++) {
-      if (random.nextDouble() < chance) {
-        RootsParticleOptions opt;
-        if (random.nextBoolean()) {
-          opt = new RootsParticleOptions(options.type(), options.color2(), options.color1(), options.entityId());
-        } else {
-          opt = new RootsParticleOptions(options.type(), options.color1(), options.color2(), options.entityId());
-        }
-        level.addParticle(opt, x + (random.nextDouble() - 0.5), y + (random.nextDouble() - 0.5), z + (random.nextDouble() - 0.5), (random.nextDouble() - 0.5) * 0.1, (random.nextDouble() - 0.5) * 0.05, (random.nextDouble() - 0.5) * 0.1);
-      }
-    }
   }
 
   @Override
@@ -64,6 +50,36 @@ public class EntityEmitter extends Particle {
     return ParticleRenderType.NO_RENDER;
   }
 
+  public static class DisarmEmitter extends EntityEmitter {
+    private final double chance;
+    private final int count;
+
+    protected DisarmEmitter(ClientLevel level, double x, double y, double z, RootsParticleOptions options, Entity entity, int lifetime, int count, double chance) {
+      super(level, x, y, z, options, entity, lifetime);
+      this.count = count;
+      this.chance = chance;
+    }
+
+    @Override
+    public void tick() {
+      super.tick();
+      if (this.removed) {
+        return;
+      }
+      for (int i = 0; i < count; i++) {
+        if (random.nextDouble() < chance) {
+          RootsParticleOptions opt;
+          if (random.nextBoolean()) {
+            opt = new RootsParticleOptions(options.type(), options.color2(), options.color1(), options.entityId());
+          } else {
+            opt = new RootsParticleOptions(options.type(), options.color1(), options.color2(), options.entityId());
+          }
+          level.addParticle(opt, x + (random.nextDouble() - 0.5), y + (random.nextDouble() - 0.5), z + (random.nextDouble() - 0.5), (random.nextDouble() - 0.5) * 0.1, (random.nextDouble() - 0.5) * 0.05, (random.nextDouble() - 0.5) * 0.1);
+        }
+      }
+    }
+  }
+
   public static class DisarmProvider implements ParticleProvider<RootsParticleOptions> {
     @Override
     public Particle createParticle(RootsParticleOptions type, ClientLevel level, double x, double y, double z, double xSpeed, double ySpeed, double zSpeed) {
@@ -71,7 +87,7 @@ public class EntityEmitter extends Particle {
       if (entity == null) {
         return null;
       }
-      return new EntityEmitter(level, x, y, z, new RootsParticleOptions(ModParticles.DISARM, type.color1(), type.color2(), type.entityId()), entity, (int) xSpeed, (int) ySpeed, zSpeed);
+      return new DisarmEmitter(level, x, y, z, new RootsParticleOptions(ModParticles.DISARM, type.color1(), type.color2(), type.entityId()), entity, (int) xSpeed, (int) ySpeed, zSpeed);
     }
   }
 }
