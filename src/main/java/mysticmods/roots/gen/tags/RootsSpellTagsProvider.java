4afd8c6468cabf999789ef99b0b8e85ebb0335dc
Jon <due@wxwhatever.com>
2025-06-23 13:02:51 +1000

    Adjust WIP/NYI.

--- src/main/java/mysticmods/roots/gen/tags/RootsSpellTagsProvider.java
+++ src/main/java/mysticmods/roots/gen/tags/RootsSpellTagsProvider.java
@@ -21,9 +21,9 @@ public class RootsSpellTagsProvider extends IntrinsicHolderTagsProvider<Spell> {
   @Override
   protected void addTags(HolderLookup.Provider provider) {
     this.tag(RootsTags.Spells.WIP)
-        .add(ModSpells.ACID_CLOUD.get(), ModSpells.AQUA_BUBBLE.get(), ModSpells.MAGNETISM.get(), ModSpells.DANDELION_WINDS.get(), ModSpells.DESATURATE.get(), ModSpells.DISARM.get(), ModSpells.EXTENSION.get(), ModSpells.NONDETECTION.get(), ModSpells.GEAS.get(), ModSpells.RAMPANT_GROWTH.get(), ModSpells.HARVEST.get(), ModSpells.LIFE_DRAIN.get(), ModSpells.ROSE_THORNS.get(), ModSpells.SANCTUARY.get(), ModSpells.SHATTER.get(), ModSpells.JAUNT.get(), ModSpells.TEMPORAL_MORASS.get(), ModSpells.WILDFIRE.get());
+        .add(ModSpells.ACID_CLOUD.get(), ModSpells.AQUA_BUBBLE.get(), ModSpells.MAGNETISM.get(), ModSpells.DANDELION_WINDS.get(), ModSpells.DESATURATE.get(), ModSpells.DISARM.get(), ModSpells.EXTENSION.get(), ModSpells.NONDETECTION.get(), ModSpells.GEAS.get(), ModSpells.RAMPANT_GROWTH.get(), ModSpells.HARVEST.get(), ModSpells.LIFE_DRAIN.get(), ModSpells.ROSE_THORNS.get(), ModSpells.SANCTUARY.get(), ModSpells.SHATTER.get(), ModSpells.JAUNT.get(), ModSpells.TEMPORAL_MORASS.get(), ModSpells.WILDFIRE.get(), ModSpells.SATURATE.get());
     this.tag(RootsTags.Spells.NYI)
-        .add(ModSpells.LIGHT_DRIFTER.get(), ModSpells.SUMMON_UNDEAD.get(), ModSpells.RADIANCE.get(), ModSpells.STORM_CLOUD.get(), ModSpells.SATURATE.get());
+        .add(ModSpells.LIGHT_DRIFTER.get(), ModSpells.SUMMON_UNDEAD.get(), ModSpells.RADIANCE.get(), ModSpells.STORM_CLOUD.get(), ModSpells.DECAY.get());
     this.tag(RootsTags.Spells.ADJUSTABLE_SPELL).add(ModSpells.SHATTER.get(), ModSpells.RAMPANT_GROWTH.get(), ModSpells.HARVEST.get());
 
     this.tag(RootsTags.Spells.FAIRY).add(ModSpells.SYLVAN_LIGHT.get(), ModSpells.PETAL_SHELL.get(), ModSpells.ROSE_THORNS.get(), ModSpells.SANCTUARY.get());
