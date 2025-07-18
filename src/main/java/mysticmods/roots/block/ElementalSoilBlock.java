960b95dacea34c990ec0059978638c7bdc4fc093
Jon <due@wxwhatever.com>
2025-07-18 13:16:00 +1000

    Fix #1105: don't convert elemental soil into dirt on tree growth.

--- src/main/java/mysticmods/roots/block/ElementalSoilBlock.java
+++ src/main/java/mysticmods/roots/block/ElementalSoilBlock.java
@@ -14,10 +14,13 @@ import net.minecraft.world.level.block.Block;
 import net.minecraft.world.level.block.FarmBlock;
 import net.minecraft.world.level.block.state.BlockState;
 import net.minecraft.world.level.block.state.StateDefinition;
+import net.minecraft.world.level.levelgen.feature.configurations.TreeConfiguration;
 import net.minecraft.world.phys.shapes.CollisionContext;
 import net.minecraft.world.phys.shapes.Shapes;
 import net.minecraft.world.phys.shapes.VoxelShape;
 import net.neoforged.neoforge.common.util.TriState;
+
+import java.util.function.BiConsumer;
 
 
 public class ElementalSoilBlock extends FarmBlock {
@@ -68,5 +71,10 @@ public class ElementalSoilBlock extends FarmBlock {
     if (i != MAX_MOISTURE) {
       level.setBlock(pos, state.setValue(MOISTURE, MAX_MOISTURE), 2);
     }
+  }
+
+  @Override
+  public boolean onTreeGrow(BlockState state, LevelReader level, BiConsumer<BlockPos, BlockState> placeFunction, RandomSource randomSource, BlockPos pos, TreeConfiguration config) {
+    return true;
   }
 }
