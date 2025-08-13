66c214e5b9cd853bfe36b0aff4cd96c79d1fd6c6
Jon <due@wxwhatever.com>
2025-08-13 16:11:18 +1000

    Fix #1114: don't allow rose thorns on top of rose thorns.

--- src/main/java/mysticmods/roots/spell/RoseThornsSpell.java
+++ src/main/java/mysticmods/roots/spell/RoseThornsSpell.java
@@ -14,16 +14,22 @@ import mysticmods.roots.init.ModSpells;
 import mysticmods.roots.snapshot.RoseThornsEntitySnapshot;
 import mysticmods.roots.snapshot.SnapshotHelper;
 import net.minecraft.ChatFormatting;
+import net.minecraft.core.BlockPos;
+import net.minecraft.core.Direction;
 import net.minecraft.core.Holder;
 import net.minecraft.world.InteractionHand;
 import net.minecraft.world.entity.player.Player;
 import net.minecraft.world.item.ItemStack;
 import net.minecraft.world.level.Level;
+import net.minecraft.world.level.block.Block;
+import net.minecraft.world.level.block.state.BlockState;
+import net.minecraft.world.phys.AABB;
 import net.minecraft.world.phys.BlockHitResult;
 
 import java.util.List;
 
 public class RoseThornsSpell extends Spell {
+  private static final AABB entityBoundsCheck = new AABB(-1, -1, -1, 1, 1, 1);
   private double radiusZX, radiusY;
   private int duration;
   private float damage;
@@ -58,6 +64,24 @@ public class RoseThornsSpell extends Spell {
   @Override
   public int cast(Level pLevel, Player pPlayer, ItemStack pStack, InteractionHand pHand, Costing costs, ISpellInstance instance, int ticks) {
     BlockHitResult result = pickBlock(pPlayer);
+
+    BlockPos starting = result.getBlockPos();
+    BlockPos supporting = starting;
+
+    BlockState stateAt = pLevel.getBlockState(supporting);
+
+    while (!Block.isFaceFull(stateAt.getBlockSupportShape(pLevel, supporting), Direction.UP)) {
+      supporting = supporting.relative(Direction.DOWN);
+      stateAt = pLevel.getBlockState(supporting);
+      if (supporting.getY() - starting.getY() > 15) {
+        break;
+      }
+    }
+
+    if (!pLevel.getEntitiesOfClass(RoseThornsEntity.class, entityBoundsCheck.move(supporting)).isEmpty()) {
+      costs.noCharge();
+      return 0;
+    }
 
     RoseThornsEntity rose = ModEntities.ROSE_THORNS.get().create(pLevel);
     if (rose != null) {
