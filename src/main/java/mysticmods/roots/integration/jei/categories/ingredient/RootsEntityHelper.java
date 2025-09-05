038ed78adad94d8bf20033fa11ff3433e58db651
Jon <due@wxwhatever.com>
2025-09-05 14:52:29 +1000

    Show entity tags in the entity renderer.

--- src/main/java/mysticmods/roots/integration/jei/categories/ingredient/RootsEntityHelper.java
+++ src/main/java/mysticmods/roots/integration/jei/categories/ingredient/RootsEntityHelper.java
@@ -4,13 +4,25 @@ import mezz.jei.api.ingredients.IIngredientHelper;
 import mezz.jei.api.ingredients.IIngredientType;
 import mezz.jei.api.ingredients.subtypes.UidContext;
 import mysticmods.roots.integration.jei.RootsJEIPlugin;
+import mysticmods.roots.util.TagUtil;
+import net.minecraft.core.Registry;
 import net.minecraft.core.registries.BuiltInRegistries;
 import net.minecraft.resources.ResourceLocation;
+import net.minecraft.tags.TagKey;
+import net.minecraft.world.entity.EntityType;
 
 import javax.annotation.Nullable;
+import java.util.Collection;
 import java.util.Objects;
+import java.util.Optional;
 
 public class RootsEntityHelper implements IIngredientHelper<RootsEntityType> {
+
+  @Override
+  public Optional<TagKey<?>> getTagKeyEquivalent(Collection<RootsEntityType> ingredients) {
+		Registry<EntityType<?>> registry = BuiltInRegistries.ENTITY_TYPE;
+		return TagUtil.getTagEquivalent(ingredients, RootsEntityType::entity, registry::getTags);
+  }
 
   @Override
   public IIngredientType<RootsEntityType> getIngredientType() {
--- src/main/java/mysticmods/roots/util/TagUtil.java
+++ src/main/java/mysticmods/roots/util/TagUtil.java
@@ -1,5 +1,6 @@
 package mysticmods.roots.util;
 
+import com.mojang.datafixers.util.Pair;
 import net.minecraft.core.Holder;
 import net.minecraft.core.HolderSet;
 import net.minecraft.core.Registry;
@@ -7,6 +8,12 @@ import net.minecraft.tags.TagKey;
 import net.minecraft.world.level.Level;
 
 import javax.annotation.Nullable;
+import java.util.Collection;
+import java.util.List;
+import java.util.Optional;
+import java.util.function.Function;
+import java.util.function.Supplier;
+import java.util.stream.Stream;
 
 public class TagUtil {
   @Nullable
@@ -28,5 +35,39 @@ public class TagUtil {
     }
 
     return value.value();
+  }
+
+  // Copied from Jei
+  public static <VALUE, STACK> Optional<TagKey<?>> getTagEquivalent(
+      Collection<STACK> stacks,
+      Function<STACK, VALUE> stackToValue,
+      Supplier<Stream<Pair<TagKey<VALUE>, HolderSet.Named<VALUE>>>> tagSupplier
+  ) {
+    List<VALUE> values = stacks.stream()
+        .map(stackToValue)
+        .toList();
+
+    return tagSupplier.get()
+        .filter(e -> {
+          HolderSet.Named<VALUE> tag = e.getSecond();
+          return areEquivalent(tag, values);
+        })
+        .<TagKey<?>>map(Pair::getFirst)
+        .findFirst();
+  }
+
+  private static <VALUE> boolean areEquivalent(HolderSet.Named<VALUE> tag, List<VALUE> values) {
+    int count = tag.size();
+    if (count != values.size()) {
+      return false;
+    }
+    for (int i = 0; i < count; i++) {
+      VALUE tagValue = tag.get(i).value();
+      VALUE value = values.get(i);
+      if (!value.equals(tagValue)) {
+        return false;
+      }
+    }
+    return true;
   }
 }
