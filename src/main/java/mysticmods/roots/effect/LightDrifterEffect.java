0438b34e90f7534cc75849cdad133a7c845b4a9d
Jon <due@wxwhatever.com>
2025-10-09 12:00:25 +1000

    Sync light drifter effect regularly.

--- src/main/java/mysticmods/roots/effect/LightDrifterEffect.java
+++ src/main/java/mysticmods/roots/effect/LightDrifterEffect.java
@@ -1,6 +1,8 @@
 package mysticmods.roots.effect;
 
+import mysticmods.roots.init.ModAttachments;
 import mysticmods.roots.network.client.ClientboundLightDrifterSyncPacket;
+import net.minecraft.server.level.ServerLevel;
 import net.minecraft.server.level.ServerPlayer;
 import net.minecraft.world.effect.MobEffectCategory;
 import net.minecraft.world.entity.LivingEntity;
@@ -12,8 +14,36 @@ public class LightDrifterEffect extends SimpleEffect{
   }
 
   @Override
+  public boolean applyEffectTick(LivingEntity livingEntity, int amplifier) {
+    if (livingEntity instanceof ServerPlayer player) {
+      var drifter = player.getData(ModAttachments.DRIFTER_SERVER_STORAGE);
+      ServerLevel level = player.serverLevel();
+      if (drifter.id() == null) {
+        player.removeData(ModAttachments.DRIFTER_SERVER_STORAGE);
+        return false;
+      }
+      var drifterEntity = level.getEntity(drifter.id());
+      if (drifterEntity == null) {
+        player.removeData(ModAttachments.DRIFTER_SERVER_STORAGE);
+        return false;
+      }
+      drifter.setEntityId(drifterEntity.getId());
+      player.setData(ModAttachments.DRIFTER_SERVER_STORAGE, drifter);
+      PacketDistributor.sendToPlayer(player, new ClientboundLightDrifterSyncPacket(drifterEntity.getId()));
+    }
+
+    return true;
+  }
+
+  @Override
+  public boolean shouldApplyEffectTickThisTick(int duration, int amplifier) {
+    return true;
+  }
+
+  @Override
   public boolean onEffectExpired(LivingEntity entity, int amplifier) {
     if (entity instanceof ServerPlayer player) {
+      player.removeData(ModAttachments.DRIFTER_SERVER_STORAGE);
       PacketDistributor.sendToPlayer(player, new ClientboundLightDrifterSyncPacket(-1));
     }
     return super.onEffectExpired(entity, amplifier);
@@ -22,6 +52,7 @@ public class LightDrifterEffect extends SimpleEffect{
   @Override
   public boolean onEffectRemoved(LivingEntity entity, int amplifier) {
     if (entity instanceof ServerPlayer player) {
+      player.removeData(ModAttachments.DRIFTER_SERVER_STORAGE);
       PacketDistributor.sendToPlayer(player, new ClientboundLightDrifterSyncPacket(-1));
     }
     return super.onEffectRemoved(entity, amplifier);
