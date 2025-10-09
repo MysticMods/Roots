bf3c6d843e92d1cbcc94b52a516b11c3bbdb0fa5
Jon <due@wxwhatever.com>
2025-10-09 12:00:57 +1000

    Place-holder to make light drifter a spectator.
    This is insufficient for most instances.

--- src/main/java/mysticmods/roots/mixin/client/MixinAbstractClientPlayer$LightDrifter.java
+++ src/main/java/mysticmods/roots/mixin/client/MixinAbstractClientPlayer$LightDrifter.java
@@ -1,4 +1,20 @@
 package mysticmods.roots.mixin.client;
 
+import com.llamalad7.mixinextras.injector.wrapmethod.WrapMethod;
+import com.llamalad7.mixinextras.injector.wrapoperation.Operation;
+import mysticmods.roots.init.ModEffects;
+import net.minecraft.client.player.AbstractClientPlayer;
+import org.spongepowered.asm.mixin.Mixin;
+
+// This is insufficient.
+@Mixin(AbstractClientPlayer.class)
 public class MixinAbstractClientPlayer$LightDrifter {
+  @WrapMethod(method="isSpectator")
+  public boolean roots$onIsSpectator(Operation<Boolean> original) {
+    // Return true if the player has the Light Drifter effect, making them behave like a spectator
+    if (((AbstractClientPlayer) (Object) this).hasEffect(ModEffects.LIGHT_DRIFTER)) {
+      return true;
+    }
+    return original.call();
+  }
 }
