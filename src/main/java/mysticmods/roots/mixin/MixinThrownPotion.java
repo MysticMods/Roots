2988ba55d62e3ff387c606e152f9447c2033e516
Jon <due@wxwhatever.com>
2025-10-12 13:35:41 +1000

    Convert an inject to a WrapMethod.

--- src/main/java/mysticmods/roots/mixin/MixinThrownPotion.java
+++ src/main/java/mysticmods/roots/mixin/MixinThrownPotion.java
@@ -1,5 +1,7 @@
 package mysticmods.roots.mixin;
 
+import com.llamalad7.mixinextras.injector.wrapmethod.WrapMethod;
+import com.llamalad7.mixinextras.injector.wrapoperation.Operation;
 import com.llamalad7.mixinextras.sugar.Local;
 import mysticmods.roots.api.RootsTags;
 import mysticmods.roots.block.PyreBlock;
@@ -8,17 +10,16 @@ import net.minecraft.core.BlockPos;
 import net.minecraft.world.entity.projectile.ThrownPotion;
 import net.minecraft.world.level.block.state.BlockState;
 import org.spongepowered.asm.mixin.Mixin;
-import org.spongepowered.asm.mixin.injection.At;
-import org.spongepowered.asm.mixin.injection.Inject;
-import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
 
 @Mixin(ThrownPotion.class)
 public class MixinThrownPotion {
-  @Inject(method = "dowseFire", at = @At("TAIL"))
-  private void rootsDowseFire(BlockPos pos, CallbackInfo ci, @Local BlockState state) {
-    if (state.is(RootsTags.Blocks.PYRES) && state.hasProperty(PyreBlock.ACTIVE) && state.getValue(PyreBlock.ACTIVE) && ((ThrownPotion) (Object) this).level()
-        .getBlockEntity(pos) instanceof PyreBlockEntity pyre) {
-      pyre.stopRitual(false);
+  @WrapMethod(method = "dowseFire")
+  private void rootsDowseFire(BlockPos pos, Operation<Void> original, @Local BlockState state) {
+    original.call(pos);
+    if (state.is(RootsTags.Blocks.PYRES) && state.hasProperty(PyreBlock.ACTIVE) && state.getValue(PyreBlock.ACTIVE)) {
+      if (((ThrownPotion) (Object) this).level().getBlockEntity(pos) instanceof PyreBlockEntity pyre) {
+        pyre.stopRitual(false);
+      }
     }
   }
 }
