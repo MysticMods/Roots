34889f1f316dea66bb5ce8e42ef477b20e1ded1a
Jon <due@wxwhatever.com>
2025-10-13 13:47:57 +1000

    Increase size of Rose Thorns check. (#1114)

--- src/main/java/mysticmods/roots/spell/RoseThornsSpell.java
+++ src/main/java/mysticmods/roots/spell/RoseThornsSpell.java
@@ -29,7 +29,7 @@ import net.minecraft.world.phys.BlockHitResult;
 import java.util.List;
 
 public class RoseThornsSpell extends Spell {
-  private static final AABB entityBoundsCheck = new AABB(-1, -1, -1, 1, 1, 1);
+  private static final AABB entityBoundsCheck = new AABB(-1.5, -1.5, -1.5, 1.5, 1.5, 1.5);
   private double radiusZX, radiusY;
   private int duration;
   private float damage;
