c6936ca465d2347591666e034241f7a3e7c99890
Jon <due@wxwhatever.com>
2025-10-13 14:21:44 +1000

    Make Rose Thorns check 3x3.

--- src/main/java/mysticmods/roots/spell/RoseThornsSpell.java
+++ src/main/java/mysticmods/roots/spell/RoseThornsSpell.java
@@ -29,7 +29,7 @@ import net.minecraft.world.phys.BlockHitResult;
 import java.util.List;
 
 public class RoseThornsSpell extends Spell {
-  private static final AABB entityBoundsCheck = new AABB(-1.5, -1.5, -1.5, 1.5, 1.5, 1.5);
+  private static final AABB entityBoundsCheck = new AABB(-3, -3, -3, 3, 3, 3);
   private double radiusZX, radiusY;
   private int duration;
   private float damage;
