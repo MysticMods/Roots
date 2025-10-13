6581a3624dcd984b29ce53a13b1331c083d0dfdb
Jon <due@wxwhatever.com>
2025-10-13 13:45:31 +1000

    Fix #1195: wandering traders always grant reputation.
    As opposed to all other villagers which require gaining XP.

--- src/main/java/mysticmods/roots/mixin/action/MixinAbstractVillager.java
+++ src/main/java/mysticmods/roots/mixin/action/MixinAbstractVillager.java
@@ -6,6 +6,7 @@ import mysticmods.roots.action.TradeVillagerAction;
 import mysticmods.roots.init.ModActions;
 import net.minecraft.server.level.ServerPlayer;
 import net.minecraft.world.entity.npc.AbstractVillager;
+import net.minecraft.world.entity.npc.WanderingTrader;
 import net.minecraft.world.item.trading.MerchantOffer;
 import org.spongepowered.asm.mixin.Mixin;
 
@@ -14,17 +15,20 @@ public class MixinAbstractVillager {
 
   @WrapMethod(method = "notifyTrade")
   public void RootsNotifyTradeStart(MerchantOffer offer, Operation<Void> original) {
+    if (!(((AbstractVillager) (Object) this).getTradingPlayer() instanceof ServerPlayer player)) {
+      return;
+    }
+
+    TradeVillagerAction.Context context = new TradeVillagerAction.Context(player.serverLevel(), player, (AbstractVillager) (Object) this, offer);
+
     int lastExperience = ((AbstractVillager) (Object) this).getVillagerXp();
     original.call(offer);
-    if (lastExperience != -1) {
+    if ((Object) (this) instanceof WanderingTrader) {
+      ModActions.TRADE_VILLAGER.get().accept(context);
+    } else if (lastExperience != -1) {
       int currentExperience = ((AbstractVillager) (Object) this).getVillagerXp();
       if (currentExperience > lastExperience) {
-        //noinspection DataFlowIssue
-        if (((AbstractVillager) (Object) this).getTradingPlayer() instanceof ServerPlayer player) {
-          //noinspection DataFlowIssue
-          TradeVillagerAction.Context context = new TradeVillagerAction.Context(player.serverLevel(), player, (AbstractVillager) (Object) this, offer);
-          ModActions.TRADE_VILLAGER.get().accept(context);
-        }
+        ModActions.TRADE_VILLAGER.get().accept(context);
       }
     }
   }
