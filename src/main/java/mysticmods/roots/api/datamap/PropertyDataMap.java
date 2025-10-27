9b0b7f086272efa29807baf0a44ccd6b59a2d406
Jon <due@wxwhatever.com>
2025-10-27 12:47:13 +1000

    Enforce uniqueness of property ids per property data map.

--- src/main/java/mysticmods/roots/api/datamap/PropertyDataMap.java
+++ src/main/java/mysticmods/roots/api/datamap/PropertyDataMap.java
@@ -6,15 +6,24 @@ import mysticmods.roots.api.property.PropertyHolder;
 import net.minecraft.network.RegistryFriendlyByteBuf;
 import net.minecraft.network.codec.ByteBufCodecs;
 import net.minecraft.network.codec.StreamCodec;
+import net.minecraft.resources.ResourceLocation;
 
+import java.util.HashSet;
 import java.util.List;
+import java.util.Set;
 
 public record PropertyDataMap(List<PropertyHolder<? extends Property<?>>> properties) {
   public static Codec<PropertyDataMap> CODEC = PropertyHolder.FULL_LIST_CODEC.xmap(PropertyDataMap::new, PropertyDataMap::properties);
   public static StreamCodec<RegistryFriendlyByteBuf, PropertyDataMap> STREAM_CODEC = StreamCodec.composite(PropertyHolder.STREAM_CODEC.apply(ByteBufCodecs.list()), o -> o.properties, PropertyDataMap::new);
 
-  public PropertyDataMap(List<PropertyHolder<?>> properties) {
-    this.properties = properties;
+  public PropertyDataMap {
+    Set<ResourceLocation> ids = new HashSet<>();
+    for (PropertyHolder<?> property : properties) {
+      if (ids.contains(property.id())) {
+        throw new IllegalArgumentException("Duplicate id found in property map: '" + property.id() + "' already exists!");
+      }
+      ids.add(property.id());
+    }
   }
 
   public <V, T extends Property<V>> V get(PropertyHolder<T> holder) {
