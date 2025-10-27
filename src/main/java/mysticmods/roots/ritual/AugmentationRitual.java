23672160e4b122fab9d26fabc4e6bf9eb588f3d4
Jon <due@wxwhatever.com>
2025-10-27 13:59:16 +1000

    More on #1183: actually use new data map.

--- src/main/java/mysticmods/roots/ritual/AugmentationRitual.java
+++ src/main/java/mysticmods/roots/ritual/AugmentationRitual.java
@@ -19,18 +19,33 @@ import net.minecraft.world.effect.MobEffectInstance;
 import net.minecraft.world.effect.MobEffects;
 import net.minecraft.world.entity.Entity;
 import net.minecraft.world.entity.EntitySelector;
+import net.minecraft.world.entity.EntityType;
 import net.minecraft.world.entity.LivingEntity;
 import net.minecraft.world.entity.ai.attributes.Attribute;
 import net.minecraft.world.level.Level;
 import net.minecraft.world.level.block.state.BlockState;
 import net.minecraft.world.level.levelgen.structure.BoundingBox;
+import org.w3c.dom.Attr;
 
 import javax.annotation.Nullable;
-import java.util.List;
+import java.util.*;
 import java.util.stream.Collectors;
 
 public class AugmentationRitual extends Ritual {
+  private static final Map<EntityType<?>, Set<Holder<Attribute>>> ELIGIBLE_ATTRIBUTES = new HashMap<>();
   private int count, glowDuration;
+
+  private static Set<Holder<Attribute>> getAttributes (EntityType<?> entity) {
+    //noinspection deprecation
+    var e = entity.builtInRegistryHolder();
+
+    var data = e.getData(DataMaps.ENTITY_AUGMENTATION_DATA);
+    if (data == null) {
+      return Collections.emptySet();
+    }
+
+    return new HashSet<>(data);
+  }
 
   @Override
   public void functionalTick(Level pLevel, BlockPos pPos, BlockState pState, @Nullable PositionCache pCache, PyreBlockEntity blockEntity, int duration, RandomSource randomSource) {
@@ -39,13 +54,11 @@ public class AugmentationRitual extends Ritual {
       return;
     }
 
-    HolderSet<Attribute> attributes = BuiltInRegistries.ATTRIBUTE.getTag(RootsTags.Atrtibutes.AUGMENTABLE).orElse(null);
+    HolderSet<Attribute> attributes = BuiltInRegistries.ATTRIBUTE.getTag(RootsTags.Atrtibutes.AUGMENTABLE).orElse(null); // All attributes must be included in the tag but that's not the only attributes that are considered
     if (attributes == null) {
       RootsAPI.LOG.error("Ritual {} requires attributes from the AUGMENTABLE tag but none were found. This will cause the ritual to not function correctly.", getOrCreateDescriptionId());
       return;
     }
-
-    List<Holder<Attribute>> attributes1 = attributes.stream().collect(Collectors.toList());
 
     if (duration % getInterval() == 0) {
       List<LivingEntity> entities = blockEntity.getLevel()
@@ -62,6 +75,14 @@ public class AugmentationRitual extends Ritual {
         if (adjusted >= count) {
           break;
         }
+
+        Set<Holder<Attribute>> eligible = ELIGIBLE_ATTRIBUTES.get(entity.getType());
+        if (eligible == null) {
+          eligible = getAttributes(entity.getType());
+          ELIGIBLE_ATTRIBUTES.put(entity.getType(), eligible);
+        }
+
+        List<Holder<Attribute>> attributes1 = attributes.stream().filter(eligible::contains).collect(Collectors.toList());
 
         while (!attributes1.isEmpty()) {
           Holder<Attribute> attribute = attributes1.remove(randomSource.nextInt(attributes1.size()));
