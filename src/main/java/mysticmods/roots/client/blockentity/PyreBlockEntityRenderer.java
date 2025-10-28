7acf6f86c2e6001cef8f31185e558875317e1804
Jon <due@wxwhatever.com>
2025-10-28 13:28:40 +1000

    Custom entity renderer.

--- src/main/java/mysticmods/roots/client/blockentity/PyreBlockEntityRenderer.java
+++ src/main/java/mysticmods/roots/client/blockentity/PyreBlockEntityRenderer.java
@@ -104,10 +104,7 @@ public class PyreBlockEntityRenderer extends BoundedBlockEntityRenderer<PyreBloc
         if (!entity.isEmpty()) {
           Entity cached = entity.cachedEntity(pBlockEntity.getLevel());
           if (cached != null) {
-            pPoseStack.pushPose();
-            pPoseStack.translate(0, 1.9, 0);
-            SpawnerRenderer.renderEntityInSpawner(pPartialTick, pPoseStack, pBufferSource, pPackedLight, cached, entityRenderer, 0, 0);
-            pPoseStack.popPose();
+            renderEntityInSpawner(pPartialTick, pPoseStack, pBufferSource, pPackedLight, cached, entityRenderer, 0, 0);
           }
         }
       }
@@ -123,5 +120,31 @@ public class PyreBlockEntityRenderer extends BoundedBlockEntityRenderer<PyreBloc
           .renderStatic(inSlot, ItemDisplayContext.GROUND, pPackedLight, pPackedOverlay, pPoseStack, pBufferSource, null, 0);
       pPoseStack.popPose();
     }
+  }
+
+  public static void renderEntityInSpawner(
+      float partialTick,
+      PoseStack poseStack,
+      MultiBufferSource buffer,
+      int packedLight,
+      Entity entity,
+      EntityRenderDispatcher entityRenderer,
+      double oSpin,
+      double spin
+  ) {
+    poseStack.pushPose();
+    poseStack.translate(0.5F, 0.0F, 0.5F);
+    float f = 0.53125F;
+    float f1 = Math.max(entity.getBbWidth(), entity.getBbHeight());
+    if ((double)f1 > 1.0) {
+      f /= f1;
+    }
+
+    poseStack.translate(0.0F, 1.8F, 0.0F);
+    poseStack.mulPose(Axis.YP.rotationDegrees((float)Mth.lerp((double)partialTick, oSpin, spin) * 10.0F));
+    poseStack.translate(0.0F, -0.2F, 0.0F);
+    poseStack.scale(f, f, f);
+    entityRenderer.render(entity, 0.0, 0.0, 0.0, 0.0F, partialTick, poseStack, buffer, packedLight);
+    poseStack.popPose();
   }
 }
