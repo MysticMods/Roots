55c274670d00a026e39cd19ef4a6bfb324eef441
Jon <due@wxwhatever.com>
2025-11-03 12:35:21 +1000

    Improve Light Drifter rendering further.
    Prevent cloak fluttering and movement while moving the observer.

---
+++ src/main/java/mysticmods/roots/mixin/MixinPlayer$LightDrifter.java
@@ -0,0 +1,18 @@
+package mysticmods.roots.mixin;
+
+import com.llamalad7.mixinextras.injector.wrapoperation.Operation;
+import com.llamalad7.mixinextras.injector.wrapoperation.WrapOperation;
+import mysticmods.roots.init.ModEffects;
+import net.minecraft.world.entity.player.Player;
+import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.injection.At;
+
+@Mixin(Player.class)
+public class MixinPlayer$LightDrifter {
+  @WrapOperation(method="tick",at=@At(value="INVOKE", target = "Lnet/minecraft/world/entity/player/Player;moveCloak()V"))
+  private void RootsCancelCloakAnimation (Player instance, Operation<Void> original) {
+    if (!instance.hasEffect(ModEffects.LIGHT_DRIFTER)) {
+      original.call(instance);
+    }
+  }
+}
---
+++ src/main/java/mysticmods/roots/mixin/client/MixinCapeLayer.java
@@ -0,0 +1,21 @@
+package mysticmods.roots.mixin.client;
+
+import com.llamalad7.mixinextras.injector.ModifyExpressionValue;
+import com.llamalad7.mixinextras.sugar.Local;
+import mysticmods.roots.init.ModEffects;
+import net.minecraft.client.player.AbstractClientPlayer;
+import net.minecraft.client.renderer.entity.layers.CapeLayer;
+import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.injection.At;
+
+@Mixin(CapeLayer.class)
+public class MixinCapeLayer {
+  @ModifyExpressionValue(method="render(Lcom/mojang/blaze3d/vertex/PoseStack;Lnet/minecraft/client/renderer/MultiBufferSource;ILnet/minecraft/client/player/AbstractClientPlayer;FFFFFF)V", at=@At(value="INVOKE", target="Lnet/minecraft/client/player/AbstractClientPlayer;isCrouching()Z"))
+  private boolean RootsLightDrifterCrouching (boolean original, @Local(argsOnly = true) AbstractClientPlayer player) {
+    if (player.hasEffect(ModEffects.LIGHT_DRIFTER)) {
+      return false;
+    }
+
+    return original;
+  }
+}
