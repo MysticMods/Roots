8cf7aacd190a49fdced0bfc1cdc091463e24e64f
Jon <due@wxwhatever.com>
2025-11-13 12:40:02 +1000

    Further disambiguate mixins.

--- src/main/java/mysticmods/roots/mixin/client/MixinCapeLayer$LightDrifter.java
+++ src/main/java/mysticmods/roots/mixin/client/MixinCapeLayer$LightDrifter.java
@@ -9,7 +9,7 @@ import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.injection.At;
 
 @Mixin(CapeLayer.class)
-public class MixinCapeLayer {
+public class MixinCapeLayer$LightDrifter {
   @ModifyExpressionValue(method="render(Lcom/mojang/blaze3d/vertex/PoseStack;Lnet/minecraft/client/renderer/MultiBufferSource;ILnet/minecraft/client/player/AbstractClientPlayer;FFFFFF)V", at=@At(value="INVOKE", target="Lnet/minecraft/client/player/AbstractClientPlayer;isCrouching()Z"))
   private boolean RootsLightDrifterCrouching (boolean original, @Local(argsOnly = true) AbstractClientPlayer player) {
     if (player.hasEffect(ModEffects.LIGHT_DRIFTER)) {
