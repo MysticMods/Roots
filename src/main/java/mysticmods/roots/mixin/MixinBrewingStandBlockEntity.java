551b2bf41631430226e85ed071d12db0926edb58
Jon <due@wxwhatever.com>
2026-01-07 14:58:48 +1000

    Fix #1224: allow inferno bulb to fuel brewing stand.

---
+++ src/main/java/mysticmods/roots/mixin/MixinBrewingStandBlockEntity.java
@@ -0,0 +1,31 @@
+package mysticmods.roots.mixin;
+
+import com.llamalad7.mixinextras.injector.wrapoperation.Operation;
+import com.llamalad7.mixinextras.injector.wrapoperation.WrapOperation;
+import mysticmods.roots.api.RootsTags;
+import net.minecraft.world.item.Item;
+import net.minecraft.world.item.ItemStack;
+import net.minecraft.world.level.block.entity.BrewingStandBlockEntity;
+import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.injection.At;
+
+@Mixin(BrewingStandBlockEntity.class)
+public class MixinBrewingStandBlockEntity {
+  @WrapOperation(method="canPlaceItem", at=@At(value="INVOKE", target="Lnet/minecraft/world/item/ItemStack;is(Lnet/minecraft/world/item/Item;)Z"))
+  private boolean lootr$canPlaceAllowInfernoBulb(ItemStack instance, Item item, Operation<Boolean> original) {
+    if (instance.is(RootsTags.Items.INFERNO_BULB_HERB)) {
+      return true;
+    }
+
+    return original.call(instance, item);
+  }
+
+  @WrapOperation(method="serverTick", at=@At(value="INVOKE", target="Lnet/minecraft/world/item/ItemStack;is(Lnet/minecraft/world/item/Item;)Z"))
+  private static boolean lootr$allowInfernoBulb(ItemStack instance, Item item, Operation<Boolean> original) {
+    if (instance.is(RootsTags.Items.INFERNO_BULB_HERB)) {
+      return true;
+    }
+
+    return original.call(instance, item);
+  }
+}
---
+++ src/main/java/mysticmods/roots/mixin/MixinBrewingStandMenu$FuelSlot.java
@@ -0,0 +1,21 @@
+package mysticmods.roots.mixin;
+
+import com.llamalad7.mixinextras.injector.wrapoperation.Operation;
+import com.llamalad7.mixinextras.injector.wrapoperation.WrapOperation;
+import mysticmods.roots.api.RootsTags;
+import net.minecraft.world.item.Item;
+import net.minecraft.world.item.ItemStack;
+import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.injection.At;
+
+@Mixin(targets={"net.minecraft.world.inventory.BrewingStandMenu$FuelSlot"})
+public class MixinBrewingStandMenu$FuelSlot {
+  @WrapOperation(method="mayPlaceItem", at=@At(value="INVOKE", target="Lnet/minecraft/world/item/ItemStack;is(Lnet/minecraft/world/item/Item;)Z"))
+  private static boolean lootr$allowInfernoBulb(ItemStack instance, Item item, Operation<Boolean> original) {
+    if (instance.is(RootsTags.Items.INFERNO_BULB_HERB)) {
+      return true;
+    }
+
+    return original.call(instance, item);
+  }
+}
