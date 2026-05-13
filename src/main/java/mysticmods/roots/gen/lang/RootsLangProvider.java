1add2c5c0782cdcbfb876cc8251098a5b68dff83
Jon <due@wxwhatever.com>
2026-05-13 13:42:19 +1000

    Fix #1297: missing translation for JEI.

--- src/main/java/mysticmods/roots/gen/lang/RootsLangProvider.java
+++ src/main/java/mysticmods/roots/gen/lang/RootsLangProvider.java
@@ -752,6 +752,7 @@ public final class RootsLangProvider extends LanguageProvider {
     add("message.dandelion_cordial", "You feel more alert!");
 
     // JEI
+    add("roots.jei.grove_power", "Grovve Power Generation");
     add("roots.jei.grove_reputation", "Grove Reputation");
     add("roots.jei.entity_interaction", "Entity Item Interaction");
     add("roots.jei.summon_creatures", "Summon Creatures");
