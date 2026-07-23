8cfb2ddb83fbff59621936f7deb8eeaa4af74f4f
Jon <due@wxwhatever.com>
2026-07-23 10:15:18 +1000

    Notes for future modifiers.

--- src/main/java/mysticmods/roots/init/ModModifiers.java
+++ src/main/java/mysticmods/roots/init/ModModifiers.java
@@ -12,6 +12,40 @@ import net.neoforged.neoforge.registries.DeferredRegister;
 public class ModModifiers {
   private static final DeferredRegister<SpellModifier> REGISTER = DeferredRegister.create(RootsRegistries.Keys.SPELL_MODIFIERS, RootsAPI.MODID);
 
+  public static final DeferredHolder<SpellModifier, SpellModifier> ACID_CLOUD_FIRE = REGISTER.register("acid_cloud/fire", () -> new SpellModifier(CostInstance.add(ModHerbs.INFERNO_BULB, SpellCosts.BASE_0250), ModSpells.ACID_CLOUD.getKey()));
+  public static final DeferredHolder<SpellModifier, SpellModifier> ACID_CLOUD_PEACEFUL = REGISTER.register("acid_cloud/peaceful", () -> new SpellModifier(CostInstance.add(ModHerbs.WILDROOT, SpellCosts.BASE_0125), ModSpells.ACID_CLOUD.getKey()));
+
+  // Sylvan Light
+  // Auto-place to fill dark
+  // Cast to remove nearby lights
+  // Adjust brightness
+  // Adjust colour
+
+  // Geas
+  // Increase quantity
+
+  // Harvest
+  // Increase radius
+  // Magnetism
+
+  // Life Drain
+  // Increase damage, decrease healing
+  // Increase healing, decrease damage
+  // Dot
+  // Peaceful
+
+  // Petal Shell
+  // Increase shells
+  // Do damage/knockback when shield ends
+
+  // Rose Thorns
+  // Peaceful
+  // Do damage while trapped
+
+
+
+  // Increase duration
+  // Decrease cooldown
   public static final DeferredHolder<SpellModifier, SpellModifier> SKY_SOARER_FRIENDLY_EARTH = REGISTER.register("sky_soarer/friendly_earth", () -> new SpellModifier(CostInstance.add(ModHerbs.STALICRIPE, SpellCosts.BASE_0125), ModSpells.SKY_SOARER.getKey()));
 
   public static final DeferredHolder<SpellModifier, SpellModifier> SKY_SOARER_AMPLIFIED_1 = REGISTER.register("sky_soarer/amplified_1", () -> new SpellModifier(CostInstance.add(ModHerbs.CLOUD_BERRY, SpellCosts.BASE_0125), ModSpells.SKY_SOARER.getKey()));
