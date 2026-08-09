2aa919fc72ea001f9bda31e2cdfd42746c5f2ca6
Jon <due@wxwhatever.com>
2026-08-09 10:57:15 +1000

    Extend changes for #1360 to cover thrown potions.

--- src/main/java/mysticmods/roots/mixin/MixinThrownPotion$ExtinguishPyre.java
+++ src/main/java/mysticmods/roots/mixin/MixinThrownPotion$ExtinguishPyre.java
@@ -2,9 +2,11 @@ package mysticmods.roots.mixin;
 
 import com.llamalad7.mixinextras.injector.wrapmethod.WrapMethod;
 import com.llamalad7.mixinextras.injector.wrapoperation.Operation;
+import mysticmods.roots.api.RootsAPI;
 import mysticmods.roots.api.RootsTags;
 import mysticmods.roots.block.PyreBlock;
 import mysticmods.roots.blockentity.PyreBlockEntity;
+import mysticmods.roots.config.ConfigManager;
 import net.minecraft.core.BlockPos;
 import net.minecraft.world.entity.projectile.ThrownPotion;
 import net.minecraft.world.level.Level;
@@ -15,11 +17,17 @@ import org.spongepowered.asm.mixin.Mixin;
 public class MixinThrownPotion$ExtinguishPyre {
   @WrapMethod(method = "dowseFire")
   private void roots$DowseFire(BlockPos pos, Operation<Void> original) {
-    Level level = ((ThrownPotion) (Object) this).level();
+    ThrownPotion potion = (ThrownPotion) (Object) this;
+
+    Level level = potion.level();
     BlockState state = level.getBlockState(pos);
     original.call(pos);
-    if (state.is(RootsTags.Blocks.PYRES) && state.hasProperty(PyreBlock.ACTIVE) && state.getValue(PyreBlock.ACTIVE)) {
+    if (state.is(RootsTags.Blocks.PYRES) && state.hasProperty(PyreBlock.ACTIVE) && state.getValue(PyreBlock.ACTIVE) && ConfigManager.ENABLE_EXTINGUISH_PYRE.getAsBoolean()) {
       if (level.getBlockEntity(pos) instanceof PyreBlockEntity pyre) {
+        if (ConfigManager.DEBUG_PYRE.getAsBoolean()) {
+            RootsAPI.LOG.info("Stopping ritual on PyreBlockEntity at {} in {} as entity {} used a throwable potion and triggered the 'dowseFire' feature", pos, level.dimension()
+                .location(), potion.getOwner());
+        }
         pyre.stopRitual(false);
       }
     }
