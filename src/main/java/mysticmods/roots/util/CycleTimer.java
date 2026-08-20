59032bb3b70efd29dd38de5165484fa8c95664b6
Jon <due@wxwhatever.com>
2026-08-20 16:17:54 +1000

    Borrow Cycle Timer from JEI. Again.

---
+++ src/main/java/mysticmods/roots/util/CycleTimer.java
@@ -0,0 +1,54 @@
+package mysticmods.roots.util;
+
+import net.minecraft.client.gui.screens.Screen;
+import org.jetbrains.annotations.Nullable;
+
+import java.util.List;
+
+// Borrowed in its entirety from
+// mezz.jei.library.gui.ingredients.CycleTimer, MIT licensed
+public class CycleTimer {
+  private static final CycleTimer ZERO_OFFSET = new CycleTimer(0);
+  private static final int MAX_INDEX = 100_000;
+  /* the amount of time in ms to display one thing before cycling to the next one */
+  private static final int CYCLE_TIME_MS = 1_000;
+
+  public static CycleTimer create(int offset) {
+    if (offset == 0) {
+      return ZERO_OFFSET;
+    }
+    return new CycleTimer(offset);
+  }
+
+  public static CycleTimer createWithRandomOffset() {
+    int cycleOffset = (int) (Math.random() * MAX_INDEX);
+    return new CycleTimer(cycleOffset);
+  }
+
+  private final int cycleOffset;
+  private int index;
+
+  private CycleTimer(int cycleOffset) {
+    this.cycleOffset = cycleOffset;
+    long now = System.currentTimeMillis();
+    this.index = calculateIndex(now, cycleOffset);
+  }
+
+  private static int calculateIndex(long now, int cycleOffset) {
+    long index = ((now / CYCLE_TIME_MS) % MAX_INDEX) + cycleOffset;
+    return Math.toIntExact(index);
+  }
+
+  public <T> @Nullable T getCycled(List<@Nullable T> list) {
+    if (list.isEmpty()) {
+      return null;
+    }
+    if (!Screen.hasShiftDown()) {
+      long now = System.currentTimeMillis();
+      this.index = calculateIndex(now, cycleOffset);
+    }
+    int index = this.index % list.size();
+    return list.get(index);
+  }
+}
+
